c0f89a37a49c03f5a50117964ec3488b557c6501
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-05-30 17:14:40 +0100

    Add deploy script for CI

--- infrastructure/scripts/deploy.sh
+++ infrastructure/scripts/deploy.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+set -ex
 
-echo "Not yet implemented"
-exit 1
+# This is the CI server script to deploy the site
+#
+# The working directory should be the git root
+#
+# The caller should have the following environment variables set:
+#
+# USERNAME: cloudfoundry username
+# PASSWORD: cloudfoundry password
+# SPACE: the space to which you want to deploy
+
+[[ -v SPACE ]] || read -p "Which space? (int,staging,live): " SPACE
+case $SPACE in
+  int | staging )
+    HOSTNAME=mspsds-$SPACE
+  ;;
+  live )
+    HOSTNAME=mspsds
+  ;;
+  * )
+    echo "Bad space value"
+    exit 1
+  ;;
+esac
+
+./infrastructure/ci-install-cf.sh
+
+cf login -a api.cloud.service.gov.uk -u $USERNAME -p $PASSWORD -o "beis-mspsds" -s $space
+
+cf push --hostname $HOSTNAME
+
+cf logout
