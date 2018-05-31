c660c32c4778378d28f4326318006afdb36cbf68
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-05-31 12:39:21 +0100

    Fix linting and enable it on Travis

--- test/controllers/homepage_controller_test.rb
+++ test/controllers/homepage_controller_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
+require "test_helper"
 
 class HomepageControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
+  test "should get index" do
     get homepage_index_url
     assert_response :success
   end
