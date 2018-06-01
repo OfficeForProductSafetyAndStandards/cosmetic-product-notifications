e8ea92ad2b6431c9d91e088a0f3a23f7b3a8fe51
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-06-01 16:36:53 +0100

    Fix linting in rails generated files

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
-    config.assets.paths << Rails.root.join('node_modules')
+    config.assets.paths << Rails.root.join("node_modules")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
