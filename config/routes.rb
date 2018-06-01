32df74a48f691356577e58e50785fd77935a8851
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-06-01 11:35:02 +0100

    Create a PagesController with the skeleton of an example page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "homepage/index"
   root "homepage#index"
+  get "/pages/:page" => "pages#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
