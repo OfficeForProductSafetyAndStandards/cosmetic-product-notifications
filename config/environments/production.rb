f025f6c411c920fdb75be7f280664356fa9c9919
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-06-05 10:06:01 +0100

    Fix syntax when setting default_url_options in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Url for mailer
-  ENV["MSPSDS_HOST"].present? && config.action_mailer.default_url_options[:host] = ENV["MSPSDS_HOST"]
+  ENV["MSPSDS_HOST"].present? && config.action_mailer.default_url_options = { host: ENV["MSPSDS_HOST"] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
