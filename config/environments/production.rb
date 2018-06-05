59be850b7bfd95984ef6ebf1e8829b06e41a90fc
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-06-05 09:55:29 +0100

    Use flow control instead of a 1 line if statement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Url for mailer
-  if ENV["MSPSDS_HOST"].present?
-    config.action_mailer.default_url_options[:host] = ENV["MSPSDS_HOST"]
-  end
+  ENV["MSPSDS_HOST"].present? && config.action_mailer.default_url_options[:host] = ENV["MSPSDS_HOST"]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
