4f5f055a68b0f63186a2d4a73c5d5ed09bceb09d
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-06-11 12:00:30 +0100

    Reset the test script to the original commands

--- infrastructure/scripts/test.sh
+++ infrastructure/scripts/test.sh
@@ -3,6 +3,6 @@ set -ex
 
 docker-compose build
 # Use bundler to stop spring checking the DB before it's ready
-docker-compose run web bin/bundle exec rake db:create:all --trace
-docker-compose run web bin/rake db:schema:load
+docker-compose run web bundle exec rake db:create
+docker-compose run web bundle exec rake db:schema:load
 docker-compose run web rails test
