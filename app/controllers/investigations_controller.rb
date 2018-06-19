682b73dbf260473cf606316f115c6e07e6d75192
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-06-19 17:14:03 +0100

    Improve styling of investigations CRUD

--- app/controllers/investigations_controller.rb
+++ app/controllers/investigations_controller.rb
@@ -1,16 +1,16 @@
 class InvestigationsController < ApplicationController
-  before_action :set_investigation, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_investigation, only: %i[show edit update destroy]
 
   # GET /investigations
   # GET /investigations.json
   def index
-    @investigations = Investigation.all
+    @investigations = Investigation.paginate(page: params[:page], per_page: 50)
   end
 
   # GET /investigations/1
   # GET /investigations/1.json
-  def show
-  end
+  def show; end
 
   # GET /investigations/new
   def new
@@ -18,8 +18,7 @@ class InvestigationsController < ApplicationController
   end
 
   # GET /investigations/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /investigations
   # POST /investigations.json
@@ -28,7 +27,7 @@ class InvestigationsController < ApplicationController
 
     respond_to do |format|
       if @investigation.save
-        format.html { redirect_to @investigation, notice: 'Investigation was successfully created.' }
+        format.html { redirect_to @investigation, notice: "Investigation was successfully created." }
         format.json { render :show, status: :created, location: @investigation }
       else
         format.html { render :new }
@@ -42,7 +41,7 @@ class InvestigationsController < ApplicationController
   def update
     respond_to do |format|
       if @investigation.update(investigation_params)
-        format.html { redirect_to @investigation, notice: 'Investigation was successfully updated.' }
+        format.html { redirect_to @investigation, notice: "Investigation was successfully updated." }
         format.json { render :show, status: :ok, location: @investigation }
       else
         format.html { render :edit }
@@ -56,19 +55,20 @@ class InvestigationsController < ApplicationController
   def destroy
     @investigation.destroy
     respond_to do |format|
-      format.html { redirect_to investigations_url, notice: 'Investigation was successfully destroyed.' }
+      format.html { redirect_to investigations_url, notice: "Investigation was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_investigation
-      @investigation = Investigation.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def investigation_params
-      params.require(:investigation).permit(:description, :is_closed, :source, :severity, :product_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_investigation
+    @investigation = Investigation.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def investigation_params
+    params.require(:investigation).permit(:description, :is_closed, :source, :severity, :product_id)
+  end
 end
