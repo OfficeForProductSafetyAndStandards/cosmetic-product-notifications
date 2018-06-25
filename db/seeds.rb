d8487f3c8fc1288e63b0f0805c11f5f29bba663b
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-06-25 11:44:43 +0100

    Create activity type data model. Seed the DB with some activity types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def create_activity_type_if_not_exist(activity_type)
+    if ActivityType.where(name: activity_type).first.nil?
+        ActivityType.create(name: activity_type)
+        puts "Activity type #{activity_type} created"
+    else
+        puts "Activity type #{activity_type} already exists"
+    end
+end
+
 puts "Creating admin..."
 if User.where(email: ENV["ADMIN_EMAIL"]).first.nil?
     user = User.create(
@@ -20,3 +29,14 @@ if User.where(email: ENV["ADMIN_EMAIL"]).first.nil?
 else
     puts "User with email #{ENV["ADMIN_EMAIL"]} already exists"
 end
+
+puts "Creating activity types..."
+create_activity_type_if_not_exist "email"
+create_activity_type_if_not_exist "purchase"
+create_activity_type_if_not_exist "call"
+create_activity_type_if_not_exist "interview"
+create_activity_type_if_not_exist "visit"
+create_activity_type_if_not_exist "test"
+create_activity_type_if_not_exist "notification"
+create_activity_type_if_not_exist "recall"
+create_activity_type_if_not_exist "research"
---
+++ test/models/activity_type_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ActivityTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
