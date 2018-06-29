6b34757f617a7c38142b718e707e81fe730dc862
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-06-29 10:54:45 +0100

    MSPSDS-92 Allow select2 to work on rails turbo-loading as well

--- app/assets/javascripts/investigations.js
+++ app/assets/javascripts/investigations.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function() {
+$(document).on("turbolinks:load", function() {
     $(".js-investigation-products").select2({
         ajax: {
             url: "/products",
