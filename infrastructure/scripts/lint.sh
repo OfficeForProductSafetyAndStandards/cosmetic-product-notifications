96b63c66c9d2e6a481d94dae3041d404d2f24459
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-07-13 14:26:20 +0100

    MSPSDS-111: Update TravisCI script

--- infrastructure/scripts/lint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-
-docker-compose -f docker-compose.yml -f docker-compose.ci.yml build
-
-docker-compose -f docker-compose.yml -f docker-compose.ci.yml run web rubocop
--- infrastructure/scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-docker-compose -f docker-compose.yml -f docker-compose.ci.yml build
-
-# Use bundler to stop spring checking the DB before it's ready
-docker-compose -f docker-compose.yml -f docker-compose.ci.yml run web rake db:create
-docker-compose -f docker-compose.yml -f docker-compose.ci.yml run web rake db:schema:load
-docker-compose -f docker-compose.yml -f docker-compose.ci.yml run web rake test
