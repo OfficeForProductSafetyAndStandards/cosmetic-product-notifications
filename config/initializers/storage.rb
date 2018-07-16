43a39bdf552edd30d82accbccf931eede399a8ec
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-07-16 14:47:24 +0100

    MSPSDS-97 Fix linting in storage initialiser

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -1,5 +1,5 @@
 # ActiveStorage does not support multiple analyzers. To get past this, we are setting the analyzers list
-# to contain just a MasterAnalyzer. This Analyzer will decide which of the other analyzers to use 
+# to contain just a MasterAnalyzer. This Analyzer will decide which of the other analyzers to use
 # document_analyzers is a list of the analyzers that we will use
 Rails.application.config.document_analyzers = Rails.application.config.active_storage.analyzers
 Rails.application.config.document_analyzers.append AntiVirusAnalyzer
