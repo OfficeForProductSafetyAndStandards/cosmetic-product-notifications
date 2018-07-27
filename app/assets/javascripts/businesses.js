35b394c5979ebefc2aa3ebac2b322c26e89d6963
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-07-27 10:52:08 +0100

    MSPSDS-193 Make the large dropdowns searcheable

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -1,4 +1,7 @@
 $(document).on("turbolinks:load", function() {
+    $('.js-business-company-type').select2();
+    $('.js-business-sic-codes').select2();
+
     searchOnTextInput(
         $('.new-business-page .companies-house-search-term'),
         '/businesses/search_companies_house',
