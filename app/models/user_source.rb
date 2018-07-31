cca903a39aac1486e1a59282f29b44377c627b42
Tom Bell <tom.bell@softwire.com>
2018-07-31 16:57:40 +0100

    MSPSDS-197: Handle missing or deleted users in source and audit messages

--- app/models/user_source.rb
+++ app/models/user_source.rb
@@ -2,6 +2,6 @@ class UserSource < Source
   belongs_to_active_hash :user, inverse_of: :user_source
 
   def show
-    "Created by " + user.email
+    "Created by #{user&.email || "anonymous"}"
   end
 end
