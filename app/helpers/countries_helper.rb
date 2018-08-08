3a5a7c8111df0bfdcf145030d7e84727dbb36be4
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-08-08 10:07:30 +0100

    MSPSDS-219 Make the products form use a list of countries, instead of a free text box

---
+++ app/helpers/countries_helper.rb
@@ -0,0 +1,11 @@
+module CountriesHelper
+  def all_countries
+    [
+      "United Kingdom",
+      "China",
+      "Germany",
+      "Democratic Republic of Congo",
+      "Brazil"
+    ]
+  end
+end
