5320980718b0b35769246931085216b24889955d
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-08-08 12:15:41 +0100

    MSPSDS-219 Fix linting

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -7,7 +7,8 @@ module CountriesHelper
 
   # JSON is of the form [["Abu Dhabi", "territory:AE-AZ"], ["Afghanistan", "country:AF"]]
   def country_from_code(code)
-    all_countries.find { |country| country[1] == code }[0]
+    country = all_countries.find { |c| c[1] == code }
+    country && country[0]
   end
 
   def all_countries
