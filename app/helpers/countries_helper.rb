3c8e8478c15d8c9f6bab1b02f64c327c22ad8446
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-08-08 11:16:12 +0100

    MSPSDS-219 Use the GOVUK register to load the list of countries

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -1,11 +1,23 @@
+require "net/http"
+require "json"
+
 module CountriesHelper
+  COUNTRIES_REGISTER_URL = "https://country.register.gov.uk/records.json?page-size=5000".freeze
+
   def all_countries
-    [
-      "United Kingdom",
-      "China",
-      "Germany",
-      "Democratic Republic of Congo",
-      "Brazil"
-    ]
+    fetch_countries
+  end
+
+  def fetch_countries
+    uri = URI(COUNTRIES_REGISTER_URL)
+    req = Net::HTTP::Get.new(uri)
+    req["Authorization"] = ENV["COMPANIES_HOUSE_API_KEY"]
+
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(req)
+    end
+    JSON.parse(res.body).collect do |country|
+      country[1]["item"][0]["name"]
+    end
   end
 end
