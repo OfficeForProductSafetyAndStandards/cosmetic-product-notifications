c744a51773e2db851d1fbd4c89e89039bb01e5c2
Tom Bell <tom.bell@softwire.com>
2018-08-21 18:43:29 +0100

    MSPSDS-231: Update manage image functionality to match wireframes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,6 +4,10 @@ class ImagesController < ApplicationController
   before_action :create_image, only: %i[create]
   before_action :update_image, only: %i[update]
 
+  helper_method :associated_image_path
+  helper_method :associated_images_path
+  helper_method :new_associated_image_path
+  helper_method :edit_associated_image_path
 
   # GET /images
   # GET /images.json
@@ -29,7 +33,7 @@ class ImagesController < ApplicationController
   def create
     respond_to do |format|
       if @image
-        format.html { redirect_to action: 'index', notice: 'Image was successfully added.' }
+        format.html { redirect_to edit_associated_image_path(@investigation, @image) }
         format.json { render :show, status: :created, location: @image }
       else
         format.html { render :new }
@@ -43,7 +47,7 @@ class ImagesController < ApplicationController
   def update
     respond_to do |format|
       if @image.blob.save
-        format.html { redirect_to action: 'index', notice: 'Image was successfully updated.' }
+        format.html { redirect_to action: 'index', notice: 'Image was successfully saved.' }
         format.json { render :show, status: :ok, location: @image }
       else
         format.html { render :edit }
@@ -55,11 +59,27 @@ class ImagesController < ApplicationController
   # DELETE /images/1
   # DELETE /images/1.json
   def destroy
-    @image.purge
+    @image.purge_later
     respond_to do |format|
-      format.html { redirect_to action: 'index', notice: 'Image was successfully destroyed.' }
+      format.html { redirect_to action: 'index', notice: 'Image was successfully deleted.' }
       format.json { head :no_content }
     end
+  end
+
+  def associated_images_path(investigation)
+    polymorphic_path([investigation, :images])
+  end
+
+  def associated_image_path(investigation, image)
+    associated_images_path(investigation) + "/" + image.id.to_s
+  end
+
+  def new_associated_image_path(investigation)
+    associated_images_path(investigation) + "/new"
+  end
+
+  def edit_associated_image_path(investigation, image)
+    associated_image_path(investigation, image) + "/edit"
   end
 
   private
@@ -75,22 +95,17 @@ class ImagesController < ApplicationController
   end
 
   def create_image
-    image = ActiveStorage::Blob.create_after_upload! \
-            io: image_params[:file].open,
-            filename: image_params[:file].original_filename,
-            content_type: image_params[:file].content_type,
-            metadata: { title: image_params[:title] }
-
-    @images = @investigation.images.attach(image)
+    @images = @investigation.images.attach(image_params[:file])
     @image = @images.last
   end
 
   def update_image
     @image.blob.metadata.update(image_params)
+    @image.blob.metadata["updated"] = Time.now
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def image_params
-    params.require(:image).permit(:title, :file)
+    params.require(:image).permit(:file, :title, :description)
   end
 end
