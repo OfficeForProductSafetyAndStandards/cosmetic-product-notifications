01703191815825acfada155ea8335f831f638db8
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-08-23 15:16:58 +0100

    MSPSDS-267 Set the investigations page to be the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ Rails.application.routes.draw do
   resources :users
 
   get "homepage/index"
-  root to: "homepage#index"
+  root to: redirect(path: "/investigations")
   get "/pages/:page" => "pages#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
