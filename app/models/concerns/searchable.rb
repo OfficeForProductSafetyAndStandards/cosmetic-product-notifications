b058e6676de1e7611a2ee16f8544d82fe3a08c35
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-08-24 16:22:59 +0100

    Temporarily disable prefix search

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -9,13 +9,15 @@ module Searchable
     # https://www.elastic.co/guide/en/elasticsearch/reference/current/term-level-queries.html
     def self.prefix_search(query)
       __elasticsearch__.search(
-        query: {
-          prefix: {
-            _all: {
-              value: query.downcase # analyzer indexes records in lowercase
-            }
-          }
-        }
+        # TODO-MSPSDS-298 Re-enable prefix search
+        # query: {
+        #   prefix: {
+        #     _all: {
+        #       value: query.downcase # analyzer indexes records in lowercase
+        #     }
+        #   }
+        # }
+        query
       )
     end
   end
