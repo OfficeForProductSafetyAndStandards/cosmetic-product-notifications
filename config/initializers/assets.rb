9c1741314ba846b3181ebab3a4bb410af2e7a20c
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-08-31 11:24:52 +0100

    MSPSDS-269: Fix issues with Design Systems assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,21 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "govuk-frontend")
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "govuk-frontend", "assets")
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "govuk-frontend", "assets", "images")
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "govuk-frontend", "assets", "fonts")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[
+  govuk-frontend/assets/images/favicon.ico
+  govuk-frontend/assets/images/govuk-mask-icon.svg
+  govuk-frontend/assets/images/govuk-apple-touch-icon-180x180.png
+  govuk-frontend/assets/images/govuk-apple-touch-icon-167x167.png
+  govuk-frontend/assets/images/govuk-apple-touch-icon-152x152.png
+  govuk-frontend/assets/images/govuk-apple-touch-icon.png
+  govuk-frontend/assets/images/govuk-opengraph-image.png
+  govuk-frontend/assets/images/govuk-logotype-crown.png
+]
