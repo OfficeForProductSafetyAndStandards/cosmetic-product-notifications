a162bc34620d53523b0a659f25ea5c6f7fbe2f43
Tom Bell <tom.bell@softwire.com>
2018-08-31 08:22:56 +0100

    MSPSDS-210: Refactor search query methods

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -52,31 +52,35 @@ module Searchable
     # "prefix" may be changed to a more appropriate query. For alternatives see:
     # https://www.elastic.co/guide/en/elasticsearch/reference/current/term-level-queries.html
     def self.prefix_search(params, field)
-      search_term = {}
-      search_term[field] = params[:q].downcase # analyzer indexes records in lowercase
-      __elasticsearch__.search({
-        query: {
-          prefix: search_term
-        },
-        sort: [
-          { "#{params[:sort]}": {order: params[:direction]} }
-        ]
-      })
+      query = {}
+      if params[:q] && !params[:q].blank?
+        query[:query] = {
+          prefix: { "#{field}": params[:q].downcase } # analyzer indexes records in lowercase
+        }
+      end
+      if params[:sort] && !params[:sort].blank?
+        query[:sort] = [{ "#{params[:sort]}.sort": { order: params[:direction] } }]
+      end
+
+      __elasticsearch__.search(query)
     end
 
     # "multi_match" searches across all fields, applying fuzzy matching to any text and keyword fields
     def self.fuzzy_search(params)
-      __elasticsearch__.search({
-        query: {
+      query = {}
+      if params[:q] && !params[:q].blank?
+        query[:query] = {
           multi_match: {
             query: params[:q].downcase, # analyzer indexes records in lowercase
             fuzziness: "AUTO"
           }
-        },
-        sort: [
-          { "#{params[:sort]}": {order: params[:direction]} }
-        ]
-      })
+        }
+      end
+      if params[:sort] && !params[:sort].blank?
+        query[:sort] = [{ "#{params[:sort]}.sort": { order: params[:direction] } }]
+      end
+
+      __elasticsearch__.search(query)
     end
   end
 end
