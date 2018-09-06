21104d32643605e48546d265c3189c71ba01e742
Tom Bell <tom.bell@softwire.com>
2018-09-06 10:57:00 +0100

    MSPSDS-197: Remove admin user setup from database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-puts "Creating admin..."
-if User.where(email: ENV["ADMIN_EMAIL"]).first.nil?
-    user = User.create(
-        email: ENV["ADMIN_EMAIL"],
-        password: ENV["ADMIN_PASSWORD"],
-        password_confirmation: ENV["ADMIN_PASSWORD"]
-    )
-
-    user.add_role(:user)
-    user.add_role(:admin)
-    user.save!
-    puts "Admin created"
-else
-    puts "User with email #{ENV["ADMIN_EMAIL"]} already exists"
-end
