99ad7d9f57cc8472d96c79b8afa3f86e7f2214af
Tom Bell <tom.bell@softwire.com>
2018-09-06 15:35:14 +0100

    MSPSDS-304: Switch to simple environment variable usage

--- keycloak/package.sh
+++ keycloak/package.sh
@@ -9,27 +9,27 @@ CONFIG_PATH=./keycloak/configuration
 ARTIFACT_PATH=./keycloak/artifacts
 PACKAGE_PATH=./keycloak/package
 
-mkdir -p ${ARTIFACT_PATH}
-mkdir -p ${PACKAGE_PATH}
+mkdir -p $ARTIFACT_PATH
+mkdir -p $PACKAGE_PATH
 
 # Download and unpack Keycloak
-curl -Lo ${ARTIFACT_PATH}/keycloak.tar.gz https://downloads.jboss.org/keycloak/4.3.0.Final/keycloak-4.3.0.Final.tar.gz
-tar -xzf ${ARTIFACT_PATH}/keycloak.tar.gz --directory ${PACKAGE_PATH} --strip 1
+curl -Lo $ARTIFACT_PATH/keycloak.tar.gz https://downloads.jboss.org/keycloak/4.3.0.Final/keycloak-4.3.0.Final.tar.gz
+tar -xzf $ARTIFACT_PATH/keycloak.tar.gz --directory $PACKAGE_PATH --strip 1
 
 
 # Download and add PostgreSQL JDBC driver
-curl -Lo ${ARTIFACT_PATH}/postgresql-42.2.5.jar https://jdbc.postgresql.org/download/postgresql-42.2.5.jar
+curl -Lo $ARTIFACT_PATH/postgresql-42.2.5.jar https://jdbc.postgresql.org/download/postgresql-42.2.5.jar
 
-mkdir -p ${PACKAGE_PATH}/modules/system/layers/keycloak/org/postgresql/main
-cp ${ARTIFACT_PATH}/postgresql-42.2.5.jar ${PACKAGE_PATH}/modules/system/layers/keycloak/org/postgresql/main/postgresql-42.2.5.jar
-cp ${CONFIG_PATH}/postgresql-module.xml ${PACKAGE_PATH}/modules/system/layers/keycloak/org/postgresql/main/module.xml
+mkdir -p $PACKAGE_PATH/modules/system/layers/keycloak/org/postgresql/main
+cp $ARTIFACT_PATH/postgresql-42.2.5.jar $PACKAGE_PATH/modules/system/layers/keycloak/org/postgresql/main/postgresql-42.2.5.jar
+cp $CONFIG_PATH/postgresql-module.xml $PACKAGE_PATH/modules/system/layers/keycloak/org/postgresql/main/module.xml
 
 
 # Copy across the modified configuration files (enabling proxy address forwarding and configuring the PostgreSQL datasource)
-cp ${CONFIG_PATH}/standalone.xml ${PACKAGE_PATH}/standalone/configuration/standalone.xml
-cp ${CONFIG_PATH}/standalone-ha.xml ${PACKAGE_PATH}/standalone/configuration/standalone-ha.xml
+cp $CONFIG_PATH/standalone.xml $PACKAGE_PATH/standalone/configuration/standalone.xml
+cp $CONFIG_PATH/standalone-ha.xml $PACKAGE_PATH/standalone/configuration/standalone-ha.xml
 
 
 # Download and add the GOV.UK theme
-curl -Lo ${ARTIFACT_PATH}/govuk.tar.gz https://github.com/UKHomeOffice/keycloak-theme-govuk/releases/download/v2.0.2/govuk.tar.gz
-tar -xzf ${ARTIFACT_PATH}/govuk.tar.gz --directory ${PACKAGE_PATH}/themes
+curl -Lo $ARTIFACT_PATH/govuk.tar.gz https://github.com/UKHomeOffice/keycloak-theme-govuk/releases/download/v2.0.2/govuk.tar.gz
+tar -xzf $ARTIFACT_PATH/govuk.tar.gz --directory $PACKAGE_PATH/themes
