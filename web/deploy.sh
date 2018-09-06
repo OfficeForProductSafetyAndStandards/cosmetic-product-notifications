fb1947d7fd3abfe416e9e6cf169ac34cade4e51c
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-09-06 15:21:29 +0100

    MSPSDS-195: Fixes to deploy scripts

--- web/deploy.sh
+++ web/deploy.sh
@@ -15,6 +15,6 @@ set -ex
 
 cf login -a api.cloud.service.gov.uk -u $USERNAME -p $PASSWORD -o "beis-mspsds" -s $SPACE
 
-cf push -f ./web/manifest.yml
+cf push -f ./web/manifest.yml --route-path mspsds-$SPACE
 
 cf logout
