8db78b9128e55436c4283fa2b3db7c9fea8cd321
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-09-21 16:12:28 +0100

    MSPSDS-485 Test search results from companies house filtering

---
+++ test/helpers/business_helper_test.rb
@@ -0,0 +1,54 @@
+require "test_helper"
+require "rspec/mocks/standalone"
+
+class BusinessHelperTest < ActiveSupport::TestCase
+  include ::RSpec::Mocks::ExampleMethods
+  include BusinessesHelper
+
+  setup do
+    client_instance = instance_double("CompaniesHouseClient")
+    allow(CompaniesHouseClient).to receive(:instance).and_return(client_instance)
+    allow(client_instance).to receive(:companies_house_businesses).with("company").and_return(
+      [
+        {
+          company_name: "company one, already in the db",
+          company_number: "1",
+          company_status_code: "active",
+          url: "urlToView/1"
+        },
+        {
+          company_name: "company two",
+          company_number: "2",
+          company_status_code: "dissolved",
+          url: "urlToView/2"
+        },
+        {
+          company_name: "company three",
+          company_number: "3",
+          company_status_code: "active",
+          url: "urlToView/3"
+        },
+        {
+          company_name: "company four",
+          company_number: "4",
+          company_status_code: "active",
+          url: "urlToView/4"
+        },
+      ]
+    )
+  end
+
+  teardown do
+    allow(CompaniesHouseClient).to receive(:instance).and_call_original
+  end
+
+  test "search_companies_house returns only new companies" do
+    results = search_companies_house("company", 3)
+    assert_equal(results.map { |company| company[:company_number] }, %w(2 3 4))
+  end
+
+  test "search_companies_house limits number of results" do
+    results = search_companies_house("company", 1)
+    assert_equal(results.length, 1)
+  end
+end
