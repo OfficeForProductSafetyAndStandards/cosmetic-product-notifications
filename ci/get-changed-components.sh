e7a643e339397a5f1140fc3b13fe4de18837a34f
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-09-28 12:37:35 +0100

    MSPSDS-490: Simplify change detection script

--- ci/get-changed-components.sh
+++ ci/get-changed-components.sh
@@ -3,18 +3,18 @@ set -ex
 
 COMMITS=$TRAVIS_COMMIT_RANGE
 
-# Get deployment components
-VALID_COMPONENTS=$(find . -name '*manifest.yml' | awk -F'/' '{ print $2 }' | sort -u)
-
 # Get top level files / folders changed in recent commits
 TOP_LEVEL_CHANGES=$(git diff --name-only $COMMITS | awk -F'/' '{ print $1 }' | sort -u)
 
-# Get intersection between the two
-INTERSECTION=$(comm -1 -2 <(echo $VALID_COMPONENTS | tr ' ' '\n') <(echo $TOP_LEVEL_CHANGES | tr ' ' '\n'))
+COMPONENTS=''
+if [[ "$TOP_LEVEL_CHANGES" =~ keycloak ]]; then
+    COMPONENTS="$COMPONENTS keycloak"
+fi
 
-# If the count between TOP_LEVEL_CHANGES and INTERSECTION is different, then we must have had changes outside of VALID_COMPONENTS,
-# so we should set CHANGED_COMPONENTS as empty and rebuild everything
-TOP_LEVEL_CHANGES_COUNT=$(echo $TOP_LEVEL_CHANGES | wc -w | awk '{ print $1 }')
-INTERSECTION_COUNT=$(echo $INTERSECTION | wc -w | awk '{ print $1 }')
+if [[ "$TOP_LEVEL_CHANGES" =~ web ]]; then
+    COMPONENTS="$COMPONENTS web worker"
+elif [[ "$TOP_LEVEL_CHANGES" =~ worker ]]; then
+    COMPONENTS="$COMPONENTS worker"
+fi
 
-[[ $TOP_LEVEL_CHANGES_COUNT == $INTERSECTION_COUNT ]] && echo $TOP_LEVEL_CHANGES || echo 'all'
+echo $COMPONENTS
