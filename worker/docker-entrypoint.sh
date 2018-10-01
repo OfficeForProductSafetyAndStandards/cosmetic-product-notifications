94342cc7b2412258fb5f82b27c45c14d029494dc
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-10-01 14:46:23 +0100

    MSPSDS-490: Fix issue with worker docker entrypoint script

--- worker/docker-entrypoint.sh
+++ worker/docker-entrypoint.sh
@@ -4,7 +4,11 @@ set -ex
 # Ensure all gems are installed.
 bin/bundle check || bin/bundle install
 
-freshclam
+# freshclam returns code 1 when it's up-to-date...
+freshclam || EXIT_CODE=$?
+if [[ $EXIT_CODE > 1 ]]; then
+    exit $EXIT_CODE
+fi
 service clamav-daemon start
 
 # Run the passed in command
