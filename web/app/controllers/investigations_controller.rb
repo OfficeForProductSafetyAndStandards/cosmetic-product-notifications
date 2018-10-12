83d35c23cfe6fd531ba1636c874d917feb347909
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-10-12 10:56:31 +0100

    MSPSDS-526 Remove investigation_presenter
    The code is much cleaner when the tabs are defined in the view alone

--- web/app/controllers/investigations_controller.rb
+++ web/app/controllers/investigations_controller.rb
@@ -16,7 +16,6 @@ class InvestigationsController < ApplicationController
   # GET /investigations/1
   # GET /investigations/1.json
   def show
-    @investigation = InvestigationPresenter.new @investigation
     respond_to do |format|
       format.html
       format.pdf do
