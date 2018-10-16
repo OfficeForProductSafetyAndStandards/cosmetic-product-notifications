3c9d1842c2b9ae2025bc4a55955b364b358312b6
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-10-16 10:39:30 +0100

    MSPSDS-522 Add date errors for dates out of range

--- web/app/controllers/incidents_controller.rb
+++ web/app/controllers/incidents_controller.rb
@@ -1,22 +1,42 @@
 class IncidentsController < ApplicationController
+  before_action :set_investigation, only: %i[new create]
+  before_action :build_incident, only: %i[new create]
   # GET investigations/1/incidents/new
   def new
-    @investigation = Investigation.find(params[:investigation_id])
-    @incident = @investigation.incidents.build
   end
 
   # POST investigations/1/incidents
   def create
-    @investigation = Investigation.find(params[:investigation_id])
-    @incident = @investigation.incidents.create(incident_params)
-    if @incident.save
+    begin
+      @incident = @investigation.incidents.create(incident_params)
+    rescue ActiveRecord::MultiparameterAssignmentErrors => e
+      matches = /error on assignment .* to date \((?<culprit>.*) out of range\)/ =~ e.message
+      if matches
+        component = case culprit
+                    when "argument"
+                      "day"
+                    when "mon"
+                      "month"
+                    end
+        @incident.errors.add(:date, component)
+      end
+    end
+    if @incident.errors.empty? && @incident.save
       redirect_to investigation_url(@investigation), notice: "Incident was successfully recorded."
     else
       render :new
     end
   end
 
-  private
+private
+
+  def build_incident
+    @incident = @investigation.incidents.build
+  end
+
+  def set_investigation
+    @investigation = Investigation.find(params[:investigation_id])
+  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def incident_params
--- web/test/system/investigation_incident_test.rb
+++ web/test/system/investigation_incident_test.rb
@@ -1,9 +1,10 @@
 require "application_system_test_case"
 
 class InvestigationIncidentTest < ApplicationSystemTestCase
-
   setup do
     sign_in_as_user
+    visit new_investigation_incident_path(investigations(:one))
+    assert_selector "h1", text: "Add incident"
   end
 
   teardown do
@@ -11,18 +12,12 @@ class InvestigationIncidentTest < ApplicationSystemTestCase
   end
 
   test "can add empty incident to investigation" do
-    visit new_investigation_incident_path(investigations(:one))
-    assert_selector "h1", text: "Add incident"
-
     click_on "Continue"
 
     assert_text "Incident was successfully recorded."
   end
 
   test "can add filled in incident to investigation" do
-    visit new_investigation_incident_path(investigations(:one))
-    assert_selector "h1", text: "Add incident"
-
     fill_in "Incident type", with: "Bad Stuff TM"
     fill_in "Incident / event description", with: "Oh, it was horrible"
     fill_in "Day", with: "7"
@@ -32,4 +27,21 @@ class InvestigationIncidentTest < ApplicationSystemTestCase
 
     assert_text "Incident was successfully recorded."
   end
+
+  test "wrongly formatted date shows an error" do
+    fill_in "Day", with: "7"
+    fill_in "Month", with: "13"
+    fill_in "Year", with: "1984"
+    click_on "Continue"
+
+    assert_text("Enter a real incident date")
+  end
+
+  # test "date with missing component shows an error" do
+  #   fill_in "Day", with: "7"
+  #   fill_in "Year", with: "1984"
+  #   click_on "Continue"
+  #
+  #   assert_text("Enter date of incident and include a day, month and year")
+  # end
 end
