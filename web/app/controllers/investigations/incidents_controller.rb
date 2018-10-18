622209dfa3f1f3c8eb9e006145a038bda5589c5c
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-10-18 15:38:01 +0100

    MSPSDS-522 Move date parsing logic and validation to the model
    This allows us to use the validation system to govern the wizard
    progress

--- web/app/controllers/investigations/incidents_controller.rb
+++ web/app/controllers/investigations/incidents_controller.rb
@@ -2,8 +2,8 @@ class Investigations::IncidentsController < ApplicationController
   include Wicked::Wizard
   steps :details, :confirmation
 
-  before_action :set_investigation, only: %i[new create show]
-  before_action :build_incident, only: %i[new create show]
+  before_action :set_investigation, only: %i[new create show update]
+  before_action :build_incident, only: %i[new create update]
 
   # GET investigations/1/incidents/new
   def new;
@@ -18,8 +18,6 @@ class Investigations::IncidentsController < ApplicationController
   end
 
   def update
-    set_investigation
-    build_incident
     session[:incident] = @incident.attributes
     if !@incident.valid?(step)
       render step
@@ -43,29 +41,10 @@ class Investigations::IncidentsController < ApplicationController
 private
 
   def build_incident
-    begin
+    if params.include? :incident
+      @incident = @investigation.incidents.build(incident_params)
+    else
       @incident = @investigation.incidents.build
-      if params.include? :incident
-        @incident = @investigation.incidents.build(incident_params)
-      end
-    rescue ActiveRecord::MultiparameterAssignmentErrors => e
-      @incident.errors.add(:date, "Enter a real incident date")
-      if /error on assignment .* to date \((?<culprit>.*) out of range\)/ =~ e.message
-        component = case culprit
-                    when "argument"
-                      :day
-                    when "mday"
-                      :day
-                    when "mon"
-                      :month
-                    end
-        @incident.errors.add(component)
-      end
-    rescue MspsdsException::IncompleteDateParsedException => e
-      e.missing_fields.each do |missing_component|
-        @incident.errors.add(:date, "Enter date of incident and include a day, month and year")
-        @incident.errors.add(missing_component)
-      end
     end
   end
 
@@ -75,43 +54,12 @@ private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def incident_params
-    parsed_params = params.require(:incident).permit(
-      :incident_type,
-      :description,
-      :affected_party,
-      :location,
-      :date
-    )
-    missing_date_components = {
-      day: parsed_params[:'date(3i)'],
-      month: parsed_params[:'date(2i)'],
-      year: parsed_params[:'date(1i)']
-    }.select { |_, value| value.blank? }
-    if (1..2) === missing_date_components.length # Date has some components entered, but not all
-      raise MspsdsException::IncompleteDateParsedException.new missing_date_components.keys
-    end
-
-    parsed_params
-  end
-
-  # The odd date-handling means that the incident creation can fail pre-validation.
-  # This leaves the model unpopulated, clearing the form.
-  # Calling this method ensures that all of the entered values remain filled in in the form
-  def set_intermediate_params
-    params_without_date = params.require(:incident).permit(
-      :incident_type,
-      :description,
-      :affected_party,
-      :location,
-    )
-    @incident.assign_attributes(params_without_date)
-    date_components = params.require(:incident).permit(
-      :'date(3i)',
-      :'date(2i)',
-      :'date(1i)'
-    )
-    @incident.day = date_components[:'date(3i)']
-    @incident.month = date_components[:'date(2i)']
-    @incident.year = date_components[:'date(1i)']
+    params.require(:incident).permit(:incident_type,
+                                     :description,
+                                     :affected_party,
+                                     :location,
+                                     :day,
+                                     :month,
+                                     :year)
   end
 end
--- web/app/models/incident.rb
+++ web/app/models/incident.rb
@@ -1,5 +1,43 @@
 class Incident < ApplicationRecord
   belongs_to :investigation
-  # These allows us to keep the value of and mark errors against individual date components, without persisting them
-  attr_accessor :day, :month, :year
+
+  # TODO This date logic (and the related view) ought to be split out into a reusable utility
+  attribute :day, :integer
+  attribute :month, :integer
+  attribute :year, :integer
+  validate :date_from_components
+
+  after_initialize do
+    date_component_strings = [year, month, day]
+    unless date_component_strings.any? &:blank?
+      date_components = date_component_strings.map(&:to_i)
+      if Date.valid_civil? *date_components
+        # This sets it if it makes sense. Validation then can compare the presence of
+        # date and its components to know if the date parsed correctly
+        self.date = Date.civil *date_components
+      end
+    end
+  end
+
+  def date_from_components
+    missing_date_components = {
+      day: day, month: month, year: year
+    }.select { |_, value| value.blank? }
+    case missing_date_components.length
+    when (1..2) # Date has some components entered, but not all
+      missing_date_components.each do |missing_component, _|
+        errors.add(:date, "Enter date of incident and include a day, month and year")
+        errors.add(missing_component)
+      end
+    when 0
+      unless date.present?
+        errors.add(:date, "Enter a real incident date")
+        errors.add(:day)
+        errors.add(:month)
+        errors.add(:year)
+      end
+    else
+      # No date is OK!
+    end
+  end
 end
