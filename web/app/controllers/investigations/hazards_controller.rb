be50a5d2fa8be5ba232425674cc56648552771fc
Unknown <michal.kreft@softwire.com>
2018-10-18 11:37:33 +0100

    MSPSDS-528: Load hazard if it exists in investigation or in params

--- web/app/controllers/investigations/hazards_controller.rb
+++ web/app/controllers/investigations/hazards_controller.rb
@@ -2,16 +2,9 @@ class Investigations::HazardsController < ApplicationController
   include Wicked::Wizard
   steps :details, :summary
 
-  # GET /hazards
-  # GET /hazards.json
-  def index
-    @hazards = Hazard.all
-  end
-
   # GET /hazards/1
   # GET /hazards/1.json
   def show
-    session[:hazard] = {} if step == steps.first
     @hazard = Hazard.new(session[:hazard])
     render_wizard
   end
@@ -19,13 +12,8 @@ class Investigations::HazardsController < ApplicationController
   # GET /hazards/new
   def new
     save_investigation
+    load_hazard
     redirect_to wizard_path(steps.first, request.query_parameters)
-  end
-
-  # GET /hazards/1/edit
-  def edit
-  end
-  def index
   end
 
   # POST /hazards
@@ -34,8 +22,7 @@ class Investigations::HazardsController < ApplicationController
     @investigation = Investigation.find_by(id: session[:invesigation_id])
     @hazard = Hazard.new(hazard_params)
     @investigation.hazard = @hazard
-     @investigation.save
-
+    @investigation.save
     session[:hazard] = {}
     redirect_to @investigation, notice: 'Hazard was successfully created.'
 
@@ -46,8 +33,6 @@ class Investigations::HazardsController < ApplicationController
   def update
     update_partial_hazard
     @investigation = Investigation.find_by(id: session[:invesigation_id])
-    p @investigation
-    p "==========="
     @investigation.hazard = @hazard
     if !@hazard.valid?
       render step
@@ -61,32 +46,42 @@ class Investigations::HazardsController < ApplicationController
   def destroy
     @hazard.destroy
     respond_to do |format|
-      format.html { redirect_to hazards_url, notice: 'Hazard was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to hazards_url, notice: 'Hazard was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_hazard
-    #   @hazard = Hazard.find(params[:id])
-    # end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def hazard_params
-      if !params[:hazard]
-        session[:hazard]
-      else
-        params.require(:hazard).permit(:hazard_type, :description, :affected_parties, :risk_level, :investigation)
-      end
-    end
+  def load_hazard
+    session[:hazard] = {}
+    @investigation = Investigation.find_by(id: session[:invesigation_id])
 
-    def save_investigation
-      session[:invesigation_id] = params[:investigation_id]
+    hazard_data = {}
+    hazard_data = hazard_data.merge(params[:hazard]) if (params[:hazard])
+    hazard_data = hazard_data.merge(@investigation.hazard.attributes) if (@investigation.hazard)
+    if (hazard_data!= {})
+      params[:hazard] = hazard_data
+      session[:hazard] = hazard_params
     end
-    def update_partial_hazard
-      session[:hazard] = (session[:hazard] || {}).merge(hazard_params)
-      @hazard = Hazard.new(session[:hazard])
-      session[:hazard] = @hazard.attributes
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def hazard_params
+    if !params[:hazard]
+      session[:hazard]
+    else
+      params.require(:hazard).permit(:hazard_type, :description, :affected_parties, :risk_level, :investigation)
     end
+  end
+
+  def save_investigation
+    session[:invesigation_id] = params[:investigation_id]
+  end
+
+  def update_partial_hazard
+    session[:hazard] = (session[:hazard] || {}).merge(hazard_params)
+    @hazard = Hazard.new(session[:hazard])
+    session[:hazard] = @hazard.attributes
+  end
 end
