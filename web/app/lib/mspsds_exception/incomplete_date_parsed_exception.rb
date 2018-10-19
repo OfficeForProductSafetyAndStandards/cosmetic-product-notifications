d12c571bade7e0b8093a93cb930d50c5e7b12ca8
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-10-19 12:39:41 +0100

    MSPSDS-522 Remove exception class created as part of WIP

--- web/app/lib/mspsds_exception/incomplete_date_parsed_exception.rb
+++
@@ -1,9 +0,0 @@
-module MspsdsException
-  class IncompleteDateParsedException < RuntimeError
-    attr_accessor :missing_fields
-
-    def initialize(missing_fields)
-      @missing_fields = missing_fields
-    end
-  end
-end
