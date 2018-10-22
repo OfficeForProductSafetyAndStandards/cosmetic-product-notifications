ef61a8fa29897c468589ca4345f5868b22936a47
MatthewBurstein <matthew.burstein@softwire.com>
2018-10-22 15:05:05 +0100

    MSPSDS-643: feat: add Investigation#get_title

--- web/app/models/investigation.rb
+++ web/app/models/investigation.rb
@@ -6,6 +6,7 @@ class Investigation < ApplicationRecord
   include Searchable
   include Documentable
   include UserService
+  include Investigations::DisplayTextHelper
 
   validates :title, presence: true, on: :question_details
   validate :validate_assignment
@@ -70,6 +71,11 @@ class Investigation < ApplicationRecord
     ::AuditActivity::Business::Add.from(business, self)
   end
 
+  def get_title
+    #TODO Once MSPSDS-528 is merged, add Hazard part of title. Format is "#{products portion} - #{hazard overview}"
+    build_title_products_portion.presence || "Untitled #{case_question_text(self)}"
+  end
+
 private
 
   def validate_assignment
@@ -81,6 +87,27 @@ private
   def send_assignee_email
     if saved_changes.key? :assignee_id
       NotifyMailer.assigned_investigation(self, assignee.full_name, assignee.email).deliver_later
+    end
+  end
+
+  def build_title_products_portion
+    case products.length
+    when 0
+      ''
+    when 1
+      product = products[0]
+      [product.brand, product.model, product.product_type].compact.join(", ")
+    else
+      count = "#{products.length} Products"
+      shared_property_values = %w(brand model type).map { |property| get_product_property_value_if_shared(property) }
+      shared_property_values.unshift(count).compact.join(", ")
+    end
+  end
+
+  def get_product_property_value_if_shared property_name
+    first_product = products.first
+    if products.all? { |product| product[property_name] == first_product[property_name] }
+      first_product[property_name]
     end
   end
 end
