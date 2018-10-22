85f86ba665a53879fad796abf307fad7a325e4ac
MatthewBurstein <matthew.burstein@softwire.com>
2018-10-22 15:35:23 +0100

    MSPSDS-647: fix: display username rather than id for assignees in _history.html.slim

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, query_params.merge(sort: column, direction: direction), class: "sort-link #{css_class}"
   end
+
+  def get_history_field_display_format field, value
+    case field
+    when "assignee_id"
+      User.find_by(id: value).full_name
+    else
+      value
+    end
+  end
 end
