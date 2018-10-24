e501e937a026256ae79da6519889e56b2d34bd96
kicferk1 <michal.kreft@softwire.com>
2018-10-24 12:28:11 +0100

    MSPSDS-516: Add tests

---
+++ web/test/system/add_correspondence_from_flow_test.rb
@@ -0,0 +1,69 @@
+require "application_system_test_case"
+
+class AddCorrespondenceFromFlowTest < ApplicationSystemTestCase
+  setup do
+    sign_in_as_admin
+    visit root_path
+    click_on "Report an unsafe product"
+    select_type_and_continue
+    fill_name_and_continue
+    click_on "Add reporter correspondance"
+  end
+
+  teardown do
+    logout
+  end
+
+  test "first step should be type" do
+    assert_text("Who is the correspondance with?")
+  end
+
+  test "first step should be populated with reporter details" do
+    assert_equal('Ben', find_field('correspondence[correspondent_name]').value, '')
+  end
+
+  test "second step should be correspondence details" do
+    click_button "Continue"
+    assert_text("Email body, transcript or notes")
+  end
+
+  test "third step should be confirmation" do
+    click_button "Continue"
+    click_button "Continue"
+    assert_text("Correspondant")
+    assert_text("Method")
+  end
+
+  test "confirmation edit details should go to first page in flow" do
+    click_button "Continue"
+    click_button "Continue"
+    click_on "Edit details"
+    assert_text("Who is the correspondance with?")
+  end
+
+  test "edit details should retain changed values" do
+    fill_in("correspondence[correspondent_name]", with: "Tom")
+    click_button "Continue"
+    click_button "Continue"
+    click_on "Edit details"
+    assert_equal('Tom', find_field('correspondence[correspondent_name]').value, '')
+    assert_not_equal('Ben', find_field('correspondence[correspondent_name]').value, '')
+  end
+
+  test "confirmation continue should go to case page" do
+    click_button "Continue"
+    click_button "Continue"
+    click_button "Continue"
+    assert_text("There are no products attached to this case")
+  end
+
+  def select_type_and_continue
+    choose("reporter[reporter_type]", visible: false, match: :first)
+    click_button "Continue"
+  end
+
+  def fill_name_and_continue
+    fill_in("reporter[name]", with: "Ben")
+    click_button "Continue"
+  end
+end
