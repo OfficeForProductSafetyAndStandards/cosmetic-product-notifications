dc44ef138a627eb5badce78cdef4661f757969f0
MatthewBurstein <matthew.burstein@softwire.com>
2018-10-24 15:55:30 +0100

    MSPSDS-545: fix: update assigned_investigation email template to use name instead of email

--- web/app/mailers/notify_mailer.rb
+++ web/app/mailers/notify_mailer.rb
@@ -1,10 +1,10 @@
 class NotifyMailer < GovukNotifyRails::Mailer
-  def assigned_investigation(investigation, email)
+  def assigned_investigation(investigation, name, email)
     set_template('b8260d95-84f8-4f45-928e-7916d27b5a80')
     set_reference('Investigation assigned')
 
     set_personalisation(
-      name: email,
+      name: name,
       investigation_url: investigation_url(investigation)
     )
 
