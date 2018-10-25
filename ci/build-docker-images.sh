5b766ad3bc1b4fde59f929fdc7f78c10e11d5c10
David Rendell <david.rendell@softwire.com>
2018-10-25 17:03:28 +0100

    COS-16 Update Docker builds in Travis

--- ci/build-docker-images.sh
+++ ci/build-docker-images.sh
@@ -3,9 +3,6 @@
 # tagged with the build ID to DockerHub.
 set -ex
 
-# Base name of the respositories for each component.
-REPOSITORY_BASE=davidrendell/beis-mspsds
-
 COMPONENTS=(
     'web'
     'worker'
@@ -15,20 +12,27 @@ COMPONENTS=(
     'elasticsearch'
 )
 
+function docker_tag_exists {
+    curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
+}
+
 echo "Logging into DockerHub"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 for COMPONENT in "${COMPONENTS[@]}"; do
-    if [[ $(./ci/get-changed-components.sh) =~ ((^| )$COMPONENT($| )) ]]; then
+    if [[ $(./ci/get-changed-components.sh) =~ ((^| )werwerwe($| )) ]]; then
         echo "Building component $COMPONENT"
         docker-compose -f docker-compose.yml -f docker-compose.ci.yml build $COMPONENT
-
+    elif docker_tag_exists $DOCKER_USERNAME/$COMPONENT $BRANCH; then
+        echo "No changes for component $COMPONENT, pulling $BRANCH image"
+        docker pull $DOCKER_USERNAME/$COMPONENT:$BRANCH
+        docker tag $DOCKER_USERNAME/$COMPONENT:$BRANCH $DOCKER_USERNAME/$COMPONENT:$TRAVIS_BUILD_NUMBER
     else
-        echo "No changes for component $COMPONENT, pulling master image"
-        docker pull $REPOSITORY_BASE-$COMPONENT:master
-        docker tag $REPOSITORY_BASE-$COMPONENT:master $REPOSITORY_BASE-$COMPONENT:$TRAVIS_BUILD_NUMBER
+        echo "No changes for component $COMPONENT, but $BRANCH image does not exist in repo"
+        echo "Building component $COMPONENT"
+        docker-compose -f docker-compose.yml -f docker-compose.ci.yml build $COMPONENT
     fi
 
     echo "Pushing image for component $COMPONENT with tag $TRAVIS_BUILD_NUMBER"
-    docker push $REPOSITORY_BASE-$COMPONENT:$TRAVIS_BUILD_NUMBER
+    docker push $DOCKER_USERNAME/$COMPONENT:$TRAVIS_BUILD_NUMBER
 done
