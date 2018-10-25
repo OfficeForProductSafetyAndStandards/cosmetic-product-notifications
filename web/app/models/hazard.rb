f12efa24bd91f1b1eed51dde68e2a7c6b638acff
Sam Lionheart <strlionheart@gmail.com>
2018-10-25 09:23:20 +0100

    MSPSDS-528 Trying to fix radials

--- web/app/models/hazard.rb
+++ web/app/models/hazard.rb
@@ -1,4 +1,4 @@
 class Hazard < ApplicationRecord
   has_one :invesigation
-  enum risk_level: %i[low medium serious severe], _suffix: true
+  enum risk_level: %i[none low medium serious severe], _suffix: true
 end
