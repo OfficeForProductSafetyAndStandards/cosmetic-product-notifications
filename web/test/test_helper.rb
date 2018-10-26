e2114b2616327235e0a4b97d288e18b770f12119
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-10-26 10:44:17 +0100

    MSPSDS-545: Refactor test_helper

--- web/test/test_helper.rb
+++ web/test/test_helper.rb
@@ -18,20 +18,18 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  # Add more helper methods to be used by all tests here...
 
+  # Add more helper methods to be used by all tests here...
   def sign_in_as_admin
-    user = build_admin_user :keycloak
-    stub_user_credentials(user: user, is_admin: true)
+    stub_user_credentials(user: admin_user, is_admin: true)
     stub_client_config
-    stub_user_data
+    stub_user_data(users: [admin_user, test_user])
   end
 
   def sign_in_as_user
-    user = build_test_user :keycloak
-    stub_user_credentials(user: user, is_admin: false)
+    stub_user_credentials(user: test_user, is_admin: false)
     stub_client_config
-    stub_user_data
+    stub_user_data(users: [admin_user, test_user])
   end
 
   def logout
@@ -39,48 +37,39 @@ class ActiveSupport::TestCase
     allow(Keycloak::Client).to receive(:get_userinfo).and_call_original
     allow(Keycloak::Client).to receive(:has_role?).and_call_original
     allow(Keycloak::Client).to receive(:auth_server_url).and_call_original
+
+    allow(Keycloak::Internal).to receive(:all_users).and_call_original
   end
 
 private
 
-  def build_admin_user source
-    id = SecureRandom.uuid
-    email = "admin@example.com"
-    first_name = "admin_first"
-    last_name = "admin_last"
-    if source == :keycloak
-      { sub: id, email: email, given_name: first_name, family_name: last_name }
-    elsif source == :rails
-      { id: id, email: email, first_name: first_name, last_name: last_name }
-    end
+  def admin_user
+    { id: SecureRandom.uuid, email: "admin@example.com", first_name: "First", last_name: "Last" }
   end
 
-  def build_test_user source
-    id = SecureRandom.uuid
-    email = "user@example.com"
-    first_name = "user_first"
-    last_name = "user_last"
-    if source == :keycloak
-      { sub: id, email: email, given_name: first_name, family_name: last_name }
-    elsif source == :rails
-      { id: id, email: email, first_name: first_name, last_name: last_name }
-    end
+  def test_user
+    { id: SecureRandom.uuid, email: "user@example.com", first_name: "First", last_name: "Last" }
   end
 
   def stub_user_credentials(user:, is_admin: false)
     allow(Keycloak::Client).to receive(:user_signed_in?).and_return(true)
-    allow(Keycloak::Client).to receive(:get_userinfo).and_return(user.to_json)
+    allow(Keycloak::Client).to receive(:get_userinfo).and_return(format_user_for_get_userinfo(user))
     allow(Keycloak::Client).to receive(:has_role?).with(:admin).and_return(is_admin)
+  end
+
+  def format_user_for_get_userinfo(user)
+    { sub: user[:id], email: user[:email], given_name: user[:first_name], family_name: user[:last_name] }.to_json
   end
 
   def stub_client_config
     allow(Keycloak::Client).to receive(:auth_server_url).and_return("localhost")
   end
 
-  def stub_user_data
-    test_user = build_test_user :rails
-    admin_user = build_admin_user :rails
-    users_json = JSON.generate [test_user, admin_user]
-    allow(Keycloak::Internal).to receive(:get_users).and_return(users_json)
+  def stub_user_data(users:)
+    allow(Keycloak::Internal).to receive(:get_users).and_return(format_user_for_get_users(users))
+  end
+
+  def format_user_for_get_users(users)
+    users.map { |user| { id: user[:id], email: user[:email], firstName: user[:first_name], lastName: user[:last_name] } }.to_json
   end
 end
