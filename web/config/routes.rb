35bacdc1f552604cce348e3a48f15dc21509188c
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-10-29 16:58:12 +0000

    MSPSDS-196: Fix error tests

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -69,8 +69,8 @@ Rails.application.routes.draw do
     end
   end
 
-  match "/404", :to => "errors#not_found", :via => :all
-  match "/500", :to => "errors#internal_server_error", :via => :all
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 
   root to: redirect(path: "/investigations")
 end
--- web/test/controllers/errors_controller_test.rb
+++ web/test/controllers/errors_controller_test.rb
@@ -1,13 +1,21 @@
 require 'test_helper'
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in_as_admin
+  end
+
+  teardown do
+    logout
+  end
+
   test "should get not_found" do
-    get errors_not_found_url
+    get "/404"
     assert_response :not_found
   end
 
   test "should get internal_server_error" do
-    get errors_internal_server_error_url
+    get "/500"
     assert_response :internal_server_error
   end
 end
