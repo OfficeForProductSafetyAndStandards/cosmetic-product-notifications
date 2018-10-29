7f13ac784049adf53005becc0b969307de7b3a1f
David Rendell <david.rendell@softwire.com>
2018-10-29 11:15:03 +0000

    COS-16 Fix issues with pushing built images, and deploying Cosmetics Web

--- ci/deploy-if-changed.sh
+++ ci/deploy-if-changed.sh
@@ -4,9 +4,9 @@ set -ex
 if [[ $(./ci/get-changed-components.sh) =~ ((^| )$COMPONENT($| )) ]]; then
     ./$COMPONENT/deploy.sh $SPACE
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-    docker pull $REPOSITORY_BASE-$COMPONENT:$REPOSITORY_BASE-$COMPONENT
-    docker tag $REPOSITORY_BASE-$COMPONENT:$BUILD_ID $REPOSITORY_BASE-$COMPONENT:$BRANCH
-    docker push $REPOSITORY_BASE-$COMPONENT:$BRANCH
+    docker pull $DOCKER_USERNAME/$COMPONENT:$BUILD_ID
+    docker tag $DOCKER_USERNAME/$COMPONENT:$BUILD_ID $DOCKER_USERNAME/$COMPONENT:$BRANCH
+    docker push $DOCKER_USERNAME/$COMPONENT:$BRANCH
 else
     echo 'Deployment not required.'
 fi
