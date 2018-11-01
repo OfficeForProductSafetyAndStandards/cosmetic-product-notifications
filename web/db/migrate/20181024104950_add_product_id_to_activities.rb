b776a969cb766f997b8a0626a310fac6d517f3bf
MatthewBurstein <matthew.burstein@softwire.com>
2018-11-01 12:19:50 +0000

    MSPSDS-559 refactor: squash all migrations for this ticket into one

--- web/db/migrate/20181024104950_add_product_id_to_activities.rb
+++
@@ -1,8 +0,0 @@
-class AddProductIdToActivities < ActiveRecord::Migration[5.2]
-  disable_ddl_transaction!
-
-  def change
-    add_reference :activities, :product, index: false, foreign_key: true
-    add_index :activities, :product_id, algorithm: :concurrently
-  end
-end
--- web/db/migrate/20181026141531_remove_subtitle_from_activity.rb
+++
@@ -1,7 +0,0 @@
-class RemoveSubtitleFromActivity < ActiveRecord::Migration[5.2]
-  def change
-    safety_assured {
-      remove_column :activities, :subtitle_slug, :string
-    }
-  end
-end
