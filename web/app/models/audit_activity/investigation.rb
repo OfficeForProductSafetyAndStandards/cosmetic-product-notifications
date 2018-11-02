e3cbec876283bbdd544c163fa85d713d5b98bc1d
Raam Chauhan <raam.chauhan@softwire.com>
2018-11-02 13:10:36 +0000

    MSPSDS-559 chore: linting

--- web/app/models/audit_activity/investigation.rb
+++ web/app/models/audit_activity/investigation.rb
@@ -1,9 +1,9 @@
 class AuditActivity::Investigation < AuditActivity
-    private_class_method def self.from(investigation)
-       self.create(
-           source: UserSource.new(user: current_user),
-           investigation: investigation,
-           title: "Case created",
-       )
-   end
+  private_class_method def self.from(investigation, title) # rubocop:disable Lint/UselessAccessModifier
+    self.create(
+      source: UserSource.new(user: current_user),
+        investigation: investigation,
+        title: title,
+    )
+  end
 end
