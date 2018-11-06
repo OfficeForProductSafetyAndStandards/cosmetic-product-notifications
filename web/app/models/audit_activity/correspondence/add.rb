c8e2de5d3d663269b1ca0c4723d294c1ca403ffb
kicferk1 <michal.kreft@softwire.com>
2018-11-06 09:52:15 +0000

    MSPSDS-516: Apply activities fix to correspondence

--- web/app/models/audit_activity/correspondence/add.rb
+++ web/app/models/audit_activity/correspondence/add.rb
@@ -1,4 +1,4 @@
-class AuditActivity::Correspondence::Add < AuditActivity::Correspondence
+class AuditActivity::Correspondence::Add < AuditActivity::Correspondence::Base
   def self.from(correspondence, investigation)
     super(correspondence, investigation)
   end
---
+++ web/app/models/audit_activity/correspondence/base.rb
@@ -0,0 +1,13 @@
+class AuditActivity::Correspondence::Base < AuditActivity::Base
+  belongs_to :correspondence
+
+  private_class_method def self.from(correspondence, investigation)
+    self.create(
+     body: correspondence.details,
+     source: UserSource.new(user: current_user),
+     investigation: investigation,
+     title: correspondence.overview,
+     correspondence: correspondence
+    )
+    end
+end
