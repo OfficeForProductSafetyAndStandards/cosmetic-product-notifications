900f5fe81129ccb9360e27e207214733bdac33be
David Rendell <david.rendell@softwire.com>
2018-11-06 14:25:40 +0000

    COS-31 Remove shared-web dummy app

--- shared-web/test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Shared::Web::Engine => "/shared-web"
-end
--- shared-web/test/shared/web_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Shared::Web::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Shared::Web
-  end
-end
