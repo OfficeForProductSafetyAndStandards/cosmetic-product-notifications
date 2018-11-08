c30752d478ab6bf8623476480f595b66c5823a36
kicferk1 <michal.kreft@softwire.com>
2018-11-08 16:37:10 +0000

    MSPSDS-673: small fixes

--- web/app/controllers/concerns/file_concern.rb
+++ web/app/controllers/concerns/file_concern.rb
@@ -26,24 +26,20 @@ module FileConcern
   end
 
   def attach_file_to_list(file, attachment_list)
-    if file
-      update_file_details(file)
-      attachments = attachment_list.attach(file)
-      attachment = attachments.last
-      attachment.blob.save
-      attachment
-    end
+    return unless file
+    update_file_details(file)
+    attachments = attachment_list.attach(file)
+    attachment = attachments.last
+    attachment.blob.save
+    attachment
   end
 
   def attach_file_to_attachment_slot(file, attachment_slot)
-    if file
-      update_file_details(file)
-      if attachment_slot.attached?
-        attachment_slot.detach
-      end
-      attachment_slot.attach(file)
-      attachment_slot.blob.save
-    end
+    return unless file
+    update_file_details(file)
+    attachment_slot.detach if attachment_slot.attached?
+    attachment_slot.attach(file)
+    attachment_slot.blob.save
   end
 
   def update_file_details(file)
