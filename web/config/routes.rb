88762baeada7f588dca585bfedeea4cf6604ef2d
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-11-09 15:54:44 +0000

    MSPSDS-472: Remove unused activities code

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
       resources :report, controller: "investigations/report", only: %i[show new create update]
       resources :question, controller: "investigations/question", only: %i[show new create update]
     end
-    resources :activities, only: %i[index new create]
+    resources :activities, controller: "investigations/activities", only: %i[create]
     resources :products, only: %i[new create], controller: "investigations/products" do
       collection do
         get :suggested
