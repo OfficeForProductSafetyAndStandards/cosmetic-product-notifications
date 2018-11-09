63be6855bae9e021d404d4c3e25bcdcdbe8d9a06
Tom Bell <tom.bell@softwire.com>
2018-11-09 16:19:27 +0000

    MSPSDS-553: Add attachments and date component properties to Test model

--- web/app/models/test.rb
+++ web/app/models/test.rb
@@ -1,4 +1,15 @@
 class Test < ApplicationRecord
+  include DateConcern
+
   belongs_to :investigation
   belongs_to :product
+
+  enum status: %i[requested passed failed]
+
+  has_many_attached :documents
+
+  attribute :day, :integer
+  attribute :month, :integer
+  attribute :year, :integer
+  validate :date_from_components
 end
