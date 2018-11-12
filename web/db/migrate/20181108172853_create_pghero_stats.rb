c2e4a8b10d182ca046dccd35638d87824369224e
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-11-12 13:40:52 +0000

    MSPSDS-472 Squash pgHero migrations

--- web/db/migrate/20181108172853_create_pghero_stats.rb
+++ web/db/migrate/20181108172853_create_pghero_stats.rb
@@ -1,4 +1,4 @@
-class CreatePgheroQueryStats < ActiveRecord::Migration[5.2]
+class CreatePgheroStats < ActiveRecord::Migration[5.2]
   def change
     create_table :pghero_query_stats do |t| # rubocop:disable Rails/CreateTableWithTimestamps
       t.text :database
@@ -10,6 +10,15 @@ class CreatePgheroQueryStats < ActiveRecord::Migration[5.2]
       t.timestamp :captured_at
     end
 
+    create_table :pghero_space_stats do |t| # rubocop:disable Rails/CreateTableWithTimestamps
+      t.text :database
+      t.text :schema
+      t.text :relation
+      t.integer :size, limit: 8
+      t.timestamp :captured_at
+    end
+
+    add_index :pghero_space_stats, %i[database captured_at]
     add_index :pghero_query_stats, %i[database captured_at]
   end
 end
