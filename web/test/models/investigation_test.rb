aa0bf0a83925934813d3d7b87b760f63c7769ede
Raam Chauhan <raam.chauhan@softwire.com>
2018-11-14 10:40:53 +0000

    MSPSDS-716: Add tests for removing products and businesses from cases

--- web/test/models/investigation_test.rb
+++ web/test/models/investigation_test.rb
@@ -11,12 +11,55 @@ class InvestigationTest < ActiveSupport::TestCase
 
   test "should create activities when investigation is created" do
     assert_difference"Activity.count" do
-      add_investigation
-      @investigation.save
+      create_investigation
     end
   end
 
-  def add_investigation
-    @investigation = Investigation.new
+  test "should create an activity when business is added to investigation" do
+    create_investigation
+    assert_difference"Activity.count" do
+      create_business
+      @investigation.businesses << @business
+    end
+  end
+
+  test "should create an activity when business is removed from investigation" do
+    create_investigation
+    create_business
+    @investigation.businesses << @business
+    assert_difference"Activity.count" do
+      @investigation.businesses.delete(@business)
+    end
+  end
+
+  test "should create an activity when product is added to investigation" do
+    create_investigation
+    assert_difference"Activity.count" do
+      create_product
+      @investigation.products << @product
+    end
+  end
+
+  test "should create an activity when product is removed from investigation" do
+    create_investigation
+    create_product
+    @investigation.products << @product
+    assert_difference"Activity.count" do
+      @investigation.products.delete(@product)
+    end
+  end
+
+  def create_investigation
+    @investigation = Investigation.create
+  end
+
+  def create_business
+    @business = Business.new
+    @business.company_name = 'TestCompany'
+  end
+
+  def create_product
+    @product = Product.new
+    @product.name = 'TestProduct'
   end
 end
