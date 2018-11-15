188f05143b0a3bb8a05e1e24600fa416db059a71
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-11-15 17:07:46 +0000

    MSPSDS-472 Add missing migration to enable pgHero
    The migration was missing despite being reflected in the schema, which
    is why there's superficially no changes to the schema.rb

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_172853) do
+ActiveRecord::Schema.define(version: 2018_11_15_164628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
