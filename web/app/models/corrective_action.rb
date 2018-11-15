939798b3d597e737562747df5561cae4c2304371
Tom Bell <tom.bell@softwire.com>
2018-11-15 17:34:38 +0000

    MSPSDS-551: Add attachments, date components to Corrective Action model

--- web/app/models/corrective_action.rb
+++ web/app/models/corrective_action.rb
@@ -1,6 +1,14 @@
 class CorrectiveAction < ApplicationRecord
+  include DateConcern
+
   belongs_to :investigation
   belongs_to :business
   belongs_to :product
 
+  has_many_attached :documents
+
+  attribute :day, :integer
+  attribute :month, :integer
+  attribute :year, :integer
+  validate :date_from_components
 end
