c687e4313469f7abd4da1410258363332f37d580
Tom Bell <tom.bell@softwire.com>
2018-11-16 12:42:55 +0000

    MSPSDS-551: Add form validation when recording corrective action

--- web/app/models/corrective_action.rb
+++ web/app/models/corrective_action.rb
@@ -15,4 +15,17 @@ class CorrectiveAction < ApplicationRecord
   def get_date_key
     :date_decided
   end
+
+  # TODO MSPSDS-551: Confirm if summary and date_decided fields should be required
+  validate :date_decided_cannot_be_in_the_future
+
+  validates :investigation, presence: true
+  validates :legislation, presence: true
+  validates :business, presence: true
+  validates :product, presence: true
+
+  def date_decided_cannot_be_in_the_future
+    errors.add(:date_decided, "can't be in the future") if
+        date_decided.present? and date_decided > Date.today
+  end
 end
