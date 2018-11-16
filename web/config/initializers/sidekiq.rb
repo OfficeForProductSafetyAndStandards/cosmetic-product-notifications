095826c5e85ad2cb5c9844ef66af77ae3edf1521
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-11-16 12:24:40 +0000

    MSPSDS-472 Enable stats collection job

--- web/config/initializers/sidekiq.rb
+++ web/config/initializers/sidekiq.rb
@@ -1,5 +1,4 @@
-Sidekiq.configure_server do |config|
-  config.redis = Rails.application.config_for(:redis)
+def create_db_stat_collection_job
   pg_hero_stats_capture_job = Sidekiq::Cron::Job.new(
     name: 'pgHero stats capture - every 5min',
     cron: '*/5 * * * *',
@@ -10,6 +9,11 @@ Sidekiq.configure_server do |config|
     Rails.logger.error "***** WARNING - pgHero stats capture job was not saved! *****"
     Rails.logger.error pg_hero_stats_capture_job.errors
   end
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = Rails.application.config_for(:redis)
+  create_db_stat_collection_job
 end
 
 Sidekiq.configure_client do |config|
