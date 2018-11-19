ffc63a8e4e9e39afa9829f61fd7f6ad245ca41ca
kicferk1 <michal.kreft@softwire.com>
2018-11-19 14:59:05 +0000

    MSPSDS-537: make sure tests pass

--- web/app/models/concerns/searchable.rb
+++ web/app/models/concerns/searchable.rb
@@ -66,7 +66,7 @@ module Searchable
       # This line makes sure elasticsearch index is recreated before we search
       # It fixes the issue of getting no results the first time case list page is loaded
       # It's only used in dev because it lowers performance and the issue it fixes should be an edge case in production
-      __elasticsearch__.refresh_index! if Rails.env.development?
+      __elasticsearch__.refresh_index! if Rails.env.development? || Rails.env.test?
       __elasticsearch__.search(query.build_query)
     end
 
