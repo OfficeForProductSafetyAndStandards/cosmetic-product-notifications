1263167234f37374f3daf63dba7802c6fc1a94b3
Tom Bell <tom.bell@softwire.com>
2018-11-20 15:04:39 +0000

    MSPSDS-551: Update form validation for corrective actions

--- web/app/models/corrective_action.rb
+++ web/app/models/corrective_action.rb
@@ -7,20 +7,15 @@ class CorrectiveAction < ApplicationRecord
 
   has_many_attached :documents
 
-  attribute :day, :integer
-  attribute :month, :integer
-  attribute :year, :integer
-  validate :date_from_components
-
   def get_date_key
     :date_decided
   end
 
-  # TODO MSPSDS-551: Confirm if summary and date_decided fields should be required
   validate :date_decided_cannot_be_in_the_future
 
+  validates :summary, presence: true
+  validates :date_decided, presence: true
   validates :investigation, presence: true
-  validates :legislation, presence: true
   validates :business, presence: true
   validates :product, presence: true
 
