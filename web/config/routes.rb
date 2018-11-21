69664aba123095f7ffa45be99392b09f7366f5fb
Tom Bell <tom.bell@softwire.com>
2018-11-21 16:39:46 +0000

    MSPSDS-553: Remove unused routes

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -68,7 +68,7 @@ Rails.application.routes.draw do
     resources :correspondences, only: %i[show new create update], controller: "investigations/correspondence",
               concerns: %i[document_attachable]
     resources :incidents, controller: "investigations/incidents", only: %i[new create show update]
-    resources :tests, controller: "investigations/tests", only: %i[show create update], concerns: %i[document_attachable] do
+    resources :tests, controller: "investigations/tests", only: %i[show create update] do
       collection do
         get :new_request
         get :new_result
