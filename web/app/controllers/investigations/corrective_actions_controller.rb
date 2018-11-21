5742c1f183c5d275659de02022e0821e31d9e8b2
Tom Bell <tom.bell@softwire.com>
2018-11-21 16:10:16 +0000

    MSPSDS-551: Refactor corrective action controller methods

--- web/app/controllers/investigations/corrective_actions_controller.rb
+++ web/app/controllers/investigations/corrective_actions_controller.rb
@@ -4,12 +4,9 @@ class Investigations::CorrectiveActionsController < ApplicationController
   steps :details, :confirmation
 
   before_action :set_investigation
-  before_action :clear_session, only: %i[new]
-  before_action :set_corrective_action, only: %i[show update create]
-  before_action :set_attachment, only: %i[show update create]
-  # before_action :build_corrective_action_from_params, only: %i[update]
+  before_action :set_corrective_action, only: %i[show create update]
+  before_action :set_attachment, only: %i[show create update]
   before_action :store_corrective_action, only: %i[update]
-  # before_action :restore_corrective_action, only: %i[show create]
 
   # GET /corrective_actions/1
   def show
@@ -18,15 +15,15 @@ class Investigations::CorrectiveActionsController < ApplicationController
 
   # GET /corrective_actions/new
   def new
+    clear_session
     redirect_to wizard_path(steps.first, request.query_parameters)
   end
 
   # POST /corrective_actions
   # POST /corrective_actions.json
   def create
-    attach_files
-
     respond_to do |format|
+      attach_files
       if @corrective_action.save
         format.html { redirect_to investigation_url(@investigation), notice: "Corrective action was successfully recorded." }
         format.json { render :show, status: :created, location: @corrective_action }
@@ -40,10 +37,8 @@ class Investigations::CorrectiveActionsController < ApplicationController
   # PATCH/PUT /corrective_actions/1
   # PATCH/PUT /corrective_actions/1.json
   def update
-    validate_blob_size(@file, @corrective_action.errors)
-
     respond_to do |format|
-      if @corrective_action.valid?(step)
+      if corrective_action_valid?
         format.html { redirect_to next_wizard_path }
         format.json { render :show, status: :ok, location: @corrective_action }
       else
@@ -65,9 +60,7 @@ private
   end
 
   def set_corrective_action
-    attributes = session[:corrective_action] || {}
-    attributes.merge!(corrective_action_params)
-    @corrective_action = @investigation.corrective_actions.build(attributes)
+    @corrective_action = @investigation.corrective_actions.build(corrective_action_params)
   end
 
   def set_attachment
@@ -78,13 +71,27 @@ private
     session[:corrective_action] = @corrective_action.attributes
   end
 
+  def corrective_action_valid?
+    @corrective_action.validate(step)
+    validate_blob_size(@file, @corrective_action.errors)
+    @corrective_action.errors.empty?
+  end
+
   def attach_files
     attach_file_to_list(@file, @corrective_action.documents)
     attach_file_to_list(@file, @investigation.documents)
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def corrective_action_params
+    session_params.merge(request_params)
+  end
+
+  def session_params
+    session[:corrective_action] || {}
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def request_params
     return {} if params[:corrective_action].blank?
 
     params.require(:corrective_action).permit(:product_id,
