bf034dffb80ecc8cc7d1a7d907c51317128f044a
Tom Bell <tom.bell@softwire.com>
2018-11-29 13:46:09 +0000

    MSPSDS-NONE: Refactor product helper tests

--- web/test/helpers/product_helper_test.rb
+++ web/test/helpers/product_helper_test.rb
@@ -18,10 +18,10 @@ class ProductHelperTest < ActiveSupport::TestCase
     results = advanced_product_search(search_model)
 
     # Assert
-    assert_includes(results, @iphone)
-    assert_includes(results, @iphone_3g)
-    assert_not_includes(results, @pixel)
-    assert_not_includes(results, @chromecast)
+    assert_results_include(results, @iphone)
+    assert_results_include(results, @iphone_3g)
+    assert_results_do_not_include(results, @pixel)
+    assert_results_do_not_include(results, @chromecast)
   end
 
   test "product search matches by product type (fuzzy)" do
@@ -30,10 +30,10 @@ class ProductHelperTest < ActiveSupport::TestCase
     results = advanced_product_search(search_model)
 
     # Assert
-    assert_includes(results, @iphone)
-    assert_includes(results, @iphone_3g)
-    assert_includes(results, @pixel)
-    assert_not_includes(results, @chromecast)
+    assert_results_include(results, @iphone)
+    assert_results_include(results, @iphone_3g)
+    assert_results_include(results, @pixel)
+    assert_results_do_not_include(results, @chromecast)
   end
 
   test "product search matches by brand (fuzzy)" do
@@ -42,10 +42,10 @@ class ProductHelperTest < ActiveSupport::TestCase
     results = advanced_product_search(search_model)
 
     # Assert
-    assert_includes(results, @pixel)
-    assert_includes(results, @chromecast)
-    assert_not_includes(results, @iphone)
-    assert_not_includes(results, @iphone_3g)
+    assert_results_include(results, @pixel)
+    assert_results_include(results, @chromecast)
+    assert_results_do_not_include(results, @iphone)
+    assert_results_do_not_include(results, @iphone_3g)
   end
 
   test "product search matches if at least one field matches, prioritising higher scores" do
@@ -54,8 +54,8 @@ class ProductHelperTest < ActiveSupport::TestCase
     results = advanced_product_search(search_model)
 
     # Assert
-    assert_includes(results, @chromecast)
-    assert_includes(results, @pixel)
+    assert_results_include(results, @chromecast)
+    assert_results_include(results, @pixel)
     results.find_index(@chromecast) < results.find_index(@pixel)
   end
 
@@ -65,8 +65,8 @@ class ProductHelperTest < ActiveSupport::TestCase
     results = advanced_product_search(search_model)
 
     # Assert
-    assert_not_includes(results, @iphone)
-    assert_not_includes(results, @iphone_3g)
+    assert_results_do_not_include(results, @iphone)
+    assert_results_do_not_include(results, @iphone_3g)
   end
 
   test "product search excludes specified ids" do
@@ -75,7 +75,15 @@ class ProductHelperTest < ActiveSupport::TestCase
     results = advanced_product_search(search_model, [@pixel.id])
 
     # Assert
-    assert_not_includes(results, @pixel)
-    assert_includes(results, @chromecast)
+    assert_results_do_not_include(results, @pixel)
+    assert_results_include(results, @chromecast)
+  end
+
+  def assert_results_include(results, product)
+    assert_includes(results.map(&:name), product.name)
+  end
+
+  def assert_results_do_not_include(results, product)
+    assert_not_includes(results.map(&:name), product.name)
   end
 end
