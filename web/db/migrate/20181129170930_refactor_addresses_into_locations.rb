6e64203ead96421367f5604f54747d00ab8f8d1c
kicferk1 <michal.kreft@softwire.com>
2018-11-30 14:45:41 +0000

    MSPSDS-777: Update migration

--- web/db/migrate/20181129170930_refactor_addresses_into_locations.rb
+++ web/db/migrate/20181129170930_refactor_addresses_into_locations.rb
@@ -2,27 +2,25 @@ class RefactorAddressesIntoLocations < ActiveRecord::Migration[5.2]
   def change
     safety_assured do
       reversible do |dir|
+        rename_table :addresses, :locations
         change_table :locations do |t|
+          t.rename :address_type, :name
+          t.string :phone_number
+
           dir.up do
             Location.all.each do |address|
               address.update! line_1: "#{address.line_1}, #{address.line_2}"
             end
-            t.rename :address_type, :name
             t.rename :line_1, :address
             t.remove :line_2
-            t.string :phone_number
-            rename_table :locations, :locations
           end
 
           dir.down do
-            rename_table :locations, :locations
-            t.rename :name, :address_type
-            t.rename :line_1, :address
+            t.rename :address, :line_1
             t.string :line_2
-            t.remove :phone_number
             Location.all.each do |address|
-              lines = address.address.split(/\s*,\s*/)
-              address.update! line_1: lines[0], line_2: lines[1]
+              lines = address.line_1.split(/\s*,\s*/)
+              address.update! line_1: lines[0], line_2: lines[1..-1].join(", ")
             end
           end
         end
