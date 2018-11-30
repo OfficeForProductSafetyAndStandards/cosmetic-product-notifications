ce4f2387ecfc7a4fe4c893632bbed747e8e3b20b
kicferk1 <michal.kreft@softwire.com>
2018-11-30 14:45:41 +0000

    MSPSDS-777: Update migration

--- web/db/migrate/20181129170930_refactor_addresses_into_locations.rb
+++ web/db/migrate/20181129170930_refactor_addresses_into_locations.rb
@@ -4,24 +4,24 @@ class RefactorAddressesIntoLocations < ActiveRecord::Migration[5.2]
       reversible do |dir|
         change_table :locations do |t|
           dir.up do
-            Location.all.each do |address|
+            Address.all.each do |address|
               address.update! line_1: "#{address.line_1}, #{address.line_2}"
             end
             t.rename :address_type, :name
             t.rename :line_1, :address
             t.remove :line_2
             t.string :phone_number
-            rename_table :locations, :locations
+            rename_table :addresses, :locations
           end
 
           dir.down do
-            rename_table :locations, :locations
+            rename_table :locations, :addresses
             t.rename :name, :address_type
-            t.rename :line_1, :address
+            t.rename :address, :line_1
             t.string :line_2
             t.remove :phone_number
-            Location.all.each do |address|
-              lines = address.address.split(/\s*,\s*/)
+            Address.all.each do |address|
+              lines = address.line_1.split(/\s*,\s*/)
               address.update! line_1: lines[0], line_2: lines[1]
             end
           end
