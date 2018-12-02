6dcc52344f3160f3267d83cb64b884d5e9670a3f
David Rendell <david.rendell@softwire.com>
2018-12-02 15:17:15 +0000

    COS-29 Only run tests and static analysis for components that have had changes in the build

---
+++ ci/test-if-changed.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+
+if [[ $(./ci/get-changed-components.sh) =~ ((^| )$COMPONENT($| )) \
+   || $(./ci/get-changed-components.sh) =~ ((^| )shared-web($| )) ]]; then
+    echo "Testing component $COMPONENT"
+    docker-compose -f docker-compose.yml -f docker-compose.ci.yml pull
+    docker-compose -f docker-compose.yml -f docker-compose.ci.yml up -d
+    docker-compose -f docker-compose.yml -f docker-compose.ci.yml run --rm start_dependencies
+    docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT \
+        bin/rake db:create db:schema:load test
+    docker-compose -f docker-compose.yml -f docker-compose.ci.yml down
+else
+    echo 'Testing not required.'
+fi
