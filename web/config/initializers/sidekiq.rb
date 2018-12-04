ec11f8e08ca0fa687b0546d252fa3c773b98c069
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-12-04 12:19:58 +0000

    MSPSDS-789: Fix test issue with logging objects

--- web/config/initializers/sidekiq.rb
+++ web/config/initializers/sidekiq.rb
@@ -15,6 +15,6 @@ def remove_empty_attachments_job
   )
   unless remove_attachments_job.save
     Rails.logger.error "***** WARNING - Removing empty attachments was not saved! *****"
-    Rails.logger.error remove_attachments_job.errors
+    Rails.logger.error remove_attachments_job.errors.join("; ")
   end
 end
