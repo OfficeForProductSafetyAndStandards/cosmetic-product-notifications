d2fac5b76c75d220bfc51ec81e07afdf812af787
David Rendell <david.rendell@softwire.com>
2018-12-04 15:00:32 +0000

    COS-29 Add coveralls to cosmetics build

--- cosmetics-web/test/test_helper.rb
+++ cosmetics-web/test/test_helper.rb
@@ -2,6 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
+require 'simplecov'
+require 'simplecov-console'
+require 'coveralls'
+SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::Console,
+    Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
--- shared-web/shared-web.gemspec
+++ shared-web/shared-web.gemspec
@@ -24,7 +24,10 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "sqlite3"
   s.add_dependency "brakeman"
+  s.add_dependency "coveralls"
   s.add_dependency "govuk-lint"
   s.add_dependency "rubocop"
+  s.add_dependency "simplecov"
+  s.add_dependency "simplecov-console"
   s.add_dependency "slim_lint"
 end
