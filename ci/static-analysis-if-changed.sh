879e019ffc0b46d1004a78564e08a047081f46d4
David Rendell <david.rendell@softwire.com>
2018-12-05 15:08:49 +0000

    COS-29 Use inheritance of rubocop config to avoid having to specify config file location

--- ci/static-analysis-if-changed.sh
+++ ci/static-analysis-if-changed.sh
@@ -6,7 +6,7 @@ if [[ $(./ci/get-changed-components.sh) =~ ((^| )$COMPONENT($| )) ]]; then
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml pull
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml run --rm --no-deps $COMPONENT echo 'Gems pre-installed'
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml up -d $COMPONENT
-    docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT bin/rubocop -c vendor/shared-web/.rubocop.yml
+    docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT bin/rubocop
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT bin/slim-lint -c vendor/shared-web/.slim-lint.yml app/views
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT yarn eslint -c vendor/shared-web/.eslintrc.yml app/assets/javascripts
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT yarn sass-lint -vq -c vendor/shared-web/.sasslint.yml 'app/assets/stylesheets/**/*.scss'
