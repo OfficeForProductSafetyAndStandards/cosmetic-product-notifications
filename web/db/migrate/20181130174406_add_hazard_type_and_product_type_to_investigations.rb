5e54c55f047dd221e4a7110a6c3265f6de532eba
Tom Bell <tom.bell@softwire.com>
2018-12-05 10:16:11 +0000

    MSPSDS-719: Wrap migration in safety assured block to appease Strong Migrations

--- web/db/migrate/20181130174406_add_hazard_type_and_product_type_to_investigations.rb
+++ web/db/migrate/20181130174406_add_hazard_type_and_product_type_to_investigations.rb
@@ -1,7 +1,9 @@
 class AddHazardTypeAndProductTypeToInvestigations < ActiveRecord::Migration[5.2]
   def change
-    change_table :investigations, bulk: true do |t|
-      t.string :hazard_type, :product_type
+    safety_assured do
+      change_table :investigations, bulk: true do |t|
+        t.string :hazard_type, :product_type
+      end
     end
   end
 end
