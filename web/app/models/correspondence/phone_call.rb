5c6b52aa5bca5cc1c0058ff969691848cbc9663e
aidayounesi <aida.younesi@gmail.com>
2018-12-05 13:15:58 +0000

    MSPSDS-690-691: need both summary and notes to continue

--- web/app/models/correspondence/phone_call.rb
+++ web/app/models/correspondence/phone_call.rb
@@ -2,7 +2,7 @@ class Correspondence::PhoneCall < Correspondence
   has_one_attached :transcript
 
   def validate_transcript_and_content file_blob
-    if file_blob.nil? && overview.empty? && details.empty?
+    if file_blob.nil? && (overview.empty? || details.empty?)
       errors.add(:base, "Please provide either a transcript or complete the summary and notes fields")
     end
   end
