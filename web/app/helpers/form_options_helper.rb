e96a54498b249b8ffac1eb288aa40cc1fdc34286
Tom Bell <tom.bell@softwire.com>
2018-12-06 11:31:02 +0000

    MSPSDS-763: Review markups

--- web/app/helpers/form_options_helper.rb
+++ web/app/helpers/form_options_helper.rb
@@ -1,7 +1,5 @@
 module FormOptionsHelper
   LEGISLATION_CACHE_KEY = "relevant_legislation".freeze
-  HAZARD_TYPES_CACHE_KEY = "hazard_types".freeze
-  PRODUCT_TYPES_CACHE_KEY = "product_types".freeze
 
   def relevant_legislation
     Rails.cache.fetch(LEGISLATION_CACHE_KEY, expires_in: 1.hour) do
@@ -10,14 +8,10 @@ module FormOptionsHelper
   end
 
   def hazard_types
-    Rails.cache.fetch(HAZARD_TYPES_CACHE_KEY, expires_in: 1.hour) do
-      Rails.application.config.hazard_constants["hazard_type"]
-    end
+    Rails.application.config.hazard_constants["hazard_type"]
   end
 
   def product_types
-    Rails.cache.fetch(PRODUCT_TYPES_CACHE_KEY, expires_in: 1.hour) do
-      Rails.application.config.product_constants["product_type"]
-    end
+    Rails.application.config.product_constants["product_type"]
   end
 end
