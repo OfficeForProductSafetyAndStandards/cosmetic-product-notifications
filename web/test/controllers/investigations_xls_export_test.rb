cc111fcb95d3b38bcae92aaecfa16a25f86ce5c8
kicferk1 <michal.kreft@softwire.com>
2018-12-10 09:36:43 +0000

    MSPSDS-617: Add tests for annoying edge cases of excel sheet

--- web/test/controllers/investigations_xls_export_test.rb
+++ web/test/controllers/investigations_xls_export_test.rb
@@ -3,51 +3,46 @@ require "test_helper"
 class InvestigationsXlsExportTest < ActionDispatch::IntegrationTest
   setup do
     sign_in_as_admin
-    @investigation_one = investigations(:one)
-    @investigation_two = investigations(:two)
-    Investigation.import refresh: true
-
-    get investigations_path format: :xlsx
-    @tempfile = File.new(Rails.root + 'test/fixtures/files/cases.xlsx')
-    File.open(@tempfile.path, 'w') {|f| f.write response.body}
-    @file = Roo::Excelx.new(@tempfile.path)
   end
 
   teardown do
     logout
   end
 
-  test "should have specified sheets" do
-    assert_includes(@file.sheets, 'Cases')
-    assert_includes(@file.sheets, 'Products')
-    assert_includes(@file.sheets, 'Businesses')
-    assert_includes(@file.sheets, 'Businesses locations')
-    assert_includes(@file.sheets, 'Actions')
-    assert_includes(@file.sheets, 'Correspondences')
-    assert_includes(@file.sheets, 'Corrective actions')
-    assert_includes(@file.sheets, 'Tests')
+  test "exports all investigations" do
+    path = Rails.root + 'test/fixtures/files/cases1.xlsx'
+    create_stub_investigations(50)
+    Investigation.import refresh: true
+
+    count = Investigation.where(is_closed: false).count
+    file = get_file(path)
+    assert_equal file.sheet('Cases').last_row, count + 1
+    File.delete(path)
   end
 
-  test "should create excel file for list of investigations" do
-    cases_rows = rows_in_sheet(@file.sheet('Cases'))
-    row_for_case_one = row_for_case(@investigation_one)
-    row_for_case_two = row_for_case(@investigation_two)
-    assert_includes cases_rows, row_for_case_one
-    assert_includes cases_rows, row_for_case_two
+  test "treats formulas as text" do
+    path = Rails.root + 'test/fixtures/files/cases2.xlsx'
+    Investigation.new(description: "=A1").save
+    Investigation.import refresh: true
+
+    file = get_file(path, "A1")
+    cell_a_1 = file.sheet('Cases').cell(1,1)
+    cell_bad_description = file.sheet('Cases').cell(2,4)
+    assert_equal cell_bad_description, "=A1"
+    assert_not_equal cell_bad_description, nil
+    assert_not_equal cell_bad_description, cell_a_1
+    File.delete(path)
   end
 
-  def row_for_case(investigation)
-    [investigation.pretty_id, investigation.status, investigation.title,
-     investigation.description, investigation.product_type, investigation.hazard_type,
-     investigation.assignee&.email_address || 'Unassigned', investigation.source&.show,
-     investigation.reporter&.name, investigation.reporter&.email_address,
-     investigation.reporter&.phone_number, investigation.reporter&.reporter_type,
-     investigation.products.count.to_s, investigation.businesses.count.to_s,
-     investigation.activities.count.to_s, investigation.correspondences.count.to_s,
-     investigation.corrective_actions.count.to_s, investigation.tests.count.to_s]
+  def create_stub_investigations(how_many)
+    [*1..how_many].each do
+      Investigation.new.save
+    end
   end
 
-  def rows_in_sheet(sheet)
-    [*1..sheet.last_row].map {|n| sheet.row(n)}
+  def get_file(path, query = "")
+    get investigations_path format: :xlsx, params: {q: query}
+    File.open(path, 'w') {|f| f.write response.body}
+    Roo::Excelx.new(path)
   end
 end
