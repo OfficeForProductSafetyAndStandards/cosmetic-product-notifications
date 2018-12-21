9b3c2eb537cb5b519e36706a275efad5bce4d089
David Broder-Rodgers <david.broder-rodgers@softwire.com>
2018-12-21 17:57:33 +0000

    COS-41: Fix eslint issues

--- cosmetics-web/app/assets/javascripts/helloworld.js
+++ cosmetics-web/app/assets/javascripts/helloworld.js
@@ -1,9 +1,6 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
-function send_email() {
+function sendEmail() { // eslint-disable-line no-unused-vars
   $.ajax({
     url: 'send',
     success: () => console.log('sent email')
-  })
+  });
 }
