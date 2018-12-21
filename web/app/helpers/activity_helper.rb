6380b44a68a7efd48f093def5a256b7ae56c09d7
Tom Bell <tom.bell@softwire.com>
2018-12-21 13:39:15 +0000

    MSPSDS-787: Change order of activity options to match design

--- web/app/helpers/activity_helper.rb
+++ web/app/helpers/activity_helper.rb
@@ -5,10 +5,10 @@ module ActivityHelper
       "email": "Record email",
       "phone_call": "Record phone call",
       "meeting": "Record meeting",
-      "product": "Add a product to the case",
       "testing_request": "Request testing activity",
       "testing_result": "Record testing activity",
       "corrective_action": "Record corrective action",
+      "product": "Add a product to the case",
       "business": "Add a business to the case"
     }
   end
