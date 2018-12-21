3b448127e338152ecb4e8a788ec8c72a6d8a8bb2
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2018-12-21 14:15:17 +0000

    MSPSDS-792 Remove obsolete correspondence flow

--- mspsds-web/config/routes.rb
+++ mspsds-web/config/routes.rb
@@ -56,8 +56,6 @@ Rails.application.routes.draw do
     end
 
     resources :corrective_actions, controller: "investigations/corrective_actions", only: %i[show new create update]
-    resources :correspondences, only: %i[show new create update], controller: "investigations/correspondence",
-              concerns: %i[document_attachable]
     resources :emails, controller: "investigations/emails", only: %i[show new create update]
     resources :phone_calls, controller: "investigations/phone_calls", only: %i[show new create update]
     resources :meetings, controller: "investigations/meetings", only: %i[show new create update]
