75fc1469a9ee239fd97ba63f3648ae7c8c1a6cf0
kicferk1 <michal.kreft@softwire.com>
2019-01-03 11:48:36 +0000

    MSPSDS-597: Use helpers to decrease ugliness

--- mspsds-web/app/helpers/login_helper.rb
+++ mspsds-web/app/helpers/login_helper.rb
@@ -1,5 +1,5 @@
 module LoginHelper
-  def keycloak_login_url(request_path=nil)
+  def keycloak_login_url(request_path = nil)
     KeycloakClient.instance.login_url(get_session_url_with_redirect(request_path))
   end
 
@@ -7,12 +7,9 @@ module LoginHelper
     Keycloak.keycloak_controller == controller_name
   end
 
-  def get_session_url_with_redirect(request_path=nil)
-    if signin_session_url.exclude? "?"
-      param = request_path.blank? ? "" : "?request_path=#{request_path}"
-    else
-      param = request_path.blank? ? "" : "&request_path=#{request_path}"
-    end
-    signin_session_url + param
+  def get_session_url_with_redirect(request_path)
+    uri = URI.parse(signin_session_url)
+    uri.query = [uri.query, "request_path=#{request_path}"].compact.join('&')
+    uri.to_s
   end
 end
