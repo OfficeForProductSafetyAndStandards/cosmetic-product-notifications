c1251deb9d940572c67d9a80d1a08fdac5a2901c
Raam Chauhan <raam.chauhan@softwire.com>
2019-01-04 14:41:38 +0000

    COS-43: Fix cosmetics test

--- cosmetics-web/spec/controllers/helloworld_controller_spec.rb
+++ cosmetics-web/spec/controllers/helloworld_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe HelloworldController, type: :controller do
   describe "GET #index" do
-    it "returns a success response" do
+    it "redirects the user if they are not logged in" do
       get :index
-      expect(response).to be_successful
+      expect(response.status).to eq(302)
     end
   end
 end
