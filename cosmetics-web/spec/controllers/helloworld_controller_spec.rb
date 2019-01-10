1a0c4a9fb20e96f995f69dddb518bf9454e61e09
Raam Chauhan <raam.chauhan@softwire.com>
2019-01-10 17:08:11 +0000

    COS-43: Remove helloworld test

--- cosmetics-web/spec/controllers/helloworld_controller_spec.rb
+++ cosmetics-web/spec/controllers/helloworld_controller_spec.rb
@@ -1,10 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe HelloworldController, type: :controller do
-  describe "GET #index" do
-    it "redirects the user if they are not logged in" do
-      get :index
-      expect(response.status).to eq(302)
-    end
-  end
 end
