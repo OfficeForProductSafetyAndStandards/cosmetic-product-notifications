79f0999787ebbe7808f83045fcd17dd302c4510f
Tom Bell <tom.bell@softwire.com>
2019-01-14 13:41:12 +0000

    COS-43: Enforce user authentication in shared web application controller

--- shared-web/app/controllers/shared/web/application_controller.rb
+++ shared-web/app/controllers/shared/web/application_controller.rb
@@ -2,6 +2,9 @@ module Shared
   module Web
     class ApplicationController < ActionController::Base
       include Shared::Web::Concerns::AuthenticationConcern
+
+      protect_from_forgery with: :exception
+      before_action :authenticate_user!
     end
   end
 end
