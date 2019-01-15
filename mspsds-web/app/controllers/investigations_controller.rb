30eb8b40219f41c75443656073ddb4751b55bb20
kicferk1 <michal.kreft@softwire.com>
2019-01-15 17:11:42 +0000

    MSPSDS-897: Use one query to load relevand data on case list page

--- mspsds-web/app/controllers/investigations_controller.rb
+++ mspsds-web/app/controllers/investigations_controller.rb
@@ -12,7 +12,8 @@ class InvestigationsController < ApplicationController
     respond_to do |format|
       format.html do
         @answer = search_for_investigations(20)
-        @results = @answer.results.map { |r| r.merge(record: @answer.records.find_by(id: r._id)) }
+        records = Investigation.eager_load(:products, :source).where(id: @answer.results.map {|i| i._id })
+        @results = @answer.results.map { |r| r.merge(record: records.detect { |rec| rec.id.to_s == r._id} )}
         @investigations = @answer.records
       end
       format.xlsx do
