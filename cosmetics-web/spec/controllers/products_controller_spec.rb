974b7c7eae3f74ca130f3f0db4b2d6b3162549f6
aidayounesi <aida.younesi@gmail.com>
2019-01-16 13:11:25 +0000

    COSBETA-22: Controller test

--- cosmetics-web/spec/controllers/products_controller_spec.rb
+++ cosmetics-web/spec/controllers/products_controller_spec.rb
@@ -24,15 +24,21 @@ require 'rails_helper'
 # `rails-controller-testing` gem.
 
 RSpec.describe ProductsController, type: :controller do
+  before do
+    allow(Keycloak::Client).to receive(:user_signed_in?).and_return(true)
+  end
+
+  after do
+    allow(Keycloak::Client).to receive(:user_signed_in?).and_call_original
+  end
+
   # This should return the minimal set of attributes required to create a valid
   # Product. As you add validations to Product, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {
+        uploaded_file: fixture_file_upload("testImage.png", "image/png")
+    }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -40,33 +46,10 @@ RSpec.describe ProductsController, type: :controller do
   # ProductsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "returns a success response" do
-      Product.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET #show" do
-    it "returns a success response" do
-      product = Product.create! valid_attributes
-      get :show, params: { id: product.to_param }, session: valid_session
-      expect(response).to be_successful
-    end
-  end
 
   describe "GET #new" do
     it "returns a success response" do
       get :new, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns a success response" do
-      product = Product.create! valid_attributes
-      get :edit, params: { id: product.to_param }, session: valid_session
       expect(response).to be_successful
     end
   end
@@ -83,57 +66,6 @@ RSpec.describe ProductsController, type: :controller do
         post :create, params: { product: valid_attributes }, session: valid_session
         expect(response).to redirect_to(Product.last)
       end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: { product: invalid_attributes }, session: valid_session
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested product" do
-        product = Product.create! valid_attributes
-        put :update, params: { id: product.to_param, product: new_attributes }, session: valid_session
-        product.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "redirects to the product" do
-        product = Product.create! valid_attributes
-        put :update, params: { id: product.to_param, product: valid_attributes }, session: valid_session
-        expect(response).to redirect_to(product)
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
-        product = Product.create! valid_attributes
-        put :update, params: { id: product.to_param, product: invalid_attributes }, session: valid_session
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested product" do
-      product = Product.create! valid_attributes
-      expect {
-        delete :destroy, params: { id: product.to_param }, session: valid_session
-      }.to change(Product, :count).by(-1)
-    end
-
-    it "redirects to the products list" do
-      product = Product.create! valid_attributes
-      delete :destroy, params: { id: product.to_param }, session: valid_session
-      expect(response).to redirect_to(products_url)
     end
   end
 end
