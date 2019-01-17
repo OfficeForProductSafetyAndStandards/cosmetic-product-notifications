b8d5161b071e2adb8530a8129d03e62a58d04d44
MatthewBurstein <matthew.burstein@softwire.com>
2019-01-17 11:29:30 +0000

    MSPSDS-869 test: remove tests for companies house fields from business_helper_test.rb

--- mspsds-web/test/helpers/business_helper_test.rb
+++ mspsds-web/test/helpers/business_helper_test.rb
@@ -12,27 +12,5 @@ class BusinessHelperTest < ActiveSupport::TestCase
   teardown do
   end
 
-  test "local search respects company type" do
-    # Act
-    search_model_ltd = Business.new(legal_name: "biscuit", company_type_code: "ltd")
-    results_ltd = search_for_similar_businesses(search_model_ltd, [])
-    search_model_pu = Business.new(legal_name: "biscuit", company_type_code: "private-unlimited")
-    results_pu = search_for_similar_businesses(search_model_pu, [])
 
-    # Assert
-    assert_includes(results_pu.map(&:legal_name), "Biscuit Base")
-    assert_not_includes(results_ltd.map(&:legal_name), "Biscuit Base")
-  end
-
-  test "local search respects company status" do
-    # Act
-    search_model_active = Business.new(legal_name: "biscuit", company_status_code: "active")
-    results_active = search_for_similar_businesses(search_model_active, [])
-    search_model_dissolved = Business.new(legal_name: "biscuit", company_status_code: "dissolved")
-    results_dissolved = search_for_similar_businesses(search_model_dissolved, [])
-
-    # Assert
-    assert_includes(results_active.map(&:legal_name), "Biscuit Base")
-    assert_not_includes(results_dissolved.map(&:legal_name), "Biscuit Base")
-  end
 end
