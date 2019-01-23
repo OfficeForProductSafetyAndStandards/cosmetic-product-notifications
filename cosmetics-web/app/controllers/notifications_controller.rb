c01d5eb83589dadd0ab4c7b841f1e9c18e948b3c
David Rendell <david.rendell@softwire.com>
2019-01-23 10:25:13 +0000

    COSBETA-8 Use form submit button on check your answers page

--- cosmetics-web/app/controllers/notifications_controller.rb
+++ cosmetics-web/app/controllers/notifications_controller.rb
@@ -1,16 +1,21 @@
 class NotificationsController < ApplicationController
+  before_action :set_notification
+
     # Check your answers page
-  def edit
-    @notification = Notification.find(params[:id])
-  end
+  def edit; end
 
     # Confirmation page
   def confirmation
-    @notification = Notification.find(params[:id])
-    if !@notification.may_submit_notification?
-      redirect_to edit_notification_path(@notification)
-    else
+    if @notification.may_submit_notification?
       @notification.submit_notification!
+    elsif @notification.state != :notification_complete
+      redirect_to edit_notification_path(@notification)
     end
+  end
+
+private
+
+  def set_notification
+    @notification = Notification.find(params[:id])
   end
 end
