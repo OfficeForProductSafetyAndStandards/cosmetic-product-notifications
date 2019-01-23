e543011ea713b5b1ab8831332ff0648150a42d4d
Raam Chauhan <raam.chauhan@softwire.com>
2019-01-23 11:36:15 +0000

    COSBETA-6: Delete test for landing page view

--- cosmetics-web/spec/views/landing_page/index.html.slim_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "landing_page/index.html.slim", type: :view do
-  it "displays the landing page title" do
-    render
-    expect(rendered).to match(/Register cosmetics products/)
-  end
-end
