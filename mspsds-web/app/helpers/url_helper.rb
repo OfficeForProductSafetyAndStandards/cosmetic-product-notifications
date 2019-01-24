ebe49076248cc88e9157427a3ca1bcff4889dcf9
kicferk1 <michal.kreft@softwire.com>
2019-01-24 13:22:28 +0000

    MSPSDS-688: Review markups

--- mspsds-web/app/helpers/url_helper.rb
+++ mspsds-web/app/helpers/url_helper.rb
@@ -26,13 +26,13 @@ module UrlHelper
 
   def build_back_link_to_case
     case_id = request.referer&.match(/cases\/(\d+)/)&.captures&.first
-    result = nil
-    if case_id.present?
-      result = { is_simple_link: true }
-      investigation = Investigation.find(case_id)
-      result[:simple_link_text] = "Back to #{investigation.pretty_description}"
-      result[:link_to] = investigation
-    end
-    result
+    return nil if case_id.blank?
+
+    investigation = Investigation.find(case_id)
+    {
+      is_simple_link: true,
+      link_text: "Back to #{investigation.pretty_description}",
+      link_to: investigation
+    }
   end
 end
