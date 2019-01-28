58c64e581601edda3dd311c028a34cfaf005a5b6
aidayounesi <aida.younesi@gmail.com>
2019-01-28 16:31:30 +0000

    COSBETA-24: Unzip and find the XML file

--- cosmetics-web/app/analyzers/read_data_analyzer.rb
+++ cosmetics-web/app/analyzers/read_data_analyzer.rb
@@ -1,11 +1,32 @@
+require 'zip'
+
 class ReadDataAnalyzer < ActiveStorage::Analyzer
+  def initialize(blob)
+    super(blob)
+  end
+
   def self.accept?(_blob)
     true
   end
 
   def metadata
-    download_blob_to_tempfile do |file|
-      {analyzed: true}
+    get_xml_file do |xml_file|
+      puts xml_file.get_input_stream.read
     end
+    {zip: 2}
+  end
+
+  private
+
+  def get_xml_file
+    download_blob_to_tempfile do |file|
+      Zip::File.open(file.path) do |zip_file|
+        yield zip_file.glob(get_xml_file_name_regex).first
+      end
+    end
+  end
+
+  def get_xml_file_name_regex
+    return blob.filename.base[0...8]+'*.xml'
   end
 end
