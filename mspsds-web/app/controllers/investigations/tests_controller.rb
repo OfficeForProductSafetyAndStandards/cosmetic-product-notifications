ff134d17372e041ebdb5a15c3fd0be9ce325ab87
MatthewBurstein <matthew.burstein@softwire.com>
2019-01-31 16:58:12 +0000

    MSPSDS-566 refactor: extract tests_helper.rb

--- mspsds-web/app/controllers/investigations/tests_controller.rb
+++ mspsds-web/app/controllers/investigations/tests_controller.rb
@@ -1,4 +1,5 @@
 class Investigations::TestsController < ApplicationController
+  include TestsHelper
   include FileConcern
   set_attachment_names :file
   set_file_params_key :test
@@ -60,31 +61,8 @@ private
     initialize_file_attachments
   end
 
-  def set_investigation
-    @investigation = Investigation.find(params[:investigation_id])
-    authorize @investigation, :show?
-  end
-
-  def set_test
-    @test = @investigation.tests.build(test_params)
-  end
-
-  def set_attachment
-    @file_blob, * = load_file_attachments
-  end
-
-  def update_attachment
-    update_blob_metadata @file_blob, file_metadata
-  end
-
   def store_test
     session[:test] = @test.attributes if @test.valid?(step)
-  end
-
-  def test_valid?
-    @test.validate(step)
-    validate_blob_size(@file_blob, @test.errors, "file")
-    @test.errors.empty?
   end
 
   def test_saved?
@@ -103,41 +81,7 @@ private
     @file_blob.save if @file_blob
   end
 
-  def test_params
-    session_params.merge(request_params)
-  end
-
-  def session_params
+  def test_session_params
     session[:test] || {}
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def request_params
-    return {} if params[:test].blank?
-
-    params.require(:test)
-        .permit(:product_id,
-                :legislation,
-                :result,
-                :details,
-                :day,
-                :month,
-                :year)
-        .merge(type: model_type)
-  end
-
-  def model_type
-    params.dig(:test, :is_result) == "true" ? Test::Result.name : Test::Request.name
-  end
-
-  def file_metadata
-    if @test.requested?
-      title = "Test requested: #{@test.product&.name}"
-      document_type = "test_request"
-    else
-      title = "#{@test.result&.capitalize} test: #{@test.product&.name}"
-      document_type = "test_results"
-    end
-    get_attachment_metadata_params(:file).merge(title: title, document_type: document_type)
   end
 end
