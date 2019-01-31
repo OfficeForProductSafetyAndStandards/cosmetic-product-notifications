93b3c8ab62e39bf885da3d9db14048c10c9a8b11
Raam Chauhan <raam.chauhan@softwire.com>
2019-01-31 14:23:11 +0000

    COSBETA-7: Add responsible person ID to notification table

--- cosmetics-web/db/schema.rb
+++ cosmetics-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_135303) do
+ActiveRecord::Schema.define(version: 2019_01_31_143532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2019_01_28_135303) do
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "responsible_person_id"
+    t.index ["responsible_person_id"], name: "index_notifications_on_responsible_person_id"
   end
 
   create_table "responsible_persons", force: :cascade do |t|
@@ -67,4 +69,5 @@ ActiveRecord::Schema.define(version: 2019_01_28_135303) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "notifications", "responsible_persons"
 end
