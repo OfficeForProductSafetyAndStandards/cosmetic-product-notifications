fb4cf3a91e60f6d05427528f4bffb99a3b880db3
Tom Bell <tom.bell@softwire.com>
2019-02-01 12:31:14 +0000

    COSBETA-36: Add form validation for responsible person details

--- cosmetics-web/app/models/responsible_person.rb
+++ cosmetics-web/app/models/responsible_person.rb
@@ -5,6 +5,17 @@ class ResponsiblePerson < ApplicationRecord
 
   enum account_type: { business: "business", individual: "individual" }
 
+  validates :account_type, presence: true
+  validates :email_address, uniqueness: true
+
+  validates :companies_house_number, presence: true, uniqueness: true, on: :enter_details, if: -> { business? }
+  validates :name, presence: true, on: :enter_details
+  validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, on: :enter_details
+  validates :phone_number, presence: true, on: :enter_details
+  validates :address_line_1, presence: true, on: :enter_details
+  validates :city, presence: true, on: :enter_details
+  validates :postal_code, presence: true, on: :enter_details
+
   def add_team_member(user)
     team_members << TeamMember.create(user: user)
   end
