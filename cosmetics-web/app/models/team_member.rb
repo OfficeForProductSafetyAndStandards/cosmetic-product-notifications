50d6d95ac55ceef27e23a7900a40d29f686c1f84
Tom Bell <tom.bell@softwire.com>
2019-02-01 12:47:01 +0000

    COSBETA-36: Populate team member details on index page

--- cosmetics-web/app/models/team_member.rb
+++ cosmetics-web/app/models/team_member.rb
@@ -1,4 +1,12 @@
 class TeamMember < ApplicationRecord
   belongs_to :responsible_person
   belongs_to :user
+
+  def full_name
+    user&.full_name
+  end
+
+  def email_address
+    user&.email
+  end
 end
