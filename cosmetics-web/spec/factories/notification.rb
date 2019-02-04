1771c963273562f9493593c4b45d96bf2090b802
Tom Bell <tom.bell@softwire.com>
2019-02-04 13:57:16 +0000

    COSBETA-36: Use factory in notification tests

---
+++ cosmetics-web/spec/factories/notification.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :notification do
+
+    factory :draft_notification do
+      state { :draft_complete }
+    end
+  end
+end
--- cosmetics-web/spec/models/notification_spec.rb
+++ cosmetics-web/spec/models/notification_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Notification, type: :model do
   describe "updating product_name" do
     it "transitions state from empty to product_name_added" do
-      notification = Notification.create
+      notification = create(:notification)
 
       notification.product_name = 'Super Shampoo'
       notification.save
@@ -12,7 +12,7 @@ RSpec.describe Notification, type: :model do
     end
 
     it "adds errors if product_name updated to be blank" do
-      notification = Notification.create
+      notification = create(:notification)
 
       notification.product_name = ''
       notification.save
