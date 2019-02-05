1cff97462c2bea62f3cf4d95882f86817f5a207e
kicferk1 <michal.kreft@softwire.com>
2019-02-05 10:35:10 +0000

    MSPSDS-547: fix migration

--- mspsds-web/db/migrate/20190129155220_add_team_assignee_to_investigation.rb
+++ mspsds-web/db/migrate/20190129155220_add_team_assignee_to_investigation.rb
@@ -3,23 +3,18 @@ class AddTeamAssigneeToInvestigation < ActiveRecord::Migration[5.2]
     safety_assured do
       reversible do |dir|
         change_table :investigations do |t|
-          t.uuid :assignable_id
           t.string :assignable_type
+          t.rename :assignee_id, :assignable_id
 
           dir.up do
-            Investigation.all.each do |investigation|
-              investigation.update! assignable_type: investigation.assignee_id.present? ? "User" : ""
-              investigation.update! assignable_id: investigation.assignee_id
-            end
-            t.remove :assignee_id
+            Investigation.where.not(assignable_id: nil).update_all(assignable_type: "User")
+            remove_index :investigations, :assignable_id
             add_index :investigations, %i[assignable_type assignable_id]
           end
 
           dir.down do
-            t.uuid :assignee_id
-            Investigation.all.each do |investigation|
-              investigation.update! assignee_id: investigation.assignable_id
-            end
+            Investigation.where(assignable_type: "Team").update_all(assignee_id: nil)
+            remove_index :investigations, %i[assignable_type assignee_id]
             add_index :investigations, :assignee_id
           end
         end
