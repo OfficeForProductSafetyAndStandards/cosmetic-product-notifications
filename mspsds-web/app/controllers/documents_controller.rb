8ed200130afd281e489f2d0dc96bcb54674efc9e
kicferk1 <michal.kreft@softwire.com>
2019-02-07 14:16:35 +0000

    MSPSDS-967: Fixed bug with no activity created for document actions

--- mspsds-web/app/controllers/documents_controller.rb
+++ mspsds-web/app/controllers/documents_controller.rb
@@ -36,7 +36,7 @@ class DocumentsController < ApplicationController
     return render :edit unless file_valid?
 
     @file.blob.save
-    audit_class::Update.from(@file.blob, @parent, previous_data) if @parent.class == Investigation
+    audit_class::Update.from(@file.blob, @parent, previous_data) if @parent.is_a? Investigation
     redirect_to @parent
   end
 
@@ -45,7 +45,7 @@ class DocumentsController < ApplicationController
   # DELETE /documents/1
   def destroy
     @file.destroy
-    audit_class::Destroy.from(@file.blob, @parent) if @parent.class == Investigation
+    audit_class::Destroy.from(@file.blob, @parent) if @parent.is_a? Investigation
     redirect_to @parent, notice: "File was successfully removed"
   end
 
