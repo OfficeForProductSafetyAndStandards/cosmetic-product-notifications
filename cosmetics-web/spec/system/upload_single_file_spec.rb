24bd4765eeda60c57389a965d4d5565b9c805043
aidayounesi <aida.younesi@gmail.com>
2019-02-11 16:21:45 +0000

    COSBETA-24: Reviews related to tests addressed

--- cosmetics-web/spec/system/upload_single_file_spec.rb
+++ cosmetics-web/spec/system/upload_single_file_spec.rb
@@ -14,26 +14,26 @@ RSpec.describe "Upload a single file", type: :system do
   end
 
   it "enables to upload a file" do
-    visit new_responsible_person_notification_file_path(responsible_person.id)
-    page.attach_file('uploaded_file', Rails.root + 'spec/fixtures/5D8F949A.zip')
+    visit new_responsible_person_notification_file_path(responsible_person)
+    page.attach_file('uploaded_file', Rails.root + 'spec/fixtures/testExportFile.zip')
     click_button "Upload"
 
     expect(page).to have_text("Your cosmetic products")
   end
 
   it "set a notification name in dashboard based on the uploaded file" do
-    visit new_responsible_person_notification_file_path(responsible_person.id)
+    visit new_responsible_person_notification_file_path(responsible_person)
     page.attach_file('uploaded_file',
-                     Rails.root + 'spec/fixtures/5D8F949A.zip')
+                     Rails.root + 'spec/fixtures/testExportFile.zip')
     click_button "Upload"
 
     5.times do
       break if not page.text.include? "Refresh the browser"
 
-      visit responsible_person_notifications_path(responsible_person.id)
+      visit responsible_person_notifications_path(responsible_person)
     end
 
-    page.find_by_id("tab_unfinished").click
+    click_link "tab_unfinished"
     expect(page).to have_text("CTPA moisture conditioner")
   end
 end
