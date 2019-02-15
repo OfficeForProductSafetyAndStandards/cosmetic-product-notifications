f0cc52d6d5b2084c891e1d2ef4da1e0efdcf6474
kicferk1 <michal.kreft@softwire.com>
2019-02-15 17:14:36 +0000

    MSPSDS-877: Fix migration for the last time I hope

--- mspsds-web/db/migrate/20190215121322_fix_incorrect_pretty_ids.rb
+++ mspsds-web/db/migrate/20190215121322_fix_incorrect_pretty_ids.rb
@@ -1,5 +1,5 @@
 class FixIncorrectPrettyIds < ActiveRecord::Migration[5.2]
   def change
-    Investigation.in_batches.each(&:add_pretty_id)
+    Investigation.in_batches { |relation| relation.each(&:add_pretty_id) }
   end
 end
