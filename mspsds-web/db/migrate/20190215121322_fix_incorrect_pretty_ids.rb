0d1c17edae486f8dfd423407876b39f77499e817
kicferk1 <michal.kreft@softwire.com>
2019-02-15 14:53:32 +0000

    MSPSDS-877: Rubocop

--- mspsds-web/db/migrate/20190215121322_fix_incorrect_pretty_ids.rb
+++ mspsds-web/db/migrate/20190215121322_fix_incorrect_pretty_ids.rb
@@ -1,7 +1,5 @@
 class FixIncorrectPrettyIds < ActiveRecord::Migration[5.2]
   def change
-    Investigation.all.each do |investigation|
-      investigation.add_pretty_id
-    end
+    Investigation.all.each(&:add_pretty_id)
   end
 end
