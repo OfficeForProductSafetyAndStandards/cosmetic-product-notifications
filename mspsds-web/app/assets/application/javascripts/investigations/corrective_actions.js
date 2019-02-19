7606e77b60c7fe71ccfddd13c1c142a08bd139c7
kicferk1 <michal.kreft@softwire.com>
2019-02-19 14:36:06 +0000

    MSPSDS-918: Add cross to autocompletes in corrective actions and tests

--- mspsds-web/app/assets/application/javascripts/investigations/corrective_actions.js
+++ mspsds-web/app/assets/application/javascripts/investigations/corrective_actions.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import { simpleAccessibleAutocomplete } from '../autocomplete';
 
 $(document).ready(() => {
-  simpleAccessibleAutocomplete('product-picker', { showAllValues: true });
-  simpleAccessibleAutocomplete('business-picker', { showAllValues: true });
-  simpleAccessibleAutocomplete('legislation-picker', { showAllValues: true });
+  simpleAccessibleAutocomplete('picker-product_id', { showAllValues: true });
+  simpleAccessibleAutocomplete('picker-business_id', { showAllValues: true });
+  simpleAccessibleAutocomplete('picker-legislation', { showAllValues: true });
 });
