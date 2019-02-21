83b436860a1922f14cc3ea77b27e0a3a855fb8fc
Raam Chauhan <raam.chauhan@softwire.com>
2019-02-21 12:02:27 +0000

    COSBETA-30: Fix race condition for RP dashboard page

--- cosmetics-web/app/controllers/responsible_persons/notifications_controller.rb
+++ cosmetics-web/app/controllers/responsible_persons/notifications_controller.rb
@@ -1,10 +1,21 @@
+require 'will_paginate/array'
+
 class ResponsiblePersons::NotificationsController < ApplicationController
   before_action :set_responsible_person
 
   def index
-    @pending_notification_files_count = get_pending_notification_files_count
+    @pending_notification_files_count = 0
+    @erroneous_notification_files = []
 
-    @erroneous_notification_files = get_erroneous_notification_files(10)
+    @responsible_person.notification_files.where(user_id: current_user.id).each do |notification_file|
+      if notification_file.upload_error
+        @erroneous_notification_files << notification_file
+      else
+        @pending_notification_files_count += 1
+      end
+    end
+
+    @erroneous_notification_files = @erroneous_notification_files.paginate(page: params[:errors], per_page: 10)
 
     @unfinished_notifications = get_unfinished_notifications(10)
 
@@ -18,10 +29,6 @@ private
     authorize @responsible_person, :show?
   end
 
-  def get_pending_notification_files_count
-    @responsible_person.notification_files.where(user_id: current_user.id, upload_error: nil).count
-  end
-
   def get_unfinished_notifications(page_size)
     @responsible_person.notifications.where(state: %i[notification_file_imported draft_complete])
         .paginate(page: params[:unfinished], per_page: page_size)
@@ -30,10 +37,5 @@ private
   def get_registered_notifications(page_size)
     @responsible_person.notifications.where(state: :notification_complete)
         .paginate(page: params[:registered], per_page: page_size)
-  end
-
-  def get_erroneous_notification_files(page_size)
-    @responsible_person.notification_files.where(user_id: current_user.id).where.not(upload_error: nil)
-    .paginate(page: params[:errors], per_page: page_size)
   end
 end
