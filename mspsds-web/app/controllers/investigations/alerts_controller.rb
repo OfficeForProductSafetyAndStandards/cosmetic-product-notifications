fc355d4e4a07e6d9257e08e58d14ee9190e374e9
MatthewBurstein <matthew.burstein@softwire.com>
2019-02-21 16:40:09 +0000

    MSPSDS-575 feat: set and store email in AlertsController

--- mspsds-web/app/controllers/investigations/alerts_controller.rb
+++ mspsds-web/app/controllers/investigations/alerts_controller.rb
@@ -1,10 +1,11 @@
 class Investigations::AlertsController < ApplicationController
   include Wicked::Wizard
 
-  steps :about_alerts, :compose, :confirmation
+  steps :about_alerts, :compose, :preview
 
   before_action :set_investigation
-
+  before_action :set_email, only: %i[show update], if: -> { %i[compose preview].include? step }
+  before_action :store_email, only: :update, if: -> { step == :compose }
   def new
     redirect_to wizard_path(steps.first)
   end
@@ -27,5 +28,21 @@ class Investigations::AlertsController < ApplicationController
   def set_investigation
     @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
     authorize @investigation, :show?
+  end
+
+  def set_email
+    @email_subject = compose_step_params[:email_subject] || session[:email_subject]
+    @email_body = compose_step_params[:email_body] || session[:email_body]
+  end
+
+  def store_email
+    session[:email_subject] = @email_subject
+    session[:email_body] = @email_body
+  end
+
+  def compose_step_params
+    return {} unless params.has_key? :alert
+
+    params.require(:alert).permit(:email_content, :email_body)
   end
 end
