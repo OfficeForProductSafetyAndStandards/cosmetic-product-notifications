95348093ce9dde39bd2d7a36eca69153cd3e3f0f
samlionheart <strlionheart@gmail.com>
2019-02-25 16:43:26 +0000

    MSPSDS-938 Fixed contacts and locations, added system tests

--- mspsds-web/app/controllers/investigations/businesses_controller.rb
+++ mspsds-web/app/controllers/investigations/businesses_controller.rb
@@ -10,13 +10,24 @@ class Investigations::BusinessesController < ApplicationController
   validates :name, presence: true
   before_action :set_investigation, only: %i[update new show remove unlink]
   before_action :set_business, only: %i[remove unlink]
-  before_action :set_countries, only: %i[show]
-  before_action :set_business_locally, only: %i[update new show]
+  before_action :set_countries, only: %i[update show]
+  before_action :set_business_location_and_contact, only: %i[update new show]
   before_action :store_business, only: %i[update]
   before_action :set_investigation_business
+  before_action :business_request_params, only: %i[new]
 
   def new
+    clear_session
     redirect_to wizard_path(steps.first)
+  end
+
+  def create
+    if @business.save
+      @investigation.add_business(@business, session[:type])
+      redirect_to_investigation_businesses_tab "Business was successfully created."
+    else
+      render_wizard
+    end
   end
 
   def show
@@ -24,28 +35,16 @@ class Investigations::BusinessesController < ApplicationController
   end
 
   def update
-    if step == :type
-      if business_type_params[:relationship].nil?
-        @business.errors.add(:base, "Please select a business type")
-        return render_wizard
-      elsif business_type_params[:relationship] == "Other" && business_type_params[:relationship_other].blank?
-        @business.errors.add(:base, "Please enter a business type \"Other\"")
-        return render_wizard
+    if business_valid?
+      if step == :type
+        assign_type
+        redirect_to next_wizard_path
+      else
+        create
       end
-      session[:relationship] = business_type_params[:relationship] == "Other" ? business_type_params[:relationship_other] : business_type_params[:relationship]
-      redirect_to next_wizard_path
     else
-      @business = Business.new(business_params)
-      if @business.valid?
-        @business.save
-        @investigation.add_business(@business, session[:relationship])
-        redirect_to_investigation_businesses_tab "Business was successfully created."
-      end
+      render_wizard
     end
-  end
-
-  def set_investigation_business
-    @investigation_business = InvestigationBusiness.new(business_id: params[:id], investigation_id: @investigation.id)
   end
 
   def remove; end
@@ -63,18 +62,62 @@ class Investigations::BusinessesController < ApplicationController
 
 private
 
-  def set_business_locally
-    @business = Business.new
-    @business.locations.build
-    @business.contacts.build
+  def set_investigation_business
+    @investigation_business = InvestigationBusiness.new(business_id: params[:id], investigation_id: @investigation.id)
+  end
+
+  def assign_type
+    session[:type] = business_type_params[:type] == "Other" ? business_type_params[:type_other] : business_type_params[:type]
+  end
+
+  def clear_session
+    session.delete(:business)
+    session.delete(:contact)
+    session.delete(:location)
+  end
+
+  def business_valid?
+    if step == :type
+      if business_type_params[:type].nil?
+        @business.errors.add(:base, "Please select a business type")
+      elsif business_type_params[:type] == "Other" && business_type_params[:type_other].blank?
+        @business.errors.add(:base, "Please enter a business type \"Other\"")
+      end
+    else
+      @business.valid?
+    end
+    @business.errors.empty?
+  end
+
+  def business_request_params
+    return {} if params[:business].blank?
+
+    business_params
+  end
+
+  def business_step_params
+    business_session_params.merge(business_request_params)
+  end
+
+  def business_session_params
+    session[:business] || {}
+  end
+
+  def set_business_location_and_contact
+    @business = Business.new(business_step_params)
+    @business.locations.build unless @business.primary_location
+    @business.contacts.build unless @business.primary_contact
+    defaults_on_primary_location @business
   end
 
   def store_business
     session[:business] = @business.attributes
+    session[:contact] = @business.contacts.first.attributes
+    session[:location] = @business.locations.first.attributes
   end
 
   def business_type_params
-    params.require(:business).permit(:relationship, :relationship_other)
+    params.require(:business).permit(:type, :type_other)
   end
 
   def redirect_to_investigation_businesses_tab(notice)
--- mspsds-web/db/schema.rb
+++ mspsds-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_110940) do
+ActiveRecord::Schema.define(version: 2019_02_12_141002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
