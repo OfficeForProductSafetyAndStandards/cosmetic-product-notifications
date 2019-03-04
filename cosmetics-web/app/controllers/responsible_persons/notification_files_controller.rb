6bb82a961c5695c2b9dc96f5e5d574e2d9e63541
Raam Chauhan <raam.chauhan@softwire.com>
2019-03-04 14:08:00 +0000

    COSBETA-31: Add ability to upload multiple files

--- cosmetics-web/app/controllers/responsible_persons/notification_files_controller.rb
+++ cosmetics-web/app/controllers/responsible_persons/notification_files_controller.rb
@@ -1,6 +1,5 @@
 class ResponsiblePersons::NotificationFilesController < ApplicationController
   before_action :set_responsible_person
-  before_action :set_notification_file
 
   def new; end
 
@@ -9,20 +8,19 @@ class ResponsiblePersons::NotificationFilesController < ApplicationController
   end
 
   def create
-    unless notification_file_params && notification_file_params[:uploaded_file]
-      @notification_file.errors.add :uploaded_file, "No file selected"
-      return render :new
+    uploaded_files_params.each do |uploaded_file|
+      notification_file = NotificationFile.new()
+      notification_file.uploaded_file.attach(uploaded_file)
+      notification_file.name = uploaded_file.original_filename
+      notification_file.responsible_person = @responsible_person
+      notification_file.user = User.current
+
+      unless notification_file.save
+        render :new
+      end
     end
 
-    @notification_file.name = notification_file_params[:uploaded_file].original_filename
-    @notification_file.responsible_person = @responsible_person
-    @notification_file.user = User.current
-
-    if @notification_file.save
-      redirect_to responsible_person_notifications_path(@responsible_person)
-    else
-      render :new
-    end
+    redirect_to responsible_person_notifications_path(@responsible_person)
   end
 
   def destroy
@@ -38,19 +36,15 @@ class ResponsiblePersons::NotificationFilesController < ApplicationController
 private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_notification_file
-    @notification_file = NotificationFile.new(notification_file_params)
-  end
-
   def set_responsible_person
     @responsible_person = ResponsiblePerson.find(params[:responsible_person_id])
     authorize @responsible_person, :show?
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
-  def notification_file_params
-    if params.has_key?(:notification_file)
-      params.require(:notification_file).permit(:name, :uploaded_file)
+  def uploaded_files_params
+    if params.has_key?(:uploaded_files)
+      params.require(:uploaded_files)
     end
   end
 end
