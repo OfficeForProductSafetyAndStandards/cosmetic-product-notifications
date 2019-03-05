10e3e3a178928701021064560ee9efc6a2d37e94
kicferk1 <michal.kreft@softwire.com>
2019-03-05 12:47:16 +0000

    COSBETA-33: Cut out unnecessary bits from searchable

--- cosmetics-web/app/models/concerns/searchable.rb
+++ cosmetics-web/app/models/concerns/searchable.rb
@@ -76,43 +76,6 @@ module Searchable
       __elasticsearch__.refresh_index! if Rails.env.development? || Rails.env.test?
       __elasticsearch__.search(query)
     end
-
-    # "prefix" may be changed to a more appropriate query. For alternatives see:
-    # https://www.elastic.co/guide/en/elasticsearch/reference/current/term-level-queries.html
-    def self.prefix_search(params, field)
-      query = {}
-      if params[:query].present?
-        query[:query] = {
-          prefix: {
-            "#{field}": params[:query]
-          }
-        }
-      end
-      query[:sort] = sort_params(params) if params[:sort].present?
-
-      __elasticsearch__.search(query)
-    end
-
-    def self.sort_params(params)
-      sort_field = params[:sort] + ".sort"
-      [{ "#{sort_field}": { order: params[:direction] } }]
-    end
-
-    def self.highlighted_fields
-      # To be overwritten by the model using it, defaults to all sent fields
-      %w[*]
-    end
-
-    def self.fuzzy_fields
-      # To be overwritten by the model using it, defaults to all fields
-      []
-    end
-
-    def self.exact_fields
-      # To be overwritten by the model using it, defaults to all fields
-      # Bear in mind that if you have a field both here and in fuzzy_fields, your result will just be fuzzy
-      []
-    end
   end
 end
 # rubocop:enable Metrics/BlockLength
