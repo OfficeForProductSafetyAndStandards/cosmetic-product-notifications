afcb035c62d1f195ebb7a8e15ad55b25bfe62363
David Rendell <david.rendell@softwire.com>
2019-03-08 09:45:59 +0000

    COSBETA-39 Update failing test

--- cosmetics-web/spec/mailers/notify_mailer_spec.rb
+++ cosmetics-web/spec/mailers/notify_mailer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe NotifyMailer, type: :mailer do
 
   describe "send_responsible_person_verification_email" do
     it "sends new email verification key to responsible person" do
-      mail = NotifyMailer.send_responsible_person_verification_email(responsible_person, user_name)
+      mail = NotifyMailer.send_responsible_person_verification_email(responsible_person.id, responsible_person.email_address, user_name)
       expect(mail.to).to eq([responsible_person.email_address])
       expect(responsible_person.reload.email_verification_keys.size).to eq(1)
     end
