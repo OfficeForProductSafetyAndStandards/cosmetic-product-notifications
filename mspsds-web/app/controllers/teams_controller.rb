ef1bd88c1658b04f217c40ac24141d0a0cc6071b
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-03-15 11:15:37 +0000

    MSPSDS-1047 Finalize messages and email content

--- mspsds-web/app/controllers/teams_controller.rb
+++ mspsds-web/app/controllers/teams_controller.rb
@@ -13,20 +13,21 @@ class TeamsController < ApplicationController
     if request.put? && @new_user.valid?
       existing_user = User.find_by email: @new_user.email_address
       if existing_user
-        if existing_user.organisation == @team.organisation
+        if existing_user.organisation.nil? || existing_user.organisation == @team.organisation
           if @team.users.include? existing_user
             @new_user.errors.add(:email_address, "#{@new_user.email_address.capitalize} is already a member of #{@team.display_name}")
           else
             @team.add_user existing_user
             email = NotifyMailer.user_added_to_team existing_user.email,
-                                            name: existing_user.full_name,
-                                            team_id: @team.id,
-                                            team_name: @team.name
+                                                    name: existing_user.full_name,
+                                                    team_page_url: team_url(@team),
+                                                    team_name: @team.name,
+                                                    service_name: "Product safety database",
+                                                    inviting_team_member_name: User.current.full_name
             email.deliver_later
           end
         else
-          # TODO MSPSDS-1047 Raise better error
-          @new_user.errors.add(:email_address, "#{@new_user.email_address.capitalize} does not belong to this organisation and connot be added to team #{@team.display_name}.")
+          @new_user.errors.add(:email_address, :member_of_another_organisation)
         end
       else
         user = User.create_new @new_user.email_address
@@ -34,8 +35,8 @@ class TeamsController < ApplicationController
         Shared::Web::KeycloakClient.instance.send_required_actions_welcome_email user.id, root_url
       end
 
-      # TODO MSPSDS-1047 Show success message
       if @new_user.errors.empty?
+        flash[:notice] = "Invite sent to #{@new_user.email_address}"
         redirect_to @team, status: :see_other
       else
         render :invite_to, status: :bad_request
