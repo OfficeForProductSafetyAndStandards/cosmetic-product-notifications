fffa2a72f412c7b86538652bdedb792da2fffbc1
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-03-15 14:04:34 +0000

    MSPSDS-1047 Refactor `invite_to` action

--- mspsds-web/app/controllers/teams_controller.rb
+++ mspsds-web/app/controllers/teams_controller.rb
@@ -10,37 +10,20 @@ class TeamsController < ApplicationController
 
   # GET /teams/:id/invite, PUT /teams/:id/invite
   def invite_to
-    if request.put? && @new_user.valid?
-      existing_user = User.find_by email: @new_user.email_address
-      if existing_user
-        if existing_user.organisation.nil? || existing_user.organisation == @team.organisation
-          if @team.users.include? existing_user
-            @new_user.errors.add(:email_address, "#{@new_user.email_address.capitalize} is already a member of #{@team.display_name}")
-          else
-            @team.add_user existing_user
-            email = NotifyMailer.user_added_to_team existing_user.email,
-                                                    name: existing_user.full_name,
-                                                    team_page_url: team_url(@team),
-                                                    team_name: @team.name,
-                                                    service_name: "Product safety database",
-                                                    inviting_team_member_name: User.current.full_name
-            email.deliver_later
-          end
-        else
-          @new_user.errors.add(:email_address, :member_of_another_organisation)
-        end
-      else
-        user = User.create_new @new_user.email_address
-        @team.add_user user
-        Shared::Web::KeycloakClient.instance.send_required_actions_welcome_email user.id, root_url
-      end
+    return unless request.put? && @new_user.valid?
 
-      if @new_user.errors.empty?
-        flash[:notice] = "Invite sent to #{@new_user.email_address}"
-        redirect_to @team, status: :see_other
-      else
-        render :invite_to, status: :bad_request
-      end
+    existing_user = User.find_by email: @new_user.email_address
+    if existing_user
+      invite_existing_user_if_able existing_user
+    else
+      create_and_invite_new_user
+    end
+
+    if @new_user.errors.empty?
+      flash[:notice] = "Invite sent to #{@new_user.email_address}"
+      redirect_to @team, status: :see_other
+    else
+      render :invite_to, status: :bad_request
     end
   end
 
@@ -57,5 +40,35 @@ private
 
   def set_new_user
     @new_user = NewUser.new params[:new_user]&.permit(:email_address)
+  end
+
+  def invite_existing_user_if_able(user)
+    if user.organisation.present? && user.organisation != @team.organisation
+      @new_user.errors.add(:email_address, :member_of_another_organisation)
+      return
+    end
+    if @team.users.include? user
+      @new_user.errors.add(:email_address,
+                           "#{@new_user.email_address.capitalize} is already a member of #{@team.display_name}")
+      return
+    end
+    invite_user user
+  end
+
+  def invite_user(user)
+    @team.add_user user
+    email = NotifyMailer.user_added_to_team user.email,
+                                            name: user.full_name,
+                                            team_page_url: team_url(@team),
+                                            team_name: @team.name,
+                                            service_name: "Product safety database",
+                                            inviting_team_member_name: User.current.full_name
+    email.deliver_later
+  end
+
+  def create_and_invite_new_user
+    user = User.create_new @new_user.email_address
+    @team.add_user user
+    Shared::Web::KeycloakClient.instance.send_required_actions_welcome_email user.id, root_url
   end
 end
