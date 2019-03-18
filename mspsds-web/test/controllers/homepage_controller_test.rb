7de9e77a336208f43288a1f145432a7f5da56b8b
MatthewBurstein <matthew.burstein@softwire.com>
2019-03-18 15:13:16 +0000

    MSPSDS-1094 test: fix homepage and keycloak tests to accommodate introduction

--- mspsds-web/test/controllers/homepage_controller_test.rb
+++ mspsds-web/test/controllers/homepage_controller_test.rb
@@ -2,16 +2,24 @@ require "test_helper"
 
 class HomepageControllerTest < ActionDispatch::IntegrationTest
   setup do
-    mock_out_keycloak_and_notify
+    @user = mock_out_keycloak_and_notify
   end
 
   teardown do
     reset_keycloak_and_notify_mocks
   end
 
-  test "displays homepage for non_opss users" do
-    set_user_as_non_opss(User.current)
+  test "displays introduction for non_opss users who have not viewed introduction before" do
+    set_user_as_non_opss(@user)
     get "/"
+    assert_redirected_to new_introduction_path
+  end
+
+  test "displays homepage for non_opss users who have viewed introduction before" do
+    set_user_as_non_opss(@user)
+    allow(@user).to receive(:has_viewed_introduction).and_return true
+
+    get '/'
     assert_response :success
   end
 
