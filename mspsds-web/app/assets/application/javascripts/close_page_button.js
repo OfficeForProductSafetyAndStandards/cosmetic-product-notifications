149e7e4fe6ca833cbd956228e96aa1276a5e513f
kicferk1 <michal.kreft@softwire.com>
2019-03-19 10:36:57 +0000

    MSPSDS-1103: Use opener instead of param to check if we should display a button

--- mspsds-web/app/assets/application/javascripts/close_page_button.js
+++ mspsds-web/app/assets/application/javascripts/close_page_button.js
@@ -3,7 +3,9 @@ import $ from 'jquery';
 function closePageOnClick(id) {
   $(document).ready(() => {
     const element = document.getElementById(id);
-    if(element){
+    const isJSWindow = parent.window.opener != null && !parent.window.opener.closed;
+    console.log(window.opener, parent.window.opener);
+    if(isJSWindow && element){
       element.style.display = "block";
       element.addEventListener('click', () => {
         window.close();
--- mspsds-web/app/controllers/help_controller.rb
+++ mspsds-web/app/controllers/help_controller.rb
@@ -1,16 +1,9 @@
 class HelpController < ApplicationController
   skip_before_action :authenticate_user!, :authorize_user
-  before_action :set_referred
 
   def terms_and_conditions; end
 
   def privacy_policy; end
 
   def about; end
-
-private
-
-  def set_referred
-    @referred = params[:referred]
-  end
 end
