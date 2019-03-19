15630e2d5f81b96a6c21e31d52c0e6f111fef91e
samlionheart <strlionheart@gmail.com>
2019-03-19 13:24:18 +0000

    MSPSDS-1086 Adding item count to tabs model, adding count to products and businesses as well

--- mspsds-web/app/presenters/tab.rb
+++ mspsds-web/app/presenters/tab.rb
@@ -1,10 +1,11 @@
 class Tab
-  attr_reader :id, :title, :render, :hide_title
+  attr_reader :id, :title, :render, :hide_title, :item_count
 
-  def initialize(id, title, render, hide_title = false)
+  def initialize(id, title, render, item_count = '', hide_title = false)
     @id = id
     @title = title
     @render = render
     @hide_title = hide_title
+    @item_count = item_count
   end
 end
