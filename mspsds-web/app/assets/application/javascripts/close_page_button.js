13887f666f7dc581ca004094e19645d693a91607
kicferk1 <michal.kreft@softwire.com>
2019-03-19 13:44:42 +0000

    MSPSDS-1103: Fix linters and add govuk styling to headers

--- mspsds-web/app/assets/application/javascripts/close_page_button.js
+++ mspsds-web/app/assets/application/javascripts/close_page_button.js
@@ -3,10 +3,9 @@ import $ from 'jquery';
 function closePageOnClick(id) {
   $(document).ready(() => {
     const element = document.getElementById(id);
-    const isJSWindow = parent.window.opener != null && !parent.window.opener.closed;
-    console.log(window.opener, parent.window.opener);
-    if(isJSWindow && element){
-      element.style.display = "block";
+    const isJSWindow = window.opener;
+    if (isJSWindow && element) {
+      element.style.display = 'block';
       element.addEventListener('click', () => {
         window.close();
       });
