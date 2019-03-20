d75e38d124a63df665bf2072a01ad36ac03898ef
kicferk1 <michal.kreft@softwire.com>
2019-03-20 11:26:28 +0000

    MSPSDS-997: Refactor editing case further to inorporate assigning

--- mspsds-web/app/controllers/investigations_controller.rb
+++ mspsds-web/app/controllers/investigations_controller.rb
@@ -60,59 +60,58 @@ class InvestigationsController < ApplicationController
   # GET /cases/1/assign
   # PUT /cases/1/assign
   def assign
-    return if request.get?
-
-    ps = assignee_update_params
-
-    potential_assignees = User.where(id: ps[:assignable_id]) + Team.where(id: ps[:assignable_id])
-    if potential_assignees.empty?
-      @investigation.errors.add(:assignable_id, :invalid, message: "Select assignee")
-      respond_to_invalid_data(:assign)
-      return
+    get_valid_update_hash = Proc.new do |ps|
+      potential_assignees = User.where(id: ps[:assignable_id]) + Team.where(id: ps[:assignable_id])
+      potential_assignees.first.present? ? { assignee: potential_assignees.first } : {}
     end
-    @investigation.assignee = potential_assignees.first
-    respond_to_update(:assign)
+    edit(model_keys: %i[assignable_id assignable_type], get_valid_update_hash: get_valid_update_hash,
+         custom_params: assignee_update_params,
+         error_message: "Select assignee")
   end
 
   # GET /cases/1/status
   # PUT /cases/1/status
   def status
-    edit(model_keys: %i[is_closed status_rationale], action_key: :status,
+    get_valid_update_hash = Proc.new { |ps| ps if ps[:is_closed].present? }
+    edit(model_keys: %i[is_closed status_rationale], get_valid_update_hash: get_valid_update_hash,
          error_message: "Status should be closed or open")
   end
 
   # GET /cases/1/visibility
   # PUT /cases/1/visibility
   def visibility
-    edit(model_keys: %i[is_private visibility_rationale], action_key: :visibility,
+    get_valid_update_hash = Proc.new { |ps| ps if ps[:is_private].present? }
+    edit(model_keys: %i[is_private visibility_rationale], get_valid_update_hash: get_valid_update_hash,
          error_message: "Visibility needs to be private or public")
   end
 
   # GET /cases/1/edit_summary
   # PUT /cases/1/edit_summary
   def edit_summary
-    edit(model_keys: [:description], action_key: :edit_summary, error_message: "Summary can not be empty")
+    get_valid_update_hash = Proc.new { |ps| ps if ps[:description].present? }
+    edit(model_keys: [:description], get_valid_update_hash: get_valid_update_hash,
+         error_message: "Summary can not be empty")
   end
 
 private
 
-  def edit(model_keys:, action_key:, error_message:)
+  def edit(model_keys:, error_message:, get_valid_update_hash:, custom_params: nil)
     return if request.get?
 
-    ps = params.require(:investigation).permit(model_keys)
-    important_model_key = model_keys.first
+    ps = custom_params || params.require(:investigation).permit(model_keys)
+    update_hash = get_valid_update_hash.call(ps)
 
-    if ps[important_model_key].blank?
-      @investigation.errors.add(important_model_key, :invalid, message: error_message)
-      respond_to_invalid_data(action_key)
+    if update_hash.blank?
+      @investigation.errors.add(model_keys.first, :invalid, message: error_message)
+      respond_to_invalid_data
       return
     end
 
-    model_keys.each do |model_key|
-      @investigation.send("#{model_key}=", ps[model_key]) if ps[model_key]
+    update_hash.each do |key, value|
+      @investigation.send("#{key}=", value) if value
     end
 
-    respond_to_update(action_key)
+    respond_to_update
   end
 
   def set_investigation_with_associations
@@ -129,24 +128,9 @@ private
     authorize @investigation
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def investigation_create_params
-    params.require(:investigation).permit(:description)
-  end
-
-  def status_update_params
-    params.require(:investigation).permit(:is_closed, :status_rationale)
-  end
-
-  def visibility_update_params
-    params.require(:investigation).permit(:is_private, :visibility_rationale)
-  end
-
-  def edit_summary_update_params
-    params.require(:investigation).permit(:description)
-  end
-
   def assignee_update_params
+    return {} if params[:investigation].blank?
+
     params[:investigation][:assignable_id] = case params[:investigation][:assignable_id]
                                              when "someone_in_your_team"
                                                params[:investigation][:select_team_member]
@@ -162,22 +146,22 @@ private
     params.require(:investigation).permit(:assignable_id)
   end
 
-  def respond_to_update(origin)
+  def respond_to_update
     respond_to do |format|
       if @investigation.save
         format.html { redirect_to @investigation, notice: "#{@investigation.case_type.titleize} was successfully updated." }
         format.json { render :show, status: :ok, location: @investigation }
       else
         @investigation.restore_attributes
-        format.html { render origin }
+        format.html { render action_name }
         format.json { render json: @investigation.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  def respond_to_invalid_data(origin)
+  def respond_to_invalid_data
     respond_to do |format|
-      format.html { render origin }
+      format.html { render action_name }
       format.json { render json: @investigation.errors, status: :unprocessable_entity }
     end
   end
