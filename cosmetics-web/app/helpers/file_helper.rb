1c309bf1f7751174b17b8e839e364dcde5ddded1
Tiago Susano Pinto <tiago.pinto@softwire.com>
2019-03-25 16:42:02 +0000

    COSBETA-194: Apply markups Extracting filetype's extensions to an helper

---
+++ cosmetics-web/app/helpers/file_helper.rb
@@ -0,0 +1,16 @@
+module FileHelper
+
+  def get_filetype_extension filetype
+    filetype_extensions[filetype]
+  end
+
+private
+
+  def filetype_extensions
+    {
+        "application/pdf" => ".pdf",
+        "application/rtf" => ".rtf",
+        "text/plain" => ".txt"
+    }
+  end
+end
