060e6cb1292b9acbf79543ee3315b62145dc1680
kicferk1 <michal.kreft@softwire.com>
2019-03-26 09:35:54 +0000

    MSPSDS-NONE: Make sure teams and organisations are loaded before populating them

--- mspsds-web/app/models/user.rb
+++ mspsds-web/app/models/user.rb
@@ -38,9 +38,12 @@ class User < Shared::Web::User
   def self.all(options = {})
     begin
       all_users = Shared::Web::KeycloakClient.instance.all_users(force: options[:force])
+      # Teams and organisations need to be loaded before populating organisation on users
+      # If they are already loaded, the results are probably cached so it's not too expensive
+      Team.all
+      Organisation.all
       self.data = all_users.map { |user| populate_organisation(user) }
                       .reject { |user| user[:organisation_id].blank? }
-      Team.all
       TeamUser.all(force: options[:force])
     rescue StandardError => error
       Rails.logger.error "Failed to fetch users from Keycloak: #{error.message}"
