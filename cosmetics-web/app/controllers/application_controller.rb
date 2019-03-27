6b5e946ef526716ec892b757f9d6447a1677c270
Tom Bell <tom.bell@softwire.com>
2019-03-27 07:56:14 +0000

    COSBETA-190: Update account wizard journey for creating or joining a RP

--- cosmetics-web/app/controllers/application_controller.rb
+++ cosmetics-web/app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ private
     return unless user_signed_in? && !poison_centre_or_msa_user?
 
     if User.current.responsible_persons.empty?
-      redirect_to create_or_join_existing_account_index_path
+      redirect_to account_path(:overview)
     elsif User.current.responsible_persons.none?(&:is_email_verified)
       responsible_person = User.current.responsible_persons.first
       redirect_to responsible_person_email_verification_keys_path(responsible_person)
