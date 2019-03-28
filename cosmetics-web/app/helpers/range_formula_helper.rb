e10885ada9fb9163f436bf011d307af3e7855660
Raam Chauhan <raam.chauhan@softwire.com>
2019-03-28 11:01:42 +0000

    COSBETA-34: Review markups

--- cosmetics-web/app/helpers/range_formula_helper.rb
+++ cosmetics-web/app/helpers/range_formula_helper.rb
@@ -1,5 +1,5 @@
 module RangeFormulaHelper
   def format_range_formulas(range_formulas)
-    range_formulas.collect { |formula| { inci_name: formula[:inci_name], range: get_unit_name(formula[:range]) } }
+    range_formulas.collect { |formula| { inci_name: formula[:inci_name], range: "#{get_unit_name(formula[:range])} %w/w" } }
   end
 end
