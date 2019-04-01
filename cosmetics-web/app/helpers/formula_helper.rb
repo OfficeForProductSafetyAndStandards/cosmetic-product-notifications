db4aa7e82e09f1f2ace93528761e1d5bcd4ed9bc
Raam Chauhan <raam.chauhan@softwire.com>
2019-04-01 16:22:51 +0100

    COSBETA-215: Format exact formulas

---
+++ cosmetics-web/app/helpers/formula_helper.rb
@@ -0,0 +1,9 @@
+module FormulaHelper
+  def format_exact_formulas(exact_formulas)
+    exact_formulas.collect { |formula| { inci_name: formula[:inci_name], quantity: display_concentration(formula[:quantity]) } }
+  end
+
+  def format_range_formulas(range_formulas)
+    range_formulas.collect { |formula| { inci_name: formula[:inci_name], range: "#{get_unit_name(formula[:range])} %w/w" } }
+  end
+end
