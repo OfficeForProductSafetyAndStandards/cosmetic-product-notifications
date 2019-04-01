93363c95e71581be32dcc467fb898cfd27775daf
kicferk1 <michal.kreft@softwire.com>
2019-04-01 10:08:55 +0100

    PSD-976: Add up direction to migration

--- psd-web/db/migrate/20190329153321_add_created_by_to_file_blob.rb
+++ psd-web/db/migrate/20190329153321_add_created_by_to_file_blob.rb
@@ -2,7 +2,12 @@ class AddCreatedByToFileBlob < ActiveRecord::Migration[5.2]
   def change
     reversible do |dir|
       dir.up do
-
+        ActiveStorage::Blob.all.each do |blob|
+          metadata = blob.metadata
+          first_activity = blob.attachments.where(record_type: "Activity").order(:created_at).first.record
+          blob.metadata.update(metadata.merge(created_by: first_activity&.source&.user_id))
+          blob.save
+        end
       end
 
       dir.down do
