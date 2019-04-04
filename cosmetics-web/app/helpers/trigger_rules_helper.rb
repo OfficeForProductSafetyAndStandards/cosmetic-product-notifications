5a1c6d39214443283ab3539cb3b81d212e09f4a5
Raam Chauhan <raam.chauhan@softwire.com>
2019-04-04 14:52:53 +0100

    COSBETA-216: Update trigger rule layout for business view

--- cosmetics-web/app/helpers/trigger_rules_helper.rb
+++ cosmetics-web/app/helpers/trigger_rules_helper.rb
@@ -19,4 +19,29 @@ module TriggerRulesHelper
       inciname_incivalue_pair << question_element.answer
     end
   end
+
+  def format_trigger_question_elements(trigger_question, trigger_question_elements)
+    trigger_question_list = []
+    trigger_question_elements.where(element: "inciname").each do |element|
+      inciname_incivalue_pair = get_inciname_incivalue_pair(trigger_question, element)
+      trigger_question_list << {
+          inci_name: inciname_incivalue_pair.first.answer,
+          quantity: display_concentration(inciname_incivalue_pair.last.answer)
+      }
+    end
+    trigger_question_list
+  end
+
+  def format_trigger_question_answers(answer)
+    case answer
+    when "NA"
+      "None"
+    when "N"
+      "No"
+    when "Y"
+      "Yes"
+    else
+      answer
+    end
+  end
 end
