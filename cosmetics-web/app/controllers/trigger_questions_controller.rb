6ae434ebfce15cb3aa60d55373bbc9d61ebc9a4f
Tiago Susano Pinto <tiago.pinto@softwire.com>
2019-04-10 13:42:22 +0100

    COSBETA-254: Create trigger rules questions templates * Substance check * Substance check with condition * Substance list * Substance text input Added also at least an example of each

--- cosmetics-web/app/controllers/trigger_questions_controller.rb
+++ cosmetics-web/app/controllers/trigger_questions_controller.rb
@@ -3,8 +3,10 @@ class TriggerQuestionsController < ApplicationController
   include CpnpNotificationTriggerRules
   include TriggerRulesHelper
 
-  steps :contains_anti_dandruff_agents,
-        :add_anti_dandruff_agents
+  steps :contains_anti_dandruff_agents, :add_anti_dandruff_agents,
+        :select_ph_range, :exact_ph, :add_alkaline_agents,
+        :ph_mixed_hair_dye,
+        :contains_ethanol
 
   before_action :set_component
   before_action :set_question
@@ -17,9 +19,19 @@ class TriggerQuestionsController < ApplicationController
   def update
     case step
     when :contains_anti_dandruff_agents
-      render_contains_anti_dandruff_agents
+      render_substance_check
     when :add_anti_dandruff_agents
-      render_add_anti_dandruff_agents
+      render_substance_list
+    when :select_ph_range
+      render_select_ph_range
+    when :exact_ph
+      render_exact_ph
+    when :add_alkaline_agents
+      render_substance_list
+    when :ph_mixed_hair_dye
+      render_substance_check_with_condition
+    when :contains_ethanol
+      render_substance_check_with_condition
     else
       redirect_to finish_wizard_path
     end
@@ -46,7 +58,7 @@ private
   end
 
   def set_question
-    question = :please_specify_the_inci_name_and_concentration_of_the_antidandruff_agents_if_antidandruff_agents_are_not_present_in_the_cosmetic_product_then_not_applicable_must_be_checked
+    question = get_question_for_step step
     @question = TriggerQuestion.find_or_create_by(component: @component, question: question)
   end
 
@@ -54,10 +66,58 @@ private
     case step
     when :add_anti_dandruff_agents
       populate_answers_for_list
+    when :select_ph_range
+      TriggerQuestion.find_or_create_by(component: @component, question: :please_indicate_the_ph)
+      TriggerQuestion.find_or_create_by(component: @component, question: :please_indicate_the_inci_name_and_concentration_of_each_alkaline_agent_including_ammonium_hydroxide_liberators)
+    when :exact_ph
+      populate_question_with_single_answer :ph
+    when :add_alkaline_agents
+      populate_answers_for_list
+    when :ph_mixed_hair_dye
+      populate_question_with_single_answer :ph
+    when :contains_ethanol
+      populate_question_with_single_answer :ethanol
     end
   end
 
-  def render_contains_anti_dandruff_agents
+  def render_select_ph_range
+    selected_value = params[:range]
+    exact_ph_question = @component.trigger_questions.where(question: get_question_for_step(:exact_ph)).first
+    alkaline_list_question = @component.trigger_questions.where(question: get_question_for_step(:add_alkaline_agents)).first
+
+    case selected_value
+    when "below"
+      exact_ph_question.update(applicable: true)
+      alkaline_list_question.update(applicable: false)
+      alkaline_list_question.trigger_question_elements.destroy_all
+      render_wizard @component
+    when "between"
+      exact_ph_question.update(applicable: false)
+      exact_ph_question.trigger_question_elements.destroy_all
+      alkaline_list_question.update(applicable: false)
+      alkaline_list_question.trigger_question_elements.destroy_all
+      skip_question
+    when "above"
+      exact_ph_question.update(applicable: true)
+      alkaline_list_question.update(applicable: true)
+      render_wizard @component
+    else
+      @errors = [{ text: "Select an option", href: "#trigger_question_applicable_true" }]
+      re_render_step
+    end
+  end
+
+  def render_exact_ph
+    @question.update(question_params)
+
+    if @component.trigger_questions.where(question: get_question_for_step(:add_alkaline_agents), applicable: true).any?
+      render_wizard @component
+    else
+      skip_question
+    end
+  end
+
+  def render_substance_check
     @question.update(question_params)
     if @question.applicable.nil?
       @errors = [{ text: "Select an option", href: "#trigger_question_applicable_true" }]
@@ -72,24 +132,62 @@ private
     end
   end
 
-  def render_add_anti_dandruff_agents
+  def render_substance_list
     @question.update(question_params)
     destroy_invalid_answers
 
     if @question.trigger_question_elements.empty?
-      @errors = [{
-                     text: "No substance added",
-                     href: "#trigger_question_trigger_question_elements_attributes_0_answer"
-                 }]
+      define_errors_for_answers "No substance added"
+      return re_render_step
     end
 
-    re_render_step
+    @question.update(applicable: true) # ensuring that question is applicable if elements are added to it
+    render_wizard @component
+  end
+
+  def render_substance_text_input
+    @question.update(question_params)
+
+    if @question.trigger_question_elements.first.answer.blank?
+      define_errors_for_answers "No value added"
+      return re_render_step
+    end
+
+    render_wizard @component
+  end
+
+  def render_substance_check_with_condition
+    @question.update(question_params)
+
+    return re_render_step unless @question.valid?
+
+    if @question.applicable.nil?
+      @errors = [{ text: "Select an option", href: "#trigger_question_applicable_true" }]
+      return re_render_step
+    end
+    @question.trigger_question_elements.destroy_all unless @question.applicable
+
+    if @question.applicable && @question.trigger_question_elements.first.answer.blank?
+      define_errors_for_answers "No value added"
+      return re_render_step
+    end
+
+    render_wizard @component
   end
 
   def skip_question
+    next_step = get_skip_question_next_step
+    redirect_to wizard_path(next_step, component_id: @component.id)
+  end
+
+  def get_skip_question_next_step
     case step
     when :contains_anti_dandruff_agents
-      redirect_to finish_wizard_path
+      :select_ph_range
+    when :select_ph_range
+      :ph_mixed_hair_dye
+    when :exact_ph
+      :ph_mixed_hair_dye
     end
   end
 
@@ -103,6 +201,15 @@ private
     end
   end
 
+  def populate_question_with_single_answer(element)
+    @question.trigger_question_elements.destroy_all if @question.trigger_question_elements.count > 1
+    trigger_question = TriggerQuestionElement.find_or_create_by(trigger_question: @question)
+    trigger_question.update_attribute(:answer_order, 0)
+    trigger_question.update_attribute(:element_order, 0)
+    trigger_question.update_attribute(:element, element)
+    @question.reload
+  end
+
   def destroy_invalid_answers
     grouped_answers = @question.trigger_question_elements.group_by(&:answer_order)
     updated_answer_order = 0
@@ -110,7 +217,7 @@ private
       if answers.any? { |answer| answer.answer.blank? }
         TriggerQuestionElement.delete(answers)
       else
-        answers.each { |answer| answer.update(answer_order: updated_answer_order) }
+        answers.each { |answer| answer.update_attribute(:answer_order, updated_answer_order) }
         updated_answer_order += 1
       end
     end
@@ -120,6 +227,27 @@ private
   def re_render_step
     initialize_step
     render step
+  end
+
+  def define_errors_for_answers(text)
+    @errors = [{ text: text, href: "#trigger_question_trigger_question_elements_attributes_0_answer" }]
+  end
+
+  def get_question_for_step(step)
+    case step
+    when :contains_anti_dandruff_agents
+      :please_specify_the_inci_name_and_concentration_of_the_antidandruff_agents_if_antidandruff_agents_are_not_present_in_the_cosmetic_product_then_not_applicable_must_be_checked
+    when :add_anti_dandruff_agents
+      :please_specify_the_inci_name_and_concentration_of_the_antidandruff_agents_if_antidandruff_agents_are_not_present_in_the_cosmetic_product_then_not_applicable_must_be_checked
+    when :exact_ph
+      :please_indicate_the_ph
+    when :add_alkaline_agents
+      :please_indicate_the_inci_name_and_concentration_of_each_alkaline_agent_including_ammonium_hydroxide_liberators
+    when :ph_mixed_hair_dye
+      :please_indicate_the_ph_of_the_mixed_hair_dye_product
+    when :contains_ethanol
+      :please_specify_the_percentage_weight_of_ethanol
+    end
   end
 
   def question_params
---
+++ cosmetics-web/spec/controllers/trigger_questions_controller_spec.rb
@@ -0,0 +1,198 @@
+require 'rails_helper'
+
+RSpec.describe TriggerQuestionsController, type: :controller do
+  let(:responsible_person) { create(:responsible_person) }
+  let(:notification) { create(:notification, components: [create(:component)], responsible_person: responsible_person) }
+  let(:multi_component_notification) { create(:notification, components: [create(:component), create(:component)], responsible_person: responsible_person) }
+  let(:component) { notification.components.first }
+  let(:multi_component) { multi_component_notification.components.first }
+
+  let(:params) {
+    {
+        responsible_person_id: responsible_person.id,
+        notification_reference_number: notification.reference_number,
+        component_id: component.id
+    }
+  }
+
+  let(:multi_component_params) {
+    {
+        responsible_person_id: responsible_person.id,
+        notification_reference_number: notification.reference_number,
+        component_id: multi_component.id
+    }
+  }
+
+  before do
+    sign_in_as_member_of_responsible_person(responsible_person)
+  end
+
+  after do
+    sign_out
+  end
+
+  describe "GET #new" do
+    it "redirects to the first step of the wizard" do
+      get(:new, params: params)
+      expect(response).to redirect_to(responsible_person_notification_component_trigger_question_path(responsible_person, notification, component, :contains_anti_dandruff_agents))
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the correct component" do
+      get(:show, params: params.merge(id: :contains_anti_dandruff_agents))
+      expect(assigns(:component)).to eq(component)
+    end
+
+    it "renders the step template" do
+      get(:show, params: params.merge(id: :contains_anti_dandruff_agents))
+      expect(response).to render_template(:contains_anti_dandruff_agents)
+    end
+
+    it "redirects to the add product image page on finish, when the notification contains only one component" do
+      get(:show, params: params.merge(id: :wicked_finish))
+      expect(response).to redirect_to(responsible_person_notification_build_path(responsible_person, notification, :add_product_image))
+    end
+
+    it "redirects to the add new component page on finish, when the notification contains only one component" do
+      get(:show, params: multi_component_params.merge(id: :wicked_finish))
+      expect(response).to redirect_to(responsible_person_notification_build_path(responsible_person, multi_component_notification, :add_new_component))
+    end
+
+    it "initialises question with the right value in contains_anti_dandruff_agents step" do
+      get(:show, params: params.merge(id: :contains_anti_dandruff_agents))
+      expect(assigns(:question).question).to eq("please_specify_the_inci_name_and_concentration_of_the_antidandruff_agents_if_antidandruff_agents_are_not_present_in_the_cosmetic_product_then_not_applicable_must_be_checked")
+      expect(assigns(:question).applicable).to eq(nil)
+    end
+
+    it "initialises 20 empty answers in add_anti_dandruff_agents step" do
+      get(:show, params: params.merge(id: :add_anti_dandruff_agents))
+      expect(assigns(:question).trigger_question_elements).to have(20).items
+      expect(assigns(:question).trigger_question_elements).to all(have_attributes(answer: be_nil))
+    end
+  end
+
+  describe "POST #update" do
+    it "assigns the applicable to true if selected as true in a check question" do
+      post(:update, params: params.merge(id: :contains_anti_dandruff_agents, trigger_question: { applicable: 'true' }))
+      expect(assigns(:question).applicable).to eq(true)
+    end
+
+    it "assigns the applicable to false if selected as false in a check question" do
+      post(:update, params: params.merge(id: :contains_anti_dandruff_agents, trigger_question: { applicable: 'false' }))
+      expect(assigns(:question).applicable).to eq(false)
+    end
+
+    it "proceeds to add_anti_dandruff_agents step if user selects as applicable before" do
+      post(:update, params: params.merge(id: :contains_anti_dandruff_agents, trigger_question: { applicable: 'true' }))
+      expect(response).to redirect_to(responsible_person_notification_component_trigger_question_path(responsible_person, notification, component, :add_anti_dandruff_agents))
+    end
+
+    it "skips add_anti_dandruff_agents step if user selects as not applicable before" do
+      post(:update, params: params.merge(id: :contains_anti_dandruff_agents, trigger_question: { applicable: 'false' }))
+      expect(response).to redirect_to(responsible_person_notification_component_trigger_question_path(responsible_person, notification, component, :select_ph_range))
+    end
+
+    # rubocop:disable RSpec/ExampleLength
+    # rubocop:disable RSpec/MultipleExpectations
+    describe "at add_anti_dandruff_agents step" do
+      let(:question) {
+        create(:trigger_question, component: component,
+               question: "please_specify_the_inci_name_and_concentration_of_the_antidandruff_agents_if_antidandruff_agents_are_not_present_in_the_cosmetic_product_then_not_applicable_must_be_checked")
+      }
+      let(:answers) {
+        [
+            create(:trigger_question_element, trigger_question: question, answer_order: 0, element_order: 0),
+            create(:trigger_question_element, trigger_question: question, answer_order: 0, element_order: 1),
+            create(:trigger_question_element, trigger_question: question, answer_order: 1, element_order: 0),
+            create(:trigger_question_element, trigger_question: question, answer_order: 1, element_order: 1)
+        ]
+      }
+
+      let(:valid_answers) {
+        [
+            { answer: "agent 1", id: answers.first.id },
+            { answer: "5", id: answers.second.id }
+        ]
+      }
+
+      let(:valid_answers_with_empty_values) {
+        [
+            { answer: "agent 1", id: answers.first.id },
+            { answer: "5", id: answers.second.id },
+            { answer: "", id: answers.third.id },
+            { answer: "", id: answers.fourth.id }
+        ]
+      }
+
+      let(:valid_answers_with_unpaired_values) {
+        [
+            { answer: "agent 1", id: answers.first.id },
+            { answer: "5", id: answers.second.id },
+            { answer: "agent 2", id: answers.third.id },
+            { answer: "", id: answers.fourth.id }
+        ]
+      }
+
+      let(:unvalid_answers) { [{ answer: "", id: answers.first.id }] }
+
+      it "add filled answers to trigger_question_elements" do
+        post(:update, params: params.merge(id: :add_anti_dandruff_agents, trigger_question: {
+            trigger_question_elements_attributes: valid_answers
+        }))
+        expect(assigns(:question).trigger_question_elements).to have(2).items
+        expect(assigns(:question).trigger_question_elements.first.answer).to eq("agent 1")
+        expect(assigns(:question).trigger_question_elements.second.answer).to eq("5")
+      end
+
+      it "ignore empty answers" do
+        post(:update, params: params.merge(id: :add_anti_dandruff_agents, trigger_question: {
+            trigger_question_elements_attributes: valid_answers_with_empty_values
+        }))
+        expect(assigns(:question).trigger_question_elements).to have(2).items
+        expect(assigns(:question).trigger_question_elements.first.answer).to eq("agent 1")
+        expect(assigns(:question).trigger_question_elements.second.answer).to eq("5")
+      end
+
+      it "ignore unpaired answers on add_anti_dandruff_agents step" do
+        post(:update, params: params.merge(id: :add_anti_dandruff_agents, trigger_question: {
+            trigger_question_elements_attributes: valid_answers_with_unpaired_values
+        }))
+        expect(assigns(:question).trigger_question_elements).to have(2).items
+        expect(assigns(:question).trigger_question_elements.first.answer).to eq("agent 1")
+        expect(assigns(:question).trigger_question_elements.second.answer).to eq("5")
+      end
+
+      it "sets question as applicable when succeed to add answers" do
+        post(:update, params: params.merge(id: :add_anti_dandruff_agents, trigger_question: {
+            trigger_question_elements_attributes: valid_answers
+        }))
+        expect(assigns(:question).applicable).to eq(true)
+      end
+
+      it "does not set errors when succeed to update an answer" do
+        post(:update, params: params.merge(id: :add_anti_dandruff_agents, trigger_question: {
+            trigger_question_elements_attributes: valid_answers
+        }))
+        expect(assigns(:errors)).to be_nil
+      end
+
+      it "set errors when fails to update" do
+        post(:update, params: params.merge(id: :add_anti_dandruff_agents, trigger_question: {
+            trigger_question_elements_attributes: unvalid_answers
+        }))
+        expect(assigns(:errors)).not_to be_nil
+      end
+
+      it "re initialize 20 empty answers when it fails to update" do
+        post(:update, params: params.merge(id: :add_anti_dandruff_agents, trigger_question: {
+            trigger_question_elements_attributes: unvalid_answers
+        }))
+        expect(assigns(:question).trigger_question_elements).to have(20).items
+        expect(assigns(:question).trigger_question_elements).to all(have_attributes(answer: be_nil))
+      end
+    end
+    # rubocop:enable RSpec/MultipleExpectations
+    # rubocop:enable RSpec/ExampleLength
+  end
+end
