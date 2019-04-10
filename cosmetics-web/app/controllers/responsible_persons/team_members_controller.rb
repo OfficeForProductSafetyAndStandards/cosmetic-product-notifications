862092742067a08eff8d826daad2d3ad8b444788
aidayounesi <aida.younesi@gmail.com>
2019-04-10 16:25:26 +0100

    COSBETA-177: Validate email address using a regex both in controller and the model

--- cosmetics-web/app/controllers/responsible_persons/team_members_controller.rb
+++ cosmetics-web/app/controllers/responsible_persons/team_members_controller.rb
@@ -8,6 +8,11 @@ class ResponsiblePersons::TeamMembersController < ApplicationController
       return redirect_to new_responsible_person_team_member_path(@responsible_person, blank_input: true)
     end
 
+    # Check the user entered a valid email address
+    if not team_member_params[:email_address] =~ URI::MailTo::EMAIL_REGEXP
+      return redirect_to new_responsible_person_team_member_path(@responsible_person, invalid_email: true)
+    end
+
     # Check if the user is already a member of the team
     if @responsible_person.responsible_person_users.any? { |user| user.email_address == team_member_params[:email_address] }
       return redirect_to new_responsible_person_team_member_path(@responsible_person, user_already_exists: true)
--- cosmetics-web/app/models/pending_responsible_person_user.rb
+++ cosmetics-web/app/models/pending_responsible_person_user.rb
@@ -1,6 +1,8 @@
 class PendingResponsiblePersonUser < ApplicationRecord
   belongs_to :responsible_person
 
+  validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  
   before_create :set_expires_at
 
   def self.key_validity_duration
