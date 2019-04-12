7ad6eb504b7375d26ddc6d09b3dbfaf92ea8f727
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-04-12 10:34:06 +0100

    PSD-1120 Move navigation-items logic to application controller
    Also allow for displaying secondary navigation only and show a "Sign in"
    link on help pages for signed-out users

--- psd-web/app/controllers/application_controller.rb
+++ psd-web/app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   before_action :has_accepted_declaration
   before_action :set_cache_headers
 
-  helper_method :hide_nav?
+  helper_method :nav_items, :secondary_nav_items
 
   def authorize_user
     raise Pundit::NotAuthorizedError unless User.current&.is_psd_user?
@@ -26,5 +26,35 @@ class ApplicationController < ActionController::Base
 
   def hide_nav?
     false
+  end
+
+  def nav_items
+    return nil if hide_nav? # On some pages we don't want to show the main navigation
+
+    items = []
+    unless User.current.is_opss?
+      items.push text: "Home", href: root_path, active: params[:controller] == "homepage"
+    end
+    items.push text: "Cases", href: investigations_path, active: params[:controller] == "investigations"
+    items.push text: "Businesses", href: businesses_path, active: params[:controller] == "businesses"
+    items.push text: "Products", href: products_path, active: params[:controller] == "products"
+    # In principle all our users belong to a team, but this saves crashes in case of a misconfiguration
+    if User.current.teams.present?
+      text = User.current.teams.count > 1 ? "Your teams" : "Your team"
+      path = User.current.teams.count > 1 ? your_teams_path: team_path(User.current.teams.first)
+      items.push text: text, href: path, active: params[:controller] == "teams", right: true
+    end
+    items
+  end
+
+  def secondary_nav_items
+    items = []
+    items.push text: "Your account", href: Shared::Web::KeycloakClient.instance.user_account_url if User.current
+    if User.current
+      items.push text: "Sign out", href: shared_engine.logout_session_path
+    else
+      items.push text: "Sign in", href: keycloak_login_url
+    end
+    items
   end
 end
