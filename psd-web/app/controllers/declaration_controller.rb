61946988b7f9d2dfb3b49d19da2c3874a8bef4b7
samlionheart <strlionheart@gmail.com>
2019-04-16 11:25:54 +0100

    PSD-1128 More appropriately named method for sending welcome email

--- psd-web/app/controllers/declaration_controller.rb
+++ psd-web/app/controllers/declaration_controller.rb
@@ -12,7 +12,7 @@ class DeclarationController < ApplicationController
       return render :index
     end
     User.current.has_accepted_declaration!
-    welcome_email unless User.current.has_been_sent_welcome_email
+    send_welcome_email unless User.current.has_been_sent_welcome_email
     redirect_to session[:redirect_path] || root_path
   end
 
@@ -20,7 +20,7 @@ class DeclarationController < ApplicationController
     @error_list = []
   end
 
-  def welcome_email
+  def send_welcome_email
     NotifyMailer.welcome(User.current.name, User.current.email).deliver_later
     User.current.has_been_sent_welcome_email!
   end
