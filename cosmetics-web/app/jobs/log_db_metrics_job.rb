4ecf5139452baaf89ea5d34dfeeeb5efca797ddc
Raam Chauhan <raam.chauhan@softwire.com>
2019-04-17 15:16:41 +0100

    COSBETA-NONE: Review markups

--- cosmetics-web/app/jobs/log_db_metrics_job.rb
+++ cosmetics-web/app/jobs/log_db_metrics_job.rb
@@ -1,11 +1,14 @@
 class LogDbMetricsJob < ApplicationJob
   def perform
+    complete_notifications_count = Notification.where(state: 'notification_complete').count
+    responsible_persons_count = ResponsiblePerson.count
+    users_per_responsible_person = ResponsiblePersonUser.group(:responsible_person_id)
+      .count.values.sort.inject(Hash.new(0)) { |h, e| h[e] += 1; h }
+
     Sidekiq.logger.info "{
-      completeNotifications: #{Notification.where(state: 'notification_complete').count},
-      responsiblePersons: #{ResponsiblePerson.count},
-      usersPerResponsiblePerson: #{ResponsiblePersonUser.group(:responsible_person_id).count.values.sort.inject(
-        Hash.new(0)
-) { |h, e| h[e] += 1; h }}
+      completeNotifications: #{complete_notifications_count},
+      responsiblePersons: #{responsible_persons_count},
+      usersPerResponsiblePerson: #{users_per_responsible_person}
       }"
   end
 end
