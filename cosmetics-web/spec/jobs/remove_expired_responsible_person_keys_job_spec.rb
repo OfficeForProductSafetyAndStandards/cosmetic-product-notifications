146a4a2b4b267f085ba3e9fd8511764a559df074
aidayounesi <aida.younesi@gmail.com>
2019-04-18 16:26:50 +0100

    COSBETA-177: improve tests by modifying the pending responsible person factory bot

--- cosmetics-web/spec/jobs/remove_expired_responsible_person_keys_job_spec.rb
+++ cosmetics-web/spec/jobs/remove_expired_responsible_person_keys_job_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.describe RemoveExpiredResponsiblePersonKeysJob, type: :job do
-  let(:responsible_person) { create(:responsible_person) }
   let(:email_verification_key) { create(:email_verification_key) }
   let(:expired_email_verification_key) { create(:expired_email_verification_key) }
-  let(:pending_responsible_person_user) { create(:pending_responsible_person_user, responsible_person: responsible_person, email_address: "pending1@example.com") }
-  let(:expired_pending_responsible_person_user) { create(:expired_pending_responsible_person_user, responsible_person: responsible_person, email_address: "pending2@example.com") }
+
+  let(:pending_responsible_person_user) { create(:pending_responsible_person_user) }
+  let(:expired_pending_responsible_person_user) { create(:expired_pending_responsible_person_user) }
 
   before do
     email_verification_key.save
@@ -17,14 +17,14 @@ RSpec.describe RemoveExpiredResponsiblePersonKeysJob, type: :job do
   describe "#perform" do
     it "deletes an expired email verification key" do
       RemoveExpiredResponsiblePersonKeysJob.perform_now
-
-      expect(EmailVerificationKey.count).to eq(1)
+      expect(EmailVerificationKey.all).to include(email_verification_key)
+      expect(EmailVerificationKey.all).not_to include(expired_email_verification_key)
     end
 
     it "deletes an expired pending responsible person user" do
       RemoveExpiredResponsiblePersonKeysJob.perform_now
-
-      expect(PendingResponsiblePersonUser.count).to eq(1)
+      expect(PendingResponsiblePersonUser.all).to include(pending_responsible_person_user)
+      expect(PendingResponsiblePersonUser.all).not_to include(expired_pending_responsible_person_user)
     end
   end
 end
--- cosmetics-web/spec/mailers/notify_mailer_spec.rb
+++ cosmetics-web/spec/mailers/notify_mailer_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe NotifyMailer, type: :mailer do
       expect(responsible_person.reload.email_verification_keys.size).to eq(1)
     end
   end
+
+  describe "send_responsible_person_invite_email" do
+    it "sends invite to join a responsible person to invited user" do
+      mail = NotifyMailer.send_responsible_person_invite_email(responsible_person.id, responsible_person.name, email_address, user_name)
+      expect(mail.to).to eq([email_address])
+    end
+  end
 end
