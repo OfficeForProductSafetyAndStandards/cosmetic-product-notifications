4c1de22c21840b59e9afb60213454f0c258301eb
aidayounesi <aida.younesi@gmail.com>
2019-04-19 18:31:04 +0100

    COSBETA-329: Writing tests for different policies

---
+++ cosmetics-web/spec/policies/poison_centre_notification_policy_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe PoisonCentreNotificationPolicy, type: :policy do
+  subject { PoisonCentreNotificationPolicy.new(user, notification) }
+
+  let(:user) { build(:user) }
+  let(:notification) { create(:notification) }
+
+  before do
+    sign_in_as_poison_centre_user(user: user)
+  end
+
+  after do
+    sign_out
+  end
+
+  it { is_expected.to permit(:index) }
+  it { is_expected.to permit(:show) }
+
+  it { is_expected.not_to permit(:create)  }
+  it { is_expected.not_to permit(:new)     }
+  it { is_expected.not_to permit(:update)  }
+  it { is_expected.not_to permit(:edit)    }
+  it { is_expected.not_to permit(:destroy) }
+end
