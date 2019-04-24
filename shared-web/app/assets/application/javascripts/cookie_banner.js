2ad270930605dce63fbb3566585c6479e0a518aa
samlionheart <strlionheart@gmail.com>
2019-04-24 14:16:48 +0100

    PSD-1119: Cookie banner added to shared web

---
+++ shared-web/app/assets/application/javascripts/cookie_banner.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+function cookieBanner() {
+  const myButton = document.getElementById('hideLink');
+  function setCookie() {
+    const d = new Date();
+    d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+    const expires = `expires=${d.toUTCString()}`;
+    document.cookie = `seen_cookie_message = true;${expires};path=/`;
+  }
+  function hideCookieBanner() {
+    setCookie(365);
+    document.getElementById('global-cookie-message').style.display = 'none';
+  }
+  myButton.addEventListener('click', hideCookieBanner);
+}
+$(document).ready(() => {
+  cookieBanner();
+});
