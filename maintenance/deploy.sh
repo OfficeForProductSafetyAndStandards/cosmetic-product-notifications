027c2e7ffbc8631d9c23a05d9fa715eea67df83b
Tom Bell <tom.bell@softwire.com>
2019-04-26 11:50:46 +0100

    PSD-6: Update comment in maintenance page deploy script

--- maintenance/deploy.sh
+++ maintenance/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-# This is the CI server script to deploy the antivirus API
+# This is the CI server script to deploy the maintenance page
 #
 # The working directory should be the git root
 
