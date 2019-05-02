dab24fd5fc73679be15e46006d993d2cdeadb8f8
Tom Bell <tom.bell@softwire.com>
2019-05-02 14:17:57 +0100

    Fix linting issues raised by new RuboCop version

--- cosmetics-web/spec/mailers/notify_mailer_spec.rb
+++ cosmetics-web/spec/mailers/notify_mailer_spec.rb
@@ -2,14 +2,15 @@ require "rails_helper"
 
 RSpec.describe NotifyMailer, type: :mailer do
   let(:responsible_person) { create(:responsible_person) }
+  let(:contact_person) { responsible_person.contact_persons.first }
+
   let(:user_name) { "Test User" }
   let(:email_address) { "user@example.com" }
 
   describe "send_responsible_person_verification_email" do
     it "sends new email verification key to responsible person" do
-      mail = NotifyMailer.send_responsible_person_verification_email(responsible_person.id,
-      responsible_person.contact_persons.first.email_address, "contact name", "responsible_person name", user_name)
-      expect(mail.to).to eq([responsible_person.contact_persons.first.email_address])
+      mail = NotifyMailer.send_responsible_person_verification_email(responsible_person.id, contact_person.email_address, contact_person.name, responsible_person.name, user_name)
+      expect(mail.to).to eq([contact_person.email_address])
       expect(responsible_person.reload.email_verification_keys.size).to eq(1)
     end
   end
