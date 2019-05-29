0cccadc09a0ffde25618e8f1477a82e2fb91ef83
aidayounesi <aida.younesi@gmail.com>
2019-05-29 14:24:48 +0100

    COSBETA-373: Show hyphens based on EC/CAS number format

--- cosmetics-web/app/models/cmr.rb
+++ cosmetics-web/app/models/cmr.rb
@@ -2,6 +2,14 @@ class Cmr < ApplicationRecord
   belongs_to :component
 
   def display_name
-    [name, cas_number, ec_number].reject(&:blank?).join(', ')
+    [name, display_cas_number, display_ec_number].reject(&:blank?).join(', ')
+  end
+
+  def display_cas_number
+    cas_number.dup.insert(-2, "-").insert(-5, "-") if cas_number.present?
+  end
+
+  def display_ec_number
+    ec_number.dup.insert(3, "-").insert(7, "-") if ec_number.present?
   end
 end
