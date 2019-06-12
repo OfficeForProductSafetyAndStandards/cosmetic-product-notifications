024646eae1894157f52a4bacf02ea5e917351a11
Tom Bell <tom.bell@softwire.com>
2019-06-12 20:07:27 +0100

    COSBETA-241: Constrain available routes based on request hostname

--- cosmetics-web/config/routes.rb
+++ cosmetics-web/config/routes.rb
@@ -1,54 +1,70 @@
+require 'constraints/domain_constraint'
+
 # rubocop:disable Metrics/BlockLength
 Rails.application.routes.draw do
   mount Shared::Web::Engine => '/', as: 'shared_engine'
 
-  root "landing_page#index"
-
-  scope module: "poison_centres", as: "poison_centre" do
-    resources :notifications, param: :reference_number, only: %i[index show]
+  constraints DomainConstraint.new(ENV["SEARCH_HOST"]) do
+    scope module: "poison_centres", as: "poison_centre" do
+      resources :notifications, param: :reference_number, only: %i[index show]
+    end
   end
 
-  resources :responsible_persons, only: %i[show] do
-    collection do
-      resources :account, controller: "responsible_persons/account_wizard", only: %i[show update]
-    end
+  constraints DomainConstraint.new(ENV["SUBMIT_HOST"]) do
+    root "landing_page#index"
 
-    resources :contact_persons, controller: "responsible_persons/contact_persons", only: %i[show new create edit update] do
-      member do
-        get :resend_email
-      end
-    end
-
-    resources :add_notification, controller: "responsible_persons/add_notification_wizard", only: %i[show new update]
-
-    resources :team_members, controller: "responsible_persons/team_members", only: %i[index new create] do
+    resources :responsible_persons, only: %i[show] do
       collection do
-        get :join
+        resources :account, controller: "responsible_persons/account_wizard", only: %i[show update]
       end
-    end
 
-    resources :notification_files, controller: "responsible_persons/notification_files", only: %i[show new create destroy] do
-      collection do
-        delete :destroy_all
-      end
-    end
-
-    resources :notifications, param: :reference_number, controller: "responsible_persons/notifications", only: %i[index show new edit] do
-      resources :build, controller: :notification_build, only: %i[show update new]
-      resources :additional_information, controller: :additional_information, only: %i[index]
-      resources :product_image_upload, controller: :product_image_upload, only: %i[new create]
-
-      resources :components do
-        resources :build, controller: :component_build, only: %i[show update new]
-        resources :nanomaterials, param: :nano_element_id do
-          resources :build, controller: :nanomaterial_build, only: %i[show update new]
+      resources :contact_persons, controller: "responsible_persons/contact_persons", only: %i[show new create edit update] do
+        member do
+          get :resend_email
         end
-        resources :trigger_question, controller: :trigger_questions, only: %i[show update new]
-        resources :formulation, controller: "formulation_upload", only: %w[new create]
       end
 
-      member do
-        post :confirm
+      resources :team_members, controller: "responsible_persons/team_members", only: %i[index new create] do
+        collection do
+          get :join
+        end
+      end
+
+      resources :add_notification, controller: "responsible_persons/add_notification_wizard", only: %i[show new update]
+
+      resources :notification_files, controller: "responsible_persons/notification_files", only: %i[show new create destroy] do
+        collection do
+          delete :destroy_all
+        end
+      end
+
+      resources :notifications, param: :reference_number, controller: "responsible_persons/notifications", only: %i[index show new edit] do
+        resources :build, controller: :notification_build, only: %i[show update new]
+        resources :additional_information, controller: :additional_information, only: %i[index]
+        resources :product_image_upload, controller: :product_image_upload, only: %i[new create]
+
+        resources :components do
+          resources :build, controller: :component_build, only: %i[show update new]
+          resources :trigger_question, controller: :trigger_questions, only: %i[show update new]
+          resources :formulation, controller: "formulation_upload", only: %w[new create]
+          resources :nanomaterials, param: :nano_element_id do
+            resources :build, controller: :nanomaterial_build, only: %i[show update new]
+          end
+        end
+
+        member do
+          post :confirm
+        end
+      end
+    end
+
+    resources :contact_persons, only: %i[] do
+      collection do
+        resources :confirmation, path: "confirm", param: :key, only: %i[show] do
+          collection do
+            get :link_expired, path: "link-expired"
+          end
+        end
       end
     end
   end
@@ -59,16 +75,6 @@ Rails.application.routes.draw do
 
   resource :dashboard, controller: :dashboard, only: %i[show]
 
-  resources :contact_persons, only: %i[] do
-    collection do
-      resources :confirmation, path: "confirm", param: :key, only: %i[show] do
-        collection do
-          get :link_expired, path: "link-expired"
-        end
-      end
-    end
-  end
-
   namespace :help, as: "" do
     get :terms_and_conditions, path: "terms-and-conditions"
     get :privacy_notice, path: "privacy-notice"
@@ -77,7 +83,6 @@ Rails.application.routes.draw do
   match "/404", to: "errors#not_found", via: :all
   match "/403", to: "errors#forbidden", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
-  # This is the page that will show for timeouts, currently showing the same as an internal error
   match "/503", to: "errors#timeout", via: :all
 end
 # rubocop:enable Metrics/BlockLength
---
+++ cosmetics-web/lib/constraints/domain_constraint.rb
@@ -0,0 +1,9 @@
+class DomainConstraint
+  def initialize(domains)
+    @domains = domains.split(',')
+  end
+
+  def matches?(request)
+    @domains.include?(request.host)
+  end
+end
