5f0b0dbc5af6af01b921f55c0569eec6945266da
Tom Bell <tom.bell@softwire.com>
2019-06-15 16:35:45 +0100

    COSBETA-241: Set domain environment variables in test configuration

--- cosmetics-web/spec/rails_helper.rb
+++ cosmetics-web/spec/rails_helper.rb
@@ -7,6 +7,9 @@ require 'file_helpers'
 require 'policy_helpers'
 
 ENV['RAILS_ENV'] ||= 'test'
+ENV['SEARCH_HOST'] ||= 'localhost'
+ENV['SUBMIT_HOST'] ||= 'localhost'
+
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
