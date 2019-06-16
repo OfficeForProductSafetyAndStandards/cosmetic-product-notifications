fc3086345b080e5c3a824a276e67f38f72c4e1e6
Tom Bell <tom.bell@softwire.com>
2019-06-16 16:38:13 +0100

    COSBETA-442: Add tests for NanoElement model

---
+++ cosmetics-web/spec/models/nano_element_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe NanoElement, type: :model do
+  let(:nano_element) { create(:nano_element) }
+
+  describe "updating purposes" do
+    it "allows multiple purposes to be specified" do
+      purposes = %w(preservative uv_filter)
+      nano_element.purposes = purposes
+
+      expect(nano_element.save(context: :select_purpose)).to be true
+      expect(nano_element.purposes).to eq(purposes)
+    end
+
+    it "adds error if invalid purpose is specified" do
+      invalid_purpose = "invalid_purpose"
+      nano_element.purposes = [invalid_purpose]
+
+      expect(nano_element.save(context: :select_purpose)).to be false
+      expect(nano_element.errors[:purposes]).to include("#{invalid_purpose} is not a valid purpose")
+    end
+
+    it "adds error if no purpose is specified" do
+      nano_element.purposes = []
+
+      expect(nano_element.save(context: :select_purpose)).to be false
+      expect(nano_element.errors[:purposes]).to include("Choose an option")
+    end
+  end
+
+  describe "#non_standard?" do
+    it "is true when purposes includes 'other'" do
+      nano_element.purposes = %w(colorant other)
+
+      expect(nano_element.non_standard?).to be true
+    end
+
+    it "is false when purposes do not include 'other'" do
+      expect(nano_element.non_standard?).to be false
+      nano_element.purposes = %w(colorant preservative uv_filter)
+      expect(nano_element.non_standard?).to be false
+    end
+  end
+end
