5b123fcde6437ea53377a0d1f9bd36c2a704b379
samlionheart <strlionheart@gmail.com>
2019-07-22 10:09:35 +0100

    PSD-1167: Rubocop fix

--- psd-web/app/helpers/investigations/user_filters_helper.rb
+++ psd-web/app/helpers/investigations/user_filters_helper.rb
@@ -1,31 +1,30 @@
 module Investigations::UserFiltersHelper
-
   def entities
     User.get_assignees(except: User.current) + Team.all
   end
 
   def assigned_to(form)
-      assigned_to_items = [{ key: "assigned_to_me", value: "checked", unchecked_value: "unchecked", text: "Me" }]
-      assignee_teams_with_keys.each do |key, team, name|
-          assigned_to_items << { key: key, value: team.id, unchecked_value: "unchecked", text: name }
-      end
-      assigned_to_items << { key: "assigned_to_someone_else",
-                   value: "checked",
-                   unchecked_value: "unchecked",
-                   text: "Other person or team",
-                   conditional: { html: other_assignee(form) } }
+    assigned_to_items = [{ key: "assigned_to_me", value: "checked", unchecked_value: "unchecked", text: "Me" }]
+    assignee_teams_with_keys.each do |key, team, name|
+      assigned_to_items << { key: key, value: team.id, unchecked_value: "unchecked", text: name }
+    end
+    assigned_to_items << { key: "assigned_to_someone_else",
+                 value: "checked",
+                 unchecked_value: "unchecked",
+                 text: "Other person or team",
+                 conditional: { html: other_assignee(form) } }
   end
 
   def created_by(form)
-      created_by_items = [{ key: "created_by_me", value: "checked", unchecked_value: "unchecked", text: "Me" }]
-      creator_teams_with_keys.each do |key, team, name|
-          created_by_items << { key: key, value: team.id, unchecked_value: "unchecked", text: name }
-      end
-      created_by_items << { key: "created_by_someone_else",
-                   value: "checked",
-                   unchecked_value: "unchecked",
-                   text: "Other person or team",
-                   conditional: { html: other_creator(form) } }
+    created_by_items = [{ key: "created_by_me", value: "checked", unchecked_value: "unchecked", text: "Me" }]
+    creator_teams_with_keys.each do |key, team, name|
+      created_by_items << { key: key, value: team.id, unchecked_value: "unchecked", text: name }
+    end
+    created_by_items << { key: "created_by_someone_else",
+                 value: "checked",
+                 unchecked_value: "unchecked",
+                 text: "Other person or team",
+                 conditional: { html: other_creator(form) } }
   end
 
   def other_assignee(form)
