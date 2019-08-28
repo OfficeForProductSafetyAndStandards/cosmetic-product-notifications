20e284fad77f48c26220919b6b47bdb85492e47a
samlionheart <strlionheart@gmail.com>
2019-08-28 10:51:33 +0100

    Set travis tests to avoid system tests

--- infrastructure/ci/test-if-changed.sh
+++ infrastructure/ci/test-if-changed.sh
@@ -8,7 +8,11 @@ docker-compose -f docker-compose.yml -f docker-compose.ci.yml up -d $COMPONENT
 if [[ $(./infrastructure/ci/get-changed-components.sh) =~ ((^| )$COMPONENT($| )) ]]; then
     echo "Testing component $COMPONENT"
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml run --rm start_dependencies
-    docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT bin/rake db:create db:schema:load test:all
+    if $COMPONENT == "psd-web"
+        docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT bin/rake db:create db:schema:load test:without_system_tests
+    else
+        docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT bin/rake db:create db:schema:load test:all
+    fi
 else
     echo 'Testing not required.'
 fi
