1c045cd4817f2fa88940ebf8938b197ca52c060f
Yomi Colledge <baphled@boodah.net>
2019-08-29 11:48:13 +0100

    Add functionality to allow user to update their naonaterial information

--- cosmetics-web/app/controllers/additional_information_controller.rb
+++ cosmetics-web/app/controllers/additional_information_controller.rb
@@ -2,7 +2,11 @@ class AdditionalInformationController < ApplicationController
   before_action :set_notification, only: %i[index]
 
   def index
-    if @notification.formulation_required?
+    if @notification.nano_material_incomplete?
+      component = @notification.components.find(&:nano_material_incomplete?)
+      nano_element = component.nano_material.nano_elements.find(&:incomplete?)
+      return redirect_to new_responsible_person_notification_component_nanomaterial_build_path(@notification.responsible_person, @notification, component, nano_element)
+    elsif @notification.formulation_required?
       component = @notification.components.find(&:formulation_required?)
       return redirect_to new_responsible_person_notification_component_formulation_path(@notification.responsible_person, @notification, component)
     else
