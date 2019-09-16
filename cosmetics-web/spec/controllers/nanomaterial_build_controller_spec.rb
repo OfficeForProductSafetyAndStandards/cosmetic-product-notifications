72247ac4aa526c0cc9b1e9aa034023dba6988571
Yomi Colledge <baphled@boodah.net>
2019-09-16 14:32:51 +0100

    Appease Rubocop

--- cosmetics-web/spec/controllers/nanomaterial_build_controller_spec.rb
+++ cosmetics-web/spec/controllers/nanomaterial_build_controller_spec.rb
@@ -141,37 +141,28 @@ RSpec.describe NanomaterialBuildController, type: :controller do
     end
 
     describe " at non_standard_nanomaterial_notified" do
-      context "answered yes" do
-        let(:confirm_toxicology_notified) do
-          params.merge(id: :non_standard_nanomaterial_notified, nano_element: { confirm_toxicology_notified: "yes" })
-        end
+      it "redirects to 'when_products_containing_nanomaterial_can_be_placed_on_market' when asnwered yes" do
+        confirm_toxicology_notified = params.merge(id: :non_standard_nanomaterial_notified, nano_element: { confirm_toxicology_notified: "yes" })
 
-        it "redirects to 'when_products_containing_nanomaterial_can_be_placed_on_market'" do
-          post(:update, params: confirm_toxicology_notified)
-          expect(response).to redirect_to(responsible_person_notification_component_nanomaterial_build_path(responsible_person, notification, component, nano_element1, :when_products_containing_nanomaterial_can_be_placed_on_market))
-        end
+        post(:update, params: confirm_toxicology_notified)
+
+        expect(response).to redirect_to(responsible_person_notification_component_nanomaterial_build_path(responsible_person, notification, component, nano_element1, :when_products_containing_nanomaterial_can_be_placed_on_market))
       end
 
-      context "answered no" do
-        let(:confirm_toxicology_notified) do
-          params.merge(id: :non_standard_nanomaterial_notified, nano_element: { confirm_toxicology_notified: "no" })
-        end
+      it "redirects to 'notify_your_nanomaterial' when answered no" do
+        confirm_toxicology_notified = params.merge(id: :non_standard_nanomaterial_notified, nano_element: { confirm_toxicology_notified: "no" })
 
-        it "redirects to 'notify_your_nanomaterial'" do
-          post(:update, params: confirm_toxicology_notified)
-          expect(response).to redirect_to(responsible_person_notification_component_nanomaterial_build_path(responsible_person, notification, component, nano_element1, :notify_your_nanomaterial))
-        end
+        post(:update, params: confirm_toxicology_notified)
+
+        expect(response).to redirect_to(responsible_person_notification_component_nanomaterial_build_path(responsible_person, notification, component, nano_element1, :notify_your_nanomaterial))
       end
 
-      context "answered not sure" do
-        let(:confirm_toxicology_notified) do
-          params.merge(id: :non_standard_nanomaterial_notified, nano_element: { confirm_toxicology_notified: "not sure" })
-        end
+      it "redirects to to the same path as answering no" do
+        confirm_toxicology_notified = params.merge(id: :non_standard_nanomaterial_notified, nano_element: { confirm_toxicology_notified: "not sure" })
 
-        it "redirects to to the same path as answering no" do
-          post(:update, params: confirm_toxicology_notified)
-          expect(response).to redirect_to(responsible_person_notification_component_nanomaterial_build_path(responsible_person, notification, component, nano_element1, :notify_your_nanomaterial))
-        end
+        post(:update, params: confirm_toxicology_notified)
+
+        expect(response).to redirect_to(responsible_person_notification_component_nanomaterial_build_path(responsible_person, notification, component, nano_element1, :notify_your_nanomaterial))
       end
     end
   end
