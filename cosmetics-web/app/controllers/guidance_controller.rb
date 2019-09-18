d74b8f9872dcfea57fe20f592682c3cbc4d8a947
Frankie Roberto <frankie@frankieroberto.com>
2019-09-18 14:56:25 +0100

    Work-in-progress on new page

--- cosmetics-web/app/controllers/guidance_controller.rb
+++ cosmetics-web/app/controllers/guidance_controller.rb
@@ -1,5 +1,13 @@
 class GuidanceController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:how_to_create_your_list_of_ingredients]
+  skip_before_action :authorize_user!, only: [:how_to_create_your_list_of_ingredients]
+
+
   def how_to_notify_nanomaterials; end
 
   def how_to_prepare_images_for_notification; end
+
+  def how_to_create_your_list_of_ingredients
+  end
 end
