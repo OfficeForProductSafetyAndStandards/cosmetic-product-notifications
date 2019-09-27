05b575f88da5ce4cd94e8b356f7911756b175780
samlionheart <strlionheart@gmail.com>
2019-09-27 21:12:39 +0100

    Added model tests

--- psd-web/test/models/enquiry_test.rb
+++ psd-web/test/models/enquiry_test.rb
@@ -15,22 +15,56 @@ class EnquiryTest < ActiveSupport::TestCase
     reset_keycloak_and_notify_mocks
   end
 
-  test "enquiry with correct date" do
-    investigation = load_case(:enquiry)
-    assert_equal "2019-01-01", investigation.date_received.to_s
+  test "Enquiry with valid date received" do
+    investigation = Investigation::Enquiry.new("date_received_day"=>1, "date_received_month"=>1, "date_received_year"=>1)
+    assert(investigation.valid?)
   end
 
-  test "enquiry received date validated" do
-    user = User.find_by(name: "Test User_one")
-    investigation = load_case(:enquiry_with_no_date)
-    investigation.update(assignee: user)
-    assert_includes investigation.past_assignees, user
+  test "Enquiry date received can be nil, for old enquiries" do
+    investigation = load_case(:enquiry)
+    investigation.date_received = nil
+    assert(investigation.valid?)
   end
 
-  test "enquiry received date validated with date" do
-    user = User.find_by(name: "Test User_one")
-    investigation = load_case(:enquiry)
-    investigation.update(assignee: user)
-    assert_includes investigation.past_assignees, user
+  test "Enquiry date received cannot be in the future" do
+    investigation = Investigation::Enquiry.new("date_received"=>Date.today + 1.year)
+    investigation.save
+    assert(investigation.invalid?)
   end
+
+  test "Enquiry date received cannot be in the future 2" do
+    investigation = Investigation::Enquiry.new("date_received_day"=>1, "date_received_month"=>1, "date_received_year"=>9999)
+    investigation.save
+    assert(investigation.invalid?)
+  end
+
+  test "Enquiry date received cannot have empty fields" do
+    investigation = Investigation::Enquiry.new("date_received_day"=>1, "date_received_month"=>1, "date_received_year"=>"")
+    investigation.save
+    assert(investigation.invalid?)
+  end
+
+  test "Enquiry date received has to be a date" do
+    investigation = Investigation::Enquiry.new("date_received_day"=>"day", "date_received_month"=>"month", "date_received_year"=>"year")
+    investigation.save
+    assert(investigation.invalid?)
+  end
+
+
+
+#   # Valid date
+# user = User.new(name: "Test", date_received: "2009-01-01")
+# assert(user.valid?)
+
+# # Invalid - date in the future
+# user = User.new(name: "Test", date_received: Date.today + 1.year)
+# assert(user.invalid?)
+
+# # Allow partially-filled date
+# user = User.new(name: "Test", date_received_year: "2009")
+# assert(user.valid?)
+
+# # Invalid date
+# user = User.new(name: "Test", date_received: "not_valid")
+# assert(user.invalid?)
 end
