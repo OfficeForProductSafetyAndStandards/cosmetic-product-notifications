OfficeForProductSafetyAndStandards/cosmetic-product-notifications

commit a493ecb8bf7f53fbfb6565d830f56cd1c869efc9

Add poison information question page (#1331)
** Add "Contains poisonous ingredients" boolean **

This stores the answer to the question "Does the product contain any poisonous ingredients?" which will be asked if you choose a "predefined frame formulation".

** Add new poisonous ingredients question **

This adds a new question page which lists poisonous ingredients and askes users to say whether their product contains any.

This is only asked if the product is being specified as a frame formulation.

After answering the question, the user is taken to the PH question, skipping the "Does the cosmetic product contain anti-dandruff agents?" question.

The response is also replayed on the check your answers page at the end.

cosmetics-web/app/controllers/component_build_controller.rb
@@ -18,7 +18,8 @@ class ComponentBuildController < ApplicationController
         :select_category,
         :select_formulation_type,
         :upload_formulation,
-        :select_frame_formulation
+        :select_frame_formulation,
+        :contains_poisonous_ingredients
 
   before_action :set_component
   before_action :set_nano_material
@@ -60,6 +61,8 @@ class ComponentBuildController < ApplicationController
       render_select_formulation_type
     when :upload_formulation
       render_upload_formulation
+    when :contains_poisonous_ingredients
+      update_contains_poisonous_ingredients
     else
       # Apply this since render_wizard(@component, context: :context) doesn't work as expected
       if @component.update_with_context(component_params, step)
@@ -138,6 +141,10 @@ private
         cmrs_attributes: %i[id name cas_number ec_number],
         shades: []
       )
+  end
+
+  def contains_poisonous_ingredients_params
+    params.require(:component).permit(:contains_poisonous_ingredients)
   end
 
   def nano_material_params
@@ -263,6 +270,17 @@ private
       @component.update(frame_formulation: nil) unless @component.frame_formulation.nil?
     end
     render_wizard @component
+  end
+
+  def update_contains_poisonous_ingredients
+    if params.fetch(:component, {})[:contains_poisonous_ingredients].present?
+      @component.update_attribute(:contains_poisonous_ingredients, params[:component][:contains_poisonous_ingredients])
+
+      redirect_to responsible_person_notification_component_trigger_question_path(@component.notification.responsible_person, @component.notification, @component, :select_ph_range)
+    else
+      @component.errors.add :contains_poisonous_ingredients, "Select whether the product contains any poisonous ingredients"
+      render :contains_poisonous_ingredients
+    end
   end
 
   def render_upload_formulation

cosmetics-web/spec/controllers/component_build_controller_spec.rb
@@ -169,6 +169,22 @@ RSpec.describe ComponentBuildController, type: :controller do
       expect(assigns(:component).cmrs.first.name).to eq(cmr_name)
     end
 
+    context "when selecting whether the component contains poisonous materials" do
+      it "saves and redirect to PH trigger question if you select an answer" do
+        post(:update, params: params.merge(id: :contains_poisonous_ingredients, component: { contains_poisonous_ingredients: 'true' }))
+
+        expect(assigns(:component).contains_poisonous_ingredients).to be(true)
+        expect(response).to redirect_to(responsible_person_notification_component_trigger_question_path(responsible_person, notification, component, :select_ph_range))
+      end
+
+      it "re-renders the question with an error if you don’t select an answer" do
+        post(:update, params: params.merge(id: :contains_poisonous_ingredients))
+
+        expect(response.status).to be(200)
+        expect(assigns(:component).errors[:contains_poisonous_ingredients]).to include("Select whether the product contains any poisonous ingredients")
+      end
+    end
+
     context "when notified pre EU-exit" do
       before do
         params.merge!(notification_reference_number: pre_eu_exit_notification.reference_number)

cosmetics-web/app/controllers/trigger_questions_controller.rb
@@ -94,6 +94,9 @@ class TriggerQuestionsController < ApplicationController
   def previous_wizard_path
     if step == steps.first
       return responsible_person_notification_component_build_path(@component.notification.responsible_person, @component.notification, @component, :select_frame_formulation)
+
+    elsif step == :select_ph_range && @component.predefined?
+      return responsible_person_notification_component_build_path(@component.notification.responsible_person, @component.notification, @component, :contains_poisonous_ingredients)
     end
 
     previous_step = get_previous_step

cosmetics-web/app/views/component_build/contains_poisonous_ingredients.html.slim
@@ -1,0 +1,144 @@
+- title = "Poisonous ingredients"
+- question = "Does the product contain any poisonous ingredients?"
+
+- content_for :page_title, title
+
+- content_for :after_header do
+  = link_to "Back", previous_wizard_path, class: "govuk-back-link"
+
+.govuk-grid-row
+  .govuk-grid-column-two-thirds
+
+    = form_with model: @component, url: wizard_path, method: :put do |form|
+
+      = render "form_components/govuk_error_summary", form: form
+
+      h1.govuk-heading-xl Poisonous ingredients
+
+      p.govuk-body
+        | To treat anyone who may be poisoned by the product,
+          National&nbsp;Poison&nbsp;Information&nbsp;Services need the exact quantities (% w/w)
+          of the most harmful&nbsp;ingredients.
+
+      h2.govuk-heading-s Glycols and glycol ethers
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" Above 1% w/w
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Buteth-2 Acetate
+        li Butoxydiglycol
+        li Butoxyethanol
+        li Butoxyethyl Acetate
+        li Diethoxydiglycol
+        li Ethoxydiglycol
+        li Ethoxydiglycol Acetate
+        li Ethylene Glycol Only (INCI name: ‘Glycol’)
+        li Glycol Ethers (not Polymers)
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" Above 10% w/w
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Dipropylene Glycol
+        li Dipropylene Glycol Dimethyl Ether
+        li Dipropylene Glycol Isobornyl Ether
+        li Methoxyisopropanol
+        li Methoxyisopropyl Acetate
+        li PPG-2 Methyl Ether
+        li PPG-2 Methyl Ether Acetate
+        li Propylene Glycol
+        li Propylene Glycol Butyl Ether
+
+      h2.govuk-heading-s Hydrocarbon solvents
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" Above 5% w/w
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Cyclohexane
+        li Hydrogenated Didodecene
+        li Isopentane
+        li Pentane
+        li Toluene
+        li Turpentine
+
+      h2.govuk-heading-s Alcohols
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" Any amount of
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Ethanol
+        li Isopropanol
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" Above 1% w/w
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Benzyl alcohol
+        li Furfuryl alcohol
+        li n-Butyl alcohol
+        li n-Propyl alcohol (INCI name: ‘propyl alcohol’)
+        li t-Butyl alcohol
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" Above 5% w/w
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Hexyl alcohol
+
+      h2.govuk-heading-s Other ingredients
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" Any amount of
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Brucine Sulfate
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" More than 0.1% w/w
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Butyrolactone
+        li Chloroplatinic Acid
+        li Copper Sulfate
+        li Dimethyltolylamine
+        li Methyl Acetate
+        li Potassium Chlorate
+        li Sodium Chlorate
+        li Triethyl Phosphate
+        li m-Cresol, o-Cresol, p-Cresol, Mixed Cresols
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" More than 0.2% w/w
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Formaldehyde
+
+      p class="govuk-body govuk-body-s govuk-body-s govuk-!-margin-bottom-1" More than 1% w/w
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Benzyl Benzoate
+        li Butyl Acetate
+        li Ethyl Acetate
+        li Hydroxylamine HCL, Hydroxylamine Sulfate
+
+      h2.govuk-heading-s Any ingredients classified as
+
+      ul.govuk-list.govuk-list--bullet.govuk-body-s
+        li Alkaline agents
+        li Anti-dandruff agents
+        li Anti-hair loss agents
+        li Anti-pigmenting and depigmenting agents
+        li Essential oils, camphor, menthol or eucalyptol
+        li Cationic surfactants
+        li Chemical exfoliating agents
+        li Compounds releasing hydrogen peroxide
+        li Fluoride compounds
+        li Hydrogen peroxide
+        li Inorganic sodium salts
+        li Persulfates
+        li Propellants
+        li Reducing agents
+        li Straightening agents
+        li Vitamin A or its derivatives
+        li Xanthine derivatives
+
+      = render "form_components/govuk_radios", form: form, key:   :contains_poisonous_ingredients,
+              fieldset: { legend: { text: question, classes: "govuk-fieldset__legend--m", isPageHeading: false } },
+              items: [{ text: "Yes", value: true },
+                      { text: "No", value: false }]
+
+      = form.submit "Continue", class: "govuk-button"

cosmetics-web/app/views/notifications/_component_details.html.slim
@@ -104,6 +104,13 @@ tr.govuk-table__row
   td.govuk-table__cell
     = component.acute_poisoning_info
 
+- if component.predefined?
+  tr.govuk-table__row
+    th.govuk-table__header[scope="row"]
+      | Contains poisonous ingredients
+    td.govuk-table__cell
+      = component.contains_poisonous_ingredients.present? ? "Yes" : "No"
+
 - if component.trigger_questions
   - component.trigger_questions.each do |question|
     = render "notifications/trigger_question_details", trigger_question: question, display_not_applicable: true

cosmetics-web/db/migrate/20190919114626_add_contains_poisonous_ingredients_to_components.rb
@@ -1,0 +1,5 @@
+class AddContainsPoisonousIngredientsToComponents < ActiveRecord::Migration[5.2]
+  def change
+    add_column :components, :contains_poisonous_ingredients, :boolean
+  end
+end

cosmetics-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_114416) do
+ActiveRecord::Schema.define(version: 2019_09_19_114626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_09_11_114416) do
     t.string "special_applicator"
     t.string "acute_poisoning_info"
     t.string "other_special_applicator"
+    t.boolean "contains_poisonous_ingredients"
     t.index ["notification_id"], name: "index_components_on_notification_id"
   end
 

cosmetics-web/spec/models/component_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe Component, type: :model do
   let(:exact_component) { create(:exact_component) }
   let(:text_file) { fixture_file_upload('/testText.txt', 'application/text') }
 
+  describe "attributes" do
+    subject(:component) { described_class.new }
+
+    it "has a contains_poisonous_ingredients boolean" do
+      expect(component).to have_attributes(contains_poisonous_ingredients: nil)
+    end
+  end
+
   describe "formulation_required" do
     it "returns false for predefined formulation even if no file attached" do
       expect(predefined_component.formulation_required?).to be false

commit 029eaeaf24af36aec081fdc39d779602740c1d91

Skip poisonous ingredients question for pre-Brexit products (#1351)
This updates the follow of the component questions so that only post-Brexit products get asked whether the product contains harmful ingredients when using a frame formulation. Pre-Brexit products skip straight to the PH question instead.

Fixes a bug introduced by https://github.com/UKGovernmentBEIS/beis-opss/pull/1331

cosmetics-web/app/controllers/component_build_controller.rb
@@ -59,6 +59,8 @@ class ComponentBuildController < ApplicationController
       render_select_category_step
     when :select_formulation_type
       render_select_formulation_type
+    when :select_frame_formulation
+      update_frame_formulation
     when :upload_formulation
       render_upload_formulation
     when :contains_poisonous_ingredients
@@ -270,6 +272,19 @@ private
       @component.update(frame_formulation: nil) unless @component.frame_formulation.nil?
     end
     render_wizard @component
+  end
+
+  def update_frame_formulation
+    if @component.update_with_context(component_params, :select_frame_formulation)
+
+      if @component.notification.notified_post_eu_exit?
+        redirect_to responsible_person_notification_component_build_path(@component.notification.responsible_person, @component.notification, @component, :contains_poisonous_ingredients)
+      else
+        redirect_to responsible_person_notification_component_trigger_question_path(@component.notification.responsible_person, @component.notification, @component, :select_ph_range)
+      end
+    else
+      render :select_frame_formulation
+    end
   end
 
   def update_contains_poisonous_ingredients

cosmetics-web/spec/controllers/component_build_controller_spec.rb
@@ -169,6 +169,21 @@ RSpec.describe ComponentBuildController, type: :controller do
       expect(assigns(:component).cmrs.first.name).to eq(cmr_name)
     end
 
+    context "when selecting a frame formulation" do
+      it "saves and redirects to the 'poisonous ingredients' question if you select an answer" do
+        post(:update, params: params.merge(id: :select_frame_formulation, component: { frame_formulation: 'skin_care_cream_lotion_gel' }))
+
+        expect(response).to redirect_to(responsible_person_notification_component_build_path(responsible_person, notification, component, :contains_poisonous_ingredients))
+      end
+
+      it "re-renders the question with an error if you don’t select a formulation" do
+        post(:update, params: params.merge(id: :select_frame_formulation, component: { frame_formulation: '' }))
+
+        expect(response.status).to be(200)
+        expect(assigns(:component).errors[:frame_formulation]).to include("Frame formulation can not be blank")
+      end
+    end
+
     context "when selecting whether the component contains poisonous materials" do
       it "saves and redirect to PH trigger question if you select an answer" do
         post(:update, params: params.merge(id: :contains_poisonous_ingredients, component: { contains_poisonous_ingredients: 'true' }))
@@ -193,6 +208,12 @@ RSpec.describe ComponentBuildController, type: :controller do
       it "skips special applicator and CMRs questions and redirects to contains_nanomaterials if pre-eu-exit" do
         post(:update, params: params.merge(id: :add_physical_form, component: { physical_form: "loose powder" }))
         expect(response).to redirect_to(responsible_person_notification_component_build_path(responsible_person, pre_eu_exit_notification, component, :contains_nanomaterials))
+      end
+
+      it "skips contains poisonous ingredients question and redirects to PH question" do
+        post(:update, params: params.merge(id: :select_frame_formulation, component: { frame_formulation: 'skin_care_cream_lotion_gel' }))
+
+        expect(response).to redirect_to(responsible_person_notification_component_trigger_question_path(responsible_person, pre_eu_exit_notification, component, :select_ph_range))
       end
     end
   end