5684078682edeb26b36bdc2de2812edfe7cfef25
Steve Lorek <steve@stevelorek.com>
2019-09-27 11:08:59 +0100

    Remove ActiveHashSafeLoadable#safe_load success logging
    This is producing a lot of noise in the log which isn't very useful.

--- psd-web/app/models/concerns/active_hash_safe_loadable.rb
+++ psd-web/app/models/concerns/active_hash_safe_loadable.rb
@@ -5,7 +5,6 @@ module ActiveHashSafeLoadable
     def safe_load(data_to_load, data_name:, retries: 3)
       begin
         self.data = data_to_load
-        Rails.logger.debug "ActiveHashSafeLoadable.safe_load: #{data_name} success with remaining tries: #{retries}"
       rescue StandardError => e
         Rails.logger.error "Failed to load #{data_name} (remaining retries #{retries}): #{e.message}"
         self.safe_load(data_to_load, data_name: data_name, retries: retries - 1) if retries.positive?
