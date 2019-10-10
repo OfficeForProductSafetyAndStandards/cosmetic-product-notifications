bd2f34293cf3d974d261e03199ee5f8d28162f88
Steve Lorek <steve@stevelorek.com>
2019-10-10 14:50:11 +0100

    Fix duplicate metadata methods on AntiVirusAnalyzer after merging shared-web code into main applciation

--- cosmetics-web/app/analyzers/anti_virus_analyzer.rb
+++ cosmetics-web/app/analyzers/anti_virus_analyzer.rb
@@ -6,16 +6,12 @@ class AntiVirusAnalyzer < ActiveStorage::Analyzer
   end
 
   def metadata
-    download_blob_to_tempfile do |file|
+    notification_file = get_notification_file_from_blob(@blob)
+    metadata = download_blob_to_tempfile do |file|
       response = RestClient::Request.execute method: :post, url: ENV["ANTIVIRUS_URL"], user: ENV["ANTIVIRUS_USERNAME"], password: ENV["ANTIVIRUS_PASSWORD"], payload: { file: file }
       body = JSON.parse(response.body)
       { safe: body["safe"] }
     end
-  end
-
-  def metadata
-    notification_file = get_notification_file_from_blob(@blob)
-    metadata = super
 
     if metadata["safe"] == false && notification_file.present?
       notification_file.update(upload_error: :file_flagged_as_virus)
