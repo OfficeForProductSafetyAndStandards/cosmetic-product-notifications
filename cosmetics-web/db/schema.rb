734fb4ad84b985d0e25c53331c8f15e04942db4b
Frankie Roberto <frankie@frankieroberto.com>
2019-10-10 10:23:13 +0100

    Update schema version

--- cosmetics-web/db/schema.rb
+++ cosmetics-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_114626) do
+ActiveRecord::Schema.define(version: 2019_09_23_133032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
