OfficeForProductSafetyAndStandards/cosmetic-product-notifications

commit b34bcd6bbbb8dc34f9e5259925e0032a2dc4dfe2

Provide meaningful error message when domains aren't set (#1375)

cosmetics-web/config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => "/sidekiq"
   end
 
-  constraints DomainConstraint.new(ENV["SEARCH_HOST"] || ENV["COSMETICS_HOST"]) do
+  constraints DomainConstraint.new(ENV.fetch("SEARCH_HOST") || ENV.fetch("COSMETICS_HOST")) do
     root "landing_page#index"
 
     scope module: "poison_centres", as: "poison_centre" do
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
     end
   end
 
-  constraints DomainConstraint.new(ENV["SUBMIT_HOST"] || ENV["COSMETICS_HOST"]) do
+  constraints DomainConstraint.new(ENV.fetch("SEARCH_HOST") || ENV.fetch("COSMETICS_HOST")) do
     root "landing_page#index"
 
     resources :responsible_persons, only: %i[show] do

cosmetics-web/lib/constraints/domain_constraint.rb
@@ -1,5 +1,7 @@
 class DomainConstraint
   def initialize(domains)
+    raise "No domains specified" unless domains
+
     @domains = domains.split(',')
   end
 

commit 8b0d23fb2b002c44a4ee3f0a174277ff66b51398

Bugfix: this contraint should match the SUBMIT_HOST
Fixes a bug accidentally introduced by #1375.

cosmetics-web/config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
     end
   end
 
-  constraints DomainConstraint.new(ENV.fetch("SEARCH_HOST") || ENV.fetch("COSMETICS_HOST")) do
+  constraints DomainConstraint.new(ENV.fetch("SUBMIT_HOST") || ENV.fetch("COSMETICS_HOST")) do
     root "landing_page#index"
 
     resources :responsible_persons, only: %i[show] do