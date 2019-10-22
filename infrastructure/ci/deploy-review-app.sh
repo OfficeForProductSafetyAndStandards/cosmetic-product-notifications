c6325fb6e2355ceaee209629ca8689d77ffc969d
Slawosz Slawinski <slawosz@gmail.com>
2019-10-22 16:03:11 +0100

    Deploy review apps with redis

---
+++ infrastructure/ci/deploy-review-app.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -ex
+
+# The caller should have the following environment variables set:
+#
+# CF_USERNAME: cloudfoundry username
+# CF_PASSWORD: cloudfoundry password
+# SPACE: the space to which you want to deploy
+
+if [[ $(./infrastructure/ci/get-changed-components.sh) =~ ((^| )$COMPONENT($| )) ]]; then
+    ./infrastructure/ci/install-cf.sh
+    cf login -a api.london.cloud.service.gov.uk -u $CF_USERNAME -p $CF_PASSWORD -o 'beis-opss' -s $SPACE
+    export DB_VERSION=`cat cosmetics-web/db/schema.rb | grep 'ActiveRecord::Schema.define' | grep -o '[0-9_]\+'`
+    export REVIEW_INSTANCE_NAME=$BRANCH
+    export DB_NAME=cosmetics-db-$DB_VERSION
+    export REDIS_NAME=cosmetics-review-redis-$BRANCH
+    ./$COMPONENT/deploy-review.sh
+    cf logout
+
+    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+    docker pull beisopss/$COMPONENT:$BUILD_ID
+    docker tag beisopss/$COMPONENT:$BUILD_ID beisopss/$COMPONENT:$BRANCH
+    docker push beisopss/$COMPONENT:$BRANCH
+else
+    echo 'Deployment not required.'
+fi
