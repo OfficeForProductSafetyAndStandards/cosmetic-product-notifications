f31a00ff5cfe9f7652192e98b8e516443f0bb596
Slawosz Slawinski <slawosz@gmail.com>
2019-10-24 11:55:33 +0100

    Simplyfy and speedup review apps deployment

--- infrastructure/ci/deploy-review-app.sh
+++ infrastructure/ci/deploy-review-app.sh
@@ -7,21 +7,12 @@ set -ex
 # CF_PASSWORD: cloudfoundry password
 # SPACE: the space to which you want to deploy
 
-#if [[ $(./infrastructure/ci/get-changed-components.sh) =~ ((^| )$COMPONENT($| )) ]]; then
-    ./infrastructure/ci/install-cf.sh
-    cf login -a api.london.cloud.service.gov.uk -u $CF_USERNAME -p $CF_PASSWORD -o 'beis-opss' -s $SPACE
-    export DB_VERSION=`cat cosmetics-web/db/schema.rb | grep 'ActiveRecord::Schema.define' | grep -o '[0-9_]\+'`
-    export REVIEW_INSTANCE_NAME=$TRAVIS_PULL_REQUEST_BRANCH
-    export DB_NAME=cosmetics-db-$DB_VERSION
-    # redis will be new for each review app
-    export REDIS_NAME=cosmetics-review-redis-$TRAVIS_PULL_REQUEST_BRANCH
-    ./$COMPONENT/deploy-review.sh
-    cf logout
-
-    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-    docker pull beisopss/$COMPONENT:$BUILD_ID
-    docker tag beisopss/$COMPONENT:$BUILD_ID beisopss/$COMPONENT:$BRANCH
-    docker push beisopss/$COMPONENT:$BRANCH
-#else
-    echo 'Deployment not required.'
-#fi
+./infrastructure/ci/install-cf.sh
+cf login -a api.london.cloud.service.gov.uk -u $CF_USERNAME -p $CF_PASSWORD -o 'beis-opss' -s $SPACE
+export DB_VERSION=`cat cosmetics-web/db/schema.rb | grep 'ActiveRecord::Schema.define' | grep -o '[0-9_]\+'`
+export REVIEW_INSTANCE_NAME=$TRAVIS_PULL_REQUEST_BRANCH
+export DB_NAME=cosmetics-db-$DB_VERSION
+# redis will be new for each review app
+export REDIS_NAME=cosmetics-review-redis-$TRAVIS_PULL_REQUEST_BRANCH
+./$COMPONENT/deploy-review.sh
+cf logout
