61e9a6924029fa12fdcffd3a697a4aea3e73dc05
Slawosz Slawinski <slawosz@gmail.com>
2019-10-25 12:08:08 +0100

    Add link to github docs

--- infrastructure/ci/cleanup-review-app.sh
+++ infrastructure/ci/cleanup-review-app.sh
@@ -1,4 +1,5 @@
 set -x
+# https://developer.github.com/v3/repos/commits/#list-pull-requests-associated-with-commit
 number=`curl -H "Accept: application/vnd.github.groot-preview+json" https://api.github.com/repos/UKGovernmentBEIS/beis-opss/commits/$TRAVIS_COMMIT/pulls | jq '.[0] | .number'`
 
 ./infrastructure/ci/install-cf.sh
