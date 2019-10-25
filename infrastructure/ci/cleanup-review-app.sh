45ad758c29a9d9d75234651dce2fdb0a433d55ea
Slawosz Slawinski <slawosz@gmail.com>
2019-10-25 12:06:56 +0100

    Force delete

--- infrastructure/ci/cleanup-review-app.sh
+++ infrastructure/ci/cleanup-review-app.sh
@@ -3,6 +3,6 @@ number=`curl -H "Accept: application/vnd.github.groot-preview+json" https://api.
 
 ./infrastructure/ci/install-cf.sh
 cf login -a api.london.cloud.service.gov.uk -u $CF_USERNAME -p $CF_PASSWORD -o 'beis-opss' -s $SPACE
-cf delete cosmetics-pr-$number-web
-cf delete-service cosmetics-review-redis-$number
+cf delete -f cosmetics-pr-$number-web
+cf delete-service -f cosmetics-review-redis-$number
 cf logout
