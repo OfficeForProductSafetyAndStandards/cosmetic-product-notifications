bdf1a0b2cbb3e7875e53602fe0a1f89f41dc5e32
Slawosz Slawinski <slawosz@gmail.com>
2019-10-25 12:09:42 +0100

    Remove worker

--- infrastructure/ci/cleanup-review-app.sh
+++ infrastructure/ci/cleanup-review-app.sh
@@ -5,5 +5,6 @@ number=`curl -H "Accept: application/vnd.github.groot-preview+json" https://api.
 ./infrastructure/ci/install-cf.sh
 cf login -a api.london.cloud.service.gov.uk -u $CF_USERNAME -p $CF_PASSWORD -o 'beis-opss' -s $SPACE
 cf delete -f cosmetics-pr-$number-web
+cf delete -f cosmetics-pr-$number-worker
 cf delete-service -f cosmetics-review-redis-$number
 cf logout
