8d43cb2ea0df6dfa6dcb218f6a496c1b46c3fb62
Frankie Roberto <frankie@frankieroberto.com>
2019-10-31 12:09:21 +0000

    Make the guidance pages public (#1441)
    * Refactor how we handle publicly accessible pages
    * Add request specs
    * Remove controller specs

---
+++ cosmetics-web/spec/requests/guidance_pages_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "Guidance pages", type: :request do
+  include RSpecHtmlMatchers
+
+  describe "'How to notify nanomaterials'" do
+    before do
+      get "/guidance/how-to-notify-nanomaterials"
+    end
+
+    it "is successful" do
+      expect(response.code).to eql("200")
+    end
+
+    it "has a page title" do
+      expect(response.body).to have_tag("title", text: /\AHow to notify nanomaterials/)
+    end
+
+    it "has a page heading" do
+      expect(response.body).to have_tag("h1", text: /\AHow to notify nanomaterials/)
+    end
+  end
+
+
+  describe "'How to prepare images for notification'" do
+    before do
+      get "/guidance/how-to-prepare-images-for-notification"
+    end
+
+    it "is successful" do
+      expect(response.code).to eql("200")
+    end
+
+    it "has a page title" do
+      expect(response.body).to have_tag("title", text: /\AHow to prepare images for notification/)
+    end
+
+    it "has a page heading" do
+      expect(response.body).to have_tag("h1", text: /\AHow to prepare images for notification/)
+    end
+  end
+end
