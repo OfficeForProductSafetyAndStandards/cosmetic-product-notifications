a244e753974cf0682c765d09d7e99155a874c8bb
Frankie Roberto <frankie@frankieroberto.com>
2019-11-04 09:39:09 +0000

    Revert migration

--- cosmetics-web/db/migrate/20191101135303_add_unique_index_on_component_names.rb
+++
@@ -1,7 +0,0 @@
-class AddUniqueIndexOnComponentNames < ActiveRecord::Migration[5.2]
-  def change
-    safety_assured do
-      add_index :components, %i[name notification_id], unique: true
-    end
-  end
-end
--- cosmetics-web/db/schema.rb
+++ cosmetics-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_135303) do
+ActiveRecord::Schema.define(version: 2019_10_31_140425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 2019_11_01_135303) do
     t.float "minimum_ph"
     t.float "maximum_ph"
     t.text "ph"
-    t.index ["name", "notification_id"], name: "index_components_on_name_and_notification_id", unique: true
     t.index ["notification_id"], name: "index_components_on_notification_id"
   end
 
