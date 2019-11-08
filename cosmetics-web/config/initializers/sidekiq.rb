e0479ee3d79fca456414275637e898019836050e
Frankie Roberto <frankie@frankieroberto.com>
2019-11-08 10:27:25 +0000

    Remove old Sidekiq job (#1455)

--- cosmetics-web/config/initializers/sidekiq.rb
+++ cosmetics-web/config/initializers/sidekiq.rb
@@ -1,16 +1,3 @@
-def create_remove_expired_email_verification_keys_job
-  remove_expired_responsible_person_keys_job = Sidekiq::Cron::Job.new(
-    name: 'remove expired email verification keys, midnight every sunday',
-    cron: '0 0 * * 0',
-    class: 'RemoveExpiredResponsiblePersonKeysJob',
-    queue: 'cosmetics'
-  )
-  unless remove_expired_responsible_person_keys_job.save
-    Rails.logger.error "***** WARNING - Removing expired responsible person keys job was not saved! *****"
-    Rails.logger.error remove_expired_responsible_person_keys_job.errors.join("; ")
-  end
-end
-
 def create_log_db_metrics_job
   log_db_metrics_job = Sidekiq::Cron::Job.new(
     name: 'log db metrics, every day at 1 am',
@@ -26,7 +13,6 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = Rails.application.config_for(:redis)
-  create_remove_expired_email_verification_keys_job
   create_log_db_metrics_job
 end
 
