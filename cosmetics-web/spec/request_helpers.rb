d718ae6cdb90ad01343da0359069902c7a926414
Frankie Roberto <frankie@frankieroberto.com>
2019-11-14 16:05:52 +0000

    Refactor into a module
    This de-duplicates the step of parsing the html with Nokogiri.

--- cosmetics-web/spec/request_helpers.rb
+++ cosmetics-web/spec/request_helpers.rb
@@ -1,18 +1,34 @@
+require "nokogiri"
+
 # Adds a custom matcher asserting that a given HTML string contains
-# a 'Back link' component to the URL path given.
-RSpec::Matchers.define :have_back_link_to do |expected_path|
-  match do |actual|
-    body = Nokogiri::HTML(actual).at_css("body")
-    back_link = body.at_css("a.govuk-back-link")
-    back_link && back_link["href"] == expected_path
-  end
-  failure_message do |actual|
-    body = Nokogiri::HTML(actual).at_css("body")
-    back_link = body.at_css("a.govuk-back-link")
-    if back_link
-      "expected a back to #{expected_path}, but was #{back_link['href']}"
-    else
-      "expected:\n\n#{body.to_xhtml(indent: 3)}\n\n ...to contain a back link using a 'govuk-back-link' class"
+# a GOV.UK 'Back link' component to the URL path given.
+module HaveBackLinkToMatcher
+  class HaveBackLinkToMatcher
+    def initialize(expected_path)
+      @expected_path = expected_path
+    end
+
+    def matches?(html)
+      @html = html
+      @body = Nokogiri::HTML(@html).at_css("body")
+      @back_link = @body.at_css("a.govuk-back-link")
+      @back_link && @back_link["href"] == @expected_path
+    end
+
+    def failure_message
+      if @back_link
+        "expected a back to #{@expected_path}, but was #{@back_link['href']}"
+      else
+        "expected:\n\n#{@body.to_xhtml(indent: 3)}\n\n ...to contain a back link using a 'govuk-back-link' class"
+      end
     end
   end
+
+  def have_back_link_to(html)
+    HaveBackLinkToMatcher.new(html)
+  end
+end
+
+RSpec.configure do |config|
+  config.include HaveBackLinkToMatcher
 end
