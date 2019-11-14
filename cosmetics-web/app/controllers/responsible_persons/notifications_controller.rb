d5d6edd494a853cfb0558b5d7af0e790d482460e
Frankie Roberto <frankie@frankieroberto.com>
2019-11-14 16:19:40 +0000

    Refactor to private method
    This hopefully improves readability.

--- cosmetics-web/app/controllers/responsible_persons/notifications_controller.rb
+++ cosmetics-web/app/controllers/responsible_persons/notifications_controller.rb
@@ -38,20 +38,7 @@ class ResponsiblePersons::NotificationsController < ApplicationController
 
   # Check your answers page
   def edit
-    if @notification.notified_post_eu_exit?
-
-      # Last question is image upload page
-      @previous_page_path = responsible_person_notification_build_path(@notification.responsible_person, @notification, :add_product_image)
-    else
-
-      # Last component questions were answered for is the one with the highest ID
-      last_component = @notification.components.order(:id).last
-
-      # Last question was either pH question or exact pH range for the component
-      page = last_component.minimum_ph ? :ph : :select_ph_range
-
-      @previous_page_path = responsible_person_notification_component_trigger_question_path(@notification.responsible_person, @notification, last_component, page)
-    end
+    @previous_page_path = previous_path_before_check_your_answers(@notification)
 
     if params[:submit_failed]
       add_image_upload_errors
@@ -67,6 +54,26 @@ class ResponsiblePersons::NotificationsController < ApplicationController
   end
 
 private
+
+  # Returns the path for the page the user must have been on prior to
+  # the 'Check your answers' page. This varies depending on the route
+  # through the various sets of questions.
+  def previous_path_before_check_your_answers(notification)
+    if notification.notified_post_eu_exit?
+
+      # Last question is image upload page
+      responsible_person_notification_build_path(notification.responsible_person, notification, :add_product_image)
+    else
+
+      # Last component questions were answered for is the one with the highest ID
+      last_component = notification.components.order(:id).last
+
+      # Last question was either pH question or exact pH range for the component
+      page = last_component.minimum_ph ? :ph : :select_ph_range
+
+      responsible_person_notification_component_trigger_question_path(notification.responsible_person, notification, last_component, page)
+    end
+  end
 
   def set_responsible_person
     @responsible_person = ResponsiblePerson.find(params[:responsible_person_id])
