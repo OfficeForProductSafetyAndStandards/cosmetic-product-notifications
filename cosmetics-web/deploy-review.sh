887e48639ce72a5034ecc9406ef3f9ed4987ac79
Frankie Roberto <frankie@frankieroberto.com>
2019-11-21 22:57:27 +0000

    Single review app with multiple processes (#1476)

--- cosmetics-web/deploy-review.sh
+++ cosmetics-web/deploy-review.sh
@@ -12,48 +12,42 @@ INSTANCE_NAME=cosmetics-$REVIEW_INSTANCE_NAME
 SEARCH_APP=$INSTANCE_NAME-search-web
 SUBMIT_APP=$INSTANCE_NAME-submit-web
 
-WEB=$INSTANCE_NAME-web
-WORKER=$INSTANCE_NAME-worker
+APP=$INSTANCE_NAME
 
 DOMAIN=london.cloudapps.digital
 
 # Please note new manifest file
 MANIFEST_FILE=./cosmetics-web/manifest.review.yml
-MANIFEST_FILE_WORKER=./cosmetics-worker/manifest.review.worker.yml
 
 if [ -z "$DB_NAME" ]
 then
   DB_NAME=cosmetics-review-database
 fi
-cf create-service postgres small-10 $DB_NAME
+cf7 create-service postgres small-10 $DB_NAME
 
 # Wait until db is prepared, might take up to 10 minutes
-until cf service $DB_NAME > /tmp/db_exists && grep "create succeeded" /tmp/db_exists; do sleep 20; echo "Waiting for db"; done
+until cf7 service $DB_NAME > /tmp/db_exists && grep "create succeeded" /tmp/db_exists; do sleep 20; echo "Waiting for db"; done
 
 if [ -z "$REDIS_NAME" ]
 then
   REDIS_NAME=cosmetics-review-redis
 fi
-cf create-service redis tiny-3.2 $REDIS_NAME
+cf7 create-service redis tiny-3.2 $REDIS_NAME
 
 # Wait until redis service is prepared, might take up to 10 minutes
-until cf service $REDIS_NAME > /tmp/redis_exists && grep "create succeeded" /tmp/redis_exists; do sleep 20; echo "Waiting for redis"; done
+until cf7 service $REDIS_NAME > /tmp/redis_exists && grep "create succeeded" /tmp/redis_exists; do sleep 20; echo "Waiting for redis"; done
 
 
 # Copy files from infrastructure env
 cp -a ./infrastructure/env/. ./cosmetics-web/env/
 
 # Deploy the submit app and set the hostname
-cf push $WEB -f $MANIFEST_FILE --no-start --var cosmetics-instance-name=$INSTANCE_NAME --var cosmetics-web-database=$DB_NAME --var submit-host=$SUBMIT_APP.$DOMAIN --var search-host=$SEARCH_APP.$DOMAIN --var cosmetics-host=$SUBMIT_APP.$DOMAIN --var cosmetics-redis-service=$REDIS_NAME
+cf7 push $APP -f $MANIFEST_FILE --no-start --var cosmetics-instance-name=$INSTANCE_NAME --var cosmetics-web-database=$DB_NAME --var submit-host=$SUBMIT_APP.$DOMAIN --var search-host=$SEARCH_APP.$DOMAIN --var cosmetics-host=$SUBMIT_APP.$DOMAIN --var cosmetics-redis-service=$REDIS_NAME
 
-# Deploy worker
-cf push $WORKER -f $MANIFEST_FILE_WORKER --no-start --var cosmetics-instance-name=$INSTANCE_NAME --var cosmetics-web-database=$DB_NAME --var submit-host=$SUBMIT_APP.$DOMAIN --var search-host=$SEARCH_APP.$DOMAIN --var cosmetics-host=$SEARCH_APP.$DOMAIN --var cosmetics-redis-service=$REDIS_NAME
+cf7 set-env $APP SENTRY_CURRENT_ENV $REVIEW_INSTANCE_NAME
 
-cf set-env $WEB SENTRY_CURRENT_ENV $REVIEW_INSTANCE_NAME
-cf set-env $WORKER SENTRY_CURRENT_ENV $REVIEW_INSTANCE_NAME
-
-cf start $WEB
-cf start $WORKER
+cf7 start $APP
+cf7 scale $APP --process worker -i 1
 
 # Remove the copied infrastructure env files to clean up
 rm -R cosmetics-web/env/
--- infrastructure/ci/install-cf.sh
+++ infrastructure/ci/install-cf.sh
@@ -11,3 +11,4 @@ wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key |
 echo 'deb https://packages.cloudfoundry.org/debian stable main' | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 sudo apt-get -qq update
 sudo apt-get install cf-cli
+sudo apt-get install cf7-cli
