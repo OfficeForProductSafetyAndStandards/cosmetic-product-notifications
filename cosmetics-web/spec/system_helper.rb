fe1c6fdca5eb216ac983b321ca538dd79027eb37
Slawosz Slawinski <slawosz@gmail.com>
2019-11-22 14:33:20 +0000

    Debug

--- cosmetics-web/spec/system_helper.rb
+++ cosmetics-web/spec/system_helper.rb
@@ -1,6 +1,5 @@
 require "capybara/rspec"
-#require "selenium-webdriver"
-require 'rack/test'
+require "selenium-webdriver"
 
 ENV["HTTP_HOST"] = "localhost"
 ENV["HTTP_PORT"] = "3003"
@@ -8,9 +7,9 @@ ENV["HTTP_PORT"] = "3003"
 ENV["SUBMIT_HOST"] = "localhost"
 ENV["SEARCH_HOST"] = "localhost"
 
-# if ENV["CHROMEDRIVER_PATH"]
-#   Selenium::WebDriver::Chrome::Service.driver_path = ENV.fetch("CHROMEDRIVER_PATH")
-# end
+if ENV["CHROMEDRIVER_PATH"]
+  Selenium::WebDriver::Chrome::Service.driver_path = ENV.fetch("CHROMEDRIVER_PATH")
+end
 
 Capybara.server_host = ENV["HTTP_HOST"]
 Capybara.server_port = ENV["HTTP_PORT"]
@@ -21,26 +20,26 @@ Rails.application.routes.default_url_options = { host: ENV["HTTP_HOST"], port: E
 Capybara.server = :puma, { Silent: true }
 
 # Register headless Chrome as a Selenium driver (default RackTest driver does not support JavaScript)
-#Capybara.register_driver :chrome_headless do |app|
-#  options = ::Selenium::WebDriver::Chrome::Options.new
-#  options.add_argument("--headless")
-#  options.add_argument("--no-sandbox")
-#  options.add_argument("--disable-dev-shm-usage")
-#  options.add_argument("--window-size=1400,1400")
-#
-#  client = ::Selenium::WebDriver::Remote::Http::Default.new
-#  client.read_timeout = 180
-#
-#  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, http_client: client)
-#end
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--window-size=1400,1400")
 
-#Capybara.javascript_driver = :chrome_headless
+  client = ::Selenium::WebDriver::Remote::Http::Default.new
+  client.read_timeout = 180
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, http_client: client)
+end
+
+Capybara.javascript_driver = :chrome_headless
 
 Capybara.ignore_hidden_elements = false
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :rack_test
+    driven_by :chrome_headless
     ActiveJob::Base.queue_adapter = :inline
   end
 end
