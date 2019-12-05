7942e9fafd6d66d53df00973c1139acd48ed5411
Frankie Roberto <frankie@frankieroberto.com>
2019-12-05 14:53:07 +0000

    Bugfix: highlight errors in file upload field
    Also switched to using an externally defined `govukFileUpload` helper for improved HTML output.

--- cosmetics-web/config/initializers/shared_rails_config.rb
+++ cosmetics-web/config/initializers/shared_rails_config.rb
@@ -11,3 +11,7 @@ ActionView::Base.include GovukDesignSystem::DetailsHelper
 ActionView::Base.include GovukDesignSystem::WarningTextHelper
 ActionView::Base.include GovukDesignSystem::BackLinkHelper
 ActionView::Base.include GovukDesignSystem::SummaryListHelper
+ActionView::Base.include GovukDesignSystem::FileUploadHelper
+ActionView::Base.include GovukDesignSystem::ErrorMessageHelper
+ActionView::Base.include GovukDesignSystem::LabelHelper
+ActionView::Base.include GovukDesignSystem::HintHelper
