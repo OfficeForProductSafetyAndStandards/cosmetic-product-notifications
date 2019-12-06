e05f63fab5dddbd21b2bc21015fd13ea30429e1c
Yomi Colledge <baphled@boodah.net>
2019-12-06 14:56:35 +0000

    Refactor ImageUpload factory so the default always has an attached file

--- cosmetics-web/spec/factories/image_upload.rb
+++ cosmetics-web/spec/factories/image_upload.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :image_upload do
+    file { Rack::Test::UploadedFile.new("spec/fixtures/testPdf.pdf", "image/png") }
+
     trait :uploaded_and_virus_scanned do
       after :create do |image_upload|
-        file = fixture_file_upload(Rails.root.join("spec", "fixtures", "testImage.png"), "image/png")
-        image_upload.file.attach(file)
         image_upload.file.metadata["safe"] = "true"
       end
     end
