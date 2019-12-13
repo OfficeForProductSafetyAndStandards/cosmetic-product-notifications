87fb158baf9b95214d611b0c3e198660e0ae82c1
Frankie Roberto <frankie@frankieroberto.com>
2019-12-13 11:48:09 +0000

    Fix variable name

--- cosmetics-web/deploy.sh
+++ cosmetics-web/deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-APP=$APP_NAME
-
 if [ -z "$SUBMIT_HOST" ]
 then
   echo "Please set your submit host, eg SUBMIT_HOST=submit.cosmetic-product-notifications.service.gov.uk"
@@ -22,9 +20,9 @@ MANIFEST_FILE=./cosmetics-web/manifest.yml
 cp -a ./infrastructure/env/. ./cosmetics-web/env/
 
 # Deploy the submit app and set the hostname
-cf7 push $APP -f $MANIFEST_FILE --var cosmetics-instance-name=$INSTANCE_NAME --var cosmetics-web-database=$DB_NAME --var submit-host=$SUBMIT_APP.$DOMAIN --var search-host=$SEARCH_APP.$DOMAIN --var cosmetics-host=$SUBMIT_APP.$DOMAIN --var cosmetics-redis-service=$REDIS_NAME --strategy rolling
+cf7 push $APP_NAME -f $MANIFEST_FILE --var app-name=$APP_NAME --var cosmetics-web-database=$DB_NAME --var submit-host=$SUBMIT_APP.$DOMAIN --var search-host=$SEARCH_APP.$DOMAIN --var cosmetics-host=$SUBMIT_APP.$DOMAIN --var cosmetics-redis-service=$REDIS_NAME --strategy rolling
 
-cf7 scale $APP --process worker -i 1
+cf7 scale $APP_NAME --process worker -i 1
 
 # Remove the copied infrastructure env files to clean up
 rm -R cosmetics-web/env/
