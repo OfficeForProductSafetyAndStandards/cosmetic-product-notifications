60917de13f40388c9fdc48f6921760c611152bfe
Slawosz Slawinski <slawosz@gmail.com>
2020-01-06 14:30:22 +0000

    Dont change logstash behaviour

--- cosmetics-web/config/initializers/lograge.rb
+++ cosmetics-web/config/initializers/lograge.rb
@@ -1,13 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.configure do
-  if Rails.env.production?
-    config.lograge.enabled = ENV.fetch("LOGRAGE_ENABLED", "true") == "true"
+  config.lograge.enabled = ENV.fetch("LOGRAGE_ENABLED", "true") == "true"
 
-    config.lograge.custom_payload do |_controller|
-      extra_payload = {}
-      extra_payload[:user_id] = User.current&.id
-      extra_payload
-    end
+  config.lograge.custom_payload do |_controller|
+    extra_payload = {}
+    extra_payload[:user_id] = User.current&.id
+    extra_payload
   end
 end
