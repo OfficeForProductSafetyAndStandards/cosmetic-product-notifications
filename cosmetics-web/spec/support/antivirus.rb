53d8806f176838bbec08342c54955346218a7a91
Frankie Roberto <frankie@frankieroberto.com>
2020-01-09 15:00:26 +0000

    Set a default for antivirus url

--- cosmetics-web/spec/support/antivirus.rb
+++ cosmetics-web/spec/support/antivirus.rb
@@ -1,7 +1,8 @@
 RSpec.shared_context "with stubbed Antivirus API", shared_context: :metadata do
   before do
+    antivirus_url = Rails.application.config.antivirus_url
     stubbed_response = JSON.generate(safe: true)
-    stub_request(:any, /#{Regexp.quote(ENV["ANTIVIRUS_URL"])}/).to_return(body: stubbed_response, status: 200)
+    stub_request(:any, /#{Regexp.quote(antivirus_url)}/).to_return(body: stubbed_response, status: 200)
   end
 end
 
@@ -12,8 +13,9 @@ end
 
 RSpec.shared_context "with stubbed Antivirus API returning false", shared_context: :metadata do
   before do
+    antivirus_url = Rails.application.config.antivirus_url
     stubbed_response = JSON.generate(safe: false)
-    stub_request(:any, /#{Regexp.quote(ENV["ANTIVIRUS_URL"])}/).to_return(body: stubbed_response, status: 200)
+    stub_request(:any, /#{Regexp.quote(antivirus_url)}/).to_return(body: stubbed_response, status: 200)
   end
 end
 
