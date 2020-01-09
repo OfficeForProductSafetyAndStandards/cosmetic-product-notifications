2758c205bba6f38efb05754a4646deb96a4d1525
Frankie Roberto <frankie@frankieroberto.com>
2020-01-09 11:50:41 +0000

    Add another zip file scenario

--- cosmetics-web/spec/features/zip_file_upload_pre_brexit_notifications_spec.rb
+++ cosmetics-web/spec/features/zip_file_upload_pre_brexit_notifications_spec.rb
@@ -127,4 +127,57 @@ RSpec.describe "ZIP file upload, pre-Brexit notifications", type: :feature do
     expect_to_be_on_your_cosmetic_products_page
     expect_to_see_message "Beautify Facial Night Cream notification submitted"
   end
+
+  scenario "Using a zip file, pre-Brexit, single item, containing nanomaterials, with missing formulation document", :with_stubbed_antivirus do
+    visit new_responsible_person_add_notification_path(responsible_person)
+
+    expect_to_be_on_was_eu_notified_about_products_page
+    answer_was_eu_notified_with "Yes"
+
+    expect_to_be_on_do_you_have_the_zip_files_page
+    answer_do_you_have_zip_files_with "Yes"
+
+    expect_to_be_on_upload_eu_notification_files_page
+    upload_zip_file "testNanomaterialAndMissingFormulation.zip"
+
+    visit responsible_person_notifications_path(responsible_person)
+
+    expect_to_see_incomplete_notification_with_eu_reference_number "1006034"
+    click_link "Add missing information"
+
+    expect_to_be_on_what_is_the_purpose_of_nanomaterial_page nanomaterial_name: "TRIS-BIPHENYL TRIAZINE / TRIS-BIPHENYL TRIAZINE (NANO)"
+    answer_what_is_purpose_of_nanomaterial_with "Colourant", nanomaterial_name: "TRIS-BIPHENYL TRIAZINE / TRIS-BIPHENYL TRIAZINE (NANO)"
+
+    expect_to_be_on_is_nanomaterial_listed_in_ec_regulation_page nanomaterial_name: "TRIS-BIPHENYL TRIAZINE / TRIS-BIPHENYL TRIAZINE (NANO)"
+    answer_is_nanomaterial_listed_in_ec_regulation_with "Yes", nanomaterial_name: "TRIS-BIPHENYL TRIAZINE / TRIS-BIPHENYL TRIAZINE (NANO)"
+
+    expect_to_be_on_does_nanomaterial_conform_to_restrictions_page nanomaterial_name: "TRIS-BIPHENYL TRIAZINE / TRIS-BIPHENYL TRIAZINE (NANO)"
+    answer_does_nanomaterial_conform_to_restrictions_with "Yes", nanomaterial_name: "TRIS-BIPHENYL TRIAZINE / TRIS-BIPHENYL TRIAZINE (NANO)"
+
+    exect_to_be_on_upload_formulation_document_page
+    upload_formulation_file
+
+
+    expect_to_be_on_check_your_answers_page(product_name: "SkinSoft shocking green hair dye")
+    expect_check_your_answers_page_to_contain(
+      product_name: "SkinSoft shocking green hair dye",
+      imported: "Yes",
+      imported_from: "China",
+      number_of_components: "1",
+      shades: "",
+      eu_notification_date: "29 November 2019",
+      contains_cmrs: "No",
+      nanomaterials: "1,3,5-Triazine, 2,4,6-tris(1,1, TRIS-BIPHENYL TRIAZINE / TRIS-BIPHENYL TRIAZINE (NANO), 31274-51-8, 31274-51-8",
+      category: "Hair and scalp products",
+      subcategory: "Hair colouring products",
+      sub_subcategory: "Oxidative hair colour products",
+      formulation_given_as: "Exact concentration",
+      frame_formulation: "Hair Colorant (Permanent, Oxidative Type) - Type 1 : Two Components - Colorant Part",
+      physical_form: "Cream or paste",
+    )
+    click_button "Accept and submit the cosmetic product notification"
+
+    expect_to_be_on_your_cosmetic_products_page
+    expect_to_see_message "SkinSoft shocking green hair dye"
+  end
 end
