2b7e8676605cf0df630fe77ee7d3dace126aa408
Frankie Roberto <frankie@frankieroberto.com>
2020-01-16 11:26:53 +0000

    Replace declaration system spec with a request spec
    This remove the "system" test (which uses a full browser) for the user declaration, with a request spec (which simulates a browser by sending HTTP requests directly to the app).
    
    The objective here is to speed up the test suite by removing the dependency on having a full browser.

---
+++ cosmetics-web/spec/requests/declaration_requests_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.describe "User declarations", :with_stubbed_antivirus, type: :request do
+  after do
+    sign_out
+  end
+
+  context "when signed in as a company user" do
+    let(:responsible_person) { create(:responsible_person) }
+    let(:user) { build(:user, first_login: true) }
+
+    before do
+      sign_in_as_member_of_responsible_person(responsible_person, user)
+    end
+
+    context "viewing the declaration page" do
+      before do
+        get "/declaration"
+      end
+
+      it "renders the page" do
+        expect(response.status).to be(200)
+        expect(response).to render_template("declaration/business_declaration")
+      end
+    end
+
+    context "submitting the declaration page" do
+      before do
+        post "/declaration/accept"
+      end
+
+      it "redirects to the homepage" do
+        expect(response).to redirect_to("/")
+      end
+
+      it "updates the user attributes" do
+        expect(user.has_accepted_declaration?).to be true
+      end
+    end
+  end
+end
