fec761f3702ef96604ee6ef97f5e21f00d7cb2fb
nasirkhanbeis <nasirkhan.beis@gmail.com>
2020-01-17 15:21:49 +0000

    updated test as per the review comments

--- cosmetics-web/spec/features/responsible_persons/create_rp_user_spec.rb
+++ cosmetics-web/spec/features/responsible_persons/create_rp_user_spec.rb
@@ -1,25 +1,24 @@
 require "rails_helper"
 
-RSpec.describe "Resposible person creation journey", type: :feature do
-  let(:responsible_person) { create(:responsible_person_with_user, :with_a_contact_person) }
+RSpec.describe "Responsible person creation journey", type: :feature do
+  context "Given as a new user," do
+    let(:user) { create(:user, first_login: true) }
 
-  before do
-    sign_in as_user: create(:user, first_login: true)
-    stub_notify_mailer
+    before do
+      sign_in as_user: user
+      stub_notify_mailer
+      configure_requests_for_submit_domain
+    end
+
+    scenario "I should be able to create responsible user as a individual sole trader" do
+      create_individual_responsible_person
+      expect(page).to have_h1("Your cosmetic products")
+    end
+
+
+    scenario "I should be able to create responsible user as a limited company" do
+      create_business_responsible_person
+      expect(page).to have_h1("Your cosmetic products")
+    end
   end
-
-   scenario "Given as a new user, I should be able to create responsible user as a individual sole trader" do
-    configure_requests_for_submit_domain
-	create_individual_responsible_person
-	expect(page).to have_h1("Your cosmetic products")
-   end
-
-
-   scenario "Given as a new user, I should be able to create responsible user as a limited company" do
-    configure_requests_for_submit_domain
-	create_business_responsible_person
-	expect(page).to have_h1("Your cosmetic products")
-   end
-
 end
-
--- cosmetics-web/spec/support/feature_helpers.rb
+++ cosmetics-web/spec/support/feature_helpers.rb
@@ -535,71 +535,67 @@ end
 #Create responsible person helpers
 
 def go_to_rp_declaration_page
-    visit(root_path)
-    expect(page).to have_h1("Responsible Person Declaration")
-    click_button 'I confirm'
-    expect(page).to have_h1("Are you or your organisation a UK Responsible Person?")
-    click_on 'Continue'
-    expect(page).to have_h1("Does anyone in your organisation have an account to submit cosmetic product notifications in the UK?")
-     choose "option_create_new", visible: false
-     click_on "Continue"
-    expect(page).to have_h1("Is the UK Responsible Person a business or an individual?")
+  visit(root_path)
+  expect(page).to have_h1("Responsible Person Declaration")
+  click_button "I confirm"
+  expect(page).to have_h1("Are you or your organisation a UK Responsible Person?")
+  click_on "Continue"
+  expect(page).to have_h1("Does anyone in your organisation have an account to submit cosmetic product notifications in the UK?")
+  choose "option_create_new", visible: false
+  click_on "Continue"
+  expect(page).to have_h1("Is the UK Responsible Person a business or an individual?")
 end
 
-
-
-
 def create_individual_responsible_person
-    go_to_rp_declaration_page
-    select_individual_account_type
+  go_to_rp_declaration_page
+  select_individual_account_type
 
-    assert_text "UK Responsible Person details"
+  assert_text "UK Responsible Person details"
 
-    fill_in "Name", with: responsible_person.name
-    fill_in "Building and street", with: responsible_person.address_line_1
-    fill_in "Town or city", with: responsible_person.city
-    fill_in "County", with: responsible_person.county
-    fill_in "Postcode", with: responsible_person.postal_code
-    click_on "Continue"
+  fill_in "Name", with: responsible_person.name
+  fill_in "Building and street", with: responsible_person.address_line_1
+  fill_in "Town or city", with: responsible_person.city
+  fill_in "County", with: responsible_person.county
+  fill_in "Postcode", with: responsible_person.postal_code
+  click_on "Continue"
 
-    assert_text "contact person"
+  assert_text "contact person"
 
-    fill_in "Full name", with: responsible_person.contact_persons.first.name
-    fill_in "Email address", with: responsible_person.contact_persons.first.email_address
-    fill_in "Phone number", with: responsible_person.contact_persons.first.phone_number
-    click_on "Continue"
+  fill_in "Full name", with: responsible_person.contact_persons.first.name
+  fill_in "Email address", with: responsible_person.contact_persons.first.email_address
+  fill_in "Phone number", with: responsible_person.contact_persons.first.phone_number
+  click_on "Continue"
   end
 
-  def create_business_responsible_person
-    go_to_rp_declaration_page
-    select_business_account_type
+def create_business_responsible_person
+  go_to_rp_declaration_page
+  select_business_account_type
 
-    assert_text "UK Responsible Person details"
+  assert_text "UK Responsible Person details"
 
-    fill_in "Business name", with: business_responsible_person.name
-    fill_in "Building and street", with: business_responsible_person.address_line_1
-    fill_in "Town or city", with: business_responsible_person.city
-    fill_in "County", with: business_responsible_person.county
-    fill_in "Postcode", with: business_responsible_person.postal_code
-    click_on "Continue"
+  fill_in "Business name", with: business_responsible_person.name
+  fill_in "Building and street", with: business_responsible_person.address_line_1
+  fill_in "Town or city", with: business_responsible_person.city
+  fill_in "County", with: business_responsible_person.county
+  fill_in "Postcode", with: business_responsible_person.postal_code
+  click_on "Continue"
 
-    assert_text "contact person"
+  assert_text "contact person"
 
-    fill_in "Full name", with: business_responsible_person.contact_persons.first.name
-    fill_in "Email address", with: business_responsible_person.contact_persons.first.email_address
-    fill_in "Phone number", with: business_responsible_person.contact_persons.first.phone_number
-    click_on "Continue"
-  end
+  fill_in "Full name", with: business_responsible_person.contact_persons.first.name
+  fill_in "Email address", with: business_responsible_person.contact_persons.first.email_address
+  fill_in "Phone number", with: business_responsible_person.contact_persons.first.phone_number
+  click_on "Continue"
+end
 
+def select_business_account_type
+  assert_text "Is the UK Responsible Person a business or an individual?"
+  choose "responsible_person_account_type_business", visible: false
+  click_on "Continue"
+end
 
-  def select_business_account_type
-    assert_text "Is the UK Responsible Person a business or an individual?"
-    choose "responsible_person_account_type_business", visible: false
-    click_on "Continue"
-  end
-
-  def select_individual_account_type
-    assert_text "Is the UK Responsible Person a business or an individual?"
-    choose "responsible_person_account_type_individual", visible: false
-    click_on "Continue"
-  end
+def select_individual_account_type
+  assert_text "Is the UK Responsible Person a business or an individual?"
+  choose "responsible_person_account_type_individual", visible: false
+  click_on "Continue"
+end
