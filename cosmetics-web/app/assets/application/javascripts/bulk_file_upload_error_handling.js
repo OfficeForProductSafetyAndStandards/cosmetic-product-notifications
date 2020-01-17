ac7082ed23355ca155f10484f0299706e0c61298
Frankie Roberto <frankie@frankieroberto.com>
2020-01-17 15:28:22 +0000

    Update all javascript files to follow standard js linter rules

--- cosmetics-web/app/assets/application/javascripts/bulk_file_upload_error_handling.js
+++ cosmetics-web/app/assets/application/javascripts/bulk_file_upload_error_handling.js
@@ -1,32 +1,32 @@
-import $ from 'jquery';
+import $ from 'jquery'
 
 $(document).ready(() => {
-  const fileUploadForm = document.getElementById('file-upload-form');
-  const fileInput = document.getElementById('uploaded_files');
-  const { maxNumberOfFiles } = fileUploadForm.dataset;
-  const errorSummary = document.getElementsByClassName('error-summary-js')[0];
-  const fileUploadFormGroup = document.getElementById('file-upload-form-group');
-  const fileUploadErrorMessage = document.getElementById('file-upload-error-message');
+  const fileUploadForm = document.getElementById('file-upload-form')
+  const fileInput = document.getElementById('uploaded_files')
+  const { maxNumberOfFiles } = fileUploadForm.dataset
+  const errorSummary = document.getElementsByClassName('error-summary-js')[0]
+  const fileUploadFormGroup = document.getElementById('file-upload-form-group')
+  const fileUploadErrorMessage = document.getElementById('file-upload-error-message')
 
-  const tooManyFilesMessage = `You can only select up to ${maxNumberOfFiles} files at the same time`;
-  const noFilesSelectedMessage = 'Select a file';
+  const tooManyFilesMessage = `You can only select up to ${maxNumberOfFiles} files at the same time`
+  const noFilesSelectedMessage = 'Select a file'
 
-  const errorSummaryText = errorSummary.getElementsByClassName('govuk-error-summary__list')[0].getElementsByTagName('A')[0];
+  const errorSummaryText = errorSummary.getElementsByClassName('govuk-error-summary__list')[0].getElementsByTagName('A')[0]
 
   fileUploadForm.addEventListener('submit', (event) => {
     if (fileInput.files.length === 0) {
-      errorSummary.style.display = 'inline';
-      fileUploadFormGroup.classList.add('govuk-form-group--error');
-      fileUploadErrorMessage.innerHTML = noFilesSelectedMessage;
-      errorSummaryText.innerHTML = noFilesSelectedMessage;
+      errorSummary.style.display = 'inline'
+      fileUploadFormGroup.classList.add('govuk-form-group--error')
+      fileUploadErrorMessage.innerHTML = noFilesSelectedMessage
+      errorSummaryText.innerHTML = noFilesSelectedMessage
     }
 
     if (fileInput.files.length > maxNumberOfFiles) {
-      event.preventDefault();
-      errorSummary.style.display = 'inline';
-      fileUploadFormGroup.classList.add('govuk-form-group--error');
-      fileUploadErrorMessage.innerHTML = tooManyFilesMessage;
-      errorSummaryText.innerHTML = tooManyFilesMessage;
+      event.preventDefault()
+      errorSummary.style.display = 'inline'
+      fileUploadFormGroup.classList.add('govuk-form-group--error')
+      fileUploadErrorMessage.innerHTML = tooManyFilesMessage
+      errorSummaryText.innerHTML = tooManyFilesMessage
     }
-  });
-});
+  })
+})
--- cosmetics-web/config/webpack/environment.js
+++ cosmetics-web/config/webpack/environment.js
@@ -1,20 +1,20 @@
-const { environment } = require('@rails/webpacker');
+const { environment } = require('@rails/webpacker')
 
 // We want to use a different postcss config for ie8 https://github.com/rails/webpacker/issues/1220
-delete environment.loaders.get('css').use.find(loader => loader.loader === 'postcss-loader').options.config;
-delete environment.loaders.get('sass').use.find(loader => loader.loader === 'postcss-loader').options.config;
+delete environment.loaders.get('css').use.find(loader => loader.loader === 'postcss-loader').options.config
+delete environment.loaders.get('sass').use.find(loader => loader.loader === 'postcss-loader').options.config
 
 // Copy JSON files to the output, rather than reading them as JS
 environment.loaders.append('json', {
   type: 'javascript/auto',
   test: /\.json$/,
-  use: [{ loader: 'file-loader' }],
-});
+  use: [{ loader: 'file-loader' }]
+})
 
 // resolve-url-loader must be used before sass-loader
 // See https://github.com/rails/webpacker/blob/master/docs/css.md#resolve-url-loader
 environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader',
-});
+  loader: 'resolve-url-loader'
+})
 
-module.exports = environment;
+module.exports = environment
