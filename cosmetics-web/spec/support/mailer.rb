adea5427e575f71b1da7d43170ad08629ee9f046
Frankie Roberto <frankie@frankieroberto.com>
2020-01-17 16:26:23 +0000

    Remove system test setup

--- cosmetics-web/spec/support/mailer.rb
+++ cosmetics-web/spec/support/mailer.rb
@@ -9,3 +9,10 @@ RSpec.configure do |rspec|
   rspec.include_context "with stubbed mailer", with_stubbed_mailer: true
 end
 # rubocop:enable RSpec/AnyInstance
+
+def stub_notify_mailer
+  result = double
+  allow(result).to receive(:deliver_later)
+  allow(NotifyMailer).to receive(:send_contact_person_verification_email) { result }
+  allow(NotifyMailer).to receive(:send_responsible_person_invite_email) { result }
+end
