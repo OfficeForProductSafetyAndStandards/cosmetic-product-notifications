95b2ab84f78877ada1fa2d6eefa85b926bb67566
Slawosz Slawinski <slawosz@gmail.com>
2020-02-04 11:55:36 +0000

    Typo

--- cosmetics-web/spec/rails_helper.rb
+++ cosmetics-web/spec/rails_helper.rb
@@ -15,7 +15,7 @@ require 'simplecov'
 require 'simplecov-lcov'
 
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov::Formatter::LcovFormatter.config.cingle_report_path = 'coverage/lcov.info'
+SimpleCov::Formatter::LcovFormatter.config.single_report_path = 'coverage/lcov.info'
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start
 
