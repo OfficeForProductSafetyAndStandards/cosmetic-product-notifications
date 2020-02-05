416ac071f23512256f0b26b92d93e3a06eafd5fa
Marc Sardon <pringaohowto@gmail.com>
2020-02-05 16:40:56 +0000

    Cover Domain Exclusion Constraint with tests
    The constraint class wasn't covered by unit tests.
    The tests are basically the same as the ones written for the inclusion
    constraint, but with the assertions expecting opposite results.

---
+++ cosmetics-web/spec/lib/domain_exclusion_constraint_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe DomainExclusionConstraint, type: :constraint do
+  subject(:constraint) { described_class.new(excluded_domain) }
+
+  let(:excluded_domain) { "www.example.com" }
+  let(:other_domain) { "other.example.com" }
+
+
+  describe "#matches?" do
+    it "returns false for requests with the excluded domain" do
+      request = ActionDispatch::Request.new("HTTP_HOST" => excluded_domain)
+      expect(constraint.matches?(request)).to be false
+    end
+
+    it "returns true for requests with different domain from excluded one" do
+      request = ActionDispatch::Request.new("HTTP_HOST" => other_domain)
+      expect(constraint.matches?(request)).to be true
+    end
+
+    context "when configured with multiple comma-separated domains" do
+      let(:excluded_domain) { "www.example.com,excluded.example.com" }
+
+      it "returns false for requests with either excluded domain" do
+        request1 = ActionDispatch::Request.new("HTTP_HOST" => "www.example.com")
+        request2 = ActionDispatch::Request.new("HTTP_HOST" => "excluded.example.com")
+        expect(constraint.matches?(request1)).to be false
+        expect(constraint.matches?(request2)).to be false
+      end
+
+      it "returns true for requests with different domain from excluded ones" do
+        request = ActionDispatch::Request.new("HTTP_HOST" => other_domain)
+        expect(constraint.matches?(request)).to be true
+      end
+    end
+  end
+end
