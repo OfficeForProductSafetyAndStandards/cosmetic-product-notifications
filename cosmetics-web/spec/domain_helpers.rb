0e35ab30a80d707d6f945730b9997e7ae109c205
Marc Sardon <pringaohowto@gmail.com>
2020-02-10 13:37:30 +0000

    Stub actual request host in spec domain helpers
    We were stubbing the domain constraint checks methods without actual
    correlation between the specific domain we wanted to simulate and the
    actual stub.
    
    This caused test brittleness once we modified how we use the domain
    constraints for the routes.
    
    Also, being used on request and feature tests, we should lean towards
    end to end integration testing instead of method stubbing.
    
    By just stubbing the request host, we allow end to end testing while
    effectively faking which domain is being used during specific tests.

--- cosmetics-web/spec/domain_helpers.rb
+++ cosmetics-web/spec/domain_helpers.rb
@@ -1,27 +1,18 @@
 module DomainHelpers
   # rubocop:disable RSpec/AnyInstance
   def configure_requests_for_submit_domain
-    allow_any_instance_of(DomainExclusionConstraint).to receive(:matches?).and_return(false)
-    allow_any_instance_of(DomainInclusionConstraint).to receive(:matches?).and_return(true)
-
-    allow_any_instance_of(ApplicationController).to receive(:submit_domain?).and_return(true)
-    allow_any_instance_of(ApplicationController).to receive(:search_domain?).and_return(false)
+    allow_any_instance_of(ActionDispatch::Request)
+      .to receive(:host).and_return(ENV.fetch("SUBMIT_HOST"))
   end
 
   def configure_requests_for_search_domain
-    allow_any_instance_of(DomainExclusionConstraint).to receive(:matches?).and_return(false)
-    allow_any_instance_of(DomainInclusionConstraint).to receive(:matches?).and_return(true)
-
-    allow_any_instance_of(ApplicationController).to receive(:submit_domain?).and_return(false)
-    allow_any_instance_of(ApplicationController).to receive(:search_domain?).and_return(true)
+    allow_any_instance_of(ActionDispatch::Request)
+      .to receive(:host).and_return(ENV.fetch("SEARCH_HOST"))
   end
 
   def reset_domain_request_mocking
-    allow_any_instance_of(DomainInclusionConstraint).to receive(:matches?).and_call_original
-    allow_any_instance_of(DomainExclusionConstraint).to receive(:matches?).and_call_original
-
-    allow_any_instance_of(ApplicationController).to receive(:submit_domain?).and_call_original
-    allow_any_instance_of(ApplicationController).to receive(:search_domain?).and_call_original
+    allow_any_instance_of(ActionDispatch::Request)
+      .to receive(:host).and_call_original
   end
   # rubocop:enable RSpec/AnyInstance
 end
