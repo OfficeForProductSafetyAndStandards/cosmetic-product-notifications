c1b40c53cd8558f5cfde3d124d7ad2febfd77299
Slawosz Slawinski <slawosz@gmail.com>
2020-07-16 09:29:53 +0100

    Fix lograge

--- cosmetics-web/config/initializers/lograge.rb
+++ cosmetics-web/config/initializers/lograge.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   config.lograge.custom_payload do |controller|
     extra_payload = {}
-    extra_payload[:user_id] = controller.current_user&.id
+    extra_payload[:user_id] = controller.current_user&.id rescue nil
     extra_payload
   end
 end
