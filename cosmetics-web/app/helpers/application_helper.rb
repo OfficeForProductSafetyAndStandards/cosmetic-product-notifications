8374b2058aadbb3d258b2b8e95509b788f7eea17
Marc Sardon <pringaohowto@gmail.com>
2020-08-25 13:31:44 +0100

    Fix order of validation errors during registration

--- cosmetics-web/app/helpers/application_helper.rb
+++ cosmetics-web/app/helpers/application_helper.rb
@@ -4,10 +4,25 @@ module ApplicationHelper
     content_for(:page_title, title)
   end
 
-  def error_summary(errors)
+  def error_summary(errors, ordered_attributes = [])
     return unless errors.any?
 
-    error_list = errors.map { |attribute, error| error ? { text: error, href: "##{attribute}" } : nil }.compact
+    ordered_errors = ActiveSupport::OrderedHash.new
+    ordered_attributes.map { |attr| ordered_errors[attr] = [] }
+
+    errors.map do |attribute, error|
+      next if error.blank?
+
+      # Errors for attributes that are not included in the ordered list will be
+      # added at the end after the errors for ordered attributes.
+      if ordered_errors[attribute]
+        ordered_errors[attribute] << { text: error, href: "##{attribute}" }
+      else
+        ordered_errors[attribute] = [{ text: error, href: "##{attribute}" }]
+      end
+    end
+    error_list = ordered_errors.values.flatten.compact
+
     govukErrorSummary(titleText: "There is a problem", errorList: error_list)
   end
 end
