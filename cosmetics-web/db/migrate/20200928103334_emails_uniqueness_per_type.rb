5fdcc8f9f281c0ed297e3b8f483daa9d5c68d96a
Marc Sardon <pringaohowto@gmail.com>
2020-09-29 09:20:52 +0100

    Rubocop style fix

--- cosmetics-web/db/migrate/20200928103334_emails_uniqueness_per_type.rb
+++ cosmetics-web/db/migrate/20200928103334_emails_uniqueness_per_type.rb
@@ -2,7 +2,7 @@ class EmailsUniquenessPerType < ActiveRecord::Migration[5.2]
   def change
     safety_assured do
       remove_index :users, name: :index_users_on_email
-      add_index :users, [:type, :email], unique: true
+      add_index :users, %i[type email], unique: true
     end
   end
 end
