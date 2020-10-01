5095f1f4254fb683be5c95d20bb8719fe91a64ac
Slawosz Slawinski <slawosz@gmail.com>
2020-10-01 10:07:14 +0100

    Fix stupid errors

--- cosmetics-web/app/jobs/send_secondary_authentication_job.rb
+++ cosmetics-web/app/jobs/send_secondary_authentication_job.rb
@@ -2,10 +2,12 @@ class SendSecondaryAuthenticationJob < ApplicationJob
   def perform(user, code)
     if user.is_a? SubmitUser
       SendSubmitSMS.otp_code(mobile_number: user.mobile_number, code: code)
+      return
     end
     if user.is_a? SearchUser
       SendSearchSMS.otp_code(mobile_number: user.mobile_number, code: code)
+      return
     end
+    raise "Imposible to find SMS sending class for '#{user.class}'"
   end
-  raise "Imposible to find SMS sending class for '#{user.class}'"
 end
