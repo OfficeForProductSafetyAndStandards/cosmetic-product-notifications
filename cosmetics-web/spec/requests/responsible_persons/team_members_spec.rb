0a4f3c166b06080c432b8757eddb15762f39bc19
Slawosz Slawinski <slawosz@gmail.com>
2020-10-06 11:04:52 +0100

    Make sure noone can resend other person invitation

---
+++ cosmetics-web/spec/requests/responsible_persons/team_members_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Team members management", type: :request, with_stubbed_notify: true do
+  let(:responsible_person) { create(:responsible_person, :with_a_contact_person) }
+  let(:other_responsible_person) { create(:responsible_person, :with_a_contact_person) }
+
+  before do
+    sign_in_as_member_of_responsible_person(responsible_person)
+  end
+
+  after do
+    sign_out(:submit_user)
+  end
+
+  describe "Resending invitation" do
+    context "when invitation does not belongs to responsible person" do
+      let(:invitation) { create(:pending_responsible_person_user, responsible_person: other_responsible_person) }
+
+      it "returns 404" do
+        get resend_invitation_responsible_person_team_member_path(responsible_person, invitation)
+        expect(response.status).to eq(404)
+      end
+    end
+  end
+end
