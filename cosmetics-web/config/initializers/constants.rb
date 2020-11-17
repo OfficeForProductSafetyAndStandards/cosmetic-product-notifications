f735e709fdff176799d9fa31e81bd2528f4b3399
Slawosz Slawinski <slawosz@gmail.com>
2020-11-17 11:30:07 +0000

    Move eu exit dates - 933 and 938

--- cosmetics-web/config/initializers/constants.rb
+++ cosmetics-web/config/initializers/constants.rb
@@ -1,4 +1,4 @@
-EU_EXIT_DATE = Time.zone.parse("2020-01-31T23:00")
-SUBMISSION_WINDOW_END_DATE = EU_EXIT_DATE + 90.days
+EU_EXIT_DATE = Time.zone.parse("2021-01-01T00:00")
+SUBMISSION_WINDOW_END_DATE = Time.zone.parse("2021-03-31T23:00")
 Rails.application.config.domains_allowing_otp_whitelisting =
   YAML.load_file(Rails.root.join("config/constants/domains_allowing_otp_whitelisting.yml"))
