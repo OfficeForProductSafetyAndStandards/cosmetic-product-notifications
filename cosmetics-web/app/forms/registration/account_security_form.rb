3136ffe94aad860a2509a2f6e41ce94a653c4a04
Marc Sardon <pringaohowto@gmail.com>
2020-12-16 10:28:30 +0000

    Move form error localers to the right section
    So the error messages can be infered from the locales without having to
    manually specify the scope for each error.

--- cosmetics-web/app/forms/registration/account_security_form.rb
+++ cosmetics-web/app/forms/registration/account_security_form.rb
@@ -8,11 +8,7 @@ module Registration
     attribute :user
     attribute :full_name
 
-    private_class_method def self.error_message(attr, key)
-      I18n.t(key, scope: "account_security.#{attr}")
-    end
-
-    validates_presence_of :full_name, message: error_message(:full_name, :blank), if: :name_required?
+    validates_presence_of :full_name, if: :name_required?
     validates :mobile_number, presence: true
     validates :mobile_number,
               phone: { message: :invalid, allow_international: -> { user.class::ALLOW_INTERNATIONAL_PHONE_NUMBER } },
