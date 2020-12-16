1173421ea4c80526c1242432e0de63cfca462782
Marc Sardon <pringaohowto@gmail.com>
2020-12-16 09:26:22 +0000

    Move Form locales to the right yaml section
    So the error can be inferred from ActiveModel translation search
    without having to specify the scope/route.

--- cosmetics-web/app/forms/resend_secondary_authentication_code_form.rb
+++ cosmetics-web/app/forms/resend_secondary_authentication_code_form.rb
@@ -6,7 +6,7 @@ class ResendSecondaryAuthenticationCodeForm
   attribute :user
 
   validates_presence_of :mobile_number,
-                        message: I18n.t(:blank, scope: %i[resend_secondary_authentication_code_form mobile_number]),
+                        message: :blank,
                         if: -> { user.mobile_number_change_allowed? }
   validates :mobile_number,
             phone: { message: :invalid, allow_international: -> { user.class::ALLOW_INTERNATIONAL_PHONE_NUMBER } },
