ee006b258d1f25a78c6af5be33a5de0ab1a7f403
Marc Sardon <pringaohowto@gmail.com>
2020-12-17 18:34:37 +0000

    Refactor controller by extracting methods
    - Use of guard clause to reduce the nesting condition levels.
    - Extract small methods with descriptive names to make the controller
      easier to follow for the readers.

--- cosmetics-web/app/controllers/responsible_persons/product_image_upload_controller.rb
+++ cosmetics-web/app/controllers/responsible_persons/product_image_upload_controller.rb
@@ -4,28 +4,38 @@ class ResponsiblePersons::ProductImageUploadController < SubmitApplicationContro
   def new; end
 
   def create
-    if params[:image_upload].present?
-      params[:image_upload].each do |image|
-        image_upload = @notification.image_uploads.build
-        image_upload.file.attach(image)
-        image_upload.filename = image.original_filename
-      end
+    return render_missing_file_error if params[:image_upload].blank?
 
-      if @notification.save
-        redirect_to responsible_person_notification_additional_information_index_path(@notification.responsible_person, @notification)
-      else
-        @notification.image_uploads.each do |image_upload|
-          image_upload.errors.messages[:file].map(&method(:add_error))
-        end
-        render :new
-      end
+    params[:image_upload].each { |img| add_image_to_notification(img) }
+    if @notification.save
+      redirect_to responsible_person_notification_additional_information_index_path(
+        @notification.responsible_person,
+        @notification,
+      )
     else
-      add_error "No file selected"
-      render :new
+      render_image_upload_errors
     end
   end
 
 private
+
+  def render_missing_file_error
+    add_error "No file selected"
+    render :new
+  end
+
+  def add_image_to_notification(image)
+    image_upload = @notification.image_uploads.build
+    image_upload.file.attach(image)
+    image_upload.filename = image.original_filename
+  end
+
+  def render_image_upload_errors
+    @notification.image_uploads.each do |image_upload|
+      image_upload.errors.messages[:file].map(&method(:add_error))
+    end
+    render :new
+  end
 
   def add_error(error_message)
     @error_list.push(text: error_message, href: "#image_upload")
