03677474793cd6516497da579356b5b3cfa0761a
Steve Lorek <steve@stevelorek.com>
2021-01-07 11:03:44 +0000

    Redirect on additional info pages when notification complete

--- cosmetics-web/spec/controllers/additional_information_controller_spec.rb
+++ cosmetics-web/spec/controllers/additional_information_controller_spec.rb
@@ -46,11 +46,14 @@ RSpec.describe ResponsiblePersons::AdditionalInformationController, :with_stubbe
       expect(response).to redirect_to(new_responsible_person_notification_product_image_upload_path(responsible_person, notification))
     end
 
-    it "raised a NotAuthorizedError if the notification has already been submitted" do
-      notification = Notification.create(responsible_person_id: responsible_person.id, components: [predefined_component], state: "notification_complete")
-      expect {
-        get :index, params: { responsible_person_id: responsible_person.id, notification_reference_number: notification.reference_number }
-      }.to raise_error(Pundit::NotAuthorizedError)
+    context "when the notification is already submitted" do
+      subject(:request) { get(:index, params: { responsible_person_id: responsible_person.id, notification_reference_number: notification.reference_number }) }
+
+      let(:notification) { create(:registered_notification, responsible_person: responsible_person, components: [predefined_component]) }
+
+      it "redirects to the notifications page" do
+        expect(request).to redirect_to(responsible_person_notification_path(responsible_person, notification))
+      end
     end
 
     context "when a notification has multiple components" do
