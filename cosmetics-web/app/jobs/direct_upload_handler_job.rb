5902dc17fbd737047f5eb257ec37dec284663529
Slawosz Slawinski <slawosz@gmail.com>
2021-01-14 11:30:42 +0000

    Remove worker

--- cosmetics-web/app/jobs/direct_upload_handler_job.rb
+++
@@ -1,5 +0,0 @@
-class DirectUploadHandlerJob < ApplicationJob
-  def perform(uploaded_files, uploaded_file_names, responsible_person_id, current_user_id)
-    DirectUploadHandler.new(uploaded_files, uploaded_file_names, responsible_person_id, current_user_id).call
-  end
-end
