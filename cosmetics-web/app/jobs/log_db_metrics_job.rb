e4afe502143fbe1bbea20782529ecc195172751d
Sławosz Sławiński <slawosz@gmail.com>
2021-01-15 16:30:04 +0000

    Data statistics (#1932)
    * Generate stats
    
    * Debug metrics job
    
    * Change job frequency
    
    * Rubocop

--- cosmetics-web/app/jobs/log_db_metrics_job.rb
+++ cosmetics-web/app/jobs/log_db_metrics_job.rb
@@ -1,14 +1,21 @@
 class LogDbMetricsJob < ApplicationJob
   def perform
-    complete_notifications_count = Notification.where(state: "notification_complete").count
-    responsible_persons_count = ResponsiblePerson.count
+    stats = {
+      complete_notifications_count: Notification.where(state: "notification_complete").count,
+      complete_notifications_count_last_hour: Notification.where(state: "notification_complete").where("created_at >= ?", 1.hour.ago).count,
+      incomplete_notifications_count: Notification.where.not(state: "notification_complete").count,
+      incomplete_notifications_count_last_hour: Notification.where("created_at >= ?", 1.hour.ago).where.not(state: "notification_complete").count,
+      responsible_persons_count: ResponsiblePerson.count,
+      submit_users_count: SubmitUser.count,
+      responsible_persons_count_last_hour: ResponsiblePerson.where("created_at >= ?", 1.hour.ago).count,
+      submit_users_count_last_hour: SubmitUser.where("created_at >= ?", 1.hour.ago).count,
+    }
+
+    Sidekiq.logger.info "CosmeticsStatistics #{stats.to_a.map { |x| x.join('=') }.join(' ')}"
+
     users_per_responsible_person = ResponsiblePersonUser.group(:responsible_person_id)
       .count.values.sort.each_with_object(Hash.new(0)) { |e, h| h[e] += 1; }
 
-    Sidekiq.logger.info "{
-      completeNotifications: #{complete_notifications_count},
-      responsiblePersons: #{responsible_persons_count},
-      usersPerResponsiblePerson: #{users_per_responsible_person}
-      }"
+    Sidekiq.logger.info "usersPerResponsiblePerson: #{users_per_responsible_person}"
   end
 end
