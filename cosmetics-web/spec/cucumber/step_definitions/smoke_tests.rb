b61a0a5e85237a5393a43905b469cbb72764f9ed
nasirkhanbeis <nasirkhan.beis@gmail.com>
2021-02-09 16:34:46 +0000

    fixed styling issues

--- cosmetics-web/spec/cucumber/step_definitions/smoke_tests.rb
+++ cosmetics-web/spec/cucumber/step_definitions/smoke_tests.rb
@@ -1,18 +1,17 @@
 Given("I login as business user") do
-   sign_in_as_business
+  sign_in_as_business
 end
 
-  Then("I should be navigated to dashboard page") do
-   expect(page).to have_selector("h1", text: "Your cosmetic products")
+Then("I should be navigated to dashboard page") do
+  expect(page).to have_selector("h1", text: "Your cosmetic products")
 end
 
 Then("I should be redirected to dashboard page") do
-   expect(page).to have_selector("h1", text: "Your cosmetic products")
+  expect(page).to have_selector("h1", text: "Your cosmetic products")
 end
 
-
 When("I click add cosmetics product") do
-  click_on('Add cosmetics products')
+  click_on("Add cosmetics products")
 end
 
 When("I select notified in EU {string}") do |string|
@@ -26,7 +25,7 @@ When("I select are you likely to notify in EU {string}") do |string|
 end
 
 When("I select {string} notified in EU") do |string|
-   answer_was_eu_notified_with(string)
+  answer_was_eu_notified_with(string)
 end
 
 When("I select {string}") do |string|
@@ -35,11 +34,9 @@ When("I select {string}") do |string|
   select_manual_notification_prebexit_or_post_brexit("Yes")
 end
 
-
 Then("I should be able to notify product manually") do
   enter_product_name("test")
   notify_post_brexit
- 
 end
 
 Then("I should be able to notify product {string}") do |string|
@@ -52,12 +49,11 @@ Then("I should be able to notify {string} product {string}") do |string, string2
   notify_product(string)
 end
 
-Then("I should be able to see the entered details in check your answer page") do
-  
+Then("I should be able to see the entered details in check your answer page") do 
 end
 
 When("I select I have zip file {string}") do |string|
-  expected_h1('EU notification ZIP files')
+  expected_h1("EU notification ZIP files")
   select_radio("Yes")
   click_button "Continue"
 end
@@ -67,7 +63,7 @@ Then("I should be redirected to zip file upload page") do
 end
 
 Then("I should be able to upload valid zip file successfully") do
-  page.attach_file('spec/cucumber/test-file.zip')
+  page.attach_file("spec/cucumber/test-file.zip")
   click_button "Continue"
   sleep 5
 end
