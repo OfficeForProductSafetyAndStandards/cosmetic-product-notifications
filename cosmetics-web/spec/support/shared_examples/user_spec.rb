edb3dfd6dbdcde5dd490433635d93ea6699dc638
Marc Sardon <pringaohowto@gmail.com>
2021-02-15 12:05:07 +0000

    Cover User methods with unit tests
    User methods used by both Search and Submit users.

--- cosmetics-web/spec/support/shared_examples/user_spec.rb
+++ cosmetics-web/spec/support/shared_examples/user_spec.rb
@@ -82,4 +82,120 @@ RSpec.shared_examples "common user tests" do
       end
     end
   end
+
+  describe "#mobile_number_verified?" do
+    context "with secondary authentication enabled" do
+      before do
+        allow(Rails.application.config).to receive(:secondary_authentication_enabled).and_return(true)
+      end
+
+      it "returns true for users with the mobile number verified" do
+        user.mobile_number_verified = true
+        expect(user.mobile_number_verified?).to eq true
+      end
+
+      it "returns false for users without the mobile number verified" do
+        user.mobile_number_verified = false
+        expect(user.mobile_number_verified?).to eq false
+      end
+    end
+
+    context "with secondary_authentication disabled" do
+      before do
+        allow(Rails.application.config).to receive(:secondary_authentication_enabled).and_return(false)
+      end
+
+      it "returns true for users with the mobile number verified" do
+        user.mobile_number_verified = true
+        expect(user.mobile_number_verified?).to eq true
+      end
+
+      it "returns true for users without the mobile number verified" do
+        user.mobile_number_verified = false
+        expect(user.mobile_number_verified?).to eq true
+      end
+    end
+  end
+
+  describe "#mobile_number_change_allowed?" do
+    context "with secondary authentication enabled" do
+      before do
+        allow(Rails.application.config).to receive(:secondary_authentication_enabled).and_return(true)
+      end
+
+      it "users with verified mobile number are not allowed to change their mobile number" do
+        user.mobile_number_verified = true
+        expect(user.mobile_number_change_allowed?).to eq false
+      end
+
+      it "users without verified mobile number are allowed to change their mobile number" do
+        user.mobile_number_verified = false
+        expect(user.mobile_number_change_allowed?).to eq true
+      end
+    end
+
+    context "with secondary_authentication disabled" do
+      before do
+        allow(Rails.application.config).to receive(:secondary_authentication_enabled).and_return(false)
+      end
+
+      it "users with verified mobile number are not allowed to change their mobile number" do
+        user.mobile_number_verified = true
+        expect(user.mobile_number_change_allowed?).to eq false
+      end
+
+      it "users without verified mobile number are not allowed to change their mobile number" do
+        user.mobile_number_verified = false
+        expect(user.mobile_number_change_allowed?).to eq false
+      end
+    end
+  end
+
+  describe "#has_completed_registration?" do
+    before do
+      user.assign_attributes(encrypted_password: "encrypted_password_hash",
+                             name: "John Doe",
+                             mobile_number: "07123456789",
+                             mobile_number_verified: true)
+    end
+
+    it "user has completed registration registration when password, name, mibile number and mobile number verification are set" do
+      expect(user.has_completed_registration?).to eq true
+    end
+
+    it "user has not completed registration when missing encrypted password" do
+      user.encrypted_password = nil
+      expect(user.has_completed_registration?).to eq false
+    end
+
+    it "user has not completed registration when blank encrypted password" do
+      user.encrypted_password = ""
+      expect(user.has_completed_registration?).to eq false
+    end
+
+    it "user has not completed registration when missing name" do
+      user.name = nil
+      expect(user.has_completed_registration?).to eq false
+    end
+
+    it "user has not completed registration when blank name" do
+      user.name = ""
+      expect(user.has_completed_registration?).to eq false
+    end
+
+    it "user has not completed registration when missing mobile number" do
+      user.mobile_number = nil
+      expect(user.has_completed_registration?).to eq false
+    end
+
+    it "user has not completed registration when blank mobile number" do
+      user.mobile_number = ""
+      expect(user.has_completed_registration?).to eq false
+    end
+
+    it "user has not completed registration when mobile number is not verified" do
+      user.mobile_number_verified = false
+      expect(user.has_completed_registration?).to eq false
+    end
+  end
 end
