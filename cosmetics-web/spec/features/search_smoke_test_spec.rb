699fd488a0a23c5cb86f901e49b876500dd4f5ac
nasirkhanbeis <nasirkhan.beis@gmail.com>
2021-02-22 10:08:48 +0000

    search smoke test

--- cosmetics-web/spec/features/search_smoke_test_spec.rb
+++ cosmetics-web/spec/features/search_smoke_test_spec.rb
@@ -1,13 +1,51 @@
 require "rails_helper"
 
-RSpec.describe "Poison center user to be able to search notifications", type: :feature do
-	let(:user) { responsible_person.responsible_person_users.first.user }
-	let(:responsible_person) { create(:responsible_person_with_user, :with_a_contact_person) }
-  before do
-    configure_requests_for_search_domain
-    sign_in user
-  end
+RSpec.feature "Search smoke test", type: :feature do 
 
-   scenario "search notification" do
+
+	
+  before do
+  	configure_requests_for_search_domain
+  	visit(ENV["ENV_URL"])
+    expect(page).to have_css("h1", text: "Search for cosmetic products")
    end
+
+
+    scenario "sign-in and search notification product as poison center user" do
+      visit "/sign-in"
+      fill_in_search_credentials
+
+       expect_to_be_on_secondary_authentication_page
+
+       complete_secondary_authentication_with(11222)
+
+       expect(page).to have_css("h1", text: "Search cosmetics products")
+       expect(page).to have_xpath("//input[contains(@id,'q')]")
+
+       fill_in("q",with: "Beautify Facial Night Cream")
+       click("Search")
+       expect(page).to have_content("Beautify Facial Nigth cream")
+
+       find("(//a[contains(.,'Beautify Facial Night Cream')])[1]").click
+
+       expect(page).to have_css("h2",text: "Product details")
+       expect(page).to have_css("h2", text: "Ingredients")
+   end
+end
+
+
+
+def fill_in_search_credentials
+	
+      fill_in "Email address", with: "nasiralikhan1982@gmail.com"
+      fill_in "Password", with: "Nasir123"
+      click_button "Continue"
+
+      expect_to_be_on_secondary_authentication_page
+end
+
+def complete_secondary_authentication_with(fake_code)
+	expect(page).to have_css("h1", text: "Check your phone")
+	fill_in "Enter security code", with: fake_code
+    click_on "Continue"
 end
