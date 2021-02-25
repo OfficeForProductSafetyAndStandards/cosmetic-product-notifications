bd455bf4272b26951bba63d13b4467fbb4c906c7
Marc Sardon <pringaohowto@gmail.com>
2021-02-25 14:56:44 +0000

    Checkbox validation errors point to the first option
    As the checkbox element may contain validation errors (when none of
    the options is selected), we want the validation message in the error
    summary to send the user to the first checkbox option.

--- cosmetics-web/app/helpers/application_helper.rb
+++ cosmetics-web/app/helpers/application_helper.rb
@@ -4,7 +4,11 @@ module ApplicationHelper
     content_for(:page_title, title)
   end
 
-  def error_summary(errors, ordered_attributes = [])
+  # The map_errors parameters can be used to link error messages for multi-answer
+  # so they focus on a specific attribute option when clicked.
+  # Eg: "map_errors: { colours: :red }" will cause validation errors messages for
+  # "colours" in the error summary to link to "#red" instead of to "#colours".
+  def error_summary(errors, ordered_attributes = [], map_errors: {})
     return unless errors.any?
 
     ordered_errors = ActiveSupport::OrderedHash.new
@@ -13,12 +17,18 @@ module ApplicationHelper
     errors.map do |attribute, error|
       next if error.blank?
 
+      href = if map_errors[attribute]
+               "##{map_errors[attribute]}"
+             else
+               "##{attribute}"
+             end
+
       # Errors for attributes that are not included in the ordered list will be
       # added at the end after the errors for ordered attributes.
       if ordered_errors[attribute]
-        ordered_errors[attribute] << { text: error, href: "##{attribute}" }
+        ordered_errors[attribute] << { text: error, href: href }
       else
-        ordered_errors[attribute] = [{ text: error, href: "##{attribute}" }]
+        ordered_errors[attribute] = [{ text: error, href: href }]
       end
     end
     error_list = ordered_errors.values.flatten.compact
