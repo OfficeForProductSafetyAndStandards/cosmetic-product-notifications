df186a209907f65461c115c0523f8e22e580a1fc
nasirkhanbeis <47983567+nasirkhanbeis@users.noreply.github.com>
2021-03-18 14:04:46 +0000

    Search smoke test (#2002)
    * added search smoke test
    
    * search smoke test
    
    * fixed stlying issues
    
    * fix code stlying issues
    
    * removed hardcoded user
    
    * Search smoke test
    
    * Test smoke test
    
    * Smoke test adjustments
    
    * Test slack notifications
    
    * Fix tests
    
    * Update readme
    
    * Update node
    
    * Change smoke test schedule
    
    * Update readme for relay app
    
    * Update readme for relay app
    
    * Update .github/workflows/smoke_test.yml
    
    Co-authored-by: Marc Sardón <pringaohowto@gmail.com>
    
    Co-authored-by: Slawosz Slawinski <slawosz@gmail.com>
    Co-authored-by: Marc Sardón <pringaohowto@gmail.com>

---
+++ cosmetics-web/spec/smoke/beis-opss-text-relay/app.rb
@@ -0,0 +1,40 @@
+require "sinatra/base"
+require "redis"
+require "pry"
+require "cf-app-utils"
+
+class App < Sinatra::Base
+  if ENV["HTTP_AUTH_USER"] && ENV["HTTP_AUTH_PASS"]
+    use Rack::Auth::Basic, "Protected Area" do |username, password|
+      username == ENV["HTTP_AUTH_USER"] && password == ENV["HTTP_AUTH_PASS"]
+    end
+  end
+
+  get "/text" do
+    redis.get("text")
+  end
+
+  post "/save" do
+    redis.set("text", params["message"])
+    redis.expire("text", 15 * 60 * 3) # 3 is for debug
+  end
+
+private
+
+  def redis
+    @redis ||= if ENV["VCAP_SERVICES"]
+                 uri = CF::App::Credentials.find_by_service_name("beis-opss-text-relay-redis")["uri"]
+                 Redis.new(url: uri)
+               else
+                 Redis.new(host: host, port: port, db: 15)
+               end
+  end
+
+  def host
+    ENV["VCAP_SERVICES"] && CF::App::Credentials.find_by_service_name("beis-opss-text-relay-redis")["host"] || ENV.fetch("REDIS_HOST", "localhost")
+  end
+
+  def port
+    ENV["VCAP_SERVICES"] && CF::App::Credentials.find_by_service_name("beis-opss-text-relay-redis")["port"] || ENV.fetch("REDIS_PORT", "6379")
+  end
+end
