ab64df53796aac9238c50445805f99f42b77b4f8
Slawosz Slawinski <slawosz@gmail.com>
2021-03-23 09:39:58 +0000

    yarn standard

--- cosmetics-web/app/assets/application/javascripts/image_size_validation.js
+++ cosmetics-web/app/assets/application/javascripts/image_size_validation.js
@@ -3,15 +3,15 @@ import $ from 'jquery'
 $(document).ready(() => {
   const input = document.getElementById('image_upload')
   if (input) {
-    input.addEventListener("change", handleFiles, false);
+    input.addEventListener('change', handleFiles, false)
   }
 
-  function handleFiles(event) {
+  function handleFiles (event) {
     const files = this.files
     for (let i = 0; i < files.length; i++) {
       console.log(files[i])
-      if (files[i].size > (30 * 1024  * 1024)) {
-        alert(`File ${files[i].name} is bigger than 30 MB. Please resize and try again.`);
+      if (files[i].size > (30 * 1024 * 1024)) {
+        window.alert(`File ${files[i].name} is bigger than 30 MB. Please resize and try again.`)
         this.value = null
         break
       }
