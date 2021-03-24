8849466b8955e748f7adef7d026c2acff6b101d6
Slawosz Slawinski <slawosz@gmail.com>
2021-03-24 10:45:52 +0000

    Different MB definition

--- cosmetics-web/app/assets/application/javascripts/image_size_validation.js
+++ cosmetics-web/app/assets/application/javascripts/image_size_validation.js
@@ -9,8 +9,7 @@ $(document).ready(() => {
   function handleFiles (event) {
     const files = this.files
     for (let i = 0; i < files.length; i++) {
-      console.log(files[i])
-      if (files[i].size > (30 * 1024 * 1024)) {
+      if (files[i].size > (30 * 1000 * 1000)) {
         window.alert(`File ${files[i].name} is bigger than 30 MB. Please resize and try again.`)
         this.value = null
         break
