0c0a41e81c0716568d363146e2298244577f7707
Slawosz Slawinski <slawosz@gmail.com>
2021-04-08 11:00:23 +0100

    Rubocop

--- cosmetics-web/app/jobs/reindex_elasticsearch_job.rb
+++ cosmetics-web/app/jobs/reindex_elasticsearch_job.rb
@@ -2,14 +2,13 @@ class ReindexElasticsearchJob < ApplicationJob
   def perform
     total = 0
     ActiveRecord::Base.descendants.each do |model|
-      if model.respond_to?(:__elasticsearch__) && !model.superclass.respond_to?(:__elasticsearch__)
-        if model.respond_to?(:elasticsearch)
-          total += 1
-          model.elasticsearch.import force: true
-        else
-          total += 1
-          model.import force: true
-        end
+      next unless model.respond_to?(:__elasticsearch__) && !model.superclass.respond_to?(:__elasticsearch__)
+
+      total += 1
+      if model.respond_to?(:elasticsearch)
+        model.elasticsearch.import force: true
+      else
+        model.import force: true
       end
     end
 
