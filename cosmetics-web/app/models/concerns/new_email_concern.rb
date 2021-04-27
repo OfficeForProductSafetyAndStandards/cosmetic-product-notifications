ea3bae665f958b546167919551c8dca6292140ec
Marc Sardon <pringaohowto@gmail.com>
2021-04-27 17:31:30 +0100

    Refactor: Remove DB transactions
    There is no need for them as we are just updating multiple fields in
    the same table and there is no risk of data inconsistency.
    
    By calling an update! we have an atomic update that will be reverted
    if any of the fields do not pass validations.

--- cosmetics-web/app/models/concerns/new_email_concern.rb
+++ cosmetics-web/app/models/concerns/new_email_concern.rb
@@ -2,12 +2,11 @@ module NewEmailConcern
   extend ActiveSupport::Concern
 
   def new_email_pending_confirmation!(email)
-    ActiveRecord::Base.transaction do
-      self.new_email_confirmation_token = SecureRandom.uuid
-      self.new_email_confirmation_token_expires_at = Time.zone.now + User::NEW_EMAIL_TOKEN_VALID_FOR.seconds
-      self.new_email = email
-      save!
-    end
+    update!(
+      new_email: email,
+      new_email_confirmation_token: SecureRandom.uuid,
+      new_email_confirmation_token_expires_at: Time.zone.now + User::NEW_EMAIL_TOKEN_VALID_FOR.seconds,
+    )
     send_new_email_confirmation_email
   end
 
@@ -16,13 +15,12 @@ module NewEmailConcern
       user = User.where("new_email_confirmation_token_expires_at > ?", Time.zone.now).find_by! new_email_confirmation_token: token
 
       old_email = user.email
-      ActiveRecord::Base.transaction do
-        user.email = user.new_email
-        user.new_email = nil
-        user.new_email_confirmation_token = nil
-        user.new_email_confirmation_token_expires_at = nil
-        user.save!
-      end
+      user.update!(
+        email: user.new_email,
+        new_email: nil,
+        new_email_confirmation_token: nil,
+        new_email_confirmation_token_expires_at: nil,
+      )
       NotifyMailer.get_mailer(user).update_email_address_notification_email(user, old_email).deliver_later
     rescue ActiveRecord::RecordNotFound
       raise ArgumentError
