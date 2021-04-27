5df5bff6565d04995c9288fc1452c4e226f17600
Marc Sardon <pringaohowto@gmail.com>
2021-04-27 17:13:58 +0100

    Bugfix: Move mail delivery out of the DB transaction
    BUG:
    Users are requesting to change their email and receiving confirmation
    links with invalid tokens.
    
    These tokens seem to be the previous token that was changed with the
    new email request. So, instead of receiving the new generated token,
    users receive the token that was previously set and is no longer valid.
    
    CAUSE OF THE BUG:
    Change of token and mailer sending the new email confirmation link were
    under the same DB transaction block.
    
    EXPLANATION:
    When code is wrapped into an Active Record transaction block, changes
    are not written in DB until the full transaction block has been run
    successfully.
    
    This means that, when the mailer is called inside the transaction, even
    when the "#save!" call has been called, the DB value has not been updated
    as the transaction has not finished.
    
    When this happens, the mailer retrieves the new email confirmation token
    from DB and gets the previous token instead of the updated value, as the
    update in DB happens when the transaction block ends.

--- cosmetics-web/app/models/concerns/new_email_concern.rb
+++ cosmetics-web/app/models/concerns/new_email_concern.rb
@@ -7,8 +7,8 @@ module NewEmailConcern
       self.new_email_confirmation_token_expires_at = Time.zone.now + User::NEW_EMAIL_TOKEN_VALID_FOR.seconds
       self.new_email = email
       save!
-      send_new_email_confirmation_email
     end
+    send_new_email_confirmation_email
   end
 
   class_methods do
@@ -16,14 +16,14 @@ module NewEmailConcern
       user = User.where("new_email_confirmation_token_expires_at > ?", Time.zone.now).find_by! new_email_confirmation_token: token
 
       old_email = user.email
-      user.email = user.new_email
-      user.new_email = nil
-      user.new_email_confirmation_token = nil
-      user.new_email_confirmation_token_expires_at = nil
       ActiveRecord::Base.transaction do
+        user.email = user.new_email
+        user.new_email = nil
+        user.new_email_confirmation_token = nil
+        user.new_email_confirmation_token_expires_at = nil
         user.save!
-        NotifyMailer.get_mailer(user).update_email_address_notification_email(user, old_email).deliver_later
       end
+      NotifyMailer.get_mailer(user).update_email_address_notification_email(user, old_email).deliver_later
     rescue ActiveRecord::RecordNotFound
       raise ArgumentError
     end
