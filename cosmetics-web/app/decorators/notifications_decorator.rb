6cfa3340c497e0b8be0b196c0aff7fbf0190b065
Sławosz Sławiński <slawosz@gmail.com>
2021-05-12 13:53:24 +0100

    1111 additional csv fields (#2119)
    * Add internal reference and categories to CSV
    
    * Rubocop
    
    * Fix spec
    
    * Change categories names
    
    * Item instead of component
    
    * Change wording
    
    * Update cosmetics-web/app/models/notification.rb
    
    Co-authored-by: Marc Sardón <pringaohowto@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Marc Sardón <pringaohowto@gmail.com>
    
    * Revert "Update cosmetics-web/app/models/notification.rb"
    
    This reverts commit db3df785d577033ba0e76d88f8a4f224d9560a65.
    
    Co-authored-by: Marc Sardón <pringaohowto@gmail.com>

--- cosmetics-web/app/decorators/notifications_decorator.rb
+++ cosmetics-web/app/decorators/notifications_decorator.rb
@@ -7,13 +7,17 @@ class NotificationsDecorator
     "Notification date",
     "EU Reference number",
     "EU Notification date",
+    "Internal reference",
+    "Number of items",
   ].freeze
 
   ATTRIBUTES = %i[product_name
                   reference_number_for_display
                   updated_at
                   cpnp_reference
-                  cpnp_notification_date].freeze
+                  cpnp_notification_date
+                  industry_reference
+                  components_count].freeze
 
   def initialize(notifications)
     @notifications = notifications
@@ -21,10 +25,25 @@ class NotificationsDecorator
 
   def to_csv
     CSV.generate do |csv|
-      csv << HEADER
+      csv << HEADER + categories_headers
       @notifications.each do |notification|
         csv << NotificationDecorator.new(notification).as_csv
       end
     end
+  end
+
+private
+
+  def categories_headers
+    categories = []
+    components_count = @notifications.map { |x| x.components.count }.max
+
+    components_count.times do |i|
+      categories << ["Item #{i + 1} Level 1 category",
+                     "Item #{i + 1} Level 2 category",
+                     "Item #{i + 1} Level 3 category"]
+    end
+
+    categories.flatten
   end
 end
