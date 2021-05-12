2cad75638f68e7da868d17bac4d0bd9ad513ff0f
Marc Sardon <pringaohowto@gmail.com>
2021-05-12 09:55:52 +0100

    Extract user password check on forms into concern

--- cosmetics-web/app/forms/secondary_authentication/app/setup_form.rb
+++ cosmetics-web/app/forms/secondary_authentication/app/setup_form.rb
@@ -3,20 +3,16 @@ module SecondaryAuthentication
     class SetupForm
       include ActiveModel::Model
       include ActiveModel::Attributes
+      include UserPasswordCheckFormValidation
 
       attribute :app_authentication_code
       attribute :secret_key
-      attribute :password
       attribute :user
 
       validates :app_authentication_code, presence: true
       validate :app_authentication_code, :validate_app_authentication_code
 
       delegate :qr_code, :last_totp_at, to: :secondary_authentication
-
-      validates :password, presence: true
-
-      validate :correct_password
 
       def [](field)
         public_send(field.to_sym)
@@ -40,14 +36,6 @@ module SecondaryAuthentication
 
       def secondary_authentication
         @secondary_authentication ||= SecondaryAuthentication::TimeOtp.new(user, secret_key)
-      end
-
-      def correct_password
-        return if errors[:password].present?
-
-        unless user.valid_password?(password)
-          errors.add(:password, :invalid)
-        end
       end
 
       def validate_app_authentication_code
--- cosmetics-web/app/forms/secondary_authentication/sms/setup_form.rb
+++ cosmetics-web/app/forms/secondary_authentication/sms/setup_form.rb
@@ -4,9 +4,9 @@ module SecondaryAuthentication
       include ActiveModel::Model
       include ActiveModel::Attributes
       include StripWhitespace
+      include UserPasswordCheckFormValidation
 
       attribute :mobile_number
-      attribute :password
       attribute :user
 
       validates :mobile_number, presence: { message: :invalid }
@@ -17,19 +17,7 @@ module SecondaryAuthentication
                 phone: { message: :invalid, allow_international: false },
                 if: :validate_uk_mobile_number?
 
-      validates :password, presence: true
-
-      validate :correct_password
-
     private
-
-      def correct_password
-        return if errors[:password].present?
-
-        unless user.valid_password?(password)
-          errors.add(:password, :invalid)
-        end
-      end
 
       def validate_uk_mobile_number?
         mobile_number.present? && user && !user.class::ALLOW_INTERNATIONAL_PHONE_NUMBER
