9d1ff2b5eab556f61c60a82b4ca5828a896413e3
Marc Sardon <pringaohowto@gmail.com>
2021-06-01 18:40:52 +0100

    Extract ActiveStorage access protection to Concern

--- cosmetics-web/app/controllers/active_storage/representations/proxy_controller.rb
+++ cosmetics-web/app/controllers/active_storage/representations/proxy_controller.rb
@@ -10,8 +10,7 @@
 class ActiveStorage::Representations::ProxyController < ActiveStorage::BaseController
   include ActiveStorage::SetBlob
   include ActiveStorage::SetHeaders
-  include Pundit
-  include DomainConcern
+  include ActiveStorageAccessProtectionConcern
 
   before_action :authorize_blob
 
@@ -26,20 +25,5 @@ private
 
   def representation
     @representation ||= @blob.representation(params[:variation_key]).processed
-  end
-
-  def pundit_user
-    current_submit_user
-  end
-
-  def authorize_blob
-    if submit_domain?
-      raise Pundit::NotAuthorizedError unless submit_user_signed_in?
-
-      rp = @blob.attachments.first.record.notification.responsible_person
-      authorize rp, :show?
-    else
-      raise Pundit::NotAuthorizedError unless search_user_signed_in?
-    end
   end
 end
