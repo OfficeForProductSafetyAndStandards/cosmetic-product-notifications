5f13756fcc2fb5d5cc8ae08ad1137c5480ec977d
Slawosz Slawinski <slawosz@gmail.com>
2021-06-17 17:27:09 +0100

    Remove pry

--- cosmetics-web/spec/features/notification_search_spec.rb
+++ cosmetics-web/spec/features/notification_search_spec.rb
@@ -4,7 +4,6 @@ require "support/feature_helpers"
 RSpec.feature "Creating a Search account from an invitation", :with_stubbed_mailer, :with_stubbed_notify, :with_2fa, :with_2fa_app, type: :feature do
   let(:user) { create(:poison_centre_user, :with_sms_secondary_authentication) }
 
-
   let(:notification1) { create(:notification, :registered, :with_component, notification_complete_at: 1.day.ago, product_name: "Cream") }
   let(:notification2) { create(:notification, :registered, :with_component, notification_complete_at: 2.day.ago, product_name: "Shower Bubbles") }
   let(:notification3) { create(:notification, :registered, :with_component, notification_complete_at: 3.day.ago, product_name: "Bath Bubbles", category: :face_care_products_other_than_face_mask) }
@@ -53,12 +52,13 @@ RSpec.feature "Creating a Search account from an invitation", :with_stubbed_mail
     expect(page).to have_link("Bath Bubbles")
 
     fill_in "notification_search_form_q", with: "Bubbles"
+    select "Date"
+    fill_in "notification_search_form_date_exact_day",   with: notification3.notification_complete_at.day
+    fill_in "notification_search_form_date_exact_month", with: notification3.notification_complete_at.month
+    fill_in "notification_search_form_date_exact_year",  with: notification3.notification_complete_at.year
+
     click_on "Search"
 
-    expect(page).not_to have_link("Cream")
-
-    expect(page).to have_link("Shower Bubbles")
-    expect(page).to have_link("Bath Bubbles")
 
     select "Skin products", from: "Product category"
     click_on "Apply"
