b6a544a9808b7ca804550a6eff92414d3ea0ca0c
Marc Sardon <pringaohowto@gmail.com>
2021-07-21 17:44:12 +0100

    Minor change using ruby instead of rails method

--- cosmetics-web/app/controllers/responsible_persons/team_members_controller.rb
+++ cosmetics-web/app/controllers/responsible_persons/team_members_controller.rb
@@ -81,7 +81,7 @@ private
 
   def create_invitation!
     # If an existing user is invited, the name of the existing user will be used instead of the one provided in the form.
-    name = SubmitUser.find_by(email: @invite_member_form.email).try(:name) || @invite_member_form.name
+    name = SubmitUser.find_by(email: @invite_member_form.email)&.name || @invite_member_form.name
     @invitation = @responsible_person.pending_responsible_person_users.create!(
       name: name,
       email_address: @invite_member_form.email,
