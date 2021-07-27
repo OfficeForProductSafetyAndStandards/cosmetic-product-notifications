a61890be8059305224d58ac300c95e43f7766e8a
Slawosz Slawinski <slawosz@gmail.com>
2021-07-27 17:43:55 +0100

    Minor fixes

--- cosmetics-web/spec/features/creating_a_responsible_person_spec.rb
+++ cosmetics-web/spec/features/creating_a_responsible_person_spec.rb
@@ -75,7 +75,7 @@ RSpec.describe "Creating a responsible person", type: :feature do
     click_button "I confirm"
 
     expect(page).to have_h1("Responsible Person")
-    click_link "Add a responsible person"
+    click_link "Add a Responsible Person"
     select_rp_business_account_type
 
     expect(page).to have_h1("UK Responsible Person details")
