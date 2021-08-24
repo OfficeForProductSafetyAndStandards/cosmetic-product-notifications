b163720a0642273d629b419fac7fce166b98352f
stdstaff <85587097+stdstaff@users.noreply.github.com>
2021-08-24 13:52:08 +0100

    Correct the error_summary helper, and don't display nil errors

--- cosmetics-web/app/controllers/users/sessions_controller.rb
+++ cosmetics-web/app/controllers/users/sessions_controller.rb
@@ -52,7 +52,7 @@ module Users
       return unless resource
 
       resource.errors.add(:email, I18n.t(:wrong_email_or_password, scope: "sign_user_in.email"))
-      resource.errors.add(:password, I18n.t(:wrong_email_or_password, scope: "sign_user_in.password"))
+      resource.errors.add(:password, nil)
     end
 
     def set_resource_as_new_user_from_params
