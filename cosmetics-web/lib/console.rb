22d4aed3214e62b1a733f569f8d07b0af1147324
Slawosz Slawinski <slawosz@gmail.com>
2021-09-14 13:30:00 +0100

    Console config

---
+++ cosmetics-web/lib/console.rb
@@ -0,0 +1,13 @@
+prompt = "Cosmetics#\e[#{IRB::Color::RED}m#{Rails.env}\e[0m"
+
+# defining custom prompt
+IRB.conf[:PROMPT][:RAILS] = {
+  :PROMPT_I => "#{prompt} > ",
+  :PROMPT_N => "#{prompt}> ",
+  :PROMPT_S => "#{prompt}* ",
+  :PROMPT_C => "#{prompt}? ",
+  :RETURN   => " => %s\n"
+}
+
+# Setting our custom prompt as prompt mode
+IRB.conf[:PROMPT_MODE] = :RAILS
