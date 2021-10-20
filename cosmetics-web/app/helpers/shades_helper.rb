ae8861126181c167e9cbb669476c907e3e8da0ff
Marc Sardon <pringaohowto@gmail.com>
2021-10-20 15:46:16 +0100

    Fix shades styling

--- cosmetics-web/app/helpers/shades_helper.rb
+++ cosmetics-web/app/helpers/shades_helper.rb
@@ -1,9 +1,9 @@
 module ShadesHelper
   def display_shades(notification)
     if notification.shades.present?
-      render "none_or_bullet_list", entities_list: [notification.shades]
+      render "none_or_bullet_list", entities_list: [notification.shades], list_item_classes: ""
     elsif notification.components&.first&.shades.present?
-      render "none_or_bullet_list", entities_list: notification.components&.first&.shades
+      render "none_or_bullet_list", entities_list: notification.components&.first&.shades, list_item_classes: ""
     else
       "None"
     end
