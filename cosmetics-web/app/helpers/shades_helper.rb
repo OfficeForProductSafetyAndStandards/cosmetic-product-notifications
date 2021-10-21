4b139f8dc957337e44ccea9a4b397d7a51f79987
Marc Sardon <pringaohowto@gmail.com>
2021-10-21 11:45:09 +0100

    Display all components shades in product level
    Until now, when a multicomponent product had multiple shades in
    different components, only the shades from the first component where
    displayed at product level.
    
    Now the product shades list will display all shades from every
    comnponent, while each component/item block will display their
    individual shades.

--- cosmetics-web/app/helpers/shades_helper.rb
+++ cosmetics-web/app/helpers/shades_helper.rb
@@ -2,10 +2,9 @@ module ShadesHelper
   def display_shades(notification)
     if notification.shades.present?
       render "none_or_bullet_list", entities_list: [notification.shades], list_item_classes: ""
-    elsif notification.components&.first&.shades.present?
-      render "none_or_bullet_list", entities_list: notification.components&.first&.shades, list_item_classes: ""
     else
-      "None"
+      shades = notification.components.pluck(:shades).flatten.compact.uniq
+      shades.any? ? render("none_or_bullet_list", entities_list: shades, list_item_classes: "") : "None"
     end
   end
 end
