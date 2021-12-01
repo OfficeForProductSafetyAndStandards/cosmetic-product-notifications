2afce3b1faf1e872b5aafc62fa75f291fcbf8f8f
Marc Sardon <pringaohowto@gmail.com>
2021-12-01 18:09:58 +0000

    Reset category selection from filters

--- cosmetics-web/app/assets/application/javascripts/notification_search.js
+++ cosmetics-web/app/assets/application/javascripts/notification_search.js
@@ -13,8 +13,14 @@ $(function () {
     const radioConditionalElements = filterForm.querySelectorAll('.govuk-radios__conditional')
     const radioInputs = filterForm.querySelectorAll('.govuk-radios--conditional .govuk-radios__input')
     const dateInputs = filterForm.querySelectorAll('.govuk-radios--conditional .govuk-date-input__input')
+    const prodSelect = document.getElementById('notification_search_form_category')
 
     document.getElementById('opss-reset').addEventListener('click', function () { // click the filter's form reset link/button
+      prodSelect.querySelectorAll('option').forEach(function (s) { // all options
+        s.removeAttribute('selected')
+      })
+      prodSelect.querySelector('option').setAttribute('selected', 'selected') // the first option
+
       radioConditionalElements.forEach(function (element) {
         element.classList.add('govuk-radios__conditional--hidden')
       })
