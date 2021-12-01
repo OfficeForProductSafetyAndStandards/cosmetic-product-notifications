0eaa3c131264cadcd57b9d5030e8ff7a02a2ba90
Marc Sardon <pringaohowto@gmail.com>
2021-12-01 11:27:08 +0000

    Replace deprecated document ready JQuery call

--- cosmetics-web/app/assets/application/javascripts/notification_search.js
+++ cosmetics-web/app/assets/application/javascripts/notification_search.js
@@ -1,10 +1,10 @@
 import $ from 'jquery'
 
-$(document).ready(() => {
+$(function() {
   const element = document.getElementById('notification_search_form_sort_by')
   if (element !== null) {
     element.addEventListener('change', function () {
       document.getElementById('new_notification_search_form_sort').submit()
     })
   }
-})
+});
