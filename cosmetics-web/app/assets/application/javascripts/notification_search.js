57b88d77f57648669f5fb3971b2cead554b2d1c9
Marc Sardon <pringaohowto@gmail.com>
2021-12-01 16:04:52 +0000

    Improve variable names

--- cosmetics-web/app/assets/application/javascripts/notification_search.js
+++ cosmetics-web/app/assets/application/javascripts/notification_search.js
@@ -10,14 +10,14 @@ $(function () {
 
   const filterForm = document.getElementById('new_notification_search_form_filters')
   if (filterForm !== null) {
-    const radioSelection = filterForm.querySelectorAll('.govuk-radios__conditional')
-    const radioInput = filterForm.querySelectorAll('.govuk-radios--conditional .govuk-radios__input')
+    const radioConditionalElements = filterForm.querySelectorAll('.govuk-radios__conditional')
+    const radioInputs = filterForm.querySelectorAll('.govuk-radios--conditional .govuk-radios__input')
 
     document.getElementById('opss-reset').addEventListener('click', function () { // click the filter's form reset link/button
-      radioSelection.forEach(function (element) {
+      radioConditionalElements.forEach(function (element) {
         element.classList.add('govuk-radios__conditional--hidden')
       })
-      radioInput.forEach(function (element) {
+      radioInputs.forEach(function (element) {
         element.setAttribute('aria-expanded', false)
       })
     }, false)
