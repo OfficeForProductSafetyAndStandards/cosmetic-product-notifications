89dc77e4adf462cd1fea4e9b0686eec98ee5c2bc
Marc Sardon <pringaohowto@gmail.com>
2021-12-01 15:19:25 +0000

    Fix JS Linter errors

--- cosmetics-web/app/assets/application/javascripts/notification_search.js
+++ cosmetics-web/app/assets/application/javascripts/notification_search.js
@@ -1,6 +1,6 @@
 import $ from 'jquery'
 
-$(function() {
+$(function () {
   const sortBy = document.getElementById('notification_search_form_sort_by')
   if (sortBy !== null) {
     sortBy.addEventListener('change', function () {
@@ -13,13 +13,13 @@ $(function() {
     const radioSelection = filterForm.querySelectorAll('.govuk-radios__conditional')
     const radioInput = filterForm.querySelectorAll('.govuk-radios--conditional .govuk-radios__input')
 
-    document.getElementById('opss-reset').addEventListener('click', function () {//click the filter's form reset link/button
-      radioSelection.forEach(function(element) {
+    document.getElementById('opss-reset').addEventListener('click', function () { // click the filter's form reset link/button
+      radioSelection.forEach(function (element) {
         element.classList.add('govuk-radios__conditional--hidden')
-      });
-      radioInput.forEach(function(element) {
+      })
+      radioInput.forEach(function (element) {
         element.setAttribute('aria-expanded', false)
-      });
-    }, false);
+      })
+    }, false)
   }
 })
