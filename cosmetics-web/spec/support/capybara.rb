97952a0567eef9db3d8a9b310d447844c6c5ef84
Marc Sardón <pringaohowto@gmail.com>
2022-01-06 10:36:18 +0000

    Migrate smoke tests to Selenium webdriver (#2349)
    Capybara mechanize has been unmaintained for years and is not compatible
    with Ruby 3 syntax (unless using a fork).
    
    Selenium webdriver is the default for Rails 6 integration testing and
    allows us to run the smoke test without any change but the driver
    itself.

--- cosmetics-web/spec/support/capybara.rb
+++ cosmetics-web/spec/support/capybara.rb
@@ -1,11 +1,6 @@
 require "capybara/rspec"
 require "capybara-screenshot/rspec"
-require "capybara/mechanize"
 
 Capybara.register_driver :rack_test do |app|
   Capybara::RackTest::Driver.new(app, respect_data_method: true, headers: { "HTTP_USER_AGENT" => "Capybara" })
-end
-
-Capybara.register_driver :mechanize do |_app|
-  Capybara::Mechanize::Driver.new(proc {})
 end
