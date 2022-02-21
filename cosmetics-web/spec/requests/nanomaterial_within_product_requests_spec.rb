5b94971c272ce8353e0123bbfe7a49d03f900b33
Slawosz Slawinski <slawosz@gmail.com>
2022-02-21 14:34:48 +0000

    Fix requests specs

--- cosmetics-web/spec/requests/nanomaterial_within_product_requests_spec.rb
+++ cosmetics-web/spec/requests/nanomaterial_within_product_requests_spec.rb
@@ -18,16 +18,15 @@ RSpec.describe "Nanomaterial usage within product notifications", type: :request
       create(:notification, responsible_person: responsible_person)
     end
 
-    let(:component) { create(:component, notification: notification) }
-    let(:nano_material) { create(:nano_material, component: component) }
+    let(:nano_material) { create(:nano_material, notification: notification) }
     let(:nano_element) { create(:nano_element, nano_material: nano_material) }
 
     before do
-      put "/responsible_persons/#{responsible_person.id}/notifications/#{notification.reference_number}/components/#{component.id}/nanomaterials/#{nano_element.id}/build/confirm_usage", params: { nano_element: { confirm_usage: "yes" } }
+      put "/responsible_persons/#{responsible_person.id}/notifications/#{notification.reference_number}/nanomaterials/#{nano_element.id}/build/confirm_usage", params: { nano_element: { confirm_usage: "yes" } }
     end
 
     it "redirects to the Category question page" do
-      expect(response).to redirect_to("/responsible_persons/#{responsible_person.id}/notifications/#{notification.reference_number}/components/#{component.id}/build/select_category")
+      expect(response).to redirect_to("/responsible_persons/#{responsible_person.id}/notifications/#{notification.reference_number}/nanomaterials/#{nano_element.id}/build/after_standard_nanomaterial_routing")
     end
   end
 end
