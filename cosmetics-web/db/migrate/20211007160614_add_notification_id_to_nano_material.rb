70f854f79988ac832ff55fedf17bf558285f8909
Slawosz Slawinski <slawosz@gmail.com>
2022-03-01 10:28:02 +0000

    Temporairly remove migrations added in master branch

--- cosmetics-web/db/migrate/20211007160614_add_notification_id_to_nano_material.rb
+++
@@ -1,5 +0,0 @@
-class AddNotificationIdToNanoMaterial < ActiveRecord::Migration[6.1]
-  def change
-    add_column :nano_materials, :notification_id, :integer
-  end
-end
--- cosmetics-web/db/migrate/20211202144553_add_exposure_condition_and_exposure_routes_to_components.rb
+++
@@ -1,8 +0,0 @@
-class AddExposureConditionAndExposureRoutesToComponents < ActiveRecord::Migration[6.1]
-  def change
-    # rubocop:disable RSpec/BulkChangeTable
-    add_column :components, :exposure_condition, :string
-    add_column :components, :exposure_routes, :string, array: true
-    # rubocop:enable RSpec/BulkChangeTable
-  end
-end
