3b2ba5f5024c6b4d66334be7adebe355d064f3e5
Marc Sardon <marc@bichito.software>
2022-04-11 18:42:30 +0100

    Refactor: Remove deprecated helper methods
    - One is pointing to a deleted route.
    
    - Other method has been replaced with the new flow back routing
      constants and wizard concern method to map them.

--- cosmetics-web/app/helpers/component_build_helper.rb
+++ cosmetics-web/app/helpers/component_build_helper.rb
@@ -3,34 +3,4 @@ module ComponentBuildHelper
     component.cmrs.each_with_index.flat_map { |cmr, index| cmr.errors.map { |error| { text: error.message, href: "#component_cmrs_attributes_#{index}_#{error.attribute}" } } } +
       component.errors.reject { |error| error.attribute.to_s.include? "cmrs." }.map { |error| { text: error.message, href: "#component_cmrs_attributes_0_name" } }
   end
-
-  def previous_wizard_path
-    previous_step = get_previous_step
-    notification = @component.notification
-
-    if step == :add_component_name
-      responsible_person_notification_build_path(notification.responsible_person, notification, :add_new_component)
-    elsif step == :number_of_shades && !notification.is_multicomponent?
-      responsible_person_notification_build_path(notification.responsible_person, notification, :add_product_image)
-    elsif step == :select_category && @category.present?
-      wizard_path(:select_category, category: Component.get_parent_category(@category))
-    elsif step == :select_category && @component&.nano_material&.nano_elements&.any?
-      last_nanoelement = @component.nano_material.nano_elements.last
-      nanoelement_step = last_nanoelement.standard? ? :confirm_usage : :when_products_containing_nanomaterial_can_be_placed_on_market
-      responsible_person_notification_component_nanomaterial_build_path(notification.responsible_person, notification, @component, last_nanoelement, nanoelement_step)
-    elsif step == :select_formulation_type
-      wizard_path(:select_category, category: @component.sub_category)
-    elsif step == :upload_formulation && @component.predefined?
-      wizard_path(:contains_poisonous_ingredients)
-    elsif previous_step.present?
-      responsible_person_notification_component_build_path(notification.responsible_person, notification, @component, previous_step)
-    else
-      super
-    end
-  end
-
-  def finish_wizard_path
-    # This should be new_responsible_person_notification_component_trigger_question_path, but the anti-dandruff questions are due to be removed separately
-    responsible_person_notification_component_trigger_question_path(@component.notification.responsible_person, @component.notification, @component, :select_ph_range)
-  end
 end
