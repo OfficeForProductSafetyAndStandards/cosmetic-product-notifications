4466a7e7e210b44e3720330d0aee841f11c801be
Marc Sardon <marc@bichito.software>
2022-05-11 13:48:45 +0100

    Refactor: Extract shared tests between jobs
    We're testing the common behaviour between all of them and the code is
    the same.

--- cosmetics-web/spec/jobs/upload_nanomaterial_notifications_job_spec.rb
+++ cosmetics-web/spec/jobs/upload_nanomaterial_notifications_job_spec.rb
@@ -1,28 +1,5 @@
 require "rails_helper"
 
 RSpec.describe UploadNanomaterialNotificationsJob do
-  it "uploads a CSV file" do
-    expect { described_class.perform_now }.to change(ActiveStorage::Blob, :count).by(1)
-    expect(ActiveStorage::Blob.last.filename).to eq(described_class::FILE_NAME)
-  end
-
-  # rubocop:disable RSpec/ExampleLength
-  it "replaces the previous upload" do
-    travel_to Time.zone.local(2022, 3, 12, 12, 0, 0) do
-      described_class.perform_now
-    end
-    travel_to Time.zone.local(2022, 3, 13, 12, 0, 0) do
-      uploads = ActiveStorage::Blob.where(filename: described_class::FILE_NAME)
-      expect { described_class.perform_now }
-        .to not_change(uploads, :count)
-        .and change { uploads.last.created_at }.from(Time.zone.local(2022, 3, 12, 12, 0, 0))
-                                               .to(Time.zone.local(2022, 3, 13, 12, 0, 0))
-    end
-  end
-  # rubocop:enable RSpec/ExampleLength
-
-  it "does not leave the CSV file in the app temp folder" do
-    described_class.perform_now
-    expect { File.open(described_class.file_path) }.to raise_error(Errno::ENOENT)
-  end
+  include_examples "Active Storage Upload jobs tests"
 end
