b6b7095bd2255804de7bf80619fb05608cb59f37
Marc Sardón <marc@bichito.software>
2022-05-17 11:56:23 +0100

    Fix RP invitation to new unconfirmed user emai (#2495)
    When a Responsible Person team member invitation was sent to an email
    address that matches an existing user "new email" that has not been
    confirmed, accepting the invitation was causing the creation of a new
    user.
    
    The new user have the email address of the existing user new email.
    This causes multiple issues:
    - Both users become invalid due to sharing email values.
    - Both users in our service belong to a single person.
    
    To resolve this, when an user receives a team invitation to their new
    unconfirmed email, and follows the link inside the valid confirmation
    time period:
    
    1. No new user will be created. Instead will find the user by new_email
       and add this user to the team.
    2. Since the user opened this confirmation link from their new_email
       inbox, this invitation email will also confirm the email change. So,
       on top of joining the team, the user will have their email change to
       the new_email address.

--- cosmetics-web/app/models/concerns/new_email_concern.rb
+++ cosmetics-web/app/models/concerns/new_email_concern.rb
@@ -10,17 +10,23 @@ module NewEmailConcern
     send_new_email_confirmation_email
   end
 
+  def confirm_new_email!
+    return if new_email.blank?
+
+    update!(
+      email: new_email,
+      new_email: nil,
+      new_email_confirmation_token: nil,
+      new_email_confirmation_token_expires_at: nil,
+    )
+  end
+
   class_methods do
     def confirm_new_email!(token)
       user = User.where("new_email_confirmation_token_expires_at > ?", Time.zone.now).find_by! new_email_confirmation_token: token
 
       old_email = user.email
-      user.update!(
-        email: user.new_email,
-        new_email: nil,
-        new_email_confirmation_token: nil,
-        new_email_confirmation_token_expires_at: nil,
-      )
+      user.confirm_new_email!
       NotifyMailer.get_mailer(user).update_email_address_notification_email(user, old_email).deliver_later
     rescue ActiveRecord::RecordNotFound
       raise ArgumentError
