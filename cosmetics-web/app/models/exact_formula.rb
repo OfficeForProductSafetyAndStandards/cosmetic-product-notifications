11c21c2d32e968cf1446d1a8ad53dd26e466cf63
Marc Sardon <marc@bichito.software>
2022-05-20 17:06:34 +0100

    Add CAS number and Posinous check to Exact Formula
    Cas number is optional and follow a specific format.
    
    Poisonous check is a boolean value to identify if an ingredient is
    poisonous or not.

--- cosmetics-web/app/models/exact_formula.rb
+++ cosmetics-web/app/models/exact_formula.rb
@@ -1,7 +1,17 @@
 class ExactFormula < ApplicationRecord
   belongs_to :component
 
+  validates_with CasNumberValidator
+
+  before_save :normalise_cas_number
+
   def display_name
     "#{inci_name}: #{quantity}"
+  end
+
+private
+
+  def normalise_cas_number
+    cas_number&.delete!("-")
   end
 end
--- cosmetics-web/db/schema.rb
+++ cosmetics-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_29_141342) do
+ActiveRecord::Schema.define(version: 2022_05_19_155041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -135,6 +135,8 @@ ActiveRecord::Schema.define(version: 2022_04_29_141342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "component_id"
+    t.string "cas_number"
+    t.boolean "poisonous", default: false, null: false
     t.index ["component_id"], name: "index_exact_formulas_on_component_id"
   end
 
