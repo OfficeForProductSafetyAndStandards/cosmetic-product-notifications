438d9633fa39c25290db8d4a7afa2dbbaabbb4d0
Sławosz Sławiński <slawosz@gmail.com>
2022-06-13 10:02:52 +0100

    Improved cookie banner (#2511)
    * Fix cookies cleanup
    
    * Add require
    
    * Fix typo
    
    * Move all cookies related methods to single concern
    
    * Proper cookie banners workflows
    
    * Use string value in cookie
    
    * Minor fixes to cookie banners
    
    * Dont show confirmation on empty form

--- cosmetics-web/app/controllers/cookie_forms_controller.rb
+++ cosmetics-web/app/controllers/cookie_forms_controller.rb
@@ -2,17 +2,20 @@ class CookieFormsController < PubliclyAccessibleController
   skip_before_action :require_secondary_authentication
 
   def create
-    session[:accept_analytics_cookies] = CookieForm.new(cookie_form_params).accept_analytics_cookies
-    redirect_back(fallback_location: root_path, confirmation: confirmation_text)
+    first_time = analytics_cookies_not_set?
+    cookie_form = CookieForm.new(cookie_form_params)
+
+    set_analytics_cookies(cookie_form.accept_analytics_cookies)
+    if first_time && !cookie_form.referrer_is_cookie_policy_page
+      redirect_back(fallback_location: root_path, cookies_banner_confirmation: true)
+    else
+      redirect_back(fallback_location: root_path, cookies_updated_successfully: true)
+    end
   end
 
 private
 
   def cookie_form_params
-    params.fetch(:cookie_form, {}).permit(:accept_analytics_cookies)
-  end
-
-  def confirmation_text
-    "Your cookies preferences has been saved. You can change your cookies preferences on the cookie page."
+    params.fetch(:cookie_form, {}).permit(:accept_analytics_cookies, :referrer_is_cookie_policy_page)
   end
 end
