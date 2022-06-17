3bce51d0c23a29d87f9df6b38c4b144a165d965c
Marc Sardon <marc@bichito.software>
2022-06-17 15:33:39 +0100

    Extract concern for CasNumber
    For validation and normalisation.

--- cosmetics-web/app/models/range_formula.rb
+++ cosmetics-web/app/models/range_formula.rb
@@ -1,13 +1,5 @@
 class RangeFormula < ApplicationRecord
+  include CasNumberConcern
+
   belongs_to :component
-
-  validates_with CasNumberValidator
-
-  before_save :normalise_cas_number
-
-private
-
-  def normalise_cas_number
-    self.cas_number = cas_number.presence&.delete("-")
-  end
 end
