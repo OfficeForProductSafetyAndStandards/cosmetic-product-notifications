38791547252854629b10d4239da03341695ab0ea
Marc Sardon <marc@bichito.software>
2022-06-23 09:56:18 +0100

    Extract feature spec helpers

--- cosmetics-web/spec/features/notification_wizard/adding_ingredients_to_components_spec.rb
+++ cosmetics-web/spec/features/notification_wizard/adding_ingredients_to_components_spec.rb
@@ -25,20 +25,14 @@ RSpec.describe "Adding ingredients to components", :with_stubbed_antivirus, type
 
   scenario "Adding exact concentration ingredients to a product" do
     answer_how_do_you_want_to_give_formulation_with "List ingredients and their exact concentration"
-
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("legend.govuk-fieldset__legend--s", text: "Ingredient 1")
+    expect_to_be_on_add_ingredients_page
 
     # First attempt with validation errors
     click_on "Save and continue"
 
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("h2#error-summary-title", text: "There is a problem")
-    expect(page).to have_link("Enter a name", href: "#name")
-
-    expect(page).to have_link("Enter the concentration", href: "#exact_concentration")
-    expect(page).to have_css("span#name-error", text: "Enter a name")
-    expect(page).to have_css("span#exact_concentration-error", text: "Enter the concentration")
+    expect_to_be_on_add_ingredients_page
+    expect_form_to_have_errors(name: { message: "Enter a name", id: "name" },
+                               exact_concentration: { message: "Enter the concentration", id: "exact_concentration" })
 
     # Successfully adds the first ingredient
     fill_in "name", with: "FooBar ingredient"
@@ -46,26 +40,17 @@ RSpec.describe "Adding ingredients to components", :with_stubbed_antivirus, type
     fill_in "cas_number", with: "123456-78-9"
     click_on "Save and continue"
 
-    # Chooses to add a second ingredient
-    expect(page).to have_css("h1", text: "Do you want to add another ingredient?")
-    expect(page).to have_css("p", text: "The ingredient was successfully added to the product.")
-    page.choose "Yes"
-    click_button "Continue"
+    answer_add_another_ingredient_with "Yes"
 
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("legend.govuk-fieldset__legend--s", text: "Ingredient 2")
-    expect(page).to have_css("h2", text: "Already added")
-    expect(page).to have_css("ol.govuk-list--number li", text: "FooBar ingredient")
+    expect_to_be_on_add_ingredients_page(ingredient_number: 2, already_added: ["FooBar ingredient"])
 
     # Attempts to add the same ingredient again
     fill_in "name", with: "foobar ingredient"
     fill_in "exact_concentration", with: "2.0"
     click_on "Save and continue"
 
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("h2#error-summary-title", text: "There is a problem")
-    expect(page).to have_link("Enter a name which is unique to this product", href: "#name")
-    expect(page).to have_css("span#name-error", text: "Enter a name which is unique to this product")
+    expect_to_be_on_add_ingredients_page(ingredient_number: 2, already_added: ["FooBar ingredient"])
+    expect_form_to_have_errors(name: { message: "Enter a name which is unique to this product", id: "name" })
 
     # Adds a new poisonous ingredient
     fill_in "name", with: "newfoo poisonous"
@@ -73,61 +58,46 @@ RSpec.describe "Adding ingredients to components", :with_stubbed_antivirus, type
     fill_in "exact_concentration", with: "7.0"
     click_on "Save and continue"
 
-    # Chooses to add a third ingredient
-    expect(page).to have_css("h1", text: "Do you want to add another ingredient?")
-    expect(page).to have_css("p", text: "The ingredient was successfully added to the product.")
-    page.choose "Yes"
-    click_button "Continue"
+    answer_add_another_ingredient_with "Yes"
 
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("legend.govuk-fieldset__legend--s", text: "Ingredient 3")
-    expect(page).to have_css("h2", text: "Already added")
-    expect(page).to have_css("ol.govuk-list--number li", text: "FooBar ingredient")
-    expect(page).to have_css("ol.govuk-list--number li", text: "newfoo poisonous")
+    expect_to_be_on_add_ingredients_page(ingredient_number: 3, already_added: ["FooBar ingredient", "newfoo poisonous"])
 
     # Skips adding the ingredient
     click_link "Skip"
-    expect(page).to have_h1("What is the pH range of the product?")
+    expect_to_be_on__what_is_ph_range_of_product_page
   end
 
   scenario "Adding range concentration ingredients to a product" do
     answer_how_do_you_want_to_give_formulation_with "List ingredients and their concentration range"
 
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("legend.govuk-fieldset__legend--s", text: "Ingredient 1")
+    expect_to_be_on_add_ingredients_page
 
     # First attempt failing due not indicating if ingredient is poisonous
     fill_in "name", with: "FooBar ingredient"
     click_on "Save and continue"
 
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("h2#error-summary-title", text: "There is a problem")
-    expect(page).to have_link("Select yes if the ingredient is poisonous", href: "#poisonous_true")
-    expect(page).to have_css("span#ingredient_concentration_form_poisonous-error", text: "Select yes if the ingredient is poisonous")
+    expect_to_be_on_add_ingredients_page
+    expect_form_to_have_errors(poisonous_true: {
+      message: "Select yes if the ingredient is poisonous",
+      id: "ingredient_concentration_form_poisonous",
+    })
 
     # Second attempt failing due to not selecting a concentration range for a non poisonous ingredient
     page.choose "No"
     click_on "Save and continue"
 
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("h2#error-summary-title", text: "There is a problem")
-    expect(page).to have_link("Select a concentration range", href: "#greater_than_75_less_than_100_percent")
-    expect(page).to have_css("span#ingredient_concentration_form_range_concentration-error", text: "Select a concentration range")
+    expect_to_be_on_add_ingredients_page
+    expect_form_to_have_errors(greater_than_75_less_than_100_percent: {
+      message: "Select a concentration range",
+      id: "ingredient_concentration_form_range_concentration",
+    })
 
     # Successfully adding the first ingredient by its concentration range
     page.choose("Above 5% w/w up to 10% w/w")
     click_on "Save and continue"
 
-    # Aks to add a second ingredient
-    expect(page).to have_css("h1", text: "Do you want to add another ingredient?")
-    expect(page).to have_css("p", text: "The ingredient was successfully added to the product.")
-    page.choose "Yes"
-    click_button "Continue"
-
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("legend.govuk-fieldset__legend--s", text: "Ingredient 2")
-    expect(page).to have_css("h2", text: "Already added")
-    expect(page).to have_css("ol.govuk-list--number li", text: "FooBar ingredient")
+    answer_add_another_ingredient_with "Yes"
+    expect_to_be_on_add_ingredients_page(ingredient_number: 2, already_added: ["FooBar ingredient"])
 
     # Attempt to add a poisonous ingredient with wrong value for concentration
     fill_in "name", with: "New ingredient"
@@ -135,50 +105,31 @@ RSpec.describe "Adding ingredients to components", :with_stubbed_antivirus, type
     fill_in "exact_concentration", with: "Not Valid"
     click_on "Save and continue"
 
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("h2#error-summary-title", text: "There is a problem")
-    expect(page).to have_link("Enter a number for the concentration", href: "#exact_concentration")
-    expect(page).to have_css("span#exact_concentration-error", text: "Enter a number for the concentration")
+    expect_to_be_on_add_ingredients_page(ingredient_number: 2, already_added: ["FooBar ingredient"])
+    expect_form_to_have_errors(exact_concentration: { message: "Enter a number for the concentration", id: "exact_concentration" })
 
     # Adds the second ingredient after entering a valid exact concentration
     fill_in "exact_concentration", with: "5.2"
     click_on "Save and continue"
 
-    # Decides not to add a third ingredient
-    expect(page).to have_css("h1", text: "Do you want to add another ingredient?")
-    expect(page).to have_css("p", text: "The ingredient was successfully added to the product.")
-    page.choose "No"
-    click_button "Continue"
-
-    expect(page).to have_h1("What is the pH range of the product?")
+    answer_add_another_ingredient_with("No")
+    expect_to_be_on__what_is_ph_range_of_product_page
   end
 
   scenario "Adding poisonous ingredients for a product with predefined formulation" do
     answer_how_do_you_want_to_give_formulation_with "Choose a predefined frame formulation"
 
     expect_to_be_on__frame_formulation_select_page
-    page.select "Skin Care Cream, Lotion, Gel", from: "component_frame_formulation"
-    click_button "Continue"
+    answer_select_formulation_with "Skin Care Cream, Lotion, Gel"
 
-    expect(page).to have_css("h1", text: "Does the product contain poisonous ingredients?")
-    page.choose "Yes"
-    click_button "Continue"
-
-    expect(page).to have_css("h1", text: "Add the poisonous ingredients")
-    expect(page).to have_css("legend.govuk-fieldset__legend--s", text: "Ingredient 1")
-    # Poisonous checkbox is pre-selected and disabled
-    expect(page).to have_checked_field("ingredient_concentration_form_poisonous", disabled: true)
+    answer_contain_poisonous_ingredients_with("Yes")
+    expect_to_be_on_add_ingredients_page(forced_poisonous: true)
 
     # First attempt with validation errors
     click_on "Save and continue"
-
     expect(page).to have_css("h1", text: "Add the poisonous ingredients")
-    expect(page).to have_css("h2#error-summary-title", text: "There is a problem")
-    expect(page).to have_link("Enter a name", href: "#name")
-
-    expect(page).to have_link("Enter the concentration", href: "#exact_concentration")
-    expect(page).to have_css("span#name-error", text: "Enter a name")
-    expect(page).to have_css("span#exact_concentration-error", text: "Enter the concentration")
+    expect_form_to_have_errors(name: { message: "Enter a name", id: "name" },
+                               exact_concentration: { message: "Enter the concentration", id: "exact_concentration" })
 
     # Successfully adds the first ingredient
     fill_in "name", with: "FooBar ingredient"
@@ -186,12 +137,7 @@ RSpec.describe "Adding ingredients to components", :with_stubbed_antivirus, type
     fill_in "cas_number", with: "123456-78-9"
     click_on "Save and continue"
 
-    # Chooses not to add a second ingredient
-    expect(page).to have_css("h1", text: "Do you want to add another ingredient?")
-    expect(page).to have_css("p", text: "The ingredient was successfully added to the product.")
-    page.choose "No"
-    click_button "Continue"
-
-    expect(page).to have_h1("What is the pH range of the product?")
+    answer_add_another_ingredient_with "No"
+    expect_to_be_on__what_is_ph_range_of_product_page
   end
 end
--- cosmetics-web/spec/support/helpers/item_wizard.rb
+++ cosmetics-web/spec/support/helpers/item_wizard.rb
@@ -165,24 +165,16 @@ end
 
 def fill_ingredients_concentrations(single_item: false)
   if page.has_no_css?("li", text: "FooBar ingredient")
-    expect(page).to have_css("h1", text: "Add the ingredients")
-    expect(page).to have_css("legend.govuk-fieldset__legend--s", text: "Ingredient 1")
+    expect_to_be_on_add_ingredients_page
     fill_in "name", with: "FooBar ingredient"
     yield # Fill/Select ingredient concentration
     fill_in "cas_number", with: "123456-78-9"
     click_on "Save and continue"
 
-    expect(page).to have_css("h1", text: "Do you want to add another ingredient?")
-    expect(page).to have_css("p", text: "The ingredient was successfully added to the #{single_item ? 'product' : 'item'}.")
-    page.choose "Yes"
-    click_button "Continue"
+    answer_add_another_ingredient_with("Yes", single_item: single_item)
   end
 
-  expect(page).to have_css("h1", text: "Add the ingredients")
-  expect(page).to have_css("legend.govuk-fieldset__legend--s", text: "Ingredient 2")
-  expect(page).to have_css("h2", text: "Already added")
-  expect(page).to have_css("ol.govuk-list--number li", text: "FooBar ingredient")
-
+  expect_to_be_on_add_ingredients_page(ingredient_number: 2, already_added: ["FooBar ingredient"])
   click_link "Skip"
 end
 
@@ -191,6 +183,47 @@ def answer_what_is_ph_range_of_product_with(answer)
     page.choose(answer)
   end
   click_button "Continue"
+end
+
+def answer_select_formulation_with(answer)
+  page.select answer, from: "component_frame_formulation"
+  click_button "Continue"
+end
+
+def answer_contain_poisonous_ingredients_with(answer)
+  expect(page).to have_css("h1", text: "Does the product contain poisonous ingredients?")
+  page.choose answer
+  click_button "Continue"
+end
+
+def select_item_and_remove(answer)
+  within_fieldset("Select which item to remove") do
+    page.choose(answer)
+  end
+  click_button "Delete and continue"
+end
+
+def answer_add_another_ingredient_with(answer, single_item: true)
+  expect(page).to have_css("h1", text: "Do you want to add another ingredient?")
+  expect(page).to have_css("p", text: "The ingredient was successfully added to the #{single_item ? 'product' : 'item'}.")
+  page.choose answer
+  click_button "Continue"
+end
+
+def expect_to_be_on_add_ingredients_page(ingredient_number: 1, already_added: [], forced_poisonous: false)
+  expect(page).to have_css("h1", text: "Add the#{forced_poisonous ? ' poisonous' : ''} ingredients")
+  expect(page).to have_css("legend.govuk-fieldset__legend--s", text: "Ingredient #{ingredient_number}")
+
+  if forced_poisonous # Poisonous checkbox is pre-selected and disabled
+    expect(page).to have_checked_field("ingredient_concentration_form_poisonous", disabled: true)
+  end
+
+  if already_added.any?
+    expect(page).to have_css("h2", text: "Already added")
+    already_added.each do |ingredient|
+      expect(page).to have_css("ol.govuk-list--number li", text: ingredient)
+    end
+  end
 end
 
 def expect_product_details_task_completed
@@ -211,11 +244,4 @@ end
 
 def expect_item_task_not_started(name)
   expect_task_not_started name
-end
-
-def select_item_and_remove(answer)
-  within_fieldset("Select which item to remove") do
-    page.choose(answer)
-  end
-  click_button "Delete and continue"
 end
