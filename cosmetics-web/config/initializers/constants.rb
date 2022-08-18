784f9a9b8bd5ec0f67c44712ab17ec018b3d0fbd
Marc Sardon <marc@bichito.software>
2022-08-18 13:24:20 +0100

    Fix YAML loading security error
    To avoid "Psych::DisallowedClass" when starting Rails we need to
    especifically allow Regexp as permited class when parsing the YAML file
    containing regexps.

--- cosmetics-web/config/initializers/constants.rb
+++ cosmetics-web/config/initializers/constants.rb
@@ -1,4 +1,4 @@
 EU_EXIT_DATE = Time.zone.parse("2021-01-01T00:00")
 SUBMISSION_WINDOW_END_DATE = Time.zone.parse("2021-03-31T23:00")
 Rails.application.config.domains_allowing_otp_whitelisting =
-  YAML.load_file(Rails.root.join("config/constants/domains_allowing_otp_whitelisting.yml"))
+  YAML.load_file(Rails.root.join("config/constants/domains_allowing_otp_whitelisting.yml"), permitted_classes: [Regexp])
