fe43279224ab7d2c65f49cd5b6163984aa4daffe
Marc Sardon <marc@bichito.software>
2022-09-05 12:32:13 +0100

    Refactor nanomaterial helper methods naming
    - Inline helper methods that is not used anywhere but by another helper.
    - Rename helpers to remove "get_" prefixing.

--- cosmetics-web/app/helpers/nanomaterial_helper.rb
+++ cosmetics-web/app/helpers/nanomaterial_helper.rb
@@ -1,11 +1,11 @@
 module NanomaterialHelper
-  def get_nanomaterial_purpose_options
+  def nanomaterial_purpose_options
     NanoElementPurposes.all.each_with_object({}) do |purpose, h|
       h[purpose.name] = purpose.display_name.upcase_first
     end
   end
 
-  def get_ec_regulation_annex_details_for_nanomaterial_purposes(purposes)
+  def ec_regulation_annex_details_for_nanomaterial_purposes(purposes)
     annex_numbers = purposes.filter_map { |purpose| NanoElementPurposes.find(purpose)&.annex_number }
     "#{'Annex'.pluralize(annex_numbers.count)} #{to_sentence(annex_numbers, last_word_connector: ' and ')}"
   end
