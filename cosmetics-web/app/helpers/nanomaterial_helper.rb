1e01de677d9628a99737f9302ca9fcdc88b9ffbe
Marc Sardon <marc@bichito.software>
2022-09-05 14:46:53 +0100

    Refactor: Remove redundant helper method
    Now that we have Purpose objects we can query attributes over them
    without complex logic. This can be called directly when building the
    checkbox items list.

--- cosmetics-web/app/helpers/nanomaterial_helper.rb
+++ cosmetics-web/app/helpers/nanomaterial_helper.rb
@@ -1,10 +1,4 @@
 module NanomaterialHelper
-  def nanomaterial_purpose_options
-    NanoElementPurposes.all.each_with_object({}) do |purpose, h|
-      h[purpose.name] = purpose.display_name.upcase_first
-    end
-  end
-
   def ec_regulation_annex_details_for_nanomaterial_purposes(purposes)
     annex_numbers = purposes.filter_map { |purpose| NanoElementPurposes.find(purpose)&.annex_number }
     "#{'Annex'.pluralize(annex_numbers.count)} #{to_sentence(annex_numbers, last_word_connector: ' and ')}"
