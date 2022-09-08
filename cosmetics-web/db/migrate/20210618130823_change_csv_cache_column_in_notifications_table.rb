9f508be9a502b5275f233d905eebf9647f754fd9
Marc Sardon <marc@bichito.software>
2022-09-08 17:54:24 +0100

    Ignore cop violation in already applied migration

--- cosmetics-web/db/migrate/20210618130823_change_csv_cache_column_in_notifications_table.rb
+++ cosmetics-web/db/migrate/20210618130823_change_csv_cache_column_in_notifications_table.rb
@@ -1,7 +1,9 @@
 class ChangeCsvCacheColumnInNotificationsTable < ActiveRecord::Migration[6.1]
   def change
     safety_assured do
+      # rubocop:disable Rails/ReversibleMigration
       change_column :notifications, :csv_cache, :text
+      # rubocop:enable Rails/ReversibleMigration
     end
   end
 end
