9385f25ace56a900e8d45059dbffde6ed5e75200
Sławosz Sławiński <slawosz@gmail.com>
2022-09-28 11:05:19 +0100

    Add advanced search section to notification search (#2601)
    * Add advanced search section to notification search
    
    * Fix specs
    
    * Fix reset button
    
    * Restrict advanced search to roles
    
    * Make some specs pending

--- cosmetics-web/app/assets/application/javascripts/notification_search.js
+++ cosmetics-web/app/assets/application/javascripts/notification_search.js
@@ -11,9 +11,11 @@ $(function () {
   const filterForm = document.getElementById('new_notification_search_form_filters')
   if (filterForm !== null) {
     const radioConditionalElements = filterForm.querySelectorAll('.govuk-radios__conditional')
-    const radioInputs = filterForm.querySelectorAll('.govuk-radios--conditional .govuk-radios__input')
+    const radioInputs = filterForm.querySelectorAll('.govuk-radios__input')
+    const checkboxes = filterForm.querySelectorAll('.govuk-checkboxes__input')
     const dateInputs = filterForm.querySelectorAll('.govuk-radios--conditional .govuk-date-input__input')
     const prodSelect = document.getElementById('notification_search_form_category')
+    const defaultInputs = filterForm.querySelectorAll('.govuk-radios input#search-by_all_fields')
 
     document.getElementById('opss-reset').addEventListener('click', function () { // click the filter's form reset link/button
       prodSelect.querySelectorAll('option').forEach(function (s) { // all options
@@ -27,6 +29,12 @@ $(function () {
       radioInputs.forEach(function (element) {
         element.setAttribute('aria-expanded', false)
         element.removeAttribute('checked')
+      })
+      checkboxes.forEach(function (element) {
+        element.removeAttribute('checked')
+      })
+      defaultInputs.forEach(function (element) {
+        element.setAttribute('checked', true)
       })
       dateInputs.forEach(function (element) {
         element.setAttribute('value', '')
