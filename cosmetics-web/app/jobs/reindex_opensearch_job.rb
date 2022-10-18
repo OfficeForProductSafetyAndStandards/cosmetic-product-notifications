59314d70cb4f4db7b3bf56c878f2005ce4346e7a
Sławosz Sławiński <slawosz@gmail.com>
2022-10-18 17:50:48 +0100

    Allow only one reindexing operation (#2641)

--- cosmetics-web/app/jobs/reindex_opensearch_job.rb
+++ cosmetics-web/app/jobs/reindex_opensearch_job.rb
@@ -1,17 +1,26 @@
 class ReindexOpensearchJob < ApplicationJob
   def perform
-    total = 0
-    ActiveRecord::Base.descendants.each do |model|
-      next unless model.respond_to?(:__elasticsearch__) && !model.superclass.respond_to?(:__elasticsearch__)
+    ActiveRecord::Base.transaction do
+      # tries to aquire lock. Wont block, and we need to return in case lock exist
+      lock = ActiveRecord::Base.connection.execute("SELECT pg_try_advisory_xact_lock(#{AdvisoryLock::NAMESPACES[AdvisoryLock::SEARCH]}, #{AdvisoryLock::OPERATIONS[AdvisoryLock::REINDEX_NOTIFICATIONS]})")
+      # lock.result returns such array: [[true]] or [[false]]
+      # when false, lock exists and we don't want to proceed
+      # when true, we aquired the lock and we can perform operation in current transaction
+      return unless lock.values.first.first
 
-      total += 1
-      if model.respond_to?(:opensearch)
-        model.opensearch.import force: true
-      else
-        model.import force: true
+      total = 0
+      ActiveRecord::Base.descendants.each do |model|
+        next unless model.respond_to?(:__elasticsearch__) && !model.superclass.respond_to?(:__elasticsearch__)
+
+        total += 1
+        if model.respond_to?(:opensearch)
+          model.opensearch.import force: true
+        else
+          model.import force: true
+        end
       end
-    end
 
-    Sidekiq.logger.info "Imported #{total} records to Opensearch"
+      Sidekiq.logger.info "Imported #{total} records to Opensearch"
+    end
   end
 end
