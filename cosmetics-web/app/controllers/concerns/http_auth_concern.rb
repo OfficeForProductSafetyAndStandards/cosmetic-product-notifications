666f8bd54b56bc54c246f9edcacdeea8a3bf793f
Marc Sardon <marc@bichito.software>
2022-10-27 10:03:52 +0100

    Use different http auth credentials
    So these users have separate credentials than the team ones.

--- cosmetics-web/app/controllers/concerns/http_auth_concern.rb
+++ cosmetics-web/app/controllers/concerns/http_auth_concern.rb
@@ -2,10 +2,10 @@ module HttpAuthConcern
   extend ActiveSupport::Concern
 
   included do
-    if ENV["BASIC_AUTH_USERNAME"].present? && ENV["BASIC_AUTH_PASSWORD"].present?
+    if ENV["ACCESSIBILITY_BASIC_AUTH_USERNAME"].present? && ENV["ACCESSIBILITY_BASIC_AUTH_PASSWORD"].present?
       http_basic_authenticate_with(
-        name: ENV.fetch("BASIC_AUTH_USERNAME"),
-        password: ENV.fetch("BASIC_AUTH_PASSWORD"),
+        name: ENV.fetch("ACCESSIBILITY_BASIC_AUTH_USERNAME"),
+        password: ENV.fetch("ACCESSIBILITY_BASIC_AUTH_PASSWORD"),
       )
     end
   end
