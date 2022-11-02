8e486543f521340188d8da13f06e5df40c7c2ce3
Andrew Cowle <acowle@byondsoftware.co.uk>
2022-11-02 13:46:50 +0000

    MVC

--- cosmetics-web/app/controllers/responsible_persons/draft_notifications_controller.rb
+++ cosmetics-web/app/controllers/responsible_persons/draft_notifications_controller.rb
@@ -1,16 +1,17 @@
 class ResponsiblePersons::DraftNotificationsController < ResponsiblePersons::NotificationsController
 
   def index
-    @unfinished_notifications = get_unfinished_notifications
+    @unfinished_notifications = get_unfinished_notifications(20)
   end
 
 private
 
-  def get_unfinished_notifications
+  def get_unfinished_notifications(page_size)
     @responsible_person.notifications
       .where("state IN (?)", NotificationStateConcern::DISPLAYABLE_INCOMPLETE_STATES)
       .where("reference_number IS NOT NULL")
       .where("product_name IS NOT NULL")
       .order("updated_at DESC")
+      .page(params[:page]).per(page_size)
   end
 end
