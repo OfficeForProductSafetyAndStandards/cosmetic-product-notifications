dfb7f5b45eacca6ae46f6a24eebc639baa8d8bd2
Andrew Cowle <acowle@byondsoftware.co.uk>
2022-11-21 16:12:37 +0000

    Failing request spec

--- cosmetics-web/spec/requests/responsible_persons/search_notifications_spec.rb
+++ cosmetics-web/spec/requests/responsible_persons/search_notifications_spec.rb
@@ -3,18 +3,16 @@ require "rails_helper"
 RSpec.describe "Search notifications page", type: :request do
   include RSpecHtmlMatchers
 
-  let(:responsible_person1) { create(:responsible_person_with_user, :with_a_contact_person) }
-  let(:submit_user) { responsible_person1.responsible_person_users.first.user }
-  let(:responsible_person) { responsible_person1 }
-  let!(:notification) { create(:notification, responsible_person:) }
+  let(:responsible_person) { create(:responsible_person_with_user, :with_a_contact_person) }
+  let(:submit_user) { responsible_person.responsible_person_users.first.user }
+
+  let(:cream) { create(:notification, :registered, :with_component, notification_complete_at: 1.day.ago, product_name: "Cream", responsible_person:) }
 
   before do
     configure_requests_for_submit_domain
-    sign_in_as_member_of_responsible_person(responsible_person1)
-  end
-
-  after do
-    sign_out(:submit_user)
+    cream
+    Notification.opensearch.import force: true
+    sign_in_as_member_of_responsible_person(responsible_person, submit_user)
   end
 
   describe "GET #show" do
@@ -40,13 +38,12 @@ RSpec.describe "Search notifications page", type: :request do
       let(:search_params) do
         {
           notification_search_form: {
-            q: "Product 1"
+            q: "Product 1",
           },
         }
       end
 
       before do
-        Notification.opensearch.import force: true
         get responsible_person_search_notifications_path(responsible_person.id, search_params)
       end
 
@@ -59,7 +56,7 @@ RSpec.describe "Search notifications page", type: :request do
       end
 
       it "displays the result" do
-        expect(response.body).to have_tag("th", text: notification.product_name)
+        expect(response.body).to have_tag("th", text: "Cream")
       end
     end
   end
