106f539c7955662748e667d9e1020e1a0282e679
Andrew Cowle <acowle@byondsoftware.co.uk>
2022-11-29 08:44:38 +0000

    Added safe to date helper and spec

--- cosmetics-web/app/helpers/date_helper.rb
+++ cosmetics-web/app/helpers/date_helper.rb
@@ -4,14 +4,14 @@ module DateHelper
   end
 
   def display_full_month_date(date)
-    date.strftime("%-d %B %Y")
+    date&.strftime("%-d %B %Y")
   end
 
   def display_date(date)
-    date.strftime("%d/%m/%Y")
+    date&.strftime("%d/%m/%Y")
   end
 
   def display_time(date)
-    date.strftime("%l\u200A%P")
+    date&.strftime("%l\u200A%P")
   end
 end
---
+++ cosmetics-web/spec/helpers/date_helper_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe DateHelper, type: :helper do
+  describe "#display_full_month_date" do
+    it "returns a Date string" do
+      date = Date.new(2022, 2, 1)
+      expect(helper.display_full_month_date(date)).to eq("1 February 2022")
+    end
+
+    it "returns nil when passed nil" do
+      date = nil
+      expect(helper.display_full_month_date(date)).to be_nil
+    end
+  end
+
+  describe "#display_date" do
+    it "returns a Date string" do
+      date = Date.new(2022, 2, 1)
+      expect(helper.display_date(date)).to eq("01/02/2022")
+    end
+
+    it "returns nil when passed nil" do
+      date = nil
+      expect(helper.display_date(date)).to be_nil
+    end
+  end
+
+  describe "#display_time" do
+    it "returns nil when passed nil" do
+      date = nil
+      expect(helper.display_time(date)).to be_nil
+    end
+  end
+end
