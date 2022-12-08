5cc5535b5470f426804a2528842479f000ff168e
Marc Sardon <marc@bichito.software>
2022-12-08 20:46:35 +0000

    Remove eager loading of lib folder

--- cosmetics-web/config/application.rb
+++ cosmetics-web/config/application.rb
@@ -31,7 +31,6 @@ module Cosmetics
 
     config.eager_load_paths << Rails.root.join("presenters")
     config.eager_load_paths << Rails.root.join("services")
-    config.eager_load_paths << Rails.root.join("lib")
 
     config.active_job.queue_adapter = :sidekiq
     config.action_mailer.deliver_later_queue_name = "cosmetics-mailers"
