f5811cc5a7528850a388dcc5808f4d0da09bc553
Marc Sardon <marc@bichito.software>
2022-12-09 17:23:56 +0000

    Remove redundant job name from log calls
    The job name is already included by Sidekiq logger, so was redundant.

--- cosmetics-web/app/jobs/reindex_opensearch_job.rb
+++ cosmetics-web/app/jobs/reindex_opensearch_job.rb
@@ -7,21 +7,21 @@ class ReindexOpensearchJob < ApplicationJob
         current_index = model.current_index_name
         new_index = model.create_new_index!
 
-        Sidekiq.logger.info "[ReindexOpensearchJob] Reindexing #{model} to Opensearch #{new_index} index..."
+        Sidekiq.logger.info "Reindexing #{model} to Opensearch #{new_index} index..."
         errors_count = model.import(index: new_index, scope: "opensearch", refresh: true)
 
         if errors_count.zero?
           import_count = model.index_docs_count(new_index)
-          Sidekiq.logger.info "[ReindexOpensearchJob] Imported #{import_count} records for #{model} to Opensearch #{new_index} index"
+          Sidekiq.logger.info "Imported #{import_count} records for #{model} to Opensearch #{new_index} index"
 
           model.swap_index_alias!(to: new_index)
-          Sidekiq.logger.info "[ReindexOpensearchJob] Swapped Opensearch index for #{model} from #{current_index} to #{new_index}"
+          Sidekiq.logger.info "Swapped Opensearch index for #{model} from #{current_index} to #{new_index}"
 
           model.__elasticsearch__.delete_index!(index: current_index)
-          Sidekiq.logger.info "[ReindexOpensearchJob] Deleted Opensearch index #{current_index} for #{model}"
+          Sidekiq.logger.info "Deleted Opensearch index #{current_index} for #{model}"
         else
-          Sidekiq.logger.info "[ReindexOpensearchJob] Got #{errors_count} errors while importing #{model} records to Opensearch #{new_index} index"
-          Sidekiq.logger.info "[ReindexOpensearchJob] Keeping #{current_index} index"
+          Sidekiq.logger.info "Got #{errors_count} errors while importing #{model} records to Opensearch #{new_index} index"
+          Sidekiq.logger.info "Keeping #{current_index} index"
 
           model.__elasticsearch__.delete_index!(index: new_index)
           Sidekiq.logger.info "#{new_index} deleted"
