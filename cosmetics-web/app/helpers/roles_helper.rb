7283c2584bd3d6ecaa37f65bd813e2b799847286
Slawosz Slawinski <slawosz@gmail.com>
2023-01-17 11:06:16 +0000

    Add documentation

--- cosmetics-web/app/helpers/roles_helper.rb
+++ cosmetics-web/app/helpers/roles_helper.rb
@@ -1,6 +1,8 @@
 # Each access method that depends on the user type, should be declared in this helper
 # Despite the fact, that the some of the roles are used only in search part of the service,
 # it is easier to have single place with all roles definded.
+#
+# All roles have to correspond with roles defined in `Privileges::AbstractConcern` concern.
 module RolesHelper
   delegate :can_view_product_ingredients?, to: :current_user
 
