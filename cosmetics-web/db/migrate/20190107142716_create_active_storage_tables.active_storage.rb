8271024a32e367d9d4293739b8417110ac2756b6
Andrew Cowle <acowle@byondsoftware.co.uk>
2023-01-19 15:08:34 +0000

    Migration fix

--- cosmetics-web/db/migrate/20190107142716_create_active_storage_tables.active_storage.rb
+++ cosmetics-web/db/migrate/20190107142716_create_active_storage_tables.active_storage.rb
@@ -1,6 +1,8 @@
 # This migration comes from active_storage (originally 20170806125915)
 class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
   def change
+    enable_extension 'citext' unless extension_enabled?('citext')
+
     create_table :active_storage_blobs do |t|
       t.string   :key,        null: false
       t.string   :filename,   null: false
