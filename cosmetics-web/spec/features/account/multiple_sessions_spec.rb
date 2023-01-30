b9983fdfb12148360b1fec3a1f7a7f09d5cf2d69
Slawosz Slawinski <slawosz@gmail.com>
2023-01-30 10:17:29 +0000

    Use different way to copy cookies

--- cosmetics-web/spec/features/account/multiple_sessions_spec.rb
+++ cosmetics-web/spec/features/account/multiple_sessions_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "One user can use only one session", :with_2fa, :with_stubbed_mailer, :with_stubbed_notify, type: :feature do
   shared_examples "multiple sessions" do
-    def sign_in
+    scenario "Invalidate all sessions when user is logged in on different browser" do
       visit "/sign-in"
 
       fill_in "Email address", with: user.email
@@ -12,14 +12,20 @@ RSpec.feature "One user can use only one session", :with_2fa, :with_stubbed_mail
       select_secondary_authentication_sms
       expect_to_be_on_secondary_authentication_sms_page
       complete_secondary_authentication_sms_with("#{otp_code} ")
-    end
 
-    scenario "Invalidate all sessions when user is logged in on different browser" do
-      sign_in
       expect(page).to have_current_path(path)
 
       Capybara.using_session("Other browser") do
-        sign_in
+        visit "/sign-in"
+
+        fill_in "Email address", with: user.email
+        fill_in "Password", with: user.password
+        click_button "Continue"
+
+        select_secondary_authentication_sms
+        expect_to_be_on_secondary_authentication_sms_page
+        complete_secondary_authentication_sms_with("#{otp_code} ")
+
         expect(page).to have_current_path(path)
       end
 
@@ -28,17 +34,37 @@ RSpec.feature "One user can use only one session", :with_2fa, :with_stubbed_mail
     end
 
     scenario "Invalidate all sessions when user is logging out" do
-      sign_in
+      visit "/sign-in"
+
+      fill_in "Email address", with: user.email
+      fill_in "Password", with: user.password
+      click_button "Continue"
+
+      select_secondary_authentication_sms
+      expect_to_be_on_secondary_authentication_sms_page
+      complete_secondary_authentication_sms_with("#{otp_code} ")
+
       expect(page).to have_current_path(path)
 
+      stolen_cookies = Capybara.current_session.driver.browser.current_session.cookie_jar.to_hash
+
       Capybara.using_session("Other browser") do
-        sign_in
+        visit path
+        expect(page).to have_current_path("/sign-in")
+        stolen_cookies.each do |k, v|
+          page.driver.browser.current_session.cookie_jar[k] = v
+        end
+        visit path
         expect(page).to have_current_path(path)
-        click_link "Sign out"
       end
 
       visit path
-      expect(page).to have_css("h2", text: "Your login credentials were used in another browser. Please sign in again to continue in this browser.")
+      click_link "Sign out"
+
+      Capybara.using_session("Other browser") do
+        visit path
+        expect(page).to have_css("h2", text: "Your login credentials were used in another browser. Please sign in again to continue in this browser.")
+      end
     end
   end
 
