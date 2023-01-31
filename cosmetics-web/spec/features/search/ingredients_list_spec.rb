478e5fc34adbafdb6d808cf06fe3834ae3ab05c5
Ruben Arakelyan <ruben@arakelyan.uk>
2023-01-31 15:15:07 +0000

    Add Responsible Persons and Notifications pages (#2760)
    Adds two new pages under the ingredients list page:
    
    * Responsible Persons - list of Responsible Persons who have created notifications of products containing a specified ingredient
    * Notifications - list of notifications from a specified Responsible Person with products containing a specified ingredient

--- cosmetics-web/spec/features/search/ingredients_list_spec.rb
+++ cosmetics-web/spec/features/search/ingredients_list_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 require "support/feature_helpers"
 
-RSpec.feature "Search", :with_stubbed_mailer, :with_stubbed_notify, :with_2fa, :with_2fa_app, type: :feature do
+RSpec.feature "Ingredients list", :with_stubbed_mailer, :with_stubbed_notify, :with_2fa, :with_2fa_app, type: :feature do
   let(:user) { create(:poison_centre_user, :with_sms_secondary_authentication) }
 
   let(:component1) { create(:component, :using_exact, with_ingredients: %w[aqua tin sodium]) }
@@ -13,7 +13,6 @@ RSpec.feature "Search", :with_stubbed_mailer, :with_stubbed_notify, :with_2fa, :
   let(:shower_bubbles) { create(:notification, :registered, responsible_person:, components: [component2], notification_complete_at: 3.days.ago, product_name: "Shower Bubbles") }
 
   before do
-    skip("Awaiting for ingredients list to be approved to go live")
     configure_requests_for_search_domain
 
     cream
@@ -34,6 +33,11 @@ RSpec.feature "Search", :with_stubbed_mailer, :with_stubbed_notify, :with_2fa, :
     expect(page).to have_link("sodium")
 
     click_link "sodium"
+
+    expect(page).to have_link(cream.responsible_person.name)
+    expect(page).not_to have_link(responsible_person.name)
+
+    click_link cream.responsible_person.name
 
     expect(page).to have_link("Cream")
     expect(page).not_to have_link("Shower Bubbles")
