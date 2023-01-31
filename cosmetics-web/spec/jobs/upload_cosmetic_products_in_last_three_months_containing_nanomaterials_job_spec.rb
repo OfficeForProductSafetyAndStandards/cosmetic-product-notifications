0774a7ea35e20b533a66e4cd4a704a3056cf29f5
eddieleeper <eddie.leeper@gmail.com>
2023-01-31 13:22:50 +0000

    COSBETA-1713 Add nanomaterial_notification_id to nanomaterials science reports

--- cosmetics-web/spec/jobs/upload_cosmetic_products_in_last_three_months_containing_nanomaterials_job_spec.rb
+++ cosmetics-web/spec/jobs/upload_cosmetic_products_in_last_three_months_containing_nanomaterials_job_spec.rb
@@ -7,8 +7,10 @@ RSpec.describe UploadCosmeticProductsInLastThreeMonthsContainingNanomaterialsJob
   # rubocop:disable Style/TrailingCommaInArguments
   it "generates a CSV containing all the notified products in the last 3 months containing nanomaterials" do
     rp = create(:responsible_person, :with_a_contact_person, name: "Soaps LTD")
+    notification = create(:notification, responsible_person: rp)
     nano = create(:nano_material, inci_name: "Zinc oxide", purposes: %w[colorant preservative])
     nano2 = create(:nano_material, inci_name: "Oxide zinc", purposes: %w[colorant preservative])
+    nano3 = create(:nano_material, :non_standard, notification:)
 
     create(:notification,
            responsible_person: rp,
@@ -22,6 +24,12 @@ RSpec.describe UploadCosmeticProductsInLastThreeMonthsContainingNanomaterialsJob
            reference_number: 321_654,
            nano_materials: [nano2],
            notification_complete_at: 4.months.ago)
+    create(:notification,
+           responsible_person: rp,
+           product_name: "Non Standard",
+           reference_number: 321_655,
+           nano_materials: [nano3],
+           notification_complete_at: 2.months.ago)
 
     described_class.perform_now
 
@@ -29,8 +37,9 @@ RSpec.describe UploadCosmeticProductsInLastThreeMonthsContainingNanomaterialsJob
     file.open do |f|
       expect(f.read).to eq(
         <<~CSV
-          Responsible Person,Cosmetic product name,Date cosmetic product was notified,UKCP number,INCI name,Nanomaterial purposes
-          Soaps LTD,Soapy Soap,#{1.day.ago.to_date},123456,Zinc oxide,"{colorant,preservative}"
+          Responsible Person,Cosmetic product name,Date cosmetic product was notified,UKCP number,INCI name,Nanomaterial purposes,UKN number
+          Soaps LTD,Non Standard,#{2.months.ago.to_date},321655,,{other},#{nano3.nanomaterial_notification_id}
+          Soaps LTD,Soapy Soap,#{1.day.ago.to_date},123456,Zinc oxide,"{colorant,preservative}",
         CSV
       )
     end
