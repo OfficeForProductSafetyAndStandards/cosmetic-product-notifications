1f0659afe54a09c3d141badd9d49bebe0e8d48fd
Steve Lorek <steve@stevelorek.com>
2023-02-06 14:21:57 +0000

    Add hosts to development whitelist

--- cosmetics-web/config/environments/development.rb
+++ cosmetics-web/config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << ENV["SEARCH_HOST"]
+  config.hosts << ENV["SUBMIT_HOST"]
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
