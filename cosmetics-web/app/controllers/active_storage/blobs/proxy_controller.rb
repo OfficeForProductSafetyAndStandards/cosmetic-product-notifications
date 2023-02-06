0bd999f02ad0a3060ad085997d93fb2ced7954b7
Steve Lorek <steve@stevelorek.com>
2023-02-06 14:13:22 +0000

    Upgrade to Rails 7

--- cosmetics-web/app/controllers/active_storage/blobs/proxy_controller.rb
+++ cosmetics-web/app/controllers/active_storage/blobs/proxy_controller.rb
@@ -6,15 +6,21 @@
 # Only owners and search users have access to files.
 class ActiveStorage::Blobs::ProxyController < ActiveStorage::BaseController
   include ActiveStorage::SetBlob
-  include ActiveStorage::SetHeaders
+  include ActiveStorage::Streaming
   include ActiveStorageAccessProtectionConcern
 
   before_action :authorize_blob
 
   def show
-    http_cache_forever public: true do
-      set_content_headers_from @blob
-      stream @blob
+    if request.headers["Range"].present?
+      send_blob_byte_range_data @blob, request.headers["Range"]
+    else
+      http_cache_forever public: true do
+        response.headers["Accept-Ranges"] = "bytes"
+        response.headers["Content-Length"] = @blob.byte_size.to_s
+
+        send_blob_stream @blob, disposition: params[:disposition]
+      end
     end
   end
 end
