5cee84b15ebe984f1953ddb4968f16630552fc8a
Steven Lorek <steve@stevelorek.com>
2023-02-06 16:22:27 +0000

    Faster review app provisioning (#2777)

--- cosmetics-web/deploy-review.sh
+++ cosmetics-web/deploy-review.sh
@@ -29,14 +29,14 @@ fi
 DB_GUID=$(cf service cosmetics-database-template --guid)
 cf create-service postgres small-13 $DB_NAME -c "{\"restore_from_point_in_time_of\": \"$DB_GUID\" }"
 
-# Wait until db is prepared, might take up to 10 minutes
-until cf service $DB_NAME > /tmp/db_exists && grep "create succeeded" /tmp/db_exists; do sleep 20; echo "Waiting for db"; done
-
 if [ -z "$REDIS_NAME" ]
 then
   REDIS_NAME=cosmetics-review-redis
 fi
 cf create-service redis micro-6.x $REDIS_NAME
+
+# Wait until db is prepared, might take up to 10 minutes
+until cf service $DB_NAME > /tmp/db_exists && grep "create succeeded" /tmp/db_exists; do sleep 20; echo "Waiting for db"; done
 
 # Wait until redis service is prepared, might take up to 10 minutes
 until cf service $REDIS_NAME > /tmp/redis_exists && grep "create succeeded" /tmp/redis_exists; do sleep 20; echo "Waiting for redis"; done
