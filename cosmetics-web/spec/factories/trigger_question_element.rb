a9e0fdd71488e4e0e4966b326fe3769dab1e59f6
Marc Sardon <marc@bichito.software>
2023-02-20 18:38:37 +0000

    Add task to cleanup dangling trigger questions
    We have TriggerQuestions in DB not associated with any Component, so
    they're effectively not associated with any Notification or any other
    model. They are unusable/unreachable.
    
    Same for TriggerQuestionElements that are not associated with any
    TriggerQuestion.
    
    We are deleting them with a rake task.

--- cosmetics-web/spec/factories/trigger_question_element.rb
+++ cosmetics-web/spec/factories/trigger_question_element.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :trigger_question_element do
+    trait :without_validations do
+      to_create { |instance| instance.save(validate: false) }
+    end
   end
 end
---
+++ cosmetics-web/spec/lib/tasks/trigger_questions_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+Rails.application.load_tasks
+
+# rubocop:disable RSpec/DescribeClass
+RSpec.describe "trigger_questions.rake" do
+  describe "delete_dangling" do
+    subject(:task) { Rake::Task["trigger_questions:delete_dangling"] }
+
+    let!(:component) { create(:component) }
+    let!(:trigger_question) { create(:trigger_question, component:) }
+    let!(:trigger_question_element) { create(:trigger_question_element, trigger_question:) }
+
+    before do
+      allow($stdout).to receive(:puts) # Silences the output on rspec run and allow to assert over its calls.
+      create(:trigger_question, :without_validations, component_id: nil)
+      create(:trigger_question_element, :without_validations, trigger_question_id: nil)
+    end
+
+    after do
+      # Rake tasks only run on the first invocation per suite. This re-enables the task for the next test.
+      task.reenable
+    end
+
+    it "deletes the trigger question that is not associated with a component" do
+      expect { task.invoke }.to change(TriggerQuestion, :count).from(2).to(1)
+      expect(TriggerQuestion.last).to eq(trigger_question)
+    end
+
+    it "deletes the trigger question element that is not associated with a trigger question" do
+      expect { task.invoke }.to change(TriggerQuestionElement, :count).from(2).to(1)
+      expect(TriggerQuestionElement.last).to eq(trigger_question_element)
+    end
+
+    it "informs the user of the number of dangling trigger questions and trigger question elements that were deleted" do
+      task.invoke
+      expect($stdout).to have_received(:puts)
+                     .with("Deleting 1 dangling trigger questions and 1 dangling trigger question elements")
+    end
+  end
+end
+# rubocop:enable RSpec/DescribeClass
