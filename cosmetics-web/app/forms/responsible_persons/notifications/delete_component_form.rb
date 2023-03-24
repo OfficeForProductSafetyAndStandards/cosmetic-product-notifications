901797cc472a0e36c5881a77f920fa6190390b35
Marc Sardon <marc@bichito.software>
2023-03-24 12:54:32 +0000

    Revert "Archive notifications (#2836)"
    This reverts commit d1e6a17e53f57b258af127a75d1fcba545cd7c2e.

--- cosmetics-web/app/forms/responsible_persons/notifications/delete_component_form.rb
+++ cosmetics-web/app/forms/responsible_persons/notifications/delete_component_form.rb
@@ -8,7 +8,7 @@ module ResponsiblePersons::Notifications
     def delete
       return false unless valid?
       raise("Can not remove item") if notification.components.count < 3
-      raise ActiveRecord::RecordNotFound if notification.notification_complete? || notification.archived? || notification.deleted?
+      raise ActiveRecord::RecordNotFound if notification.notification_complete? || notification.deleted?
 
       notification.components.find(component_id).destroy
       notification.try_to_complete_components!
