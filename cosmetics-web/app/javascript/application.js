21be7cb3a59de9d792befe7642a4a8090199daa1
Toby Privett <toby@snaplab.co.uk>
2023-05-10 13:39:04 +0100

    Remove unused govuk-country-and-territory-autocomplete
    COSBETA-1949

--- cosmetics-web/app/javascript/application.js
+++ cosmetics-web/app/javascript/application.js
@@ -1,7 +1,6 @@
 // JS
 import * as GOVUKFrontend from 'govuk-frontend'
 
-import './location_picker'
 import './bulk_file_upload_error_handling'
 import './image_size_validation'
 import './form_reset'
