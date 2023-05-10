1b73d5ad3cfa7e78d2f800b9d30d27b81a0d10c6
Toby Privett <toby@snaplab.co.uk>
2023-05-10 13:31:16 +0100

    Remove auto-complete libraries
    COSBETA-1949

--- cosmetics-web/app/javascript/application.js
+++ cosmetics-web/app/javascript/application.js
@@ -2,7 +2,6 @@
 import * as GOVUKFrontend from 'govuk-frontend'
 
 import './location_picker'
-import './autocomplete'
 import './bulk_file_upload_error_handling'
 import './image_size_validation'
 import './form_reset'
