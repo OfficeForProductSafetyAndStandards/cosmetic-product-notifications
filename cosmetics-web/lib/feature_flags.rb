ab464ed864ade64f72eb16e9deaa428209df5338
Ruben Arakelyan <ruben@arakelyan.uk>
2023-05-11 13:42:11 +0100

    Enable user targeting for feature flags (#2929)
    Adds the ability to enable a feature flag for certain users while keeping it disabled for everyone else.

--- cosmetics-web/lib/feature_flags.rb
+++ cosmetics-web/lib/feature_flags.rb
@@ -1,5 +1,5 @@
 class FeatureFlags
-  def self.csv_upload_exact_with_shades_enabled?
-    Flipper.enabled?(:csv_upload_exact_with_shades)
+  def self.csv_upload_exact_with_shades_enabled?(user)
+    Flipper.enabled?(:csv_upload_exact_with_shades, user)
   end
 end
