1b3b0d46b299a643a1fcf786b38ba31b6de40fbf
Toby Privett <toby@snaplab.co.uk>
2023-06-02 10:54:04 +0100

    Search for and Display some Users
    COSBETA-2041

--- cosmetics-web/support_portal/app/controllers/support_portal/users_controller.rb
+++ cosmetics-web/support_portal/app/controllers/support_portal/users_controller.rb
@@ -1,5 +1,20 @@
 module SupportPortal
   class UsersController < ApplicationController
-    def index; end
+    def index
+      search_users if params[:search_term]
+    end
+
+  protected
+
+    def search_users
+      search_term = params[:search_term]
+
+      @users =
+        if search_term.blank?
+          User.page(params[:page]).per(10)
+        else
+          User.search(search_term).page(params[:page]).per(10)
+        end
+    end
   end
 end
