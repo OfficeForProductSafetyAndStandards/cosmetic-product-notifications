745e28e6e2cabe86311876997a4f9233a95c99db
Ruben Arakelyan <ruben@arakelyan.uk>
2023-06-15 14:59:02 +0100

    Only add indexes if they don’t already exist

--- cosmetics-web/db/migrate/20230614104811_add_indexes_to_notifications.rb
+++ cosmetics-web/db/migrate/20230614104811_add_indexes_to_notifications.rb
@@ -5,11 +5,11 @@ class AddIndexesToNotifications < ActiveRecord::Migration[7.0]
     # `product_name` needs to use a GIN index rather than BTREE since it can
     # contain strings that exceed the index size limit.
     enable_extension "pg_trgm"
-    add_index :notifications, :product_name, algorithm: :concurrently, using: :gin, opclass: :gin_trgm_ops
-    add_index :notifications, :state, algorithm: :concurrently
-    add_index :notifications, :notification_complete_at, algorithm: :concurrently
-    add_index :deleted_notifications, :product_name, algorithm: :concurrently, using: :gin, opclass: :gin_trgm_ops
-    add_index :deleted_notifications, :state, algorithm: :concurrently
-    add_index :deleted_notifications, :notification_complete_at, algorithm: :concurrently
+    add_index :notifications, :product_name, algorithm: :concurrently, using: :gin, opclass: :gin_trgm_ops, if_not_exists: true
+    add_index :notifications, :state, algorithm: :concurrently, if_not_exists: true
+    add_index :notifications, :notification_complete_at, algorithm: :concurrently, if_not_exists: true
+    add_index :deleted_notifications, :product_name, algorithm: :concurrently, using: :gin, opclass: :gin_trgm_ops, if_not_exists: true
+    add_index :deleted_notifications, :state, algorithm: :concurrently, if_not_exists: true
+    add_index :deleted_notifications, :notification_complete_at, algorithm: :concurrently, if_not_exists: true
   end
 end
