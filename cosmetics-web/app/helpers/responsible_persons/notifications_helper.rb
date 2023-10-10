fd39b7ffbf8989ff0747045a9ad89d100f237d74
Andy Callaghan <github@andycallaghan.com>
2023-10-10 10:54:09 +0100

    show applicator type for all notifications

--- cosmetics-web/app/helpers/responsible_persons/notifications_helper.rb
+++ cosmetics-web/app/helpers/responsible_persons/notifications_helper.rb
@@ -226,6 +226,110 @@ module ResponsiblePersons::NotificationsHelper
      .compact
   end
 
+  def notification_summary_component_search_result_rows(component, include_shades: true)
+    cmrs = component.cmrs
+    nano_materials = component.nano_materials
+
+    [
+      if include_shades
+        {
+          key: { text: "Shades" },
+          value: { html: render("none_or_bullet_list",
+                                entities_list: component.shades,
+                                list_classes: "",
+                                list_item_classes: "") },
+        }
+      end,
+      {
+        key: { html: "Contains <abbr title='Carcinogenic, mutagenic, reprotoxic'>CMR</abbr> substances".html_safe },
+        value: { text: cmrs.any? ? "Yes" : "No" },
+      },
+      if cmrs.any?
+        {
+          key: { html: "<abbr title='Carcinogenic, mutagenic, reprotoxic'>CMR</abbr> substances".html_safe },
+          value: { html: render("application/none_or_bullet_list",
+                                entities_list: cmrs.map(&:display_name),
+                                list_classes: "",
+                                list_item_classes: "") },
+        }
+      end,
+      {
+        key: { text: "Nanomaterials" },
+        value: { html: render("application/none_or_bullet_list",
+                              entities_list: nano_materials_details(nano_materials),
+                              list_classes: "",
+                              list_item_classes: "") },
+      },
+      if nano_materials.non_standard.any?
+        {
+          key: { text: "Nanomaterials review period end date" },
+          value: { text: render("application/none_or_bullet_list",
+                                entities_list: nano_materials_with_review_period_end_date(nano_materials.non_standard),
+                                list_classes: "",
+                                list_item_classes: "") },
+        }
+      end,
+      if nano_materials.present?
+        {
+          key: { text: "Application instruction" },
+          value: { text: get_exposure_routes_names(component.exposure_routes) },
+        }
+      end,
+      if nano_materials.present?
+        {
+          key: { text: "Exposure condition" },
+          value: { text: get_exposure_condition_name(component.exposure_condition) },
+        }
+      end,
+      {
+        key: { text: "Category of product" },
+        value: { text: get_category_name(component.root_category) },
+      },
+      {
+        key: { text: "Category of #{get_category_name(component.root_category)&.downcase&.singularize}" },
+        value: { text: get_category_name(component.sub_category) },
+      },
+      {
+        key: { text: "Category of #{get_category_name(component.sub_category)&.downcase&.singularize}" },
+        value: { text: get_category_name(component.sub_sub_category) },
+      },
+      {
+        key: { text: "Physical form" },
+        value: { text: get_physical_form_name(component.physical_form) },
+      },
+      {
+        key: { text: "Special applicator" },
+        value: { text: component.special_applicator.present? ? "Yes" : "No" },
+      },
+      if component.special_applicator.present?
+        {
+          key: { text: "Applicator type" },
+          value: { text: component_special_applicator_name(component) },
+        }
+      end,
+      if component.acute_poisoning_info.present?
+        {
+          key: { text: "Acute poisoning information" },
+          value: { text: component.acute_poisoning_info },
+        }
+      end,
+      if component.predefined?
+        {
+          key: { html: "Contains ingredients <abbr title='National Poisons Information Service'>NPIS</abbr> needs to know about".html_safe },
+          value: { text: component.poisonous_ingredients_answer },
+        }
+      end,
+      if component.predefined? && component.contains_poisonous_ingredients && component.formulation_file.present?
+        {
+          key: { html: "Ingredients <abbr title='National Poisons Information Service'>NPIS</abbr> needs to know about".html_safe },
+          value: { html: render("notifications/component_details_poisonous_ingredients",
+                                component:) },
+        }
+      end,
+    ].concat(component_ph_trigger_questions_rows(component))
+     .compact
+  end
+
   def notification_summary_label_image_link(image, responsible_person, notification)
     if image.passed_antivirus_check?
       link_to(image.filename, url_for(image.file), class: "govuk-link govuk-link--no-visited-state", target: "_blank", rel: "noopener")
