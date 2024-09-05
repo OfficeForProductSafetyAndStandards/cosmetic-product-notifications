074a22de6211478ddf207235515b86598af45199
tahir-khalid <tahir.khalid@triad.co.uk>
2024-09-05 19:35:43 +0100

    manual merge cut-paste hell fix

--- cosmetics-web/app/controllers/responsible_persons_controller.rb
+++ cosmetics-web/app/controllers/responsible_persons_controller.rb
@@ -18,7 +18,7 @@ class ResponsiblePersonsController < SubmitApplicationController
       set_current_responsible_person(
         current_user.responsible_persons.find(@responsible_persons_selection_form.selection),
       )
-      redirect_to responsible_person_path(find(current_responsible_person || session[:current_responsible_person_id]), confirmation: message
+      redirect_to responsible_person_path(find(current_responsible_person || session[:current_responsible_person_id])), confirmation: message
     end
   end
 
