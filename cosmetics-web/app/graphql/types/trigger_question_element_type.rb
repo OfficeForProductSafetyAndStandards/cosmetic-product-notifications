627d10afef9823327958e8c9bb058cf50241f27d
Alan Reid <alan.reid@businessandtrade.gov.uk>
2024-11-29 12:26:19 +0000

    feat/PSD-4051-update_to_graphql_types_for_nulls

--- cosmetics-web/app/graphql/types/trigger_question_element_type.rb
+++ cosmetics-web/app/graphql/types/trigger_question_element_type.rb
@@ -1,10 +1,10 @@
 module Types
   class TriggerQuestionElementType < Types::BaseObject
     field :id, ID, null: false
-    field :answer_order, Integer, null: false, camelize: false
-    field :answer, String, null: false
-    field :element_order, Integer, null: false, camelize: false
-    field :element, String, null: false
+    field :answer_order, Integer, null: true, camelize: false
+    field :answer, String, null: true
+    field :element_order, Integer, null: true, camelize: false
+    field :element, String, null: true
     field :created_at, Types::CustomDateTimeType, null: false, camelize: false, description: "The date and time when the element was created"
     field :updated_at, Types::CustomDateTimeType, null: false, camelize: false, description: "The date and time when the element was last updated"
     field :trigger_question, TriggerQuestionType, null: false, camelize: false, description: "The associated trigger question"
