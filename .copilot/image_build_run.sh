93f473964da1a2832417c8e157e94aaf522b30f9
James Francis <james.francis@madetech.com>
2025-03-10 12:03:05 +0000

    small changes required for rails apps

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -4,3 +4,11 @@
 set -e
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
+
+rm ./config/application.yml
+
+cp -r /workspace/db/ /workspace/db-copy/
+
+rm -rf /workspace/tmp
+rm -rf /workspace/db
+rm -rf /workspace/log
