a500e1c0feaa9dd15fcf3c233b035d7a8473ff3b
James Francis <james.francis@madetech.com>
2025-03-10 16:26:39 +0000

    debug

--- cosmetics-web/config/environments/development.rb
+++ cosmetics-web/config/environments/development.rb
@@ -2,9 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << ENV.fetch("SEARCH_HOST", 'cosmetics-search')
-  config.hosts << ENV.fetch("SUBMIT_HOST", 'cosmetics-submit')
-  config.hosts << ENV.fetch("SUPPORT_HOST", 'cosmetics-support')
+  # config.hosts << ENV.fetch("SEARCH_HOST", 'cosmetics-search')
+  # config.hosts << ENV.fetch("SUBMIT_HOST", 'cosmetics-submit')
+  # config.hosts << ENV.fetch("SUPPORT_HOST", 'cosmetics-support')
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
