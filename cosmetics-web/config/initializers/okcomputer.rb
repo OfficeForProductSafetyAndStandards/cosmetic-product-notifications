7dc6f3fb774691464ab158b7cdae947c8bbef4d3
James Francis <james.francis@madetech.com>
2025-03-17 14:06:37 +0000

    set default env vars for okcomputer

--- cosmetics-web/config/initializers/okcomputer.rb
+++ cosmetics-web/config/initializers/okcomputer.rb
@@ -1,8 +1,7 @@
 return if Sidekiq.server?
-return
 
 OkComputer.mount_at = ENV["HEALTH_CHECK_USERNAME"].present? && ENV["HEALTH_CHECK_PASSWORD"].present? && "health"
-OkComputer.require_authentication(ENV["HEALTH_CHECK_USERNAME"], ENV["HEALTH_CHECK_PASSWORD"])
+OkComputer.require_authentication(ENV.fetch("HEALTH_CHECK_USERNAME", "username"), ENV.fetch("HEALTH_CHECK_PASSWORD", "password"))
 
 OkComputer::Registry.register "elasticsearch", OkComputer::ElasticsearchCheck.new(Rails.application.config_for(:opensearch)[:url])
 OkComputer::Registry.register "redis", OkComputer::RedisCheck.new(Rails.application.config_for(:redis))
