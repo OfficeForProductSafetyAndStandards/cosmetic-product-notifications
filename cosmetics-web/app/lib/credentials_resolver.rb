baa23ee07bbfea96bf6c2bee79a385aa4c6f4716
James Francis <james.francis@madetech.com>
2025-03-20 16:41:53 +0000

    decode opensearch url

--- cosmetics-web/app/lib/credentials_resolver.rb
+++ cosmetics-web/app/lib/credentials_resolver.rb
@@ -1,3 +1,6 @@
+require 'cgi'
+require 'uri'
+
 module CredentialsResolver
   module_function
 
@@ -15,5 +18,14 @@ module CredentialsResolver
 
     # Construct the database URI
     "#{config["engine"]}://#{config["username"]}:#{config["password"]}@#{config["host"]}:#{config["port"]}/#{config["dbname"]}"
+  end
+
+  def opensearch_url
+    if ENV["OPENSEARCH_URL"].present?
+      URI::parse(CGI.unescape(ENV.fetch('OPENSEARCH_URL')))
+    else
+      # Fallback to the original logic
+      ENV["VCAP_SERVICES"] && CF::App::Credentials.find_by_service_name("cosmetics-opensearch-1")["uri"]
+    end
   end
 end
