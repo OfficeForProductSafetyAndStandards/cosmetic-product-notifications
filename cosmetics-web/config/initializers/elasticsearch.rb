cd6775a384baee346bcd3fb1fb3cf0b8602d7b2d
James Francis <james.francis@madetech.com>
2025-03-21 10:18:01 +0000

    move opensearch config into initializer

--- cosmetics-web/config/initializers/elasticsearch.rb
+++ cosmetics-web/config/initializers/elasticsearch.rb
@@ -1,5 +1,22 @@
-require 'CredentialsResolver'
+# Elasticsearch::Model.client = Elasticsearch::Client.new(Rails.application.config_for(:opensearch))
+# # bypasses the recently introduced version check to allow ES gems to connect to an Opensearch 1 server
+# Elasticsearch::Model.client.instance_variable_set("@verified", true)
 
-Elasticsearch::Model.client = Elasticsearch::Client.new(Rails.application.config_for(:opensearch))
-# bypasses the recently introduced version check to allow ES gems to connect to an Opensearch 1 server
-Elasticsearch::Model.client.instance_variable_set("@verified", true)
+require 'cgi'
+require 'json'
+require 'uri'
+
+if Rails.env.production?
+  if ENV['COPILOT_ENVIRONMENT_NAME'] # DBT Platform
+    url = { url: URI::parse(CGI.unescape(ENV.fetch('OPENSEARCH_URL'))) }
+  elsif ENV['VCAP_SERVICES'] # Govt PaaS / Cloud Foundry Platform
+    url = { url: JSON.parse(ENV["VCAP_SERVICES"] && CF::App::Credentials.find_by_service_name("cosmetics-opensearch-1")["uri"] }
+  else
+    raise Exception, 'Platform type not identified'
+  end
+else
+  url = { host: 'http://localhost:9200' }
+end
+
+
+Elasticsearch::Model.client = Elasticsearch::Client.new(url: url, transport_options: { request: { timeout: 5 })
