6f9d0a978e1e569baa802c48ae7e80dbf01ca437
Alan Reid <alan.reid@businessandtrade.gov.uk>
2025-03-28 14:33:55 +0000

    Remove the invisible_captcha gem, add own version

--- cosmetics-web/app/controllers/registration/new_accounts_controller.rb
+++ cosmetics-web/app/controllers/registration/new_accounts_controller.rb
@@ -1,7 +1,5 @@
 module Registration
   class NewAccountsController < SubmitApplicationController
-    invisible_captcha only: [:create], honeypot: :subtitle, on_spam: :spam_callback
-
     skip_before_action :authorize_user!
     skip_before_action :authenticate_user!
     skip_before_action :require_secondary_authentication
@@ -9,9 +7,14 @@ module Registration
     def new
       sign_out
       @new_account_form = NewAccountForm.new
+      session[:form_timestamp] = Time.current.to_i
     end
 
     def create
+      if spam_detected?
+        return redirect_to registration_new_submit_user_path, alert: "Sign-ups are disabled at present"
+      end
+
       if new_account_form.save
         render "users/check_your_email/show", locals: { email: new_account_form.email }
       else
@@ -41,12 +44,44 @@ module Registration
 
   protected
 
-    # Overwrites Devise::RegistrationsController#after_inactive_sign_up_path_for
     def after_inactive_sign_up_path_for(_resource)
       check_your_email_path
     end
 
   private
+
+    def spam_detected?
+      email = params.dig(:registration_new_account_form, :email) || "unknown"
+      ip_address = request.remote_ip || "unknown"
+
+      if params[:subtitle].present?
+        Rails.logger.info "SPAM DETECTED: HONEYPOT FIELD 'subtitle' FILLED | Email: #{email} | IP: #{ip_address}"
+        return true
+      end
+
+      params.each_key do |key|
+        if key =~ /[a-z]{8,}-[a-z]/ && params[key].present?
+          Rails.logger.info "SPAM DETECTED: SUSPICIOUS FIELD '#{key}' FILLED WITH '#{params[key]}' | Email: #{email} | IP: #{ip_address}"
+          return true
+        end
+      end
+
+      timestamp = session[:form_timestamp]
+      if timestamp
+        elapsed_time = Time.current.to_i - timestamp.to_i
+        if elapsed_time < 2
+          Rails.logger.info "SPAM DETECTED: SUBMISSION TOO QUICK (#{elapsed_time} seconds) | Email: #{email} | IP: #{ip_address}"
+          return true
+        end
+      end
+
+      if Flipper.enabled?(:spam_logging)
+        Rails.logger.info "FORM SUBMISSION PARAMETERS: #{params.inspect} | IP: #{ip_address}"
+        Rails.logger.info "TIMESTAMP CHECK: Form loaded at #{timestamp}, submitted at #{Time.current.to_i}"
+      end
+
+      false
+    end
 
     def new_account_form
       @new_account_form ||= NewAccountForm.new(new_account_form_params)
