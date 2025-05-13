ff7f0835479f1ce199724f8df2ac5ee1c3587eb4
Miro Palka <miroslav.palka@businessandtrade.gov.uk>
2025-05-13 16:27:24 +0100

    PSD-5541: Add Flipper feature flag for 2FA to enable/disable as needed for load testing

--- cosmetics-web/spec/support/flipper.rb
+++ cosmetics-web/spec/support/flipper.rb
@@ -1,5 +1,8 @@
 RSpec.configure do |config|
   config.before do
     Flipper.instance = Flipper.new(Flipper::Adapters::Memory.new)
+    # Enable two-factor authentication by default for tests
+    # Individual tests can disable it as needed
+    Flipper.enable(:two_factor_authentication)
   end
 end
